cb6079f0842a31c8e2e801e22428943b8cdd5241
Kristinn Gudjonsson <kristinn@log2timeline.net>
2012-11-01 10:36:10 -0700

    Adjusting counter.

--- lib/storage.py
+++ lib/storage.py
@@ -301,7 +301,8 @@ class PlasoStorage(object):
     # Add values to counters.
     if self._pre_obj:
       self._pre_obj.counter['total'] += 1
-      self._pre_obj.counter[event.source_long] += 1
+      if hasattr(event, 'parser'):
+        self._pre_obj.counter[event.parser] += 1
 
     heapq.heappush(self._buffer, (event.timestamp, serialized))
     self._buffer_size += len(serialized)
