57149b40a3019b78d22308956edd773564e08c84
Kristinn Gudjonsson <kristinn@log2timeline.net>
2012-11-12 11:08:21 -0800

    Code review: 6826087:  Small changes to l2t_csv, implement ToDo, make slight changes.

--- output/l2t_csv.py
+++ output/l2t_csv.py
@@ -19,15 +19,22 @@ Author description at: http://code.google.com/p/log2timeline/wiki/l2t_csv
 """
 import datetime
 import pytz
+import re
 
+from plaso.proto import plaso_storage_pb2
 from plaso.lib import output
 
 
 class L2tCsv(output.LogOutputFormatter):
   """Contains functions for outputting as l2t_csv."""
 
-  SKIP = frozenset(['username', 'inode', 'hostname', 'body'])
-  US = int(1e6)  # 1,000,000
+  SKIP = frozenset(['username', 'inode', 'hostname', 'body', 'parser'])
+  MICROSEC = int(1e6)  # 1,000,000
+
+  # Few regular expressions.
+  MODIFIED_RE = re.compile(r'modif', re.I)
+  ACCESS_RE = re.compile(r'visit', re.I)
+  CREATE_RE = re.compile(r'(create|written)', re.I)
 
   def Usage(self):
     """Returns a short descrition of what this formatter outputs."""
@@ -46,7 +53,8 @@ class L2tCsv(output.LogOutputFormatter):
       proto_read: Protobuf to format.
     """
 
-    mydate = datetime.datetime.utcfromtimestamp(proto_read.timestamp / 1e6)
+    mydate = datetime.datetime.utcfromtimestamp(
+        proto_read.timestamp / self.MICROSEC)
     date_use = mydate.replace(tzinfo=pytz.utc).astimezone(self.zone)
 
     attributes = {}
@@ -58,11 +66,17 @@ class L2tCsv(output.LogOutputFormatter):
       extra.append('%s: %s ' % (key, value))
     extra = ' '.join(extra)
 
+    inode = attributes.get('inode', '')
+    if not inode:
+      if proto_read.pathspec.HasField('image_inode'):
+        inode = proto_read.pathspec.image_inode
+      else:
+        inode = '-'
+
     row = (date_use.strftime('%m/%d/%Y'),
            date_use.strftime('%H:%M:%S'),
            self.zone,
-           # TODO: Implement a MACB function. Hard coding for now.
-           '....',
+           self.GetLegacy(proto_read),
            proto_read.DESCRIPTOR.enum_types_by_name['SourceShort'].values_by_number[
                proto_read.source_short].name,
            proto_read.source_long,
@@ -72,13 +86,56 @@ class L2tCsv(output.LogOutputFormatter):
            proto_read.description_short.replace('\r', '').replace('\n', ''),
            proto_read.description_long.replace('\r', '').replace('\n', ''),
            '2',
-           proto_read.display_name,
-           # TODO: Consider extracting from proto_read.pathspec.image_ino
-           attributes.get('inode', '-'),
+           proto_read.filename,
+           inode,
            attributes.get('notes', '-'),  # Notes field placeholder.
-           '-',  # Name of the parser (still missing).
+           attributes.get('parser', '-'),
            extra)
 
     out_write = u'{0}\n'.format(
         u','.join(unicode(x).replace(',', ' ') for x in row))
     self.filehandle.write(out_write.encode('utf-8'))
+
+  def GetLegacy(self, event_proto):
+    """Return a legacy MACB representation of the event."""
+    # TODO: Fix this function when the MFT parser has been implemented.
+    # The filestat parser is somewhat limited.
+    # Also fix this when duplicate entries have been implemented so that
+    # the function actually returns more than a single entry (as in combined).
+    if event_proto.source_short == plaso_storage_pb2.EventObject.FILE:
+      letter = event_proto.timestamp_desc[0]
+
+      if letter == 'm':
+        return 'M...'
+      elif letter == 'a':
+        return '.A..'
+      elif letter == 'c':
+        return '..C.'
+      else:
+        return '....'
+
+    letters = []
+    m = self.MODIFIED_RE.search(event_proto.timestamp_desc)
+
+    if m:
+      letters.append('M')
+    else:
+      letters.append('.')
+
+    m = self.ACCESS_RE.search(event_proto.timestamp_desc)
+
+    if m:
+      letters.append('A')
+    else:
+      letters.append('.')
+
+    m = self.CREATE_RE.search(event_proto.timestamp_desc)
+
+    if m:
+      letters.append('C')
+    else:
+      letters.append('.')
+
+    letters.append('.')
+
+    return ''.join(letters)
