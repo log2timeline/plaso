61b6dc35f0b2b84e30192874a12ee8c48002b2fc
David Nides <david.nides@gmail.com>
2013-03-03 12:30:01 -0600

    Code review: 7095064: New Symantec SEP log Parser

--- formatters/__init__.py
+++ formatters/__init__.py
@@ -24,5 +24,6 @@ from plaso.formatters import lnk
 from plaso.formatters import ls_quarantine
 from plaso.formatters import mactime
 from plaso.formatters import syslog
+from plaso.formatters import symantec
 from plaso.formatters import winevt
 from plaso.formatters import winreg
