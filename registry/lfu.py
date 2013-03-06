b04b2884c992454279cd702aa8c73767d6ba405f
Elizabeth Schweinsberg <bethlogic@gmail.com>
2013-03-06 10:20:47 -0800

    Adding the Less Frequently Used Registry key Parser.

---
+++ registry/lfu.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Plug-in to collect the Less Frequently Used Keys."""
+import logging
+
+from plaso.lib import event
+from plaso.lib import win_registry_interface
+
+
+class BootVerificationPlugin(win_registry_interface.KeyPlugin):
+  """Plug-in to collect the Boot Verification Key."""
+
+  REG_KEY = '\\{current_control_set}\\Control\\BootVerificationProgram'
+  REG_TYPE = 'SYSTEM'
+  URLS = ['http://technet.microsoft.com/en-us/library/cc782537(v=ws.10).aspx']
+
+  def GetEntries(self):
+    """Gather the BootVerification key values and return one event for all."""
+    """This key is rare, so its presence is suspect"""
+    text_dict = {}
+    text_dict['BootVerification'] = 'REGALERT'
+    for value in self._key.GetValues():
+      text_dict[value.name] = value.GetData()
+    event_object = event.WinRegistryEvent(self._key.path, text_dict,
+                                          self._key.timestamp)
+    yield event_object
+
+
+class BootExecutePlugin(win_registry_interface.KeyPlugin):
+  """Plug-in to collect the BootExecute Value from the Session Manager key."""
+
+  REG_KEY = '\\{current_control_set}\\Control\\Session Manager'
+  REG_TYPE = 'SYSTEM'
+  URLS = ['http://technet.microsoft.com/en-us/library/cc963230.aspx']
+
+  def GetEntries(self):
+    """Gather the BootExecute Value, compare to default, return event"""
+    """The rest of the Session Manager key is in a separate event"""
+    text_dict = {}
+
+    for value in self._key.GetValues():
+      if value.name == 'BootExecute':
+        boot_dict = {}
+        boot = value.GetData(unicode)
+        if boot != u'autocheck autochk *':
+          boot_dict['BootExecute'] = 'REGALERT: ' + boot
+        else:
+          boot_dict['BootExecute'] = 'REGNORMAL: '+ boot
+
+        event_object = event.WinRegistryEvent(self._key.path, boot_dict,
+                                              self._key.timestamp)
+        yield event_object
+      else:
+        text_dict[value.name] = value.GetData()
+    event_object = event.WinRegistryEvent(self._key.path, text_dict,
+                                          self._key.timestamp)
+    yield event_object
---
+++ registry/lfu_test.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This file contains tests for Less Frequently Used Keys parsing in Plaso."""
+import unittest
+
+from plaso.lib import eventdata
+from plaso.lib import preprocess
+from plaso.registry import lfu
+from plaso.registry import test_lib
+
+
+class TestBootExecuteRegistry(unittest.TestCase):
+  """The unit test for BootExecute registry value parsing."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    self.maxDiff = None
+    values = []
+    values.append(test_lib.TestRegValue(
+        'BootExecute', 'autocheck autochk *\x00'.encode('utf_16_le'), 7, 123))
+    values.append(test_lib.TestRegValue(
+        'CriticalSectionTimeout', '2592000'.encode('utf_16_le'), 1, 153))
+    values.append(test_lib.TestRegValue('ExcludeFromKnownDlls',
+                                        '\x00'.encode('utf_16_le'), 7, 163))
+    values.append(test_lib.TestRegValue('GlobalFlag', '0'.encode('utf_16_le'),
+                                        1, 173))
+    values.append(test_lib.TestRegValue('HeapDeCommitFreeBlockThreshold',
+                                        '0'.encode('utf_16_le'), 1, 183))
+    values.append(test_lib.TestRegValue('HeapDeCommitTotalFreeThreshold',
+                                        '0'.encode('utf_16_le') , 1, 203))
+    values.append(test_lib.TestRegValue('HeapSegmentCommit',
+                                        '0'.encode('utf_16_le'), 1, 213))
+    values.append(test_lib.TestRegValue('HeapSegmentReserve',
+                                        '0'.encode('utf_16_le'), 1, 223))
+    values.append(test_lib.TestRegValue('NumberOfInitialSessions',
+                                        '2'.encode('utf_16_le'), 1, 243))
+    self.regkey = test_lib.TestRegKey(
+        '\\ControlSet001\\Control\\Session Manager', 1346445929000000, values,
+        153)
+    self.preprocess = preprocess.PlasoPreprocess()
+    self.preprocess.current_control_set = 'ControlSet001'
+
+  def testBootExecute(self):
+    plugin = lfu.BootExecutePlugin(None, self.preprocess)
+    entries = list(plugin.Process(self.regkey))
+
+    line1 = (u'[\ControlSet001\Control\Session Manager] BootExecute: REGNORMAL:'
+             ' autocheck autochk *')
+    line2 = (u'[\ControlSet001\Control\Session Manager] CriticalSectionTimeout'
+             ': 2592000 ExcludeFromKnownDlls: [] GlobalFlag: 0 HeapDeCommitFre'
+             'eBlockThreshold: 0 HeapDeCommitTotalFreeThreshold: 0 HeapSegment'
+             'Commit: 0 HeapSegmentReserve: 0 NumberOfInitialSessions: 2')
+
+    self.assertEquals(len(entries), 2)
+    self.assertEquals(entries[0].timestamp, int(1346445929 * 1e6))
+    msg, _ = eventdata.EventFormatterManager.GetMessageStrings(entries[0])
+    self.assertEquals(msg, line1)
+    msg, _ = eventdata.EventFormatterManager.GetMessageStrings(entries[1])
+    self.assertEquals(msg, line2)
+
+
+class TestBootVerificationRegistry(unittest.TestCase):
+  """The unit test for BootVerification key registry parsing."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    values = []
+    values.append(test_lib.TestRegValue(
+        'ImagePath',
+        'C:\\WINDOWS\\system32\\googleupdater.exe'.encode('utf_16_le'), 1,
+        123))
+    self.regkey = test_lib.TestRegKey(
+        '\\ControlSet001\\Control\\BootVerificationProgram',
+        1346445929000000, values, 153)
+    self.preprocess = preprocess.PlasoPreprocess()
+    self.preprocess.current_control_set = 'ControlSet001'
+
+  def testBootVerification(self):
+    plugin = lfu.BootVerificationPlugin(None, self.preprocess)
+    entries = list(plugin.Process(self.regkey))
+
+    line = (u'[\ControlSet001\Control\BootVerificationProgram] Boot'
+            'Verification: REGALERT ImagePath: C:\WINDOWS\system32\google'
+            'updater.exe')
+
+    self.assertEquals(len(entries), 1)
+    self.assertEquals(entries[0].timestamp, int(1346445929 * 1e6))
+    msg, _ = eventdata.EventFormatterManager.GetMessageStrings(entries[0])
+    self.assertEquals(msg, line)
+
+
+if __name__ == '__main__':
+  unittest.main()
