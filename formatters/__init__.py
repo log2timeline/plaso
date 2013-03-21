0261dd4276caa215a34db73eaf41100ef2131b0c
Joachim Metz <joachim.metz@gmail.com>
2013-03-21 20:55:30 +0100

    Code review: 7940043:  Fix for missing includes.

--- formatters/__init__.py
+++ formatters/__init__.py
@@ -23,6 +23,7 @@ from plaso.formatters import gdrive
 from plaso.formatters import hachoir
 from plaso.formatters import ls_quarantine
 from plaso.formatters import mactime
+from plaso.formatters import msiecf
 from plaso.formatters import selinux
 from plaso.formatters import syslog
 from plaso.formatters import symantec
