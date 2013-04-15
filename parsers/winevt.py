75645c68b2761409b52067cd722cd8a15b12df1e
Kristinn Gudjonsson <kristinn@log2timeline.net>
2013-04-15 11:36:52 -0700

    YABF: One tiny bug fix.

--- parsers/winevt.py
+++ parsers/winevt.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Parser for Windows EventLog (EVT) files."""
+import logging
+
 from plaso.lib import errors
 from plaso.lib import event
 from plaso.lib import eventdata
