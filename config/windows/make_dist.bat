672c7420d959525cdae28db71c5ef9fff4489b94
Joachim Metz <joachim.metz@gmail.com>
2013-04-24 19:40:57 +0200

    Code review: 8625045:  More changed for packaging configs

--- config/windows/make_dist.bat
+++ config/windows/make_dist.bat
@@ -5,6 +5,10 @@ rmdir /q /s dist\plaso 2> NUL
 
 mkdir dist\plaso
 
+xcopy /q /y ACKNOWLEDGEMENT dist\plaso
+xcopy /q /y AUTHORS dist\plaso
+xcopy /q /y LICENSE.TXT dist\plaso
+
 xcopy /q /y /s dist\log2timeline\* dist\plaso
 xcopy /q /y /s dist\plaso_console\* dist\plaso
 xcopy /q /y /s dist\plaso_information\* dist\plaso
