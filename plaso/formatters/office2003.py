3bb37e68324280a7ce19c40b2a0104e08cb594f0
david nides <david.nides@gmail.com>
2013-05-13 13:57:05 -0500

    Code review: 9055046: New Parser Office2003 ole

---
+++ plaso/formatters/office2003.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Formatter for MS Office 2003 events."""
+from plaso.lib import eventdata
+
+__author__ = 'David Nides (david.nides@gmail.com)'
+
+
+class Office2003Formatter(eventdata.ConditionalEventFormatter):
+  """Formatter for MS Office 2003 based events."""
+  
+  DATA_TYPE = 'metadata:office2003'
+
+  FORMAT_STRING_PIECES = [
+      u'Title: {title}',
+      u'Subject: {subject}',
+      u'Author: {author}',
+      u'Keywords: {keywords}',
+      u'Comments: {comments}',
+      u'Template: {template}',
+      u'Last saved by: {last_saved_by}',
+      u'Revision number: {revision_number}',
+      u'Total edit time: {total_edit_time}',
+      u'Num pages: {num_pages}',
+      u'Num words: {num_words}',
+      u'Num chars: {num_chars}',
+      u'Security: {security}',
+      u'Codepage: {codepage}',
+      u'Creating application: {creating_application}']
+
+  FORMAT_STRING_SHORT_PIECES = [
+      u'Title: {title}',
+      u'Subject: {subject}',
+      u'Author: {author}']
