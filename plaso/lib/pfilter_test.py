1085c86c217de0356e3e62fc4d473a5631603b62
Kristinn Gudjonsson <kristinn@log2timeline.net>
2013-05-16 22:12:51 -0700

    Code review: 9235047: Copyright, forgot to update pfilter, should be the done now.

--- plaso/lib/pfilter_test.py
+++ plaso/lib/pfilter_test.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python
-# Copyright 2012 Google Inc.
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2012 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
