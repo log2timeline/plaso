724be7deef620dd3d3f918e9576fecc8691d5e4e
Joachim Metz <joachim.metz@gmail.com>
2013-08-08 11:00:07 +0200

    Code review: 12601043: Registry refactor, some more file renaming.

--- plaso/registry/mrulistex_test.py
+++ plaso/registry/mrulistex_test.py
@@ -14,56 +14,48 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This file contains the tests for the MRUList Registry plugins."""
-# TODO rename this file to mrulist_test.py in a separate CL.
+"""This file contains a test for MRUx registry parsing in Plaso."""
 
 import unittest
 
 from plaso.formatters import winreg   # pylint: disable-msg=W0611
 from plaso.lib import eventdata
-from plaso.registry import mru
+from plaso.registry import mrulistex
 from plaso.winreg import test_lib
 
 
-class TestMRUListRegistry(unittest.TestCase):
-  """The unit test for MRUList registry parsing."""
+class TestMRUxRegistry(unittest.TestCase):
+  """The unit test for MRU registry parsing."""
 
   def setUp(self):
     """Sets up the needed objects used throughout the test."""
     values = []
+    # The order is: 201
     values.append(test_lib.TestRegValue(
-        'MRUList', 'acb'.encode('utf_16_le'),
-        test_lib.TestRegValue.REG_SZ, offset=123))
+        'MRUListEx', '\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00',
+        3, 123))
     values.append(test_lib.TestRegValue(
-        'a', 'Some random text here'.encode('utf_16_le'),
-        test_lib.TestRegValue.REG_SZ, offset=1892))
+        '0', 'Some random text here'.encode('utf_16_le'), 1, 1892))
     values.append(test_lib.TestRegValue(
-        'b', 'c:/evil.exe'.encode('utf_16_le'),
-        test_lib.TestRegValue.REG_BINARY, offset=612))
+        '1', 'c:/evil.exe'.encode('utf_16_le'), 3, 612))
     values.append(test_lib.TestRegValue(
-        'c', 'C:/looks_legit.exe'.encode('utf_16_le'),
-        test_lib.TestRegValue.REG_SZ, offset=1001))
+        '2', 'C:/looks_legit.exe'.encode('utf_16_le'), 1, 1001))
 
     self.regkey = test_lib.TestRegKey(
-        '\\Microsoft\\Some Windows\\InterestingApp\\MRU', 1346145829002031,
+        '\\Microsoft\\Some Windows\\InterestingApp\\MRUlist', 1346145829002031,
         values, 1456)
 
-  def testMRUListPlugin(self):
-    """Run a simple test against a mocked key with values."""
-    plugin = mru.MRUListPlugin(None, None, None)
-    generator = plugin.Process(self.regkey)
-    self.assertTrue(generator)
-    entries = list(generator)
+  def testMRUX(self):
+    """Test the MRUexPlugin."""
+    plugin = mrulistex.MRUexPlugin(None, None, None)
+    entries = list(plugin.Process(self.regkey))
 
-    expected_line1 = (
-        u'[\\Microsoft\\Some Windows\\InterestingApp\\MRU] '
-        u'MRUList Entry 1: Some random text here')
-    expected_line2 = (
-        u'[\\Microsoft\\Some Windows\\InterestingApp\\MRU] '
-        u'MRUList Entry 2: C:/looks_legit.exe')
-    expected_line3 = (
-        u'[\\Microsoft\\Some Windows\\InterestingApp\\MRU] '
-        u'MRUList Entry 3: REGALERT: Unsupported MRU value: b data type.')
+    line0 = ('[\\Microsoft\\Some Windows\\InterestingApp\\MRUlist] MRUListEx '
+             'Entry 0 (nr. 2): Some random text here')
+    line1 = ('[\\Microsoft\\Some Windows\\InterestingApp\\MRUlist] MRUListEx '
+             'Entry 1 (nr. 3): c:/evil.exe')
+    line2 = ('[\\Microsoft\\Some Windows\\InterestingApp\\MRUlist] MRUListEx '
+             'Entry 2 (nr. 1): C:/looks_legit.exe')
 
     self.assertEquals(len(entries), 3)
     self.assertEquals(entries[0].timestamp, 1346145829002031)
@@ -74,9 +66,9 @@ class TestMRUListRegistry(unittest.TestCase):
     msg2, _ = eventdata.EventFormatterManager.GetMessageStrings(entries[1])
     msg3, _ = eventdata.EventFormatterManager.GetMessageStrings(entries[2])
 
-    self.assertEquals(msg1, expected_line1)
-    self.assertEquals(msg2, expected_line2)
-    self.assertEquals(msg3, expected_line3)
+    self.assertEquals(msg1, line2)
+    self.assertEquals(msg2, line0)
+    self.assertEquals(msg3, line1)
 
 
 if __name__ == '__main__':
