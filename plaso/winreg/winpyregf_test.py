29aeb904e46953f97a35c4641b1212b6c8a71d1f
Kristinn Gudjonsson <kristinn@log2timeline.net>
2013-09-18 16:05:33 -0700

    Code review: 13263055: Small bugfix in the registry parsing code and a first version of a winreg parser test.

--- plaso/winreg/winpyregf_test.py
+++ plaso/winreg/winpyregf_test.py
@@ -15,24 +15,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This file contains tests for the pyregf Windows Registry back-end."""
-
 import os
 import unittest
 
+from plaso.lib import putils
 from plaso.winreg import winpyregf
 
 
 class RegistryUnitTest(unittest.TestCase):
   """An unit test for the plaso winpyregf library."""
 
+  def setUp(self):
+    self.base_path = 'test_data'
+
   def testListKeys(self):
-    test_file = os.path.join('test_data', 'NTUSER.DAT')
-    file_object = open(test_file, 'rb')
+    test_file = os.path.join(self.base_path, 'NTUSER.DAT')
+    file_object = putils.OpenOSFile(test_file)
     reg = winpyregf.WinRegistry(file_object)
     keys = list(reg)
 
     # Count the number of registry keys in the hive.
     self.assertEquals(len(keys), 1126)
+
+  def testWinPyregf(self):
+    test_file = os.path.join(self.base_path, 'NTUSER.DAT')
+    file_object = putils.OpenOSFile(test_file)
+    reg = winpyregf.WinPyregfFile()
+    reg.Open(file_object)
+
+    self._KeyPathCompare(reg, '\\')
+    self._KeyPathCompare(reg, '\\Printers')
+    self._KeyPathCompare(reg, '\\Printers\\Connections')
+    self._KeyPathCompare(reg, '\\Software')
+
+  def _KeyPathCompare(self, reg, path):
+    """Get a key, compare it's path to the path requested."""
+    key = reg.GetKeyByPath(path)
+    self.assertEquals(key.path, path)
 
 
 if __name__ == '__main__':
