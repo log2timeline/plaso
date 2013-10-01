aebb0b448cc85a59090e4342617098f922db2ad0
Francesco Picasso <francesco.picasso@gmail.com>
2013-10-01 23:28:14 +0200

    Code review: 13828047:  SkyDrive Log Parser

---
+++ plaso/parsers/skydrivelog_test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2013 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This file contains a unit test for the skydrivelog parser in plaso."""
+
+import os
+import pytz
+import unittest
+
+from plaso.lib import eventdata
+from plaso.lib import preprocess
+from plaso.lib import putils
+from plaso.parsers import skydrivelog as skydrivelog_parser
+# Shut up pylint on W0611: Unused import skydrivelog_formatter.
+# pylint: disable=W0611
+from plaso.formatters import skydrivelog as skydrivelog_formatter
+
+__author__ = 'Francesco Picasso (francesco.picasso@gmail.com)'
+
+
+class SkyDriveLogUnitTest(unittest.TestCase):
+  """A unit test for the SkyDriveLog Parser."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    test_file = os.path.join('test_data', 'skydrive.log')
+    self.filehandle = putils.OpenOSFile(test_file)
+
+  def testParsing(self):
+    """Test parsing of a SkyDrive log file."""
+    pre_obj = preprocess.PlasoPreprocess()
+    pre_obj.zone = pytz.timezone('UTC')
+    myparser = skydrivelog_parser.SkyDriveLogParser(pre_obj)
+    events = list(myparser.Parse(self.filehandle))
+
+    self.assertEquals(len(events), 18)
+
+    # expr `date -u -d "2013-08-01 21:22:28.999" +"%s%N"` \/ 1000
+    self.assertEquals(events[0].timestamp, 1375392148999000)
+    # expr `date -u -d "2013-08-01 21:22:29.702" +"%s%N"` \/ 1000
+    self.assertEquals(events[1].timestamp, 1375392149702000)
+    self.assertEquals(events[2].timestamp, 1375392149702000)
+    self.assertEquals(events[3].timestamp, 1375392149702000)
+    # expr `date -u -d "2013-08-01 21:22:58.344 " +"%s%N"` \/ 1000
+    self.assertEquals(events[4].timestamp, 1375392178344000)
+    self.assertEquals(events[5].timestamp, 1375392178344000)
+
+    self._TestText(events[0],
+      u'[global.cpp:626!logVersionInfo] (DETAIL) 17.0.2011.0627 (Ship)')
+
+    self._TestText(events[3], (
+      u'SyncToken = LM%3d12345678905670%3bID%3d1234567890E059C0!'
+      u'103%3bLR%3d12345678905623%3aEP%3d2'))
+
+    self._TestText(events[17],
+      u'SyncToken = Not a sync token (\xe0\xe8\xec\xf2\xf9)!')
+
+  def _TestText(self, evt, text):
+    msg, _ = eventdata.EventFormatterManager.GetMessageStrings(evt)
+    self.assertEquals(msg, text)
+
+if __name__ == '__main__':
+  unittest.main()
