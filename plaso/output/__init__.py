d3ee2b756ff509cb562ecabeb45b46ff88c99093
David Nides <david.nides@gmail.com>
2013-10-07 10:49:31 -0500

    Code review: 13926043: 4n6time mySQL output

--- plaso/output/__init__.py
+++ plaso/output/__init__.py
@@ -18,6 +18,10 @@
 
 from plaso.output import dynamic
 from plaso.output import l2t_csv
+try:
+  from plaso.output import mysql_4n6
+except ImportError:
+  pass
 from plaso.output import pstorage
 from plaso.output import raw
 from plaso.output import rawpy
