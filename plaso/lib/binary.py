519ba1de5e8fa49f38544d900aab7e6ba693b9be
Joachim Metz <joachim.metz@gmail.com>
2013-10-22 16:46:45 +0200

    Code review: 15690043: Updated prefetch file parser.

--- plaso/lib/binary.py
+++ plaso/lib/binary.py
@@ -15,47 +15,115 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This file contains a helper library to read binary files."""
+
 import binascii
 import logging
+import os
 
 
-def ReadUtf16Stream(filehandle, offset=None, byte_size=0):
-  """Read an UTF-16 unicode line and returns it.
+def ReadUtf16Stream(file_object, offset=None, byte_size=0):
+  """Reads an UTF-16 formatted stream from a file-like object.
 
-  Read an UTF-16 encoded line that's terminated by a
-  specific end of line character (by default \x00).
+  Read an UTF-16 formatted stream that's terminated by
+  an end-of-line character (\x00\x00) or upto the byte size.
 
   Args:
-    filehandle: A file like object to read the data from.
-    offset: An offset into the filehandle, if -1 or not set
-    the current location into the filehandle is used.
-    byte_size: Size in bytes of the string to be read.
+    file_object: A file-like object to read the data from.
+    offset: An offset into the file object data, if -1 or not set
+            the current location into the file object data is used.
+    byte_size: Maximum number of bytes to read or 0 if the function
+               should keep reading upto the end of file.
 
   Returns:
-    The UTF 16 string read from the file like object.
+    An Unicode string.
   """
   if offset is not None:
-    filehandle.seek(offset)
+    file_object.seek(offset, os.SEEK_SET)
 
   char_buffer = []
 
-  size_counter = 2
-  char_raw = filehandle.read(2)
+  stream_index = 0
+  char_raw = file_object.read(2)
   while char_raw:
-    if byte_size and size_counter >= byte_size:
+    if byte_size and stream_index >= byte_size:
       break
 
     if '\x00\x00' in char_raw:
       break
     char_buffer.append(char_raw)
-    size_counter += 2
-    char_raw = filehandle.read(2)
+    stream_index += 2
+    char_raw = file_object.read(2)
 
   return ReadUtf16(''.join(char_buffer))
 
 
+def Ut16StreamCopyToString(byte_stream, byte_stream_size=None):
+  """Copies an UTF-16 formatted byte stream to a string.
+
+  The UTF-16 formatted byte stream should be terminated by an end-of-line
+  character (\x00\x00). Otherwise the function read upto the byte stream size.
+
+  Args:
+    byte_stream: The UTF-16 formatted byte stream.
+    byte_stream_size: The byte stream size or None if the entire byte stream
+                      should be used.
+
+  Returns:
+    An Unicode string.
+  """
+  byte_stream_index = 0
+  if not byte_stream_size:
+    byte_stream_size = len(byte_stream)
+
+  while byte_stream_index + 1 < byte_stream_size:
+    if (byte_stream[byte_stream_index] == '\x00' and
+        byte_stream[byte_stream_index + 1] == '\x00'):
+      break
+
+    byte_stream_index += 2
+
+  return byte_stream[0:byte_stream_index].decode('utf-16-le')
+
+
+def ArrayOfUt16StreamCopyToString(byte_stream, byte_stream_size=None):
+  """Copies an array of UTF-16 formatted byte streams to an array of strings.
+
+  The UTF-16 formatted byte stream should be terminated by an end-of-line
+  character (\x00\x00). Otherwise the function read upto the byte stream size.
+
+  Args:
+    byte_stream: The UTF-16 formatted byte stream.
+    byte_stream_size: The byte stream size or None if the entire byte stream
+                      should be used.
+
+  Returns:
+    An array of Unicode strings.
+  """
+  array_of_strings = []
+  utf16_stream_start = 0
+  byte_stream_index = 0
+  if not byte_stream_size:
+    byte_stream_size = len(byte_stream)
+
+  while byte_stream_index + 1 < byte_stream_size:
+    if (byte_stream[byte_stream_index] == '\x00' and
+        byte_stream[byte_stream_index + 1] == '\x00'):
+
+      if byte_stream_index - utf16_stream_start <= 2:
+        break
+
+      array_of_strings.append(
+          byte_stream[utf16_stream_start:byte_stream_index].decode(
+          'utf-16-le'))
+      utf16_stream_start = byte_stream_index + 2
+
+    byte_stream_index += 2
+
+  return array_of_strings
+
+
 def ReadUtf16(string_buffer):
-  """Return a decoded utf-16 string from a string_buffer."""
+  """Returns a decoded UTF-16 string from a string_buffer."""
   if type(string_buffer) in (list, tuple):
     use_buffer = u''.join(string_buffer)
   else:
--- plaso/parsers/winprefetch.py
+++ plaso/parsers/winprefetch.py
@@ -28,131 +28,292 @@ from plaso.lib import parser
 class WinPrefetchEventContainer(event.EventContainer):
   """Convenience class for a Windows Prefetch event container."""
 
-  def __init__(self, magic_header, header, executable, run, volume, vol_path):
-    """Initializes the event container."""
+  def __init__(self, file_header, file_information, volume_information,
+               volume_path):
+    """Initializes the event container.
+
+    Args:
+      file_header: The file header structure.
+      file_information: The file information structure.
+      volume_information: The volume information structure.
+      volume_path: The volume path string.
+    """
     super(WinPrefetchEventContainer, self).__init__()
 
-    self.data_type = 'windows:prefetch:prefetch'
+    self.data_type = 'windows:prefetch'
 
     self.offset = 0
 
-    self.executable = executable
-    self.hash_value = '0x{:08X}'.format(header.get('hash_value', 0))
-    self.version = magic_header.get('version', 0x11)
-    self.run_count = run.get('run_count', 0)
-    self.volume_serial = '0x{:08X}'.format(volume.get('volume_serial', 0))
-    self.volume_path = vol_path
+    self.version = file_header.get('version', None)
+    self.executable = binary.Ut16StreamCopyToString(
+        file_header.get('executable', ''))
+    self.prefetch_hash = file_header.get('prefetch_hash', None)
+
+    self.run_count = file_information.get('run_count', None)
+
+    if volume_information:
+      self.volume_serial = volume_information.get('volume_serial', None)
+
+    self.volume_path = volume_path
 
 
 class WinPrefetchParser(parser.PlasoParser):
-  """Parses Windows Prefetch and Superfetch files."""
+  """A parser for Windows Prefetch files."""
 
-  # Define a list of all structs used.
+  FILE_SIGNATURE = 'SCCA'
+
   FILE_HEADER_STRUCT = construct.Struct(
-      'header_signature', construct.ULInt16('version'), construct.Padding(2),
-      construct.String('magic', 4), construct.Padding(4),
-      construct.ULInt32('end_of_file'))
+      'file_header',
+      construct.ULInt32('version'),
+      construct.String('signature', 4),
+      construct.Padding(4),
+      construct.ULInt32('file_size'),
+      construct.String('executable', 60),
+      construct.ULInt32('prefetch_hash'),
+      construct.ULInt32('flags'))
 
-  HEADER_STRUCT = construct.Struct(
-      'header', construct.ULInt32('hash_value'), construct.Padding(4),
-      construct.ULInt32('header_size'), construct.Padding(12),
-      construct.ULInt32('dll_ofs'), construct.ULInt32('dll_length'),
-      construct.ULInt32('vol_start'))
+  FILE_INFORMATION_V17 = construct.Struct(
+      'file_information_v17',
+      construct.Padding(16),
+      construct.ULInt32('dll_offset'),
+      construct.ULInt32('dll_size'),
+      construct.ULInt32('volume_information_offset'),
+      construct.Padding(8),
+      construct.ULInt64('last_run_time'),
+      construct.Padding(16),
+      construct.ULInt32('run_count'),
+      construct.Padding(4))
 
-  VOLUME_STRUCT = construct.Struct(
-      'volume', construct.ULInt32('path_bytes'),
-      construct.ULInt32('path_length'), construct.ULInt64('created'),
-      construct.ULInt32('volume_serial'), construct.ULInt32('dir_bytes'),
-      construct.ULInt32('dir_numbers'))
+  FILE_INFORMATION_V23 = construct.Struct(
+      'file_information_v23',
+      construct.Padding(16),
+      construct.ULInt32('dll_offset'),
+      construct.ULInt32('dll_size'),
+      construct.ULInt32('volume_information_offset'),
+      construct.Padding(16),
+      construct.ULInt64('last_run_time'),
+      construct.Padding(16),
+      construct.ULInt32('run_count'),
+      construct.Padding(84))
+
+  FILE_INFORMATION_V26 = construct.Struct(
+      'file_information_v26',
+      construct.Padding(16),
+      construct.ULInt32('dll_offset'),
+      construct.ULInt32('dll_size'),
+      construct.ULInt32('volume_information_offset'),
+      construct.Padding(16),
+      construct.ULInt64('last_run_time'),
+      construct.ULInt64('last_run_time1'),
+      construct.ULInt64('last_run_time2'),
+      construct.ULInt64('last_run_time3'),
+      construct.ULInt64('last_run_time4'),
+      construct.ULInt64('last_run_time5'),
+      construct.ULInt64('last_run_time6'),
+      construct.ULInt64('last_run_time7'),
+      construct.Padding(16),
+      construct.ULInt32('run_count'),
+      construct.Padding(96))
+
+  # Note that at the moment for the purpose of this parser
+  # the v23 and v26 volume information structures are the same as v17.
+  VOLUME_INFORMATION_V17 = construct.Struct(
+      'volume_information_v17',
+      construct.ULInt32('name_offset'),
+      construct.ULInt32('name_number_of_characters'),
+      construct.ULInt64('creation_time'),
+      construct.ULInt32('volume_serial'),
+      construct.Padding(8),
+      construct.ULInt32('directory_strings_offset'),
+      construct.Padding(4))
+
+  def _ParseFileHeader(self, file_object):
+    """Parses the file header.
+
+    Args:
+      file_object: A file-like object to read data from.
+
+    Returns:
+      The file header construct object.
+    """
+    try:
+      file_header = self.FILE_HEADER_STRUCT.parse_stream(file_object)
+    except (IOError, construct.FieldError) as exception:
+      raise errors.UnableToParseFile(
+          u'Unable to parse file header. '
+          u'Reason given: {}'.format(exception))
+
+    if not file_header:
+      raise errors.UnableToParseFile('Unable to read file header')
+
+    if file_header.get('signature', None) != self.FILE_SIGNATURE:
+      raise errors.UnableToParseFile('Unsupported file signature')
+
+    return file_header
+
+  def _ParseFileInformation(self, file_object, format_version):
+    """Parses the file information.
+
+    Args:
+      file_object: A file-like object to read data from.
+      format_version: The format version.
+
+    Returns:
+      The file information construct object.
+    """
+    try:
+      if format_version == 17:
+        file_information = self.FILE_INFORMATION_V17.parse_stream(file_object)
+      elif format_version == 23:
+        file_information = self.FILE_INFORMATION_V23.parse_stream(file_object)
+      elif format_version == 26:
+        file_information = self.FILE_INFORMATION_V26.parse_stream(file_object)
+      else:
+        file_information = None
+    except (IOError, construct.FieldError) as exception:
+      raise errors.UnableToParseFile(
+          u'Unable to parse file information v{0:d}. '
+          u'Reason given: {1}'.format(format_version, exception))
+
+    if not file_information:
+      raise errors.UnableToParseFile('Unable to read file information')
+    return file_information
+
+  def _ParseVolumeInformation(self, file_object, volume_information_offset):
+    """Parses the volume information.
+
+    Args:
+      file_object: A file-like object to read data from.
+      volume_information_offset: The offset relative from start of the file
+                                 to the volume information.
+
+    Returns:
+      The volume information construct object or None if not available.
+    """
+    if volume_information_offset > 0:
+      file_object.seek(volume_information_offset, os.SEEK_SET)
+
+      try:
+        volume_information = self.VOLUME_INFORMATION_V17.parse_stream(
+            file_object)
+      except (IOError, construct.FieldError) as exception:
+        raise errors.UnableToParseFile(
+            u'Unable to parse volume information v17. '
+            u'Reason given: {}'.format(exception))
+    else:
+      volume_information = None
+    return volume_information
+
+  def _ParseVolumePath(self, file_object, volume_path_offset, volume_path_size):
+    """Parses the volume path.
+
+    This function expects the current offset of the file-like object to point
+    as the end of the volume information structure.
+
+    Args:
+      file_object: A file-like object to read data from.
+      volume_path_offset: The offset relative from start of the volume
+                          information to the volume path.
+      volume_path_size: The byte size of the volume path data.
+
+    Returns:
+      A Unicode string containing the volume path or None if not available.
+    """
+    if volume_path_offset >= 36 and volume_path_size > 0:
+      # Correct for the part of volume information structure we've already read.
+      volume_path_offset -= 36
+      file_object.seek(volume_path_offset, os.SEEK_CUR)
+
+      volume_path = binary.ReadUtf16Stream(
+          file_object, byte_size=volume_path_size)
+    else:
+      volume_path = None
+
+    return volume_path
 
   def Parse(self, file_object):
     """Extract data from a Windows Prefetch file.
 
     Args:
-      file_object: a file-like object to read data from.
+      file_object: A file-like object to read data from.
 
-    Returns:
-      An event container (EventContainer) that contains the parsed
-      attributes.
+    Yields:
+      An event container (EventContainer) that contains the parsed attributes.
     """
-    try:
-      magic_header = self.FILE_HEADER_STRUCT.parse_stream(file_object)
-    except construct.FieldError as e:
+    file_header = self._ParseFileHeader(file_object)
+
+    format_version = file_header.get('version', None)
+    if format_version not in [17, 23, 26]:
       raise errors.UnableToParseFile(
-          u'Not a prefetch file, unable to parse. Reason given: {}'.format(e))
-    except IOError as e:
-      raise errors.UnableToParseFile(
-          u'Not a prefetch file, unable to parse. Reason given: {}'.format(e))
+          'Unsupported format version: %d' % format_version)
 
-    if not magic_header:
-      raise errors.UnableToParseFile('Unable to read in data')
+    file_information = self._ParseFileInformation(file_object, format_version)
 
-    magic = magic_header.get('magic', None)
-    if magic != 'SCCA':
-      raise errors.UnableToParseFile('Wrong Magic Value')
+    volume_information_offset = file_information.get(
+        'volume_information_offset', 0)
 
-    executable = binary.ReadUtf16Stream(file_object)
+    volume_information = self._ParseVolumeInformation(
+        file_object, volume_information_offset)
 
-    # TODO: Change this fixed location for a more elegant solution or change
-    # the header struct to properly seek to this location.
-    # Seek to the beginning of the header struct.
-    file_object.seek(0x4c - file_object.tell(), os.SEEK_CUR)
-    header = self.HEADER_STRUCT.parse_stream(file_object)
-
-    if magic_header.get('version', 0x11) is 0x11:
-      last_run_ofs = 0x78
-      run_count_ofs = 0x90
+    if volume_information:
+      volume_path_offset = volume_information.get('name_offset', 0)
+      volume_path_size = 2 * volume_information.get(
+          'name_number_of_characters', 0)
     else:
-      last_run_ofs = 0x80
-      run_count_ofs = 0x98
+      volume_path_offset = 0
+      volume_path_size = 0
 
-    file_object.seek(last_run_ofs - file_object.tell(), os.SEEK_CUR)
-    run_struct = construct.Struct(
-        'run', construct.ULInt64('last_run'), construct.Padding(
-            run_count_ofs - last_run_ofs - 8), construct.ULInt32('run_count'))
-
-    run = run_struct.parse_stream(file_object)
-
-    file_object.seek(
-        header.get('vol_start', 0) - file_object.tell(), os.SEEK_CUR)
-    volume = self.VOLUME_STRUCT.parse_stream(file_object)
-
-    file_object.seek(
-        header.get('vol_start', 0) + volume.get('path_bytes', 0) -
-        file_object.tell(), os.SEEK_CUR)
-    volume_path = binary.ReadUtf16Stream(
-        file_object, byte_size=(volume.get('path_length', 0) * 2 + 2))
-
-    file_object.seek(
-        header.get('dll_ofs', 0) - file_object.tell(), os.SEEK_CUR)
+    volume_path = self._ParseVolumePath(
+        file_object, volume_path_offset, volume_path_size)
 
     container = WinPrefetchEventContainer(
-        magic_header, header, executable, run, volume, volume_path)
+        file_header, file_information, volume_information, volume_path)
 
-    mapped_files = []
-    dll_read = 0
-    while dll_read < header.get('dll_length', 0):
-      dll_name = binary.ReadUtf16Stream(file_object)
+    dll_offset = file_information.get('dll_offset', 0)
+    dll_size = file_information.get('dll_size', 0)
 
-      # UTF-16 so multiply by 2, and then add 4 for end of line for each line.
-      dll_read += len(dll_name) * 2 + 4
-      mapped_files.append(dll_name)
+    if dll_offset > 0 and dll_size > 0:
+      file_object.seek(dll_offset, os.SEEK_SET)
+      dll_data = file_object.read(dll_size)
+      mapped_files = binary.ArrayOfUt16StreamCopyToString(dll_data)
 
-      if dll_name.endswith(executable):
-        path, _, name = dll_name.partition(volume_path)
-        container.path = name
+      if volume_path:
+        executable = binary.Ut16StreamCopyToString(
+            file_header.get('executable', ''))
+
+        for dll_name in mapped_files:
+          if dll_name.endswith(executable):
+            _, _, path = dll_name.partition(volume_path)
+            container.path = path
+    else:
+      mapped_files = []
 
     container.mapped_files = mapped_files
 
+    timestamp = file_information.get('last_run_time', 0)
     container.Append(event.FiletimeEvent(
-        run.get('last_run', 0),
-        eventdata.EventTimestamp.CREATION_TIME,
+        timestamp, eventdata.EventTimestamp.LAST_RUNTIME,
         container.data_type))
 
-    container.Append(event.FiletimeEvent(
-        volume.get('created', 0),
-        eventdata.EventTimestamp.LAST_RUNTIME,
-        container.data_type))
+    # Check for the 7 older last run time values available in v26.
+    if format_version == 26:
+      for last_run_time_index in range(1, 8):
+        last_run_time_identifier = 'last_run_time{0:d}'.format(
+            last_run_time_index)
 
-    return container
+        timestamp = file_information.get(last_run_time_identifier, 0)
+        if timestamp:
+          container.Append(event.FiletimeEvent(
+              timestamp,
+              u'Previous {0:s}'.format(eventdata.EventTimestamp.LAST_RUNTIME),
+              container.data_type))
 
+    if volume_information:
+      timestamp = volume_information.get('creation_time', 0)
+
+      if timestamp:
+        container.Append(event.FiletimeEvent(
+            timestamp, eventdata.EventTimestamp.CREATION_TIME,
+            container.data_type))
+
+    yield container
