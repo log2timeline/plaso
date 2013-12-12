6c94740cd3b71a3a6ca0dd6f6ad1e9b50a6a9596
Joaquin Moren Garijo <bastionado@gmail.com>
2013-12-12 19:37:54 +0000

    Code review: 38750044:  [Small Fix] Change mac parsers nomenclature and formatter update.

--- plaso/formatters/mac_wifi.py
+++ plaso/formatters/mac_wifi.py
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Formatter for the Mac wifi.log file."""
+"""Formatter for Mac wifi.log file."""
 
 from plaso.lib import eventdata
 
