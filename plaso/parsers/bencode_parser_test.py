f62920ab3dfc30cca5227340e614243f85d6a41e
Kristinn Gudjonsson <kristinn@log2timeline.net>
2013-12-17 07:45:31 -0800

    Code review: 43060043: Plugin Infra, pt III: Moving bencode to new plugin infrastructure.

--- plaso/parsers/bencode_parser_test.py
+++ plaso/parsers/bencode_parser_test.py
@@ -19,12 +19,14 @@
 import os
 import unittest
 
-from plaso.formatters import bencode_parser
-from plaso.parsers import bencode_parser
+# pylint: disable-msg=unused-import
+from plaso.formatters import bencode_parser as bencode_formatter
 
 from plaso.lib import eventdata
 from plaso.lib import preprocess
 from plaso.lib import putils
+
+from plaso.parsers import bencode_parser
 
 import pytz
 
