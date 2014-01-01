284f23a3734f4db5b206fcb2de5c371beb657021
Joaquin Moren Garijo <bastionado@gmail.com>
2014-01-01 19:21:38 +0000

    Code review: 39820043:  Basic Security Module for Mac OS X.

---
+++ plaso/formatters/bsm.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2013 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Formatter for Basic Security Module binary files."""
+
+from plaso.lib import eventdata
+
+
+class MacBSMFormatter(eventdata.ConditionalEventFormatter):
+  """Formatter for an BSM log entry."""
+  DATA_TYPE = 'mac:bsm:event'
+
+  FORMAT_STRING_PIECES = [
+      u'Type: {event_type}',
+      u'Return: {return_value}',
+      u'Information: {extra_tokens}']
+
+  FORMAT_STRING_SHORT_PIECES = [
+      u'Type: {event_type}',
+      u'Return: {return_value}']
+
+  SOURCE_LONG = 'BSM entry'
+  SOURCE_SHORT = 'LOG'
+
+
+class BSMFormatter(eventdata.ConditionalEventFormatter):
+  """Formatter for an BSM log entry."""
+  DATA_TYPE = 'bsm:event'
+
+  FORMAT_STRING_PIECES = [
+      u'Type: {event_type}',
+      u'Information: {extra_tokens}']
+
+  FORMAT_STRING_SHORT_PIECES = [
+      u'Type: {event_type}']
+
+  SOURCE_LONG = 'BSM entry'
+  SOURCE_SHORT = 'LOG'
+
--- plaso/unix/__init__.py
+++ plaso/unix/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Copyright 2012 The Plaso Project Authors.
+# Copyright 2013 The Plaso Project Authors.
 # Please see the AUTHORS file for details on individual authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,16 +14,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-__version__ = '1.1.0-dev'
-
-VERSION_DEV = True
-VERSION_DATE = '20140101'
-
-
-def GetVersion():
-  """Returns a version information for plaso."""
-  if not VERSION_DEV:
-    return __version__
-
-  return u'{}_{}'.format(__version__, VERSION_DATE)
