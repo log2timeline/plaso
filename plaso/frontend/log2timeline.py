61f710720a0405e70e69967df8f8162ad541127b
Joachim Metz <joachim.metz@gmail.com>
2014-01-02 19:03:04 +0100

    Code review: 38960044:  PyVFS preparations refactor step 10 - Merged OS and TSK collectors.

--- plaso/frontend/log2timeline.py
+++ plaso/frontend/log2timeline.py
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This file contains log2timeline, the friendly front-end to plaso."""
+
 import argparse
 import locale
 import logging
