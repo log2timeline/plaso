a677af36b2c5c1de5d72dba2225b1380b00475bb
Joachim Metz <joachim.metz@gmail.com>
2014-01-02 18:45:20 +0100

    Code review: 46850043:  Updated utility scripts.

--- utils/submit.sh
+++ utils/submit.sh
@@ -23,8 +23,9 @@ EXIT_SUCCESS=0;
 SCRIPTNAME=`basename $0`;
 
 BROWSER_PARAM="";
+CACHE_PARAM="";
 CL_NUMBER="";
-USE_CL_FILE="";
+USE_CL_FILE=0;
 
 while test $# -gt 0;
 do
@@ -41,23 +42,24 @@ do
   esac
 done
 
-if test -z $CL_NUMBER;
+if test -z "${CL_NUMBER}";
 then
   if test -f ._code_review_number;
   then
     CL_NUMBER=`cat ._code_review_number`
+    RESULT=`echo ${CL_NUMBER} | sed -e 's/[0-9]//g'`;
 
-    if test "x`echo $CL_NUMBER | sed -e 's/[0-9]//g'`" != "x";
+    if ! test -z "${RESULT}";
     then
-      echo "File ._code_review_number exists but contains an invalid CL number.";
+      echo "File ._code_review_number exists but contains an incorrect CL number.";
+
       exit ${EXIT_FAILURE};
     fi
-
     USE_CL_FILE=1;
   fi
 fi
 
-if test -z $CL_NUMBER;
+if test -z "${CL_NUMBER}";
 then
   echo "Usage: ./${SCRIPTNAME} [--nobrowser] CL_NUMBER";
   echo "";
@@ -79,48 +81,23 @@ fi
 # Source the common library.
 . utils/common.sh
 
-if ! linter;
-then
-  echo "Sumbit aborted - fix the issues reported by the linter.";
-
-  exit ${EXIT_FAILURE};
-fi
-
-echo "Running tests."
-# python run_tests.py
-
-if test $? -ne 0;
-then
-  echo "Sumbit aborted - fix the issues reported by the failing test.";
-
-  exit ${EXIT_FAILURE};
-fi
-
-echo "All came out clean, let's submit the code."
-
-URL_CODEREVIEW="https://codereview.appspot.com";
-
-# Get the description of the change list
-if test "x`which json_xs`" != "x";
-then
-  DESCRIPTION=`curl -s ${URL_CODEREVIEW}/api/${CL_NUMBER} | json_xs | grep '"subject"' | awk -F '"' '{print $(NF-1)}'`;
-else
-  DESCRIPTION=`curl ${URL_CODEREVIEW}/${CL_NUMBER}/ -s | grep "Issue ${CL_NUMBER}" | awk -F ':' '{print $2}' | tail -1`;
-fi
-
-if test "x${DESCRIPTION}" == "x";
-then
-  echo "Submit aborted - unable to find change list with number: ${CL_NUMBER}.";
-
-  exit ${EXIT_FAILURE};
-fi
-
 # Check if we're on the master branch.
 BRANCH=`git branch | grep -e "^[*]" | sed "s/^[*] //"`;
 
 if test "${BRANCH}" != "master";
 then
-  echo "Sumbit aborted - current branch is not master";
+  echo "Submit aborted - current branch is not master.";
+
+  exit ${EXIT_FAILURE};
+fi
+
+# Check for double status codes, upload.py cannot handle these correctly.
+STATUS_CODES=`git status -s | cut -b1,2 | grep '\S\S' | grep -v '??' | sort | uniq`;
+
+if ! test -z "${STATUS_CODES}";
+then
+  echo "Submit aborted - detected double git status codes."
+  echo "Run: 'git stash && git stash pop'.";
 
   exit ${EXIT_FAILURE};
 fi
@@ -130,7 +107,7 @@ git fetch
 
 if test $? -ne 0;
 then
-  echo "Sumbit aborted - unable to fetch updates from origin repo";
+  echo "Submit aborted - unable to fetch updates from origin repo";
 
   exit ${EXIT_FAILURE};
 fi
@@ -139,14 +116,56 @@ NUMBER_OF_CHANGES=`git log HEAD..origin/master --oneline | wc -l`;
 
 if test $? -ne 0;
 then
-  echo "Sumbit aborted - unable to determine if local repo is in sync with origin";
+  echo "Submit aborted - unable to determine if local repo is in sync with origin";
 
   exit ${EXIT_FAILURE};
 fi
 
 if test ${NUMBER_OF_CHANGES} -ne 0;
 then
-  echo "Sumbit aborted - local repo out of sync with origin, run: 'git stash && git pull && git stash pop' before sumbit.";
+  echo "Submit aborted - local repo out of sync with origin."
+  echo "Run: 'git stash && git pull && git stash pop'.";
+
+  exit ${EXIT_FAILURE};
+fi
+
+# Check if the linting is correct.
+if ! linter;
+then
+  echo "Submit aborted - fix the issues reported by the linter.";
+
+  exit ${EXIT_FAILURE};
+fi
+
+# Check if all the tests pass.
+if test -e run_tests.py;
+then
+  echo "Running tests.";
+  python run_tests.py
+
+  if test $? -ne 0;
+  then
+    echo "Submit aborted - fix the issues reported by the failing test.";
+
+    exit ${EXIT_FAILURE};
+  fi
+fi
+
+URL_CODEREVIEW="https://codereview.appspot.com";
+
+# Get the description of the change list.
+RESULT=`which json_xs`;
+
+if ! test -z "${RESULT}";
+then
+  DESCRIPTION=`curl -s ${URL_CODEREVIEW}/api/${CL_NUMBER} | json_xs | grep '"subject"' | awk -F '"' '{print $(NF-1)}'`;
+else
+  DESCRIPTION=`curl ${URL_CODEREVIEW}/${CL_NUMBER}/ -s | grep "Issue ${CL_NUMBER}" | awk -F ':' '{print $2}' | tail -1`;
+fi
+
+if test -z "${DESCRIPTION}";
+then
+  echo "Submit aborted - unable to find change list with number: ${CL_NUMBER}.";
 
   exit ${EXIT_FAILURE};
 fi
@@ -156,9 +175,25 @@ echo "Updating version information to match today's date."
 DATE_NOW=`date +"%Y%m%d"`
 sed -i -e "s/^VERSION_DATE.*$/VERSION_DATE = '${DATE_NOW}'/g" plaso/__init__.py
 
-python utils/upload.py --oauth2 $BROWSER_PARAM -y -i ${CL_NUMBER} -t "Submitted." -m "Code Submitted." --send_mail
+COMMIT_DESCRIPTION="Code review: ${CL_NUMBER}: ${DESCRIPTION}";
+echo "Submitting ${COMMIT_DESCRIPTION}";
 
-git commit -a -m "Code review: ${CL_NUMBER}: ${DESCRIPTION}";
+# Check if we need to set --cache.
+STATUS_CODES=`git status -s | cut -b1,2 | sed 's/\s//g' | sort | uniq`;
+
+for STATUS_CODE in ${STATUS_CODES};
+do
+  if test "${STATUS_CODE}" = "A";
+  then
+    CACHE_PARAM="--cache";
+  fi
+done
+
+python utils/upload.py \
+    --oauth2 ${BROWSER_PARAM} -y -i ${CL_NUMBER} ${CACHE_PARAM} \
+    -t "Submitted." -m "Code Submitted." --send_mail
+
+git commit -a -m "${COMMIT_DESCRIPTION}";
 git push
 
 if test -f "~/codereview_upload_cookies";
@@ -169,7 +204,9 @@ else
   echo "manually close the ticket on the code review site."
 fi
 
-if ! test -z ${USE_CL_FILE} && test -f "._code_review_number";
+if ! test -z "${USE_CL_FILE}" && test -f "._code_review_number";
 then
   rm -f ._code_review_number
 fi
+
+exit ${EXIT_SUCCESS};
