f4f5de763ac4e33c99de9534823e5d9684a9863d
Joaquin Moren Garijo <bastionado@gmail.com>
2014-01-18 00:00:29 +0000

    Code review: 47920043:  Mac CUPS IPP file parser

--- plaso/formatters/cups_ipp.py
+++ plaso/formatters/cups_ipp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2012 The Plaso Project Authors.
+# Copyright 2014 The Plaso Project Authors.
 # Please see the AUTHORS file for details on individual authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,16 +15,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Formatter for Mac CUPS IPP file."""
 
-__version__ = '1.1.0-dev'
-
-VERSION_DEV = True
-VERSION_DATE = '20140118'
+from plaso.lib import eventdata
 
 
-def GetVersion():
-  """Returns a version information for plaso."""
-  if not VERSION_DEV:
-    return __version__
+class CupsIppFormatter(eventdata.ConditionalEventFormatter):
+  """Formatter for CUPS IPP file."""
 
-  return u'{}_{}'.format(__version__, VERSION_DATE)
+  DATA_TYPE = 'cups:ipp:event'
+
+  FORMAT_STRING_PIECES = [
+      u'Status: {status}',
+      u'User: {user}',
+      u'Owner: {owner}',
+      u'Job Name: {job_name}',
+      u'Application: {application}',
+      u'Document type: {type_doc}',
+      u'Printer: {printer_id}']
+
+  FORMAT_STRING_SHORT_PIECES = [
+      u'Status: {status}',
+      u'Job Name: {job_name}']
+
+  SOURCE_LONG = 'CUPS IPP Log'
+  SOURCE_SHORT = 'LOG'
