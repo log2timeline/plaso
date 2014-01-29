029e23594a5a8c4686387222427663d269acff16
Joaquin Moren Garijo <bastionado@gmail.com>
2014-01-29 00:27:40 +0000

    Code review: 45910043:  Mac OS X ASL security.log.

--- plaso/formatters/mac_securityd.py
+++ plaso/formatters/mac_securityd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2012 The Plaso Project Authors.
+# Copyright 2014 The Plaso Project Authors.
 # Please see the AUTHORS file for details on individual authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,16 +15,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Formatter for ASL securityd log file."""
 
-__version__ = '1.1.0-dev'
-
-VERSION_DEV = True
-VERSION_DATE = '20140129'
+from plaso.lib import eventdata
 
 
-def GetVersion():
-  """Returns a version information for plaso."""
-  if not VERSION_DEV:
-    return __version__
+class MacSecuritydLogFormatter(eventdata.ConditionalEventFormatter):
+  """Formatter for ASL Securityd file."""
 
-  return u'{}_{}'.format(__version__, VERSION_DATE)
+  DATA_TYPE = 'mac:asl:securityd:line'
+
+  FORMAT_STRING_PIECES = [
+      u'Sender: {sender}',
+      u'({sender_pid})',
+      u'Level: {level}',
+      u'Facility: {facility}',
+      u'Text: {message}']
+
+  FORMAT_STRING_SHORT_PIECES = [u'Text: {message}']
+
+  SOURCE_LONG = 'Mac ASL Securityd Log'
+  SOURCE_SHORT = 'LOG'
+
