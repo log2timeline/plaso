a0745da4487825b1badc3498595489229cea4d22
Joaquin Moren Garijo <bastionado@gmail.com>
2014-01-30 19:35:20 +0000

    Code review: 52450043:  Mac OS X Plist artifacts.

--- plaso/parsers/plist_plugins/interface.py
+++ plaso/parsers/plist_plugins/interface.py
@@ -25,6 +25,7 @@ and operation of plugins for plist files which will be used by PlistParser.
 """
 
 import logging
+import os
 
 from plaso.lib import errors
 from plaso.lib import plugin
@@ -125,7 +126,7 @@ class PlistPlugin(plugin.BasePlugin):
     self.match = GetKeys(top_level, self.PLIST_KEYS)
     return self.GetEntries()
 
-  def GetEntries(self):
+  def GetEntries(self, unused_cache=None):
     """Yields PlistEvents from the values of entries within a plist.
 
     This is the main method that a plist plugin needs to implement.
@@ -288,3 +289,66 @@ def GetKeysDefaultEmpty(top_level, keys, depth=1):
         if set(match.keys()) == keys:
           return match
   return match
+
+
+# INFO: Plist works over only one level of binary plist and return
+#       attributes that contains another binary plist as string data.
+#       This class is used to re-read this string information and parse it.
+# TODO: Remove this once support is built into dfVFS.
+class FakeFile(object):
+  """Class that simulates a file-like object."""
+
+  def __init__(self, data):
+    """Initializes the event object.
+
+    Args:
+      data: binary stream that we are exposing as a file-like-object.
+    """
+    self._data = data
+    self._offset = 0
+
+  def tell(self):
+    """The actual position in the data.
+
+    Returns:
+      An integer with the actual position.
+    """
+    return self._offset
+
+  def seek(self, offset, whence=os.SEEK_SET):
+    """Move to new file position.
+
+    Args:
+      offset: Number of bytes that the pointer must be displaced.
+      whence: The direction of the displacement.
+    """
+    if whence == os.SEEK_SET:
+      self._offset = offset
+    elif whence == os.SEEK_CUR:
+      self._offset += offset
+    elif whence == os.SEEK_END:
+      self._offset = len(self._data) + offset
+
+  def read(self, size=0):
+    """Read size bytes from byte stream and return them."""
+    if self._offset > len(self._data):
+      return ''
+
+    if not size:
+      data_to_return = self._data[self._offset:]
+      self._offset = len(self._data)
+      return data_to_return
+
+    if size >= len(self._data[self._offset:]):
+      data_to_return = self._data[self._offset:]
+      self._offset = len(self._data)
+      return data_to_return
+
+    data_to_return = self._data[self._offset:self._offset + size]
+    self._offset += size
+    return data_to_return
+
+  def close(self):
+    """Simulate the closing file."""
+    pass
+
---
+++ plaso/parsers/plist_plugins/macuser_test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the Mac OS X local users plist plugin."""
+
+import unittest
+
+# pylint: disable-msg=unused-import
+from plaso.formatters import plist as plist_formatter
+from plaso.lib import event
+from plaso.parsers import plist
+from plaso.parsers.plist_plugins import macuser
+from plaso.parsers.plist_plugins import test_lib
+
+
+class MacUserPluginTest(test_lib.PlistPluginTestCase):
+  """Tests for the Mac OS X local user plist plugin."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    self._plugin = macuser.MacUserPlugin(None)
+    self._parser = plist.PlistParser(event.PreprocessObject(), None)
+
+  def testProcess(self):
+    """Tests the Process function."""
+    plist_name = u'user.plist'
+    test_file = self._GetTestFilePath([plist_name])
+    events = self._ParsePlistFileWithPlugin(
+        self._parser, self._plugin, test_file, plist_name)
+    event_objects = self._GetEventObjects(events)
+
+    self.assertEquals(len(event_objects), 1)
+
+    event_object = event_objects[0]
+    # date -u -d"Sat, 28 Dec 2013 04:35:47"
+    self.assertEqual(event_object.timestamp, 1388205347000000)
+    self.assertEqual(event_object.key, u'passwordLastSetTime')
+    self.assertEqual(event_object.root, u'/')
+    expected_desc = (
+        u'Last time user (501) changed the password: '
+        u'$ml$37313$fa6cac1869263baa85cffc5e77a3d4ee164b7'
+        u'5536cae26ce8547108f60e3f554$a731dbb0e386b169af8'
+        u'9fbb33c255ceafc083c6bc5194853f72f11c550c42e4625'
+        u'ef113b66f3f8b51fc3cd39106bad5067db3f7f1491758ff'
+        u'e0d819a1b0aba20646fd61345d98c0c9a411bfd1144dd4b'
+        u'3c40ec0f148b66d5b9ab014449f9b2e103928ef21db6e25'
+        u'b536a60ff17a84e985be3aa7ba3a4c16b34e0d1d2066ae178')
+    self.assertEqual(event_object.desc, expected_desc)
+    expected_string = u'//passwordLastSetTime {}'.format(expected_desc)
+    expected_short = u'{}...'.format(expected_string[:77])
+    self._TestGetMessageStrings(
+        event_object, expected_string, expected_short)
+
+
+if __name__ == '__main__':
+  unittest.main()
