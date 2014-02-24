95f7ae507020beec41dc15b180e7d8cbc8159244
Kristinn Gudjonsson <kristinn@log2timeline.net>
2014-02-24 10:12:33 -0800

    Code review: 67180043: Changing OLECF parser into a plugin infrastructure.

--- plaso/parsers/olecf_plugins/__init__.py
+++ plaso/parsers/olecf_plugins/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 #
-# Copyright 2012 The Plaso Project Authors.
+# -*- coding: utf-8 -*-
+# Copyright 2014 The Plaso Project Authors.
 # Please see the AUTHORS file for details on individual authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,16 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""This file contains an import statement for each OLECF plugin."""
 
-__version__ = '1.1.0-dev'
-
-VERSION_DEV = True
-VERSION_DATE = '20140224'
-
-
-def GetVersion():
-  """Returns a version information for plaso."""
-  if not VERSION_DEV:
-    return __version__
-
-  return u'{}_{}'.format(__version__, VERSION_DATE)
+from plaso.parsers.olecf_plugins import default
+from plaso.parsers.olecf_plugins import summary
