7586197728305bdfed7073797d1bbaa36bccda6f
Eric Mak <ericmak@gmail.com>
2014-03-18 12:34:35 -0700

    Code review: 73880043: L2T 0.65 Style TLN Outputter

--- plaso/output/tln_test.py
+++ plaso/output/tln_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2012 The Plaso Project Authors.
+# Copyright 2014 The Plaso Project Authors.
 # Please see the AUTHORS file for details on individual authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,75 +15,69 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for the L2tCsv output class."""
+"""Tests for the TLN output class."""
 import StringIO
 import unittest
 
 from plaso.lib import event
 from plaso.lib import eventdata
-from plaso.output import l2t_csv
+from plaso.output import tln
 
 
-class L2tTestEvent(event.EventObject):
+class TlnTestEvent(event.EventObject):
   """Simplified EventObject for testing."""
-  DATA_TYPE = 'test:l2t_csv'
+  DATA_TYPE = 'test:l2ttln'
 
   def __init__(self):
     """Initialize event with data."""
-    super(L2tTestEvent, self).__init__()
+    super(TlnTestEvent, self).__init__()
     self.timestamp = 1340821021000000
-    self.timestamp_desc = eventdata.EventTimestamp.WRITTEN_TIME
     self.hostname = u'ubuntu'
-    self.filename = u'log/syslog.1'
-    self.display_name = u'log/syslog.1'
+    self.display_name = u'OS: log/syslog.1'
+    self.inode = 12345678
     self.text = (
-        u'Reporter <CRON> PID: 8442 (pam_unix(cron:session): session\n '
+        u'Reporter <CRON> PID: |8442| (pam_unix(cron:session): session\n '
         u'closed for user root)')
+    self.username = u'root'
 
 
-class L2tTestEventFormatter(eventdata.EventFormatter):
+class L2TTlnTestEventFormatter(eventdata.EventFormatter):
   """Formatter for the test event."""
-  DATA_TYPE = 'test:l2t_csv'
+  DATA_TYPE = 'test:l2ttln'
   FORMAT_STRING = u'{text}'
-
   SOURCE_SHORT = 'LOG'
   SOURCE_LONG = 'Syslog'
 
 
-class L2tCsvTest(unittest.TestCase):
-  """Contains tests to validate the L2tCSV outputter."""
+class TlnTest(unittest.TestCase):
+  """Tests for the TLN outputter."""
+
   def setUp(self):
+    """Sets up the objects needed for this test."""
     self.output = StringIO.StringIO()
-    self.formatter = l2t_csv.L2tcsv(None, self.output)
-    self.event_object = L2tTestEvent()
+    self.formatter = tln.Tln(None, self.output)
+    self.event_object = TlnTestEvent()
 
   def testStart(self):
     """Test ensures header line is outputted as expected."""
-
-    correct_line = (
-        u'date,time,timezone,MACB,source,sourcetype,type,user,host,short,desc,'
-        u'version,filename,inode,notes,format,extra\n')
+    correct_line = u'Time|Source|Host|User|Description\n'
 
     self.formatter.Start()
     self.assertEquals(self.output.getvalue(), correct_line)
 
   def testEventBody(self):
-    """Test ensures that returned lines returned are formatted as L2tCSV."""
+    """Test ensures that returned lines returned are formatted as TLN."""
 
     self.formatter.EventBody(self.event_object)
-    correct = (
-        u'06/27/2012,18:17:01,UTC,M...,LOG,Syslog,Content Modification Time,-,'
-        u'ubuntu,Reporter <CRON> PID: 8442 (pam_unix(cron:session): session '
-        u'closed for user root),Reporter <CRON> PID: 8442 '
-        u'(pam_unix(cron:session): '
-        u'session closed for user root),2,log/syslog.1,-,-,-,\n')
+    correct = (u'1340821021|LOG|ubuntu|root|Reporter <CRON> PID:  8442  '
+               u'(pam_unix(cron:session): session closed for user root)\n')
     self.assertEquals(self.output.getvalue(), correct)
 
-  def testEventBodyNoExtraCommas(self):
-    """Test ensures that the only commas returned are the 16 delimeters."""
+  def testEventBodyNoStrayPipes(self):
+    """Test ensures that the only pipes are the four field delimiters."""
 
     self.formatter.EventBody(self.event_object)
-    self.assertEquals(self.output.getvalue().count(u','), 16)
+    self.assertEquals(self.output.getvalue().count(u'|'), 4)
 
 
 if __name__ == '__main__':
