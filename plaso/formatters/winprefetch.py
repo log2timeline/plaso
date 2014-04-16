ef7267b6bb5dd95fea7038dad4bd23533954c802
Joachim Metz <joachim.metz@gmail.com>
2014-04-16 23:30:47 +0200

    Code review: 87270044:  Changed prefetch parser to support multiple values.

--- plaso/formatters/winprefetch.py
+++ plaso/formatters/winprefetch.py
@@ -14,25 +14,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Formatter for the Windows Prefetch files."""
+"""Formatter for the Windows Prefetch events."""
+
+from plaso.lib import errors
 from plaso.lib import eventdata
 
 
-class WinPrefetchFormatter(eventdata.ConditionalEventFormatter):
-  """Class that formats Windows Prefetch events."""
-  DATA_TYPE = 'windows:prefetch'
+class WinPrefetchExecutionFormatter(eventdata.ConditionalEventFormatter):
+  """Class that formats Windows Prefetch execution events."""
+  DATA_TYPE = 'windows:prefetch:execution'
 
-  # The format string.
   FORMAT_STRING_PIECES = [
-      u'{prefetch_type}',
+      u'Prefetch',
       u'[{executable}] was executed -',
       u'run count {run_count}',
       u'path: {path}',
       u'hash: 0x{prefetch_hash:08X}',
-      u'[',
-      u'volume serial: 0x{volume_serial:08X}',
-      u'volume path: {volume_path}'
-      u']']
+      u'{volumes_string}']
 
   FORMAT_STRING_SHORT_PIECES = [
       u'{executable} was run',
@@ -42,11 +40,35 @@ class WinPrefetchFormatter(eventdata.ConditionalEventFormatter):
   SOURCE_SHORT = 'LOG'
 
   def GetMessages(self, event_object):
-    """Return the message strings."""
-    if event_object.version == 0x11:
-      event_object.prefetch_type = 'Prefetch'
-    else:
-      event_object.prefetch_type = 'Superfetch'
+    """Returns a list of messages extracted from an event object.
 
-    return super(WinPrefetchFormatter, self).GetMessages(event_object)
+    Args:
+      event_object: The event object (instance of EventObject) containing
+                    the event specific data.
 
+    Returns:
+      A list that contains both the longer and shorter version of the message
+      string.
+
+    Raises:
+      WrongFormatter: when the data type of the formatter does not match
+                      that of the event object.
+    """
+    if self.DATA_TYPE != event_object.data_type:
+      raise errors.WrongFormatter(
+          u'Invalid event object - unsupported data type: {0:s}'.format(
+              event_object.data_type))
+
+    volumes_strings = []
+    for volume_index in range(0, event_object.number_of_volumes):
+      volumes_strings.append((
+          u'volume: {0:d} [serial number: 0x{1:08X}, device path: '
+          u'{2:s}]').format(
+              volume_index + 1,
+              event_object.volume_serial_numbers[volume_index],
+              event_object.volume_device_paths[volume_index]))
+
+    if volumes_strings:
+      event_object.volumes_string = u', '.join(volumes_strings)
+
+    return super(WinPrefetchExecutionFormatter, self).GetMessages(event_object)
