dafe7f594af5926a1ae78fb69113458d51028b27
Joachim Metz <joachim.metz@gmail.com>
2014-05-06 06:56:14 +0200

    Code review: 92060043:  Fix for tempfile holding an exclusive lock on Windows.

--- plaso/output/pstorage_test.py
+++ plaso/output/pstorage_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
 # Copyright 2013 The Plaso Project Authors.
 # Please see the AUTHORS file for details on individual authors.
 #
@@ -15,7 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for plaso.output.pstorage."""
+
 import os
+import shutil
 import tempfile
 import unittest
 
@@ -25,44 +28,65 @@ from plaso.lib import storage
 from plaso.output import pstorage   # pylint: disable=unused-import
 
 
+class TempDirectory(object):
+  """A self cleaning temporary directory."""
+
+  def __init__(self):
+    """Initializes the temporary directory."""
+    super(TempDirectory, self).__init__()
+    self.name = u''
+
+  def __enter__(self):
+    """Make this work with the 'with' statement."""
+    self.name = tempfile.mkdtemp()
+    return self.name
+
+  def __exit__(self, unused_type, unused_value, unused_traceback):
+    """Make this work with the 'with' statement."""
+    shutil.rmtree(self.name, True)
+
+
 class PstorageTest(unittest.TestCase):
   def setUp(self):
     self.test_filename = os.path.join('test_data', 'psort_test.out')
-    self.dump_file = tempfile.NamedTemporaryFile(delete=True)
+
     # Show full diff results, part of TestCase so does not follow our naming
     # conventions.
     self.maxDiff = None
     pfilter.TimeRangeCache.ResetTimeConstraints()
 
   def testOutput(self):
-    # Copy events to pstorage dump.
-    with storage.StorageFile(self.test_filename, read_only=True) as store:
-      formatter_cls = output.GetOutputFormatter('Pstorage')
-      formatter = formatter_cls(store, self.dump_file)
-      with output.EventBuffer(formatter, check_dedups=False) as output_buffer:
-        event_object = formatter.FetchEntry()
-        while event_object:
-          output_buffer.Append(event_object)
+    with TempDirectory() as dirname:
+      dump_file = os.path.join(dirname, 'plaso.db')
+      # Copy events to pstorage dump.
+      with storage.StorageFile(self.test_filename, read_only=True) as store:
+        formatter_cls = output.GetOutputFormatter('Pstorage')
+        formatter = formatter_cls(store, dump_file)
+        with output.EventBuffer(formatter, check_dedups=False) as output_buffer:
           event_object = formatter.FetchEntry()
+          while event_object:
+            output_buffer.Append(event_object)
+            event_object = formatter.FetchEntry()
 
-    # Make sure original and dump have the same events.
-    original = storage.StorageFile(self.test_filename, read_only=True)
-    dump = storage.StorageFile(self.dump_file.name, read_only=True)
-    event_object_original = original.GetSortedEntry()
-    event_object_dump = dump.GetSortedEntry()
-    original_list = []
-    dump_list = []
-
-    while event_object_original:
-      original_list.append(event_object_original.EqualityString())
-      dump_list.append(event_object_dump.EqualityString())
+      # Make sure original and dump have the same events.
+      original = storage.StorageFile(self.test_filename, read_only=True)
+      dump = storage.StorageFile(dump_file, read_only=True)
       event_object_original = original.GetSortedEntry()
       event_object_dump = dump.GetSortedEntry()
+      original_list = []
+      dump_list = []
 
-    self.assertFalse(event_object_dump)
+      while event_object_original:
+        original_list.append(event_object_original.EqualityString())
+        dump_list.append(event_object_dump.EqualityString())
+        event_object_original = original.GetSortedEntry()
+        event_object_dump = dump.GetSortedEntry()
 
-    for original_str, dump_str in zip(sorted(original_list), sorted(dump_list)):
-      self.assertEqual(original_str, dump_str)
+      self.assertFalse(event_object_dump)
+
+      for original_str, dump_str in zip(
+          sorted(original_list), sorted(dump_list)):
+        self.assertEqual(original_str, dump_str)
 
 
 if __name__ == '__main__':
