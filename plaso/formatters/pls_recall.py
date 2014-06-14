292cc840b2a00b5e636f9e577a8a4b9ed709355d
plaso <plaso@plaso-virtual-machine.(none)>
2014-06-13 21:37:53 -0400

    Done with code review

---
+++ plaso/formatters/pls_recall.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Formatter for PL-Sql Recall events."""
+
+from plaso.lib import eventdata
+
+
+class PlsRecallFormatter(eventdata.EventFormatter):
+  """Formatter for a for a PL-Sql Recall file container."""
+  DATA_TYPE = 'PLSRecall:event'
+  SOURCE_LONG = 'PL-Sql Developer Recall file'
+  SOURCE_SHORT = 'PLSRecall'
+
+  # The format string.
+  FORMAT_STRING = (u'Sequence #{sequence} User: {username} '
+                     u'Database Name: {database_name} Query: {query}')
+  FORMAT_STRING_SHORT = u'{sequence} {username} {database_name} {query}'
+
---
+++ plaso/parsers/pls_recall.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specifiSc language governing permissions and
+# limitations under the License.
+"""Parser for PL-Sql Developer Recall files."""
+
+import construct
+import os
+
+from plaso.lib import errors
+from plaso.lib import event
+from plaso.lib import parser
+from plaso.lib import timelib
+
+
+class PlsRecallEvent(event.EventObject):
+  """Convenience class for a PL-Sql Recall file container."""
+  DATA_TYPE = 'PLSRecall:event'
+
+  def __init__(self, timestamp, sequence, user, database, query):
+    """Initializes the event object.
+
+    Args:
+      timestamp: The timestamp when the entry was created.
+      sequence: Sequence indicates the order of execution.
+      username: The username that made the query.
+      database_name: String containing the databe name.
+      query: String containing the PS-Sql query.
+    """
+    super(PlsRecallEvent, self).__init__()
+    self.timestamp = timestamp
+    self.sequence = sequence
+    self.username = user
+    self.database_name = database
+    self.query = query
+
+
+class PlsRecallParser(parser.BaseParser):
+  """Parse PS-Sql Recall files.
+
+  Parser is based on a:
+    TRecallRecord = packed record
+      Sequence: Integer;
+      TimeStamp: TDateTime;
+      Username: array[0..30] of Char;
+      Database: array[0..80] of Char;
+      Text: array[0..4000] of Char;
+    end;
+
+    Delphi TDateTime is a little endian 64-bit
+    floating point without any time zone information
+  """
+
+  NAME = 'pls_recall'
+
+  PLS_STRUCT = construct.Struct(
+    'PS-Sql_Recall',
+    construct.ULInt32('Sequence'),
+    construct.LFloat64('TimeStamp'),
+    construct.String('Username', 31, None, '\x00'),
+    construct.String('Database', 81, None, '\x00'),
+    construct.String('Query', 4001, None, '\x00'))
+
+  def Parse(self, file_entry):
+    """Extract entries from a PLSRecall.dat file.
+
+    Args:
+      file_entry: A file entry object (instance of dfvfs.FileEntry).
+
+    Yields:
+      An pls_recall event for each entry in the file.
+    """
+    file_object = file_entry.GetFileObject()
+
+    try:
+      is_pls = self.VerifyFile(file_object)
+    except (IOError, construct.FieldError) as exception:
+      raise errors.UnableToParseFile((
+          u'Not a PLSrecall File, unable to parse.'
+          u'with error: {0:s}').format(exception))
+
+    if not is_pls:
+      raise errors.UnableToParseFile(
+          u'Not a PLSRecall File, unable to parse.')
+
+    file_object.seek(0, os.SEEK_SET)
+    pls_record = self.PLS_STRUCT.parse_stream(file_object)
+
+    while pls_record:
+      yield PlsRecallEvent(
+        timelib.Timestamp.FromDelphiTime(pls_record.TimeStamp),
+        pls_record.Sequence, pls_record.Username,
+        pls_record.Database, pls_record.Query)
+
+      try:
+        pls_record = self.PLS_STRUCT.parse_stream(file_object)
+      except (construct.FieldError) as exception:
+        # The code has reached the end of file (EOF).
+        break
+
+    file_object.close()
+
+  def VerifyFile(self, file_object):
+    """Check if the file is a PLSRecall.dat file.
+
+    Args:
+      file_object: file that we want to check.
+
+    Returns:
+      True if this is a valid PLSRecall.dat file, otherwise False.
+    """
+    file_object.seek(0, os.SEEK_SET)
+
+    # The file consists of PL-Sql structures that are equal
+    # size (4125 bytes) TRecallRecord records. It should be
+    # noted that the query value is free form.
+    try:
+      self.PLS_STRUCT.parse_stream(file_object)
+    except (IOError, construct.FieldError):
+      return False
+
+    return True
