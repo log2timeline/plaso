8bfcb83be793e5b03250d8547ac5a1d08877fed2
Joachim Metz <joachim.metz@gmail.com>
2014-07-15 20:44:27 +0200

    Code review: 117820043: Bug fix for BagMRU parser.

--- plaso/parsers/winreg_plugins/__init__.py
+++ plaso/parsers/winreg_plugins/__init__.py
@@ -18,6 +18,7 @@
 """This file contains the import statements for the Registry plugins."""
 
 from plaso.parsers.winreg_plugins import appcompatcache
+from plaso.parsers.winreg_plugins import bagmru
 from plaso.parsers.winreg_plugins import ccleaner
 from plaso.parsers.winreg_plugins import default
 from plaso.parsers.winreg_plugins import lfu
