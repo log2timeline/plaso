3756d8b514afc547e4947392f391b21ac88d7493
Francesco Picasso <francesco.picasso@gmail.com>
2015-01-08 18:08:03 +0100

    Code review: 60530048: WinReg Timezone Plugin

---
+++ plaso/parsers/winreg_plugins/timezone.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Plug-in to collect information about the Windows timezone settings."""
+
+from plaso.events import windows_events
+from plaso.parsers import winreg
+from plaso.parsers.winreg_plugins import interface
+
+
+__author__ = 'Francesco Picasso (francesco.picasso@gmail.com)'
+
+
+class WinRegTimezonePlugin(interface.KeyPlugin):
+  """Plug-in to collect information about the Windows timezone settings."""
+
+  NAME = 'winreg_timezone'
+  DESCRIPTION = u'Parser for Windows timezone settings.'
+
+  REG_TYPE = 'SYSTEM'
+  REG_KEYS = [u'\\{current_control_set}\\Control\\TimeZoneInformation']
+  URLS = []
+
+  WIN_TIMEZONE_VALUE_NAMES = (
+      u'ActiveTimeBias', u'Bias', u'DaylightBias', u'DaylightName',
+      u'DynamicDaylightTimeDisabled', u'StandardBias', u'StandardName',
+      u'TimeZoneKeyName')
+
+  def _GetValue(self, value_name, key):
+    """Get value helper, it returns the key value if exists.
+
+    Given the Registry key and the value_name it returns the data in the value
+    or None if value_name does not exist.
+
+    Args:
+      value_name: the name of the value.
+      key: Registry key (instance of winreg.WinRegKey).
+
+    Returns:
+      The data inside a Registry value or None.
+    """
+    value = key.GetValue(value_name)
+    if value:
+      return value.data
+
+  def GetEntries(
+      self, parser_context, key=None, registry_type=None, file_entry=None,
+      parser_chain=None, **unused_kwargs):
+    """Collect values and return an event.
+
+    Args:
+      parser_context: A parser context object (instance of ParserContext).
+      key: Optional Registry key (instance of winreg.WinRegKey).
+           The default is None.
+      registry_type: Optional Registry type string. The default is None.
+      file_entry: Optional file entry object (instance of dfvfs.FileEntry).
+                  The default is None.
+      parser_chain: Optional string containing the parsing chain up to this
+                    point. The default is None.
+    """
+    if key is None:
+      return
+    text_dict = {}
+    for value_name in self.WIN_TIMEZONE_VALUE_NAMES:
+      text_dict[value_name] = self._GetValue(value_name, key)
+
+    event_object = windows_events.WindowsRegistryEvent(
+        key.last_written_timestamp, key.path, text_dict, offset=key.offset,
+        registry_type=registry_type)
+
+    parser_context.ProduceEvent(
+        event_object, parser_chain=parser_chain, file_entry=file_entry)
+
+
+# Let's register the plugin.
+winreg.WinRegistryParser.RegisterPlugin(WinRegTimezonePlugin)
---
+++ plaso/parsers/winreg_plugins/timezone_test.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 The Plaso Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the timezone Windows Registry plugin."""
+
+import unittest
+
+from plaso.lib import timelib_test
+from plaso.parsers.winreg_plugins import test_lib
+from plaso.parsers.winreg_plugins import timezone as winreg_timezone
+from plaso.winreg import test_lib as winreg_test_lib
+
+
+class WinRegTimezonePluginTest(test_lib.RegistryPluginTestCase):
+  """Tests for the timezone Windows Registry plugin."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    self._plugin = winreg_timezone.WinRegTimezonePlugin()
+
+  def testProcessMock(self):
+    """Tests the Process function on created key."""
+    knowledge_base_values = {'current_control_set': u'ControlSet001'}
+    key_path = u'\\ControlSet001\\Control\\TimeZoneInformation'
+    values = []
+
+    values.append(winreg_test_lib.TestRegValue(
+        'ActiveTimeBias', '\xFF\xFF\xFF\xC4',
+        winreg_test_lib.TestRegValue.REG_DWORD_BIG_ENDIAN))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'Bias', '\xFF\xFF\xFF\xC4',
+        winreg_test_lib.TestRegValue.REG_DWORD_BIG_ENDIAN))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'DaylightBias', '\xFF\xFF\xFF\xC4',
+        winreg_test_lib.TestRegValue.REG_DWORD_BIG_ENDIAN))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'DaylightName', '@tzres.dll,-321'.encode('utf_16_le'),
+        winreg_test_lib.TestRegValue.REG_SZ))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'DaylightStart', '\x00\x00\x03\x00\x05\x00\x02\00'
+        '\x00\x00\x00\x00\x00\x00\x00\x00',
+        winreg_test_lib.TestRegValue.REG_BINARY))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'DynamicDaylightTimeDisabled', '\x00\x00\x00\x00',
+        winreg_test_lib.TestRegValue.REG_DWORD_BIG_ENDIAN))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'StandardBias', '\x00\x00\x00\x00',
+        winreg_test_lib.TestRegValue.REG_DWORD_BIG_ENDIAN))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'StandardName', '@tzres.dll,-322'.encode('utf_16_le'),
+        winreg_test_lib.TestRegValue.REG_SZ))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'StandardStart', '\x00\x00\x0A\x00\x05\x00\x03\00'
+        '\x00\x00\x00\x00\x00\x00\x00\x00',
+        winreg_test_lib.TestRegValue.REG_BINARY))
+
+    values.append(winreg_test_lib.TestRegValue(
+        'TimeZoneKeyName', 'W. Europe Standard Time'.encode('utf_16_le'),
+        winreg_test_lib.TestRegValue.REG_SZ))
+
+    # expr `date -u -d "2013-01-30 10:47:57" +"%s%N"` \/ 1000
+    winreg_key = winreg_test_lib.TestRegKey(
+        key_path, 1359542877000000, values, 153)
+
+    event_queue_consumer = self._ParseKeyWithPlugin(
+        self._plugin, winreg_key, knowledge_base_values=knowledge_base_values)
+    event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
+
+    self.assertEquals(len(event_objects), 1)
+
+    expected_timestamp = timelib_test.CopyStringToTimestamp(
+        '2013-01-30 10:47:57.000000')
+    self.assertEquals(event_objects[0].timestamp, expected_timestamp)
+
+    expected_msg = (
+        u'[{0:s}] '
+        u'ActiveTimeBias: -60 '
+        u'Bias: -60 '
+        u'DaylightBias: -60 '
+        u'DaylightName: @tzres.dll,-321 '
+        u'DynamicDaylightTimeDisabled: 0 '
+        u'StandardBias: 0 '
+        u'StandardName: @tzres.dll,-322 '
+        u'TimeZoneKeyName: W. Europe Standard Time').format(key_path)
+
+    expected_msg_short = (
+        u'[{0:s}] '
+        u'ActiveTimeBias: -60 '
+        u'Bias: -60 '
+        u'Da...').format(key_path)
+
+    self._TestGetMessageStrings(
+        event_objects[0], expected_msg, expected_msg_short)
+
+  def testProcessFile(self):
+    """Tests the Process function on registry file."""
+    knowledge_base_values = {'current_control_set': u'ControlSet001'}
+    test_file = self._GetTestFilePath(['SYSTEM'])
+    key_path = u'\\ControlSet001\\Control\\TimeZoneInformation'
+    winreg_key = self._GetKeyFromFile(test_file, key_path)
+
+    event_queue_consumer = self._ParseKeyWithPlugin(
+        self._plugin, winreg_key, knowledge_base_values=knowledge_base_values)
+    event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
+
+    self.assertEquals(len(event_objects), 1)
+
+    expected_timestamp = timelib_test.CopyStringToTimestamp(
+        '2012-03-11 07:00:00.000642')
+    self.assertEquals(event_objects[0].timestamp, expected_timestamp)
+
+    expected_msg = (
+        u'[{0:s}] '
+        u'ActiveTimeBias: 240 '
+        u'Bias: 300 '
+        u'DaylightBias: -60 '
+        u'DaylightName: @tzres.dll,-111 '
+        u'DynamicDaylightTimeDisabled: 0 '
+        u'StandardBias: 0 '
+        u'StandardName: @tzres.dll,-112 '
+        u'TimeZoneKeyName: Eastern Standard Time').format(key_path)
+
+    expected_msg_short = (
+        u'[{0:s}] '
+        u'ActiveTimeBias: 240 '
+        u'Bias: 300 '
+        u'Da...').format(key_path)
+
+    self._TestGetMessageStrings(
+        event_objects[0], expected_msg, expected_msg_short)
+
+
+if __name__ == '__main__':
+  unittest.main()
