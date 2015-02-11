79bef23e1de837b2b63f65894504d6f2921872ff
Joachim Metz <joachim.metz@gmail.com>
2015-02-11 22:33:50 +0100

    Code review: 199170043:  Removed unused APL headers and shebangs.

--- plaso/lib/utils_test.py
+++ plaso/lib/utils_test.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2013 The Plaso Project Authors.
-# Please see the AUTHORS file for details on individual authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""This file contains the unit tests for the utils library of methods."""
 import unittest
 
 from plaso.lib import utils
@@ -31,7 +16,7 @@ class UtilsTestCase(unittest.TestCase):
     bytes_in = u'Plaso Síar Og Raðar Þessu'
     self.assertTrue(utils.IsText(bytes_in))
 
-    bytes_in = '\x01\62LSO\xFF'
+    bytes_in = '\x01\\62LSO\xFF'
     self.assertFalse(utils.IsText(bytes_in))
 
     bytes_in = 'T\x00h\x00i\x00s\x00\x20\x00'
