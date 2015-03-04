f59801b57eebee1db118322244cdc8771a85ef29
Daniel White <onager@deerpie.com>
2015-03-04 22:43:53 +0100

    Code review: 197660043: Initial support for hashing input files.

---
+++ plaso/hashers/manager_test.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""Tests for the hasher manager."""
+
+import unittest
+
+from plaso.hashers import interface
+from plaso.hashers import manager
+
+
+class TestHasher(interface.BaseHasher):
+  """A dummy test hasher."""
+
+  NAME = u'testhash'
+
+  def Update(self, data):
+    return
+
+  def GetBinaryDigest(self):
+    # Chosen by fair dice roll. Guaranteed to be random.
+    # Compliant with RFC 1149.4. See http://xkcd.com/221/.
+    return '4'
+
+  def GetStringDigest(self):
+    # Chosen by fair dice roll. Guaranteed to be random.
+    # Compliant with RFC 1149.4. See http://xkcd.com/221/.
+    return u'4'
+
+
+
+class HashersManagerTest(unittest.TestCase):
+  """Tests for the hashers manager."""
+
+  def testHasherRegistration(self):
+    """Tests the registration and deregistration of hashers."""
+    # pylint: disable=protected-access
+    number_of_parsers = len(manager.HashersManager._hasher_classes)
+    manager.HashersManager.RegisterHasher(TestHasher)
+    self.assertEqual(
+        number_of_parsers + 1, len(manager.HashersManager._hasher_classes))
+
+    with self.assertRaises(KeyError):
+      manager.HashersManager.RegisterHasher(TestHasher)
+
+    manager.HashersManager.DeregisterHasher(TestHasher)
+
+    self.assertEqual(
+        number_of_parsers, len(manager.HashersManager._hasher_classes))
+
+  def testGetHasherNamesFromString(self):
+    """Tests the GetHasherNamesFromString method."""
+    test_strings = u'md5,sha256,testhash'
+    manager.HashersManager.RegisterHasher(TestHasher)
+    names = manager.HashersManager.GetHasherNamesFromString(test_strings)
+    self.assertEqual(3, len(names))
+    manager.HashersManager.DeregisterHasher(TestHasher)
+    names = manager.HashersManager.GetHasherNamesFromString(test_strings)
+    self.assertEqual(2, len(names))
+
+  def testGetHasherObjects(self):
+    """Tests getting hasher objects by name."""
+    hasher_names = manager.HashersManager.GetHasherNames()
+    hashers = manager.HashersManager.GetHasherObjects(hasher_names)
+    self.assertEqual(len(hasher_names), len(hashers))
+    for hasher in hashers:
+      self.assertIsInstance(hasher, interface.BaseHasher)
