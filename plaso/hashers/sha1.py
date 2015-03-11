9124cd05c3b97c309ad9b6b63223050d41024c0b
Daniel White <onager@deerpie.com>
2015-03-11 17:30:43 +0100

    Code review: 216750043: Add SHA-1 hasher.

---
+++ plaso/hashers/sha1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""The SHA-1 Hasher implementation"""
+
+import hashlib
+
+from plaso.hashers import interface
+from plaso.hashers import manager
+
+
+class SHA1Hasher(interface.BaseHasher):
+  """This class provides SHA-1 hashing functionality."""
+
+  NAME = u'sha1'
+  DESCRIPTION = u'Calculates a SHA-1 digest hash over input data.'
+
+  def __init__(self):
+    """Initializes the SHA-1 hasher."""
+    super(SHA1Hasher, self).__init__()
+    self._sha1_context = hashlib.sha1()
+
+  def Update(self, data):
+    """Updates the current state of the hasher with a new block of data.
+
+    Repeated calls to update are equivalent to one single call with the
+    concatenation of the arguments.
+
+    Args:
+      data: a string of data with which to update the context of the hasher.
+    """
+    self._sha1_context.update(data)
+
+  def GetStringDigest(self):
+    """Returns the digest of the hash function expressed as a unicode string.
+
+    Returns:
+      A string hash digest calculated over the data blocks passed to
+      Update(). The string will consist of printable Unicode characters.
+    """
+    return unicode(self._sha1_context.hexdigest())
+
+  def GetBinaryDigest(self):
+    """Returns the digest of the hash function as a binary string.
+
+    Returns:
+      A binary string hash digest calculated over the data blocks passed to
+      Update().
+    """
+    return self._sha1_context.digest()
+
+
+manager.HashersManager.RegisterHasher(SHA1Hasher)
---
+++ plaso/hashers/sha1_test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Tests for the SHA-1 hasher implementation."""
+
+import unittest
+
+from plaso.hashers import sha1
+from plaso.hashers import test_lib
+
+class SHA1Test(test_lib.HasherTestCase):
+  """Tests the SHA-1 hasher."""
+
+  def testFileHashMatches(self):
+    """Tests that a few known files match the expected hashes."""
+    empty_file_sha1 = u'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+    hasher = sha1.SHA1Hasher()
+    self._AssertTestPathStringDigestMatch(
+        hasher, [u'empty_file'], empty_file_sha1)
+    hasher = sha1.SHA1Hasher()
+    self._AssertTestPathBinaryDigestMatch(
+        hasher, [u'empty_file'], empty_file_sha1.decode('hex'))
+
+    unicode_file_sha1 = u'd9f264323004fd9518c0474967f80421e60e9813'
+    hasher = sha1.SHA1Hasher()
+    self._AssertTestPathStringDigestMatch(
+        hasher, [u'ímynd.dd'], unicode_file_sha1)
+    hasher = sha1.SHA1Hasher()
+    self._AssertTestPathBinaryDigestMatch(
+        hasher, [u'ímynd.dd'], unicode_file_sha1.decode('hex'))
+
+if __name__ == '__main__':
+  unittest.main()
