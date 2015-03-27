410fa34d5fd13867f3f8786397143582e177adc8
Joachim Metz <joachim.metz@gmail.com>
2015-03-27 13:49:51 +0100

    Code review: 221880043:  Added more formatter tests for issue #99.

--- plaso/formatters/safari.py
+++ plaso/formatters/safari.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
-"""Formatter for the Safari History events."""
+"""The Safari history event formatter."""
 
 from plaso.formatters import interface
 from plaso.formatters import manager
 
 
 class SafariHistoryFormatter(interface.ConditionalEventFormatter):
-  """Formatter for Safari history events."""
+  """Formatter for a Safari history event."""
 
   DATA_TYPE = 'safari:history:visit'
 
   FORMAT_STRING_PIECES = [
-      u'Visited: {url}', u'({title}', u'- {display_title}', ')',
-      'Visit Count: {visit_count}']
+      u'Visited: {url}',
+      u'({title}',
+      u'- {display_title}',
+      u')',
+      u'Visit Count: {visit_count}']
 
   SOURCE_LONG = 'Safari History'
   SOURCE_SHORT = 'WEBHIST'
