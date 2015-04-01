c1cc21e54f6c7c936c30664ebfeffca8ec48c140
Joachim Metz <joachim.metz@gmail.com>
2015-04-01 06:16:33 +0200

    Code review: 220440043:  Moving dependency check code into plaso.

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,16 @@
 import unittest
 import sys
 
+# Change PYTHONPATH to include plaso.
+sys.path.insert(0, u'.')
+
+import plaso.dependencies
+
 
 if __name__ == '__main__':
+  if not plaso.dependencies.CheckDependencies():
+    sys.exit(1)
+
   test_suite = unittest.TestLoader().discover('.', pattern='*_test.py')
   test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
   if not test_results.wasSuccessful():
