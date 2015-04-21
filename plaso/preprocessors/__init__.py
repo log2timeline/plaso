64f6ad5defc7a9cda613da852c72d28459946b75
Joachim Metz <joachim.metz@gmail.com>
2015-04-21 23:24:53 +0200

    Small bug fix for code review: 221710043 issue #155.

--- plaso/preprocessors/__init__.py
+++ plaso/preprocessors/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """This file contains an import statement for each preprocess plugin."""
 
-from plaso.preprocessors import artifact
 from plaso.preprocessors import linux
 from plaso.preprocessors import macosx
 from plaso.preprocessors import windows
