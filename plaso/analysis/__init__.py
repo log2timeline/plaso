ca44cc3e45c7ef47ee8f9489719f8d855c44513a
Joachim Metz <joachim.metz@gmail.com>
2015-04-22 07:18:39 +0200

    Code review: 224580043:  Clean up of analysis plugins #162.

--- plaso/analysis/__init__.py
+++ plaso/analysis/__init__.py
@@ -1,68 +1,7 @@
 # -*- coding: utf-8 -*-
-"""Import statements for analysis plugins and common methods."""
+"""This file contains an import statement for each analysis plugin."""
 
-from plaso.analysis import interface
-from plaso.lib import errors
-
-# Import statements of analysis plugins.
 from plaso.analysis import browser_search
 from plaso.analysis import chrome_extension
 from plaso.analysis import file_hashes
 from plaso.analysis import windows_services
-
-
-# TODO: move these functions to a manager class. And add a test for this
-# function.
-def ListAllPluginNames(show_all=True):
-  """Return a list of all available plugin names and their docstrings."""
-  results = []
-  for cls_obj in interface.AnalysisPlugin.classes.itervalues():
-    doc_string, _, _ = cls_obj.__doc__.partition('\n')
-
-    obj = cls_obj(None)
-    if not show_all and cls_obj.ENABLE_IN_EXTRACTION:
-      results.append((obj.plugin_name, doc_string, obj.plugin_type))
-    elif show_all:
-      results.append((obj.plugin_name, doc_string, obj.plugin_type))
-
-  return sorted(results)
-
-
-def LoadPlugins(plugin_names, incoming_queues, options=None):
-  """Yield analysis plugins for a given list of plugin names.
-
-  Given a list of plugin names this method finds the analysis
-  plugins, initializes them and returns a generator.
-
-  Args:
-    plugin_names: A list of plugin names that should be loaded up. This
-                  should be a list of strings.
-    incoming_queues: A list of queues (QueueInterface object) that the plugin
-                     uses to read in incoming events to analyse.
-    options: Optional command line arguments (instance of
-        argparse.Namespace). The default is None.
-
-  Yields:
-    Analysis plugin objects (instances of AnalysisPlugin).
-
-  Raises:
-    errors.BadConfigOption: If plugins_names does not contain a list of
-                            strings.
-  """
-  try:
-    plugin_names_lower = [word.lower() for word in plugin_names]
-  except AttributeError:
-    raise errors.BadConfigOption(u'Plugin names should be a list of strings.')
-
-  for plugin_object in interface.AnalysisPlugin.classes.itervalues():
-    plugin_name = plugin_object.NAME.lower()
-
-    if plugin_name in plugin_names_lower:
-      queue_index = plugin_names_lower.index(plugin_name)
-
-      try:
-        incoming_queue = incoming_queues[queue_index]
-      except (TypeError, IndexError):
-        incoming_queue = None
-
-      yield plugin_object(incoming_queue, options)
--- plaso/analysis/manager.py
+++ plaso/analysis/manager.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """This file contains the analysis plugin manager class."""
 
+from plaso.analysis import interface
+from plaso.lib import errors
+
 
 class AnalysisPluginManager(object):
   """Class that implements the analysis plugin manager."""
@@ -36,6 +39,68 @@ class AnalysisPluginManager(object):
     """
     for plugin_name, plugin_class in cls._plugin_classes.iteritems():
       yield plugin_name, plugin_class
+
+  @classmethod
+  def ListAllPluginNames(cls, show_all=True):
+    """Retrieves a list of the registered analysis plugins.
+
+    Args:
+      show_all: optional boolean value to indicate to list all the analysis
+                plugin names. The default is True.
+
+    Returns:
+      A sorted lists of available plugin names and their docstrings.
+    """
+    results = []
+    for cls_obj in interface.AnalysisPlugin.classes.itervalues():
+      doc_string, _, _ = cls_obj.__doc__.partition(u'\n')
+
+      obj = cls_obj(None)
+      if show_all or cls_obj.ENABLE_IN_EXTRACTION:
+        results.append((obj.plugin_name, doc_string, obj.plugin_type))
+
+    return sorted(results)
+
+  # TODO: refactor options out of function.
+  @classmethod
+  def LoadPlugins(cls, plugin_names, incoming_queues, options=None):
+    """Yield analysis plugins for a given list of plugin names.
+
+    Given a list of plugin names this method finds the analysis
+    plugins, initializes them and returns a generator.
+
+    Args:
+      plugin_names: A list of plugin names that should be loaded up. This
+                    should be a list of strings.
+      incoming_queues: A list of queues (instances of QueueInterface) that
+                       the plugin uses to read in incoming events to analyse.
+      options: Optional command line arguments (instance of
+               argparse.Namespace). The default is None.
+
+    Yields:
+      Analysis plugin objects (instances of AnalysisPlugin).
+
+    Raises:
+      errors.BadConfigOption: If plugins_names does not contain a list of
+                              strings.
+    """
+    try:
+      plugin_names_lower = [word.lower() for word in plugin_names]
+    except AttributeError:
+      raise errors.BadConfigOption(u'Plugin names should be a list of strings.')
+
+    for plugin_object in interface.AnalysisPlugin.classes.itervalues():
+      plugin_name = plugin_object.NAME.lower()
+
+      if plugin_name in plugin_names_lower:
+        queue_index = plugin_names_lower.index(plugin_name)
+
+        try:
+          incoming_queue = incoming_queues[queue_index]
+        except (TypeError, IndexError):
+          incoming_queue = None
+
+        yield plugin_object(incoming_queue, options)
 
   @classmethod
   def RegisterPlugin(cls, plugin_class):
