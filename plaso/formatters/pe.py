4eb923fc3cd2ae522a811acad59515e0bc75443d
Daniel White <onager@deerpie.com>
2015-04-22 16:02:59 +0200

    Code review: 228510043: Add PE file parser.

---
+++ plaso/formatters/pe.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""The PE event formatter."""
+
+from plaso.formatters import interface
+from plaso.formatters import manager
+
+
+class PEEventFormatter(interface.ConditionalEventFormatter):
+  """Parent class for PE event formatters."""
+  DATA_TYPE = 'pe'
+  FORMAT_STRING_SEPARATOR = u' '
+
+  FORMAT_STRING_PIECES = [
+      u'PE Type: {pe_type}',
+      u'Import hash: {imphash}',]
+
+  FORMAT_STRING_SHORT_PIECES = [u'pe_type']
+
+  SOURCE_LONG = u'PE Event'
+  SOURCE_SHORT = u'PE'
+
+
+class PECompilationFormatter(PEEventFormatter):
+  """Formatter for a PE compilation event."""
+  DATA_TYPE = 'pe:compilation:compilation_time'
+  SOURCE_LONG = u'PE Compilation time'
+
+
+class PEImportFormatter(PEEventFormatter):
+  """Formatter for a PE import section event."""
+  DATA_TYPE = 'pe:import:import_time'
+  FORMAT_STRING_PIECES = [
+      u'DLL name: {dll_name}',
+      u'PE Type: {pe_type}',
+      u'Import hash: {imphash}',]
+  FORMAT_STRING_SHORT_PIECES = [u'{dll_name}']
+  SOURCE_LONG = u'PE Import Time'
+
+
+class PEDelayImportFormatter(PEEventFormatter):
+  """Formatter for a PE delay import section event."""
+  DATA_TYPE = 'pe:delay_import:import_time'
+  FORMAT_STRING_PIECES = [
+      u'DLL name: {dll_name}',
+      u'PE Type: {pe_type}',
+      u'Import hash: {imphash}',]
+  FORMAT_STRING_SHORT_PIECES = [u'{dll_name}']
+  SOURCE_LONG = u'PE Delay Import Time'
+
+
+class PEResourceCreationFormatter(PEEventFormatter):
+  """Formatter for a PE resource creation event."""
+  DATA_TYPE = 'pe:resource:creation_time'
+  SOURCE_LONG = u'PE Resource Creation Time'
+
+
+class PELoadConfigModificationEvent(PEEventFormatter):
+  """Formatter for a PE load configuration table event."""
+  DATA_TYPE = 'pe:load_config:modification_time'
+  SOURCE_LONG = u'PE Load Configuration Table Time'
+
+
+manager.FormattersManager.RegisterFormatters([
+    PECompilationFormatter, PEImportFormatter,
+    PEDelayImportFormatter, PEResourceCreationFormatter,
+    PELoadConfigModificationEvent])
---
+++ plaso/parsers/pe_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Tests for the PE file parser."""
+
+import unittest
+
+from plaso.formatters import pe as _  # pylint: disable=unused-import
+from plaso.lib import timelib
+from plaso.parsers import pe
+from plaso.parsers import test_lib
+
+
+class PECOFFTest(test_lib.ParserTestCase):
+  """Tests for the PE file parser."""
+
+  def testParseFileObjectEXE(self):
+    """Tests the ParseFileObject method against an EXE PE file."""
+    test_path = self._GetTestFilePath([u'test_pe.exe'])
+    parser = pe.PEParser()
+
+    event_queue_consumer = self._ParseFile(parser, test_path)
+    events = self._GetEventObjectsFromQueue(event_queue_consumer)
+
+    self.assertEqual(len(events), 3)
+    first_event = events[0]
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2015-04-21 14:53:56')
+    self.assertEqual(first_event.pe_type, u'Executable (EXE)')
+    self.assertEqual(first_event.timestamp, expected_timestamp)
+    self.assertEqual(first_event.data_type, 'pe:compilation:compilation_time')
+
+    second_event = events[1]
+    expected_timestamp2 = timelib.Timestamp.CopyFromString(
+        u'2015-04-21 14:53:55')
+    self.assertEqual(second_event.timestamp, expected_timestamp2)
+    self.assertEqual(second_event.data_type, 'pe:import:import_time')
+
+    third_event = events[2]
+    expected_timestamp3 = timelib.Timestamp.CopyFromString(
+        u'2015-04-21 14:53:54')
+    self.assertEqual(third_event.timestamp, expected_timestamp3)
+    self.assertEqual(third_event.data_type, 'pe:delay_import:import_time')
+
+  def testDriver(self):
+    """Tests the ParseFileObject method against a driver (SYS) PE file."""
+    test_path = self._GetTestFilePath([u'test_driver.sys'])
+    parser = pe.PEParser()
+
+    event_queue_consumer = self._ParseFile(parser, test_path)
+    events = self._GetEventObjectsFromQueue(event_queue_consumer)
+    self.assertEqual(len(events), 1)
+    first_event = events[0]
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2015-04-21 14:53:54')
+    self.assertEqual(first_event.pe_type, u'Driver (SYS)')
+    self.assertEqual(first_event.timestamp, expected_timestamp)
+
+
+if __name__ == '__main__':
+  unittest.main()
