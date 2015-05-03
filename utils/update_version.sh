7a45890fbe03fd08e5706049b8b1376e36762e2e
Joachim Metz <joachim.metz@gmail.com>
2015-05-03 13:33:37 +0200

    Code review: 237890043: Code clean up.

--- utils/update_version.sh
+++ utils/update_version.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-# A small helper script to update the version information.
-#
-# Copyright 2015 The Plaso Project Authors.
-# Please see the AUTHORS file for details on individual authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Script to update the version information.
 
 VERSION=`grep -e "^__version__ = '" plaso/__init__.py | sed "s/^__version__ = '\([^']*\)'/\1/"`;
 DATE_VERSION=`date +"%Y%m%d"`;
