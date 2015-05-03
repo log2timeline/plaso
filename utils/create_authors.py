539daa46debf6ec45347f836b9915ddf727eab9d
Joachim Metz <joachim.metz@gmail.com>
2015-05-03 13:33:37 +0200

    Code review: 237890043: Code clean up.

--- utils/create_authors.py
+++ utils/create_authors.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Copyright 2012 The Plaso Project Authors.
-# Please see the AUTHORS file for details on individual authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """This file simply creates the AUTHOR file based on parser content."""
 
 import os
@@ -66,7 +51,7 @@ Keith Wall (kwallster@gmail.com)
         author = ProcessFile(os.path.join(path, filematch))
         if not author:
           continue
-        if type(author) in (list, tuple):
+        if isinstance(author, (list, tuple)):
           for author_name in author:
             if author_name not in authors:
               authors.append(author)
