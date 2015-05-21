538d33386cf75f87712240c37cf17ede4a89f03e
Jason Blanks <jason.blanks@gmail.com>
2015-05-21 11:58:42 -0400

    Code review: 235810044: Adding sql4n6 & mysql4n6 test along with edits to original code.

--- plaso/output/mysql_4n6.py
+++ plaso/output/mysql_4n6.py
@@ -1,25 +1,16 @@
 # -*- coding: utf-8 -*-
 """Defines the output module for the MySQL database used by 4n6time."""
 
-# TODO: Add a unit test for this output module.
 
 import logging
 
-try:
-  import MySQLdb
-except ImportError:
-  MySQLdb = None
+import MySQLdb
 
-from plaso import formatters
-from plaso.formatters import interface as formatters_interface
 from plaso.lib import definitions
 from plaso.lib import errors
 from plaso.lib import timelib
 from plaso.output import interface
 from plaso.output import manager
-
-
-__author__ = 'David Nides (david.nides@gmail.com)'
 
 
 class MySQL4n6OutputModule(interface.OutputModule):
@@ -29,8 +20,8 @@ class MySQL4n6OutputModule(interface.OutputModule):
   DESCRIPTION = u'MySQL database output for the 4n6time tool.'
 
   META_FIELDS = frozenset([
-      'sourcetype', 'source', 'user', 'host', 'MACB', 'color', 'type',
-      'record_number'])
+      u'sourcetype', u'source', u'user', u'host', u'MACB', u'color', u'type',
+      u'record_number'])
 
   ARGUMENTS = [
       ('--db_user', {
@@ -114,21 +105,49 @@ class MySQL4n6OutputModule(interface.OutputModule):
         u'append', default_value=False)
     self.fields = self._output_mediator.GetConfigurationValue(
         u'fields', default_value=self._DEFAULT_FIELDS)
+    self.db_insert_string = (
+        u'INSERT INTO log2timeline(timezone, MACB, source, '
+        u'sourcetype, type, user, host, description, filename, '
+        u'inode, notes, format, extra, datetime, reportnotes, inreport,'
+        u'tag, color, offset, store_number, store_index, vss_store_number,'
+        u'URL, record_number, event_identifier, event_type,'
+        u'source_name, user_sid, computer_name, evidence) '
+        u'VALUES (:timezone, :MACB, :source, :sourcetype, :type, :user, :host, '
+        u':description, :filename, :inode, :notes, :format, :extra, :datetime, '
+        u':reportnotes, :inreport, '
+        u':tag, :color, :offset, :store_number, :store_index, '
+        u':vss_store_number,'
+        u':URL, :record_number, :event_identifier, :event_type,'
+        u':source_name, :user_sid, :computer_name, :evidence)')
+
+  def _GetVSSNumber(self, event_object):
+    """Return the vss_store_number of the event or -1.
+
+    Args:
+      event_object: the event object (instance of EventObject).
+    """
+    if not hasattr(event_object, u'pathspec'):
+      return -1
+
+    return getattr(event_object.pathspec, u'vss_store_number', -1)
 
   def _GetDistinctValues(self, field_name):
     """Query database for unique field types.
 
     Args:
-      field_name: name of the filed to retrieve.
+      field_name: name of the fieled to retrieve.
+
+    Returns:
+      A dictionary containing fields from META_FIELDS.
     """
     self.curs.execute(
-        u'SELECT {0}, COUNT({0}) FROM log2timeline GROUP BY {0}'.format(
+        u'SELECT {0:s}, COUNT({0:s}) FROM log2timeline GROUP BY {0:s}'.format(
             field_name))
-    res = {}
+    result = {}
     for row in self.curs.fetchall():
-      if row[0] != '':
-        res[row[0]] = int(row[1])
-    return res
+      if row[0]:
+        result[row[0]] = row[1]
+    return result
 
   def _ListTags(self):
     """Query database for unique tag types."""
@@ -140,7 +159,7 @@ class MySQL4n6OutputModule(interface.OutputModule):
     for tag_row in self.curs.fetchall():
       tag_string = tag_row[0]
       if tag_string:
-        tags = tag_string.split(',')
+        tags = tag_string.split(u',')
         for tag in tags:
           if tag not in all_tags:
             all_tags.append(tag)
@@ -186,17 +205,19 @@ class MySQL4n6OutputModule(interface.OutputModule):
     self.conn.close()
 
   def Open(self):
-    """Connects to the database and creates the required tables."""
-    if self.dbname == '':
-      raise IOError(u'Specify a database name.')
+    """Connects to the database and creates the required tables.
+
+    Raises:
+      IOError: If Unable to insert into database.
+      ValueError: If no database name given.
+    """
+    if not self.dbname:
+      raise ValueError(u'Missing database name.')
 
     try:
       if self.append:
         self.conn = MySQLdb.connect(self.host, self.user,
                                     self.password, self.dbname)
-        self.curs = self.conn.cursor()
-      else:
-        self.conn = MySQLdb.connect(self.host, self.user, self.password)
         self.curs = self.conn.cursor()
 
       self.conn.set_character_set(u'utf8')
@@ -269,8 +290,8 @@ class MySQL4n6OutputModule(interface.OutputModule):
 
     self.count = 0
 
-  def WriteEventBody(self, event_object):
-    """Writes the body of an event object to the output.
+  def _GetSanitizedEventValues(self, event_object):
+    """Formats event output and returns a dictionary.
 
     Args:
       event_object: the event object (instance of EventObject).
@@ -279,42 +300,23 @@ class MySQL4n6OutputModule(interface.OutputModule):
       NoFormatterFound: If no event formatter can be found to match the data
                         type in the event object.
     """
-    if not hasattr(event_object, u'timestamp'):
-      return
 
-    # TODO: remove this hack part of the storage/output refactor.
-    # The event formatter class constants should not be changed directly.
+    data_type = getattr(event_object, u'data_type', u'UNKNOWN')
     event_formatter = self._output_mediator.GetEventFormatter(event_object)
     if not event_formatter:
       raise errors.NoFormatterFound(
-          u'Unable to find event formatter for: {0:s}.'.format(
-              getattr(event_object, u'data_type', u'UNKNOWN')))
-
-    if (isinstance(
-        event_formatter, formatters.winreg.WinRegistryGenericFormatter) and
-        event_formatter.FORMAT_STRING.find('<|>') == -1):
-      event_formatter.FORMAT_STRING = u'[{keyname}]<|>{text}<|>'
-
-    elif isinstance(
-        event_formatter, formatters_interface.ConditionalEventFormatter):
-      event_formatter.FORMAT_STRING_SEPARATOR = u'<|>'
-
-    elif isinstance(event_formatter, formatters_interface.EventFormatter):
-      event_formatter.FORMAT_STRING = event_formatter.FORMAT_STRING.replace(
-          '}', '}<|>')
+          u'Unable to find event formatter for: {0:s}.'.format(data_type))
 
     message, _ = self._output_mediator.GetFormattedMessages(event_object)
     if message is None:
       raise errors.NoFormatterFound(
-          u'Unable to find event formatter for: {0:s}.'.format(
-              getattr(event_object, u'data_type', u'UNKNOWN')))
+          u'Unable to find event formatter for: {0:s}.'.format(data_type))
 
     source_short, source = self._output_mediator.GetFormattedSources(
         event_object)
     if source is None or source_short is None:
       raise errors.NoFormatterFound(
-          u'Unable to find event formatter for: {0:s}.'.format(
-              getattr(event_object, u'data_type', u'UNKNOWN')))
+          u'Unable to find event formatter for: {0:s}.'.format(data_type))
 
     date_use = timelib.Timestamp.CopyToDatetime(
         event_object.timestamp, self._output_mediator.timezone)
@@ -338,67 +340,73 @@ class MySQL4n6OutputModule(interface.OutputModule):
 
     extra = u' '.join(extra)
 
-    inode = getattr(event_object, 'inode', '-')
-    if inode == '-':
-      if (hasattr(event_object, 'pathspec') and
-          hasattr(event_object.pathspec, 'image_inode')):
-        inode = event_object.pathspec.image_inode
+    inode = getattr(event_object, u'inode', u'-')
+    if inode == u'-':
+      if (hasattr(event_object, u'pathspec') and
+          hasattr(event_object.pathspec, u'inode')):
+        inode = event_object.pathspec.inode
 
-    date_use_string = u'{0:d}-{1:d}-{2:d} {3:d}:{4:d}:{5:d}'.format(
+    date_use_string = u'{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d}:{5:02d}'.format(
         date_use.year, date_use.month, date_use.day, date_use.hour,
         date_use.minute, date_use.second)
 
-    tags = []
-    if hasattr(event_object, 'tag') and hasattr(event_object.tag, 'tags'):
-      tags = event_object.tag.tags
+    if getattr(event_object, u'tag', None):
+      tags_value = getattr(event_object.tag, u'tags', None)
     else:
-      tags = u''
+      tags_value = None
+
+    if isinstance(tags_value, (list, tuple)):
+      tags = tags_value
+    else:
+      tags = []
 
     taglist = u','.join(tags)
-    row = (
-        str(self._output_mediator.timezone),
-        self._output_mediator.GetMACBRepresentation(event_object),
-        source_short,
-        source,
-        getattr(event_object, 'timestamp_desc', '-'),
-        getattr(event_object, 'username', '-'),
-        getattr(event_object, 'hostname', '-'),
-        message,
-        getattr(event_object, 'filename', '-'),
-        inode,
-        getattr(event_object, 'notes', '-'),
-        getattr(event_object, 'parser', '-'),
-        extra,
-        date_use_string,
-        '',
-        '',
-        taglist,
-        '',
-        getattr(event_object, 'offset', 0),
-        event_object.store_number,
-        event_object.store_index,
-        self.GetVSSNumber(event_object),
-        getattr(event_object, 'url', '-'),
-        getattr(event_object, 'record_number', 0),
-        getattr(event_object, 'event_identifier', '-'),
-        getattr(event_object, 'event_type', '-'),
-        getattr(event_object, 'source_name', '-'),
-        getattr(event_object, 'user_sid', '-'),
-        getattr(event_object, 'computer_name', '-'),
-        self.evidence)
+    row = {u'timezone': unicode(self._output_mediator.timezone),
+           u'MACB': self._output_mediator.GetMACBRepresentation(event_object),
+           u'source': source_short,
+           u'sourcetype': source,
+           u'type': getattr(event_object, u'timestamp_desc', u'-'),
+           u'user': getattr(event_object, u'username', u'-'),
+           u'host': getattr(event_object, u'hostname', u'-'),
+           u'description': message,
+           u'filename': getattr(event_object, u'filename', u'-'),
+           u'inode': inode,
+           u'notes': getattr(event_object, u'notes', u'-'),
+           u'format': getattr(event_object, u'parser', u'-'),
+           u'extra': extra,
+           u'datetime': date_use_string,
+           u'reportnotes': u'',
+           u'inreport': u'',
+           u'tag': taglist,
+           u'color': u'',
+           u'offset': getattr(event_object, u'offset', 0),
+           u'store_number': event_object.store_number,
+           u'store_index': event_object.store_index,
+           u'vss_store_number': self._GetVSSNumber(event_object),
+           u'URL': getattr(event_object, u'url', u'-'),
+           u'record_number': getattr(event_object, u'record_number', 0),
+           u'event_identifier': getattr(
+               event_object, u'event_identifier', u'-'),
+           u'event_type': getattr(event_object, u'event_type', u'-'),
+           u'source_name': getattr(event_object, u'source_name', u'-'),
+           u'user_sid': getattr(event_object, u'user_sid', u'-'),
+           u'computer_name': getattr(event_object, u'computer_name', u'-'),
+           u'evidence': self.evidence}
 
+    return row
+
+  def WriteEventBody(self, event_object):
+    """Writes the body of an event object to the output.
+
+    Args:
+      event_object: the event object (instance of EventObject).
+    """
+    if not hasattr(event_object, u'timestamp'):
+      return
+
+    row = self._GetSanitizedEventValues(event_object)
     try:
-      self.curs.execute(
-          'INSERT INTO log2timeline(timezone, MACB, source, '
-          'sourcetype, type, user, host, description, filename, '
-          'inode, notes, format, extra, datetime, reportnotes, '
-          'inreport, tag, color, offset, store_number, '
-          'store_index, vss_store_number, URL, record_number, '
-          'event_identifier, event_type, source_name, user_sid, '
-          'computer_name, evidence) VALUES ('
-          '%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, '
-          '%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, '
-          '%s, %s, %s, %s)', row)
+      self.curs.execute(self.db_insert_string, row)
     except MySQLdb.Error as exception:
       logging.warning(
           u'Unable to insert into database with error: {0:s}.'.format(
@@ -413,13 +421,5 @@ class MySQL4n6OutputModule(interface.OutputModule):
       if self.set_status:
         self.set_status(u'Inserting event: {0:d}'.format(self.count))
 
-  def GetVSSNumber(self, event_object):
-    """Return the vss_store_number of the event."""
-    if not hasattr(event_object, 'pathspec'):
-      return -1
 
-    return getattr(event_object.pathspec, 'vss_store_number', -1)
-
-
-manager.OutputManager.RegisterOutput(
-    MySQL4n6OutputModule, disabled=MySQLdb is None)
+manager.OutputManager.RegisterOutput(MySQL4n6OutputModule)
