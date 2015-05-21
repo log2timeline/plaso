7011582af4b52046992d6369fb642bcf183c078e
Jason Blanks <jason.blanks@gmail.com>
2015-05-21 11:58:42 -0400

    Code review: 235810044: Adding sql4n6 & mysql4n6 test along with edits to original code.

--- plaso/output/sqlite_4n6.py
+++ plaso/output/sqlite_4n6.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 """Defines the output module for the SQLite database used by 4n6time."""
 
-# TODO: Add a unit test for this output module.
 
 import logging
 import os
-import sys
 
 import sqlite3
-
-from plaso import formatters
-from plaso.formatters import interface as formatters_interface
 from plaso.lib import definitions
 from plaso.lib import errors
 from plaso.lib import timelib
@@ -18,34 +13,37 @@ from plaso.output import interface
 from plaso.output import manager
 
 
-__author__ = 'David Nides (david.nides@gmail.com)'
-
-
 class SQLite4n6OutputModule(interface.OutputModule):
-  """Saves the data in a SQLite database, used by the tool 4n6Time."""
+  """Saves the data in a SQLite database, used by the tool 4n6time."""
 
   NAME = u'sql4n6'
   DESCRIPTION = (
       u'Saves the data in a SQLite database, used by the tool 4n6Time.')
 
   META_FIELDS = frozenset([
-      'sourcetype', 'source', 'user', 'host', 'MACB', 'color', 'type',
-      'record_number'])
+      u'sourcetype', u'source', u'user', u'host', u'MACB', u'color', u'type',
+      u'record_number'])
 
   _DEFAULT_FIELDS = [
       u'host', u'user', u'source', u'sourcetype', u'type', u'datetime',
       u'color']
 
-  def __init__(self, output_mediator, filehandle=sys.stdout, **kwargs):
+  def __init__(self, output_mediator, filehandle=None, **kwargs):
     """Initializes the output module object.
 
     Args:
       output_mediator: The output mediator object (instance of OutputMediator).
-      filehandle: Optional file-like object that can be written to.
-                  The default is sys.stdout.
+      filehandle: Holds the filename.
+
+    Raises:
+      ValueError: if the file handle is missing.
     """
     super(SQLite4n6OutputModule, self).__init__(output_mediator, **kwargs)
-    self._file_object = filehandle
+
+    if not filehandle:
+      raise errors.ValueError(u'Missing file handle.')
+
+    self._filename = filehandle
 
     self.set_status = self._output_mediator.GetConfigurationValue(u'set_status')
     self.evidence = self._output_mediator.GetConfigurationValue(
@@ -54,6 +52,31 @@ class SQLite4n6OutputModule(interface.OutputModule):
         u'append', default_value=False)
     self.fields = self._output_mediator.GetConfigurationValue(
         u'fields', default_value=self._DEFAULT_FIELDS)
+    self.db_create_table_string = (
+        u'CREATE TABLE log2timeline (timezone TEXT, '
+        u'MACB TEXT, source TEXT, sourcetype TEXT, type TEXT, '
+        u'user TEXT, host TEXT, description TEXT, filename TEXT, '
+        u'inode TEXT, notes TEXT, format TEXT, extra TEXT, '
+        u'datetime datetime, reportnotes TEXT, '
+        u'inreport TEXT, tag TEXT, color TEXT, offset INT,'
+        u'store_number INT, store_index INT, vss_store_number INT,'
+        u'url TEXT, record_number TEXT, event_identifier TEXT, '
+        u'event_type TEXT, source_name TEXT, user_sid TEXT, '
+        u'computer_name TEXT, evidence TEXT)')
+    self.db_insert_string = (
+        u'INSERT INTO log2timeline(timezone, MACB, source, '
+        u'sourcetype, type, user, host, description, filename, '
+        u'inode, notes, format, extra, datetime, reportnotes, inreport,'
+        u'tag, color, offset, store_number, store_index, vss_store_number,'
+        u'URL, record_number, event_identifier, event_type,'
+        u'source_name, user_sid, computer_name, evidence) '
+        u'VALUES (:timezone, :MACB, :source, :sourcetype, :type, :user, :host, '
+        u':description, :filename, :inode, :notes, :format, :extra, :datetime, '
+        u':reportnotes, :inreport, '
+        u':tag, :color, :offset, :store_number, :store_index, '
+        u':vss_store_number,'
+        u':URL, :record_number, :event_identifier, :event_type,'
+        u':source_name, :user_sid, :computer_name, :evidence)')
 
   def _GetDistinctValues(self, field_name):
     """Query database for unique field types.
@@ -64,11 +87,23 @@ class SQLite4n6OutputModule(interface.OutputModule):
     self.curs.execute(
         u'SELECT {0:s}, COUNT({0:s}) FROM log2timeline GROUP BY {0:s}'.format(
             field_name))
-    res = {}
+
+    result = {}
     for row in self.curs.fetchall():
-      if row[0] != '':
-        res[row[0]] = int(row[1])
-    return res
+      if row[0]:
+        result[row[0]] = row[1]
+    return result
+
+  def _GetVSSNumber(self, event_object):
+    """Return the vss_store_number of the event or -1.
+
+    Args:
+      event_object: the event object (instance of EventObject).
+    """
+    if not hasattr(event_object, u'pathspec'):
+      return -1
+
+    return getattr(event_object.pathspec, u'vss_store_number', -1)
 
   def _ListTags(self):
     """Query database for unique tag types."""
@@ -85,100 +120,7 @@ class SQLite4n6OutputModule(interface.OutputModule):
             all_tags.append(tag)
     return all_tags
 
-  def Close(self):
-    """Disconnects from the database.
-
-    This method will create the necessary indices and commit outstanding
-    transactions before disconnecting.
-    """
-    # Build up indices for the fields specified in the args.
-    # It will commit the inserts automatically before creating index.
-    if not self.append:
-      for field_name in self.fields:
-        sql = 'CREATE INDEX {0}_idx ON log2timeline ({0})'.format(field_name)
-        self.curs.execute(sql)
-        if self.set_status:
-          self.set_status('Created index: {0:s}'.format(field_name))
-
-    # Get meta info and save into their tables.
-    if self.set_status:
-      self.set_status('Creating metadata...')
-
-    for field in self.META_FIELDS:
-      vals = self._GetDistinctValues(field)
-      self.curs.execute('DELETE FROM l2t_{0:s}s'.format(field))
-      for name, freq in vals.items():
-        self.curs.execute((
-            'INSERT INTO l2t_{0:s}s ({1:s}s, frequency) '
-            'VALUES("{2:s}", {3:d}) ').format(field, field, name, freq))
-    self.curs.execute('DELETE FROM l2t_tags')
-    for tag in self._ListTags():
-      self.curs.execute('INSERT INTO l2t_tags (tag) VALUES (?)', [tag])
-
-    if self.set_status:
-      self.set_status('Database created.')
-
-    self.conn.commit()
-    self.curs.close()
-    self.conn.close()
-
-  def Open(self):
-    """Connects to the database and creates the required tables."""
-    if self._file_object == sys.stdout:
-      raise IOError(
-          u'Unable to connect to stdout as database, please specify a file.')
-
-    if (not self.append) and os.path.isfile(self._file_object):
-      raise IOError((
-          u'Unable to use an already existing file for output '
-          u'[{0:s}]').format(self._file_object))
-
-    self.conn = sqlite3.connect(self._file_object)
-    self.conn.text_factory = str
-    self.curs = self.conn.cursor()
-
-    # Create table in database.
-    if not self.append:
-      self.curs.execute(
-          ('CREATE TABLE log2timeline (timezone TEXT, '
-           'MACB TEXT, source TEXT, sourcetype TEXT, type TEXT, '
-           'user TEXT, host TEXT, description TEXT, filename TEXT, '
-           'inode TEXT, notes TEXT, format TEXT, extra TEXT, '
-           'datetime datetime, reportnotes TEXT, '
-           'inreport TEXT, tag TEXT, color TEXT, offset INT,'
-           'store_number INT, store_index INT, vss_store_number INT,'
-           'url TEXT, record_number TEXT, event_identifier TEXT, '
-           'event_type TEXT, source_name TEXT, user_sid TEXT, '
-           'computer_name TEXT, evidence TEXT)'))
-      if self.set_status:
-        self.set_status('Created table: log2timeline')
-
-      for field in self.META_FIELDS:
-        self.curs.execute(
-            'CREATE TABLE l2t_{0}s ({0}s TEXT, frequency INT)'.format(field))
-        if self.set_status:
-          self.set_status('Created table: l2t_{0:s}'.format(field))
-
-      self.curs.execute('CREATE TABLE l2t_tags (tag TEXT)')
-      if self.set_status:
-        self.set_status('Created table: l2t_tags')
-
-      self.curs.execute('CREATE TABLE l2t_saved_query (name TEXT, query TEXT)')
-      if self.set_status:
-        self.set_status('Created table: l2t_saved_query')
-
-      self.curs.execute('CREATE TABLE l2t_disk (disk_type INT, mount_path TEXT,'
-                        ' dd_path TEXT, dd_offset TEXT, storage_file TEXT,'
-                        ' export_path TEXT)')
-      self.curs.execute('INSERT INTO l2t_disk (disk_type, mount_path, dd_path,'
-                        'dd_offset, storage_file, export_path) VALUES '
-                        '(0, "", "", "", "", "")')
-      if self.set_status:
-        self.set_status('Created table: l2t_disk')
-
-    self.count = 0
-
-  def WriteEventBody(self, event_object):
+  def _GetSanitizedEventValues(self, event_object):
     """Writes the body of an event object to the output.
 
     Args:
@@ -188,29 +130,11 @@ class SQLite4n6OutputModule(interface.OutputModule):
       NoFormatterFound: If no event formatter can be found to match the data
                         type in the event object.
     """
-    if u'timestamp' not in event_object.GetAttributes():
-      return
-
-    # TODO: remove this hack part of the storage/output refactor.
-    # The event formatter class constants should not be changed directly.
     event_formatter = self._output_mediator.GetEventFormatter(event_object)
     if not event_formatter:
       raise errors.NoFormatterFound(
           u'Unable to find event formatter for: {0:s}.'.format(
               getattr(event_object, u'data_type', u'UNKNOWN')))
-
-    if (isinstance(
-        event_formatter, formatters.winreg.WinRegistryGenericFormatter) and
-        event_formatter.FORMAT_STRING.find('<|>') == -1):
-      event_formatter.FORMAT_STRING = u'[{keyname}]<|>{text}<|>'
-
-    elif isinstance(
-        event_formatter, formatters_interface.ConditionalEventFormatter):
-      event_formatter.FORMAT_STRING_SEPARATOR = u'<|>'
-
-    elif isinstance(event_formatter, formatters_interface.EventFormatter):
-      event_formatter.FORMAT_STRING = event_formatter.FORMAT_STRING.replace(
-          '}', '}<|>')
 
     message, _ = self._output_mediator.GetFormattedMessages(event_object)
     if message is None:
@@ -230,7 +154,8 @@ class SQLite4n6OutputModule(interface.OutputModule):
     if not date_use:
       logging.error(u'Unable to process date for entry: {0:s}'.format(message))
       return
-    format_variables = self._output_mediator.GetFormatStringAttributeNames()
+    format_variables = self._output_mediator.GetFormatStringAttributeNames(
+        event_object)
     if format_variables is None:
       raise errors.NoFormatterFound(
           u'Unable to find event formatter for: {0:s}.'.format(
@@ -238,85 +163,164 @@ class SQLite4n6OutputModule(interface.OutputModule):
 
     extra = []
     for key in event_object.GetAttributes():
-      if (key in definitions.RESERVED_VARIABLE_NAMES or
-          key in format_variables):
+      if (key in definitions.RESERVED_VARIABLE_NAMES or key in
+          format_variables):
         continue
       extra.append(u'{0:s}: {1!s} '.format(
           key, getattr(event_object, key, None)))
     extra = u' '.join(extra)
 
-    inode = getattr(event_object, 'inode', '-')
+    inode = getattr(event_object, u'inode', u'-')
     if inode == '-':
-      if (hasattr(event_object, 'pathspec') and
-          hasattr(event_object.pathspec, 'image_inode')):
-        inode = event_object.pathspec.image_inode
+      if (hasattr(event_object, u'pathspec') and
+          hasattr(event_object.pathspec, u'inode')):
+        inode = event_object.pathspec.inode
 
     date_use_string = u'{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d}:{5:02d}'.format(
         date_use.year, date_use.month, date_use.day, date_use.hour,
         date_use.minute, date_use.second)
-
     tags = []
-    if hasattr(event_object, 'tag'):
-      if hasattr(event_object.tag, 'tags'):
+    if hasattr(event_object, u'tag'):
+      if hasattr(event_object.tag, u'tags'):
         tags = event_object.tag.tags
     taglist = ','.join(tags)
-    row = (str(self._output_mediator.timezone),
-           self._output_mediator.GetMACBRepresentation(event_object),
-           source_short,
-           source,
-           getattr(event_object, 'timestamp_desc', '-'),
-           getattr(event_object, 'username', '-'),
-           getattr(event_object, 'hostname', '-'),
-           message,
-           getattr(event_object, 'filename', '-'),
-           inode,
-           getattr(event_object, 'notes', '-'),
-           getattr(event_object, 'parser', '-'),
-           extra,
-           date_use_string,
-           '',
-           '',
-           taglist,
-           '',
-           getattr(event_object, 'offset', 0),
-           event_object.store_number,
-           event_object.store_index,
-           GetVSSNumber(event_object),
-           getattr(event_object, 'url', '-'),
-           getattr(event_object, 'record_number', 0),
-           getattr(event_object, 'event_identifier', '-'),
-           getattr(event_object, 'event_type', '-'),
-           getattr(event_object, 'source_name', '-'),
-           getattr(event_object, 'user_sid', '-'),
-           getattr(event_object, 'computer_name', '-'),
-           self.evidence
-          )
+    row = {u'timezone': unicode(self._output_mediator.timezone),
+           u'MACB': self._output_mediator.GetMACBRepresentation(event_object),
+           u'source': source_short,
+           u'sourcetype': source,
+           u'type': getattr(event_object, u'timestamp_desc', u'-'),
+           u'user': getattr(event_object, u'username', u'-'),
+           u'host': getattr(event_object, u'hostname', u'-'),
+           u'description': message,
+           u'filename': getattr(event_object, u'filename', u'-'),
+           u'inode': inode,
+           u'notes': getattr(event_object, u'notes', u'-'),
+           u'format': getattr(event_object, u'parser', u'-'),
+           u'extra': extra,
+           u'datetime': date_use_string,
+           u'reportnotes': u'',
+           u'inreport': u'',
+           u'tag': taglist,
+           u'color': u'',
+           u'offset': getattr(event_object, u'offset', 0),
+           u'store_number': event_object.store_number,
+           u'store_index': event_object.store_index,
+           u'vss_store_number': self._GetVSSNumber(event_object),
+           u'URL': getattr(event_object, u'url', u'-'),
+           u'record_number': getattr(event_object, u'record_number', 0),
+           u'event_identifier': getattr(
+               event_object, u'event_identifier', u'-'),
+           u'event_type': getattr(event_object, u'event_type', u'-'),
+           u'source_name': getattr(event_object, u'source_name', u'-'),
+           u'user_sid': getattr(event_object, u'user_sid', u'-'),
+           u'computer_name': getattr(event_object, u'computer_name', u'-'),
+           u'evidence': self.evidence}
 
-    self.curs.execute(
-        ('INSERT INTO log2timeline(timezone, MACB, source, '
-         'sourcetype, type, user, host, description, filename, '
-         'inode, notes, format, extra, datetime, reportnotes, inreport,'
-         'tag, color, offset, store_number, store_index, vss_store_number,'
-         'URL, record_number, event_identifier, event_type,'
-         'source_name, user_sid, computer_name, evidence)'
-         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
-         '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'), row)
+    return row
 
+  def Close(self):
+    """Disconnects from the database.
+
+    This method will create the necessary indices and commit outstanding
+    transactions before disconnecting.
+    """
+    # Build up indices for the fields specified in the args.
+    # It will commit the inserts automatically before creating index.
+    if not self.append:
+      for field_name in self.fields:
+        sql = u'CREATE INDEX {0}_idx ON log2timeline ({0})'.format(field_name)
+        self.curs.execute(sql)
+        if self.set_status:
+          self.set_status(u'Created index: {0:s}'.format(field_name))
+
+    # Get meta info and save into their tables.
+    if self.set_status:
+      self.set_status(u'Creating metadata...')
+
+    for field in self.META_FIELDS:
+      vals = self._GetDistinctValues(field)
+      self.curs.execute(u'DELETE FROM l2t_{0:s}s'.format(field))
+      for name, freq in vals.items():
+        self.curs.execute((
+            u'INSERT INTO l2t_{0:s}s ({1:s}s, frequency) '
+            u'VALUES("{2:s}", {3:d}) ').format(field, field, name, freq))
+    self.curs.execute(u'DELETE FROM l2t_tags')
+    for tag in self._ListTags():
+      self.curs.execute(u'INSERT INTO l2t_tags (tag) VALUES (?)', [tag])
+
+    if self.set_status:
+      self.set_status(u'Database created.')
+
+    self.conn.commit()
+    self.curs.close()
+    self.conn.close()
+
+  def Open(self):
+    """Connects to the database and creates the required tables.
+
+    Raises:
+      IOError: if a file with filename already exists.
+    """
+
+    if (not self.append) and os.path.isfile(self._filename):
+      raise IOError((
+          u'Unable to use an already existing file for output '
+          u'[{0:s}]').format(self._filename))
+
+    self.conn = sqlite3.connect(self._filename)
+    self.curs = self.conn.cursor()
+
+    # Create table in database.
+    if not self.append:
+      self.curs.execute(self.db_create_table_string)
+
+      for field in self.META_FIELDS:
+        self.curs.execute(
+            u'CREATE TABLE l2t_{0}s ({0}s TEXT, frequency INT)'.format(field))
+        if self.set_status:
+          self.set_status(u'Created table: l2t_{0:s}'.format(field))
+
+      self.curs.execute(u'CREATE TABLE l2t_tags (tag TEXT)')
+      if self.set_status:
+        self.set_status(u'Created table: l2t_tags')
+
+      self.curs.execute(u'CREATE TABLE l2t_saved_query (name TEXT, query TEXT)')
+      if self.set_status:
+        self.set_status(u'Created table: l2t_saved_query')
+
+      self.curs.execute(
+          u'CREATE TABLE l2t_disk (disk_type INT, mount_path TEXT, '
+          u'dd_path TEXT, dd_offset TEXT, storage_file TEXT, export_path TEXT)')
+
+      self.curs.execute(
+          u'INSERT INTO l2t_disk (disk_type, mount_path, dd_path,dd_offset, '
+          u'storage_file, export_path) VALUES (0, "", "", "", "", "")')
+      if self.set_status:
+        self.set_status(u'Created table: l2t_disk')
+
+    self.count = 0
+
+  def WriteEventBody(self, event_object):
+    """Writes the body of an event object to the output.
+
+    Args:
+      event_object: the event object (instance of EventObject).
+
+    Raises:
+      NoFormatterFound: If no event formatter can be found to match the data
+      type in the event object.
+    """
+    if u'timestamp' not in event_object.GetAttributes():
+      return
+
+    row = self._GetSanitizedEventValues(event_object)
+    self.curs.execute(self.db_insert_string, row)
     self.count += 1
-
     # Commit the current transaction every 10000 inserts.
     if self.count % 10000 == 0:
       self.conn.commit()
       if self.set_status:
-        self.set_status('Inserting event: {0:d}'.format(self.count))
-
-
-def GetVSSNumber(event_object):
-  """Return the vss_store_number of the event."""
-  if not hasattr(event_object, 'pathspec'):
-    return -1
-
-  return getattr(event_object.pathspec, 'vss_store_number', -1)
+        self.set_status(u'Inserting event: {0:d}'.format(self.count))
 
 
 manager.OutputManager.RegisterOutput(SQLite4n6OutputModule)
