246d74e89225e49909065976e9c9ca3e0a9b7b9a
Joachim Metz <joachim.metz@gmail.com>
2015-06-03 22:22:27 +0200

    Code review: 235710044:  Migration to print as a function and removed unused files.

--- utils/check_dependencies.py
+++ utils/check_dependencies.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """Script to check for the availability and version of dependencies."""
 
+from __future__ import print_function
 import sys
 
 # Change PYTHONPATH to include plaso.
@@ -16,5 +17,5 @@ if __name__ == u'__main__':
         u'https://sites.google.com/a/kiddaland.net/plaso/developer'
         u'/building-the-tool')
 
-    print u'See: {0:s} on how to set up plaso.'.format(build_instructions_url)
-    print u''
+    print(u'See: {0:s} on how to set up plaso.'.format(build_instructions_url))
+    print(u'')
