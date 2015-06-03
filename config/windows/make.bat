f3b9a4b6ebd98223e30f2b35911f6a5c770d3bdc
Joachim Metz <joachim.metz@gmail.com>
2015-06-03 12:38:29 +0200

    Code review: 243000043: Removed plasm from distributed tools.

--- config/windows/make.bat
+++ config/windows/make.bat
@@ -7,7 +7,6 @@ set PYTHONPATH=.
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\image_export.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\log2timeline.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\pinfo.py
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\plasm.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\preg.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\psort.py
 
