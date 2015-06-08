d6e4d44bad4e1f165f71db592fdf29c81c96fb33
Joachim Metz <joachim.metz@gmail.com>
2015-06-08 07:52:39 +0200

    Code review: 240380044:  Fixes for Windows build script.

--- config/windows/make.bat
+++ config/windows/make.bat
@@ -4,10 +4,10 @@ rmdir /q /s build dist 2> NUL
 
 set PYTHONPATH=.
 
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\image_export.py
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\log2timeline.py
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\pinfo.py
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\preg.py
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir plaso\frontend\psort.py
+C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir tools\image_export.py
+C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir tools\log2timeline.py
+C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir tools\pinfo.py
+C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir tools\preg.py
+C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --onedir tools\psort.py
 
 set PYTHONPATH=
