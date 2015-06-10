a968b73eb58c149d1ea0dbeb266cc6b2ad705d4b
Joachim Metz <joachim.metz@gmail.com>
2015-06-10 18:28:50 +0200

    Code review: 246840043:  Migrated cookie plugins to manager.

--- plaso/parsers/sqlite_plugins/chrome_cookies.py
+++ plaso/parsers/sqlite_plugins/chrome_cookies.py
@@ -7,7 +7,7 @@ from plaso.lib import eventdata
 # Register the cookie plugins.
 from plaso.parsers import cookie_plugins  # pylint: disable=unused-import
 from plaso.parsers import sqlite
-from plaso.parsers.cookie_plugins import interface as cookie_interface
+from plaso.parsers.cookie_plugins import manager as cookie_plugins_manager
 from plaso.parsers.sqlite_plugins import interface
 
 
@@ -88,7 +88,8 @@ class ChromeCookiePlugin(interface.SQLitePlugin):
   def __init__(self):
     """Initializes a plugin object."""
     super(ChromeCookiePlugin, self).__init__()
-    self._cookie_plugins = cookie_interface.GetPlugins()
+    self._cookie_plugins = (
+        cookie_plugins_manager.CookiePluginsManager.GetPlugins())
 
   def ParseCookieRow(self, parser_mediator, row, query=None, **unused_kwargs):
     """Parses a cookie row.
