65a9c1c885c7b33f4a4dbc4e188982135ad0930c
Joachim Metz <joachim.metz@gmail.com>
2015-06-15 22:32:22 +0200

    Code review: 246010045:  Added Windows 10 AppCompatCache key format support #174

--- plaso/parsers/winreg_plugins/appcompatcache.py
+++ plaso/parsers/winreg_plugins/appcompatcache.py
@@ -82,6 +82,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
   _FORMAT_TYPE_VISTA = 4
   _FORMAT_TYPE_7 = 5
   _FORMAT_TYPE_8 = 6
+  _FORMAT_TYPE_10 = 7
 
   # AppCompatCache format signature used in Windows XP.
   _HEADER_SIGNATURE_XP = 0xdeadbeef
@@ -202,6 +203,17 @@ class AppCompatCachePlugin(interface.KeyPlugin):
   # AppCompatCache format used in Windows 8.1.
   _CACHED_ENTRY_SIGNATURE_8_1 = b'10ts'
 
+  # AppCompatCache format used in Windows 10
+  _HEADER_SIGNATURE_10 = 0x00000030
+
+  _HEADER_10_STRUCT = construct.Struct(
+      u'appcompatcache_header_8',
+      construct.ULInt32(u'signature'),
+      construct.ULInt32(u'unknown1'),
+      construct.Padding(28),
+      construct.ULInt32(u'number_of_cached_entries'),
+      construct.Padding(8))
+
   def _CheckSignature(self, value_data):
     """Parses and validates the signature.
 
@@ -227,6 +239,12 @@ class AppCompatCachePlugin(interface.KeyPlugin):
           self._CACHED_ENTRY_SIGNATURE_8_0, self._CACHED_ENTRY_SIGNATURE_8_1]:
         return self._FORMAT_TYPE_8
 
+    elif signature == self._HEADER_SIGNATURE_10:
+      # Windows 10 uses the same cache entry signature as Windows 8.1
+      if value_data[signature:signature + 4] in [
+          self._CACHED_ENTRY_SIGNATURE_8_1]:
+        return self._FORMAT_TYPE_10
+
   def _DetermineCacheEntrySize(
       self, format_type, value_data, cached_entry_offset):
     """Determines the size of a cached entry.
@@ -246,7 +264,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
     """
     if format_type not in [
         self._FORMAT_TYPE_XP, self._FORMAT_TYPE_2003, self._FORMAT_TYPE_VISTA,
-        self._FORMAT_TYPE_7, self._FORMAT_TYPE_8]:
+        self._FORMAT_TYPE_7, self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       raise RuntimeError(
           u'[{0:s}] Unsupported format type: {1:d}'.format(
               self.NAME, format_type))
@@ -296,7 +314,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
         elif format_type == self._FORMAT_TYPE_7:
           cached_entry_size = self._CACHED_ENTRY_7_32BIT_STRUCT.sizeof()
 
-    elif format_type == self._FORMAT_TYPE_8:
+    elif format_type in [self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       cached_entry_size = self._CACHED_ENTRY_HEADER_8_STRUCT.sizeof()
 
     return cached_entry_size
@@ -316,7 +334,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
     """
     if format_type not in [
         self._FORMAT_TYPE_XP, self._FORMAT_TYPE_2003, self._FORMAT_TYPE_VISTA,
-        self._FORMAT_TYPE_7, self._FORMAT_TYPE_8]:
+        self._FORMAT_TYPE_7, self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       raise RuntimeError(
           u'[{0:s}] Unsupported format type: {1:d}'.format(
               self.NAME, format_type))
@@ -345,9 +363,13 @@ class AppCompatCachePlugin(interface.KeyPlugin):
       header_object.header_size = self._HEADER_8_STRUCT.sizeof()
       header_struct = self._HEADER_8_STRUCT.parse(value_data)
 
+    elif format_type == self._FORMAT_TYPE_10:
+      header_object.header_size = self._HEADER_10_STRUCT.sizeof()
+      header_struct = self._HEADER_10_STRUCT.parse(value_data)
+
     if format_type in [
         self._FORMAT_TYPE_XP, self._FORMAT_TYPE_2003, self._FORMAT_TYPE_VISTA,
-        self._FORMAT_TYPE_7]:
+        self._FORMAT_TYPE_7, self._FORMAT_TYPE_10]:
       header_object.number_of_cached_entries = header_struct.get(
           u'number_of_cached_entries')
 
@@ -373,7 +395,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
     """
     if format_type not in [
         self._FORMAT_TYPE_XP, self._FORMAT_TYPE_2003, self._FORMAT_TYPE_VISTA,
-        self._FORMAT_TYPE_7, self._FORMAT_TYPE_8]:
+        self._FORMAT_TYPE_7, self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       raise RuntimeError(
           u'[{0:s}] Unsupported format type: {1:d}'.format(
               self.NAME, format_type))
@@ -415,7 +437,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
         cached_entry_struct = self._CACHED_ENTRY_7_64BIT_STRUCT.parse(
             cached_entry_data)
 
-    elif format_type == self._FORMAT_TYPE_8:
+    elif format_type in [self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       if cached_entry_data[0:4] not in [
           self._CACHED_ENTRY_SIGNATURE_8_0, self._CACHED_ENTRY_SIGNATURE_8_1]:
         raise RuntimeError(
@@ -459,25 +481,26 @@ class AppCompatCachePlugin(interface.KeyPlugin):
       path_size = cached_entry_struct.get(u'path_size')
       path_offset = cached_entry_struct.get(u'path_offset')
 
-    elif format_type == self._FORMAT_TYPE_8:
+    elif format_type in [self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       path_size = cached_entry_struct.get(u'path_size')
 
       cached_entry_data_offset = 14 + path_size
       cached_entry_object.path = binary.Ut16StreamCopyToString(
           cached_entry_data[14:cached_entry_data_offset])
 
-      remaining_data = cached_entry_data[cached_entry_data_offset:]
+      if format_type == self._FORMAT_TYPE_8:
+        remaining_data = cached_entry_data[cached_entry_data_offset:]
 
-      cached_entry_object.insertion_flags = construct.ULInt32(
-          u'insertion_flags').parse(remaining_data[0:4])
-      cached_entry_object.shim_flags = construct.ULInt32(
-          u'shim_flags').parse(remaining_data[4:8])
+        cached_entry_object.insertion_flags = construct.ULInt32(
+            u'insertion_flags').parse(remaining_data[0:4])
+        cached_entry_object.shim_flags = construct.ULInt32(
+            u'shim_flags').parse(remaining_data[4:8])
 
-      if cached_entry_data[0:4] == self._CACHED_ENTRY_SIGNATURE_8_0:
-        cached_entry_data_offset += 8
+        if cached_entry_data[0:4] == self._CACHED_ENTRY_SIGNATURE_8_0:
+          cached_entry_data_offset += 8
 
-      elif cached_entry_data[0:4] == self._CACHED_ENTRY_SIGNATURE_8_1:
-        cached_entry_data_offset += 10
+        elif cached_entry_data[0:4] == self._CACHED_ENTRY_SIGNATURE_8_1:
+          cached_entry_data_offset += 10
 
       remaining_data = cached_entry_data[cached_entry_data_offset:]
 
@@ -487,7 +510,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
       cached_entry_object.last_modification_time = cached_entry_struct.get(
           u'last_modification_time')
 
-    elif format_type == self._FORMAT_TYPE_8:
+    elif format_type in [self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       cached_entry_object.last_modification_time = construct.ULInt64(
           u'last_modification_time').parse(remaining_data[0:8])
 
@@ -507,7 +530,7 @@ class AppCompatCachePlugin(interface.KeyPlugin):
       data_offset = cached_entry_struct.get(u'data_offset')
       data_size = cached_entry_struct.get(u'data_size')
 
-    elif format_type == self._FORMAT_TYPE_8:
+    elif format_type in [self._FORMAT_TYPE_8, self._FORMAT_TYPE_10]:
       data_offset = cached_entry_offset + cached_entry_data_offset + 12
       data_size = construct.ULInt32(u'data_size').parse(remaining_data[8:12])
 
