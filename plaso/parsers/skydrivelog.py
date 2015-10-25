bb1de0631bbd0dbaf248f7c3c59a97e8a650e815
Francesco Picasso <francesco.picasso@gmail.com>
2015-10-25 08:25:53 +0100

    Code review: 256160044: refactored SkyDrive log parsers

--- plaso/parsers/skydrivelog.py
+++ plaso/parsers/skydrivelog.py
@@ -16,152 +16,209 @@ __author__ = 'Francesco Picasso (francesco.picasso@gmail.com)'
 
 
 class SkyDriveLogEvent(time_events.TimestampEvent):
-  """Convenience class for a SkyDrive log line event."""
+  """Convenience class for a SkyDrive log line event.
+
+  Attributes:
+    detail: The log line details.
+    module: Optional, the module name that generated the log line.
+    source_code: Optional, logging source file and line number.
+    log_level: Optional, the SkyDrive log level short name.
+  """
   DATA_TYPE = u'skydrive:log:line'
 
-  def __init__(self, timestamp, offset, source_code, log_level, text):
+  def __init__(
+      self, timestamp, detail, module=None, source_code=None, log_level=None):
     """Initializes the event object.
 
     Args:
       timestamp: The timestamp which is an integer containing the number
                  of micro seconds since January 1, 1970, 00:00:00 UTC.
-      source_code: Details of the source code file generating the event.
-      log_level: The log level used for the event.
-      text: The log message.
+      detail: The log line details.
+      module: Optional, the module name that generated the log line.
+      source_code: Optional, logging source file and line number.
+      log_level: Optional, the SkyDrive log level short name.
     """
     super(SkyDriveLogEvent, self).__init__(
         timestamp, eventdata.EventTimestamp.ADDED_TIME)
+    self.detail = detail
     self.log_level = log_level
-    self.offset = offset
+    self.module = module
     self.source_code = source_code
-    self.text = text
 
 
-class SkyDriveLogParser(text_parser.PyparsingSingleLineTextParser):
+class SkyDriveLogParser(text_parser.PyparsingMultiLineTextParser):
   """Parse SkyDrive log files."""
 
   NAME = u'skydrive_log'
   DESCRIPTION = u'Parser for OneDrive (or SkyDrive) log files.'
 
-  _ENCODING = u'UTF-8-SIG'
+  _ENCODING = u'utf-8'
 
-  # Common SDL (SkyDriveLog) pyparsing objects.
-  SDL_COLON = pyparsing.Literal(u':')
-  SDL_EXCLAMATION = pyparsing.Literal(u'!')
+  # Common SDF (SkyDrive Format) structures.
+  INTEGER_CAST = text_parser.PyParseIntCast
+  HYPHEN = text_parser.PyparsingConstants.HYPHEN
+  TWO_DIGITS = text_parser.PyparsingConstants.TWO_DIGITS
+  TIME_MSEC = text_parser.PyparsingConstants.TIME_MSEC
+  MSEC = pyparsing.Word(pyparsing.nums, max=3).setParseAction(INTEGER_CAST)
+  COMMA = pyparsing.Literal(u',').suppress()
+  DOT = pyparsing.Literal(u'.').suppress()
+  IGNORE_FIELD = pyparsing.CharsNotIn(u',').suppress()
 
-  # Timestamp (08-01-2013 21:22:28.999).
-  SDL_TIMESTAMP = (
-      text_parser.PyparsingConstants.DATE_REV +
-      text_parser.PyparsingConstants.TIME_MSEC).setResultsName(u'timestamp')
+  # Header line timestamp (2013-07-25-160323.291): the timestamp format is
+  # YYYY-MM-DD-hhmmss.msec.
+  SDF_HEADER_TIMESTAMP = pyparsing.Group(
+      text_parser.PyparsingConstants.DATE.setResultsName(u'date') + HYPHEN +
+      TWO_DIGITS.setResultsName(u'hh') + TWO_DIGITS.setResultsName(u'mm') +
+      TWO_DIGITS.setResultsName(u'ss') + DOT +
+      MSEC.setResultsName(u'ms')).setResultsName(u'hdr_timestamp')
 
-  # SkyDrive source code pyparsing structures.
-  SDL_SOURCE_CODE = pyparsing.Combine(
-      pyparsing.CharsNotIn(u':') +
-      SDL_COLON +
-      text_parser.PyparsingConstants.INTEGER +
-      SDL_EXCLAMATION +
-      pyparsing.Word(pyparsing.printables)).setResultsName(u'source_code')
+  # Line timestamp (07-25-13,16:06:31.820): the timestamp format is
+  # MM-DD-YY,hh:mm:ss.msec.
+  SDF_TIMESTAMP = (
+      TWO_DIGITS.setResultsName(u'month') + HYPHEN +
+      TWO_DIGITS.setResultsName(u'day') + HYPHEN +
+      TWO_DIGITS.setResultsName(u'year_short') + COMMA +
+      TIME_MSEC.setResultsName(u'time')).setResultsName(u'timestamp')
 
-  # SkyDriveLogLevel pyparsing structures.
-  SDL_LOG_LEVEL = (
-      pyparsing.Literal(u'(').suppress() +
-      pyparsing.SkipTo(u')').setResultsName(u'log_level') +
-      pyparsing.Literal(u')').suppress())
+  # Header start.
+  SDF_HEADER_START = (
+      pyparsing.Literal(u'######').suppress() +
+      pyparsing.Literal(u'Logging started.').setResultsName(u'log_start'))
+
+  # Multiline entry end marker, matched from right to left.
+  SDF_ENTRY_END = pyparsing.StringEnd() | SDF_HEADER_START | SDF_TIMESTAMP
 
   # SkyDrive line pyparsing structure.
-  SDL_LINE = (
-      SDL_TIMESTAMP + SDL_SOURCE_CODE + SDL_LOG_LEVEL +
-      SDL_COLON + pyparsing.SkipTo(pyparsing.lineEnd).setResultsName(u'text'))
+  SDF_LINE = (
+      SDF_TIMESTAMP + COMMA +
+      IGNORE_FIELD + COMMA + IGNORE_FIELD + COMMA + IGNORE_FIELD + COMMA +
+      pyparsing.CharsNotIn(u',').setResultsName(u'module') + COMMA +
+      pyparsing.CharsNotIn(u',').setResultsName(u'source_code') + COMMA +
+      IGNORE_FIELD + COMMA + IGNORE_FIELD + COMMA +
+      pyparsing.CharsNotIn(u',').setResultsName(u'log_level') + COMMA +
+      pyparsing.SkipTo(SDF_ENTRY_END).setResultsName(u'detail') +
+      pyparsing.ZeroOrMore(pyparsing.lineEnd()))
 
-  # Sometimes the timestamped log line is followed by an empy line,
-  # then by a file name plus other data and finally by another empty
-  # line. It could happen that a logline is split in two parts.
-  # These lines will not be discarded and an event will be generated
-  # ad-hoc (see source), based on the last one if available.
-  SDL_NO_HEADER_SINGLE_LINE = (
-      pyparsing.Optional(pyparsing.Literal(u'->').suppress()) +
-      pyparsing.SkipTo(pyparsing.lineEnd).setResultsName(u'text'))
+  # SkyDrive header pyparsing structure.
+  SDF_HEADER = (
+      SDF_HEADER_START +
+      pyparsing.Literal(u'Version=').setResultsName(u'version_string') +
+      pyparsing.Word(pyparsing.nums + u'.').setResultsName(u'version_number') +
+      pyparsing.Literal(u'StartSystemTime:').suppress() +
+      SDF_HEADER_TIMESTAMP +
+      pyparsing.Literal(u'StartLocalTime:').setResultsName(
+          u'local_time_string') +
+      pyparsing.SkipTo(pyparsing.lineEnd()).setResultsName(u'details') +
+      pyparsing.lineEnd())
 
   # Define the available log line structures.
   LINE_STRUCTURES = [
-      (u'logline', SDL_LINE),
-      (u'no_header_single_line', SDL_NO_HEADER_SINGLE_LINE),
+      (u'logline', SDF_LINE),
+      (u'header', SDF_HEADER)
   ]
 
   def __init__(self):
     """Initializes a parser object."""
     super(SkyDriveLogParser, self).__init__()
-    self.offset = 0
-    self.last_event = None
+    self.use_local_zone = False
 
-  def _GetTimestamp(self, timestamp_pypr):
-    """Gets a timestamp from a pyparsing ParseResults timestamp.
+  def _GetTimestampFromHeader(self, structure):
+    """Gets a timestamp from the structure.
 
-    This is a timestamp_string as returned by using
-    text_parser.PyparsingConstants structures:
-    [[8, 1, 2013], [21, 22, 28], 999]
+    The following is an example of the timestamp structure expected
+    [[2013, 7, 25], 16, 3, 23, 291]: DATE (year, month, day)  is the
+    first list element, than hours, minutes, seconds and milliseconds follow.
 
     Args:
-      timestamp_string: The pyparsing ParseResults object
+      structure: The parsed structure, which should be a timestamp.
 
     Returns:
-      The timestamp which is an integer containing the number of micro seconds
-      since January 1, 1970, 00:00:00 UTC or 0 on error.
+      timestamp: An integer containing the timestamp or 0 on error.
     """
-    month, day, year = timestamp_pypr[0]
-    hour, minute, second = timestamp_pypr[1]
-    millisecond = timestamp_pypr[2]
-    try:
-      return timelib.Timestamp.FromTimeParts(
-          year, month, day, hour, minute, second,
-          microseconds=(millisecond * 1000))
-    except ValueError:
-      pass
+    year, month, day = structure.date
+    hour = structure.get(u'hh', 0)
+    minute = structure.get(u'mm', 0)
+    second = structure.get(u'ss', 0)
+    microsecond = structure.get(u'ms', 0) * 1000
 
-    return 0
+    return timelib.Timestamp.FromTimeParts(
+        year, month, day, hour, minute, second, microseconds=microsecond)
 
-  def _ParseLogLine(self, parser_mediator, structure):
-    """Parse a single log line and produce an event object.
+  def _GetTimestampFromLine(self, structure):
+    """Gets a timestamp from string from the structure
+
+    The following is an example of the timestamp structure expected
+    [7, 25, 13, [16, 3, 24], 649]: month, day, year, a list with three
+    element (hours, minutes, seconds) and finally milliseconds.
 
     Args:
-      parser_mediator: A parser mediator object (instance of ParserMediator).
-      structure: A pyparsing.ParseResults object from a line in the
-                 log file.
+      structure: The parsed structure.
+
+    Returns:
+      timestamp: An integer containing the timestamp or 0 on error.
     """
-    timestamp = self._GetTimestamp(structure.timestamp)
+    hour, minute, second = structure.time[0]
+    microsecond = structure.time[1] * 1000
+    # TODO: Verify if timestamps are locale dependent.
+    year = structure.get(u'year_short', 0)
+    month = structure.get(u'month', 0)
+    day = structure.get(u'day', 0)
+    if year < 0 or not month or not day:
+      return 0
+
+    year += 2000
+
+    return timelib.Timestamp.FromTimeParts(
+        year, month, day, hour, minute, second, microseconds=microsecond)
+
+  def _ParseHeader(self, structure):
+    """Parse header lines and store appropriate attributes.
+
+    [u'Logging started.', u'Version=', u'17.0.2011.0627',
+    [2013, 7, 25], 16, 3, 23, 291, u'StartLocalTime', u'<details>']
+
+    Args:
+      structure: A pyparsing.ParseResults object from an header line in the
+                 log file.
+
+    Returns:
+      An event object (instance of SkyDriveLogEvent) or None on error.
+    """
+    timestamp = self._GetTimestampFromHeader(structure.hdr_timestamp)
     if not timestamp:
-      logging.debug(u'Invalid timestamp {0:s}'.format(structure.timestamp))
+      logging.debug(
+          u'SkyDriveLog invalid timestamp {0:d}'.format(
+              structure.hdr_timestamp))
       return
+    detail = u'{0:s} {1:s} {2:s} {3:s} {4:s}'.format(
+        structure.log_start, structure.version_string,
+        structure.version_number, structure.local_time_string,
+        structure.details)
+    return SkyDriveLogEvent(timestamp, detail)
 
-    event_object = SkyDriveLogEvent(
-        timestamp, self.offset, structure.source_code, structure.log_level,
-        structure.text)
-    parser_mediator.ProduceEvent(event_object)
-
-    self.last_event = event_object
-
-  def _ParseNoHeaderSingleLine(self, parser_mediator, structure):
-    """Parse an isolated line and and produce an event object.
+  def _ParseLine(self, structure):
+    """Parse a logline and store appropriate attributes.
 
     Args:
-      parser_mediator: A parser mediator object (instance of ParserMediator).
-      structure: A pyparsing.ParseResults object from a line in the
-                 log file.
+      structure: A pyparsing.ParseResults object from a line in the log file.
+
+    Returns:
+      An event object (instance of SkyDriveLogEvent) or None.
     """
-    if not self.last_event:
-      logging.debug(u'SkyDrive, found isolated line with no previous events')
+    timestamp = self._GetTimestampFromLine(structure.timestamp)
+    if not timestamp:
+      logging.debug(u'SkyDriveLog invalid timestamp {0:s}'.format(
+          structure.timestamp))
       return
 
-    event_object = SkyDriveLogEvent(
-        self.last_event.timestamp, self.last_event.offset, None, None,
-        structure.text)
-    parser_mediator.ProduceEvent(event_object)
-
-    # TODO think to a possible refactoring for the non-header lines.
-    self.last_event = None
+    # Replace newlines with spaces in structure.detail to preserve output.
+    detail = structure.detail.replace(u'\n', u' ')
+    return SkyDriveLogEvent(
+        timestamp, detail, module=structure.module,
+        source_code=structure.source_code, log_level=structure.log_level)
 
   def ParseRecord(self, parser_mediator, key, structure):
-    """Parses a log record structure and produces events.
+    """Parse each record structure and return an EventObject if applicable.
 
     Args:
       parser_mediator: A parser mediator object (instance of ParserMediator).
@@ -170,13 +227,18 @@ class SkyDriveLogParser(text_parser.PyparsingSingleLineTextParser):
       structure: A pyparsing.ParseResults object from a line in the
                  log file.
     """
+    event_object = None
+
     if key == u'logline':
-      self._ParseLogLine(parser_mediator, structure)
-    elif key == u'no_header_single_line':
-      self._ParseNoHeaderSingleLine(parser_mediator, structure)
+      event_object = self._ParseLine(structure)
+    elif key == u'header':
+      event_object = self._ParseHeader(structure)
     else:
       logging.warning(
           u'Unable to parse record, unknown structure: {0:s}'.format(key))
+
+    if event_object:
+      parser_mediator.ProduceEvent(event_object)
 
   def VerifyStructure(self, parser_mediator, line):
     """Verify that this file is a SkyDrive log file.
@@ -188,20 +250,19 @@ class SkyDriveLogParser(text_parser.PyparsingSingleLineTextParser):
     Returns:
       True if this is the correct parser, False otherwise.
     """
-    structure = self.SDL_LINE
-    parsed_structure = None
-    timestamp = None
     try:
-      parsed_structure = structure.parseString(line)
+      parsed_structure = self.SDF_HEADER.parseString(line)
     except pyparsing.ParseException:
       logging.debug(u'Not a SkyDrive log file')
       return False
-    else:
-      timestamp = self._GetTimestamp(parsed_structure.timestamp)
+
+    timestamp = self._GetTimestampFromHeader(parsed_structure.hdr_timestamp)
     if not timestamp:
-      logging.debug(u'Not a SkyDrive log file, invalid timestamp {0:s}'.format(
-          parsed_structure.timestamp))
+      logging.debug(
+          u'Not a SkyDrive log file, invalid timestamp {0:s}'.format(
+              parsed_structure.timestamp))
       return False
+
     return True
 
 
--- tests/parsers/skydrivelog.py
+++ tests/parsers/skydrivelog.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""Tests for the skydrivelog parser."""
+"""Tests for the SkyDriveLog log parser."""
 
 import unittest
 
-from plaso.formatters import skydrivelog as _  # pylint: disable=unused-import
+# pylint: disable=unused-import
+from plaso.formatters import skydrivelog as skydrivelog_formatter
 from plaso.lib import timelib
 from plaso.parsers import skydrivelog
 
@@ -15,55 +16,135 @@ __author__ = 'Francesco Picasso (francesco.picasso@gmail.com)'
 
 
 class SkyDriveLogUnitTest(test_lib.ParserTestCase):
-  """Tests for the skydrivelog parser."""
+  """A unit test for the SkyDriveLog parser."""
 
   def setUp(self):
     """Sets up the needed objects used throughout the test."""
     self._parser = skydrivelog.SkyDriveLogParser()
 
-  def testParse(self):
-    """Tests the Parse function."""
+  def testParseErrorLog(self):
+    """Tests the Parse function or error log."""
+    test_file = self._GetTestFilePath([u'skydriveerr.log'])
+    event_queue_consumer = self._ParseFile(self._parser, test_file)
+    event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
+
+    self.assertEqual(len(event_objects), 19)
+
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2013-07-25 16:03:23.291')
+    self.assertEqual(event_objects[0].timestamp, expected_timestamp)
+
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2013-07-25 16:03:24.649')
+    self.assertEqual(event_objects[1].timestamp, expected_timestamp)
+
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2013-08-01 21:27:44.124')
+    self.assertEqual(event_objects[18].timestamp, expected_timestamp)
+
+    expected_string = (
+        'Logging started. Version= 17.0.2011.0627 StartLocalTime: '
+        '2013-07-25-180323.291 PID=0x8f4 TID=0x718 ContinuedFrom=')
+    expected_string_short = (
+        u'Logging started. Version= 17.0.2011.0627 StartLocalTime: '
+        '2013-07-25-180323.29...')
+    self._TestGetMessageStrings(
+        event_objects[0], expected_string, expected_string_short)
+
+    expected_string = (
+        u'[AUTH authapi.cpp(280) ERR] Sign in failed : '
+        u'DRX_E_AUTH_NO_VALID_CREDENTIALS,')
+    expected_string_short = (
+        u'Sign in failed : DRX_E_AUTH_NO_VALID_CREDENTIALS,')
+    self._TestGetMessageStrings(
+        event_objects[1], expected_string, expected_string_short)
+
+    expected_string = (
+        u'[WNS absconn.cpp(177) VRB] Received data from server,'
+        u'dwID=0x0;dwSize=0x3e;pbData=PNG 9 CON 48  <ping-response>'
+        u'<wait>44</wait></ping-response>')
+    expected_string_short = (
+        u'Received data from server,dwID=0x0;dwSize=0x3e;pbData=PNG 9 CON 48  '
+        '<ping-res...')
+    self._TestGetMessageStrings(
+        event_objects[18], expected_string, expected_string_short)
+
+  def testParseErrorLogUnicode(self):
+    """Tests the Parse function on Unicode error log."""
+    test_file = self._GetTestFilePath([u'skydriveerr-unicode.log'])
+    event_queue_consumer = self._ParseFile(self._parser, test_file)
+    event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
+
+    self.assertEqual(len(event_objects), 19)
+
+    # TODO: check if this test passes because the encoding on my system
+    # is UTF-8.
+    expected_detail = (
+        u'No node found named Passport-Jméno-člena, no user name available,')
+    self.assertEqual(event_objects[3].detail, expected_detail)
+
+  def testParseLog(self):
+    """Tests the Parse function on normal log."""
     test_file = self._GetTestFilePath([u'skydrive.log'])
     event_queue_consumer = self._ParseFile(self._parser, test_file)
     event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
 
-    self.assertEqual(len(event_objects), 18)
+    self.assertEqual(len(event_objects), 17)
 
     expected_timestamp = timelib.Timestamp.CopyFromString(
-        u'2013-08-01 21:22:28.999')
+        u'2013-08-12 01:08:52.985')
     self.assertEqual(event_objects[0].timestamp, expected_timestamp)
 
     expected_timestamp = timelib.Timestamp.CopyFromString(
-        u'2013-08-01 21:22:29.702')
+        u'2013-08-12 01:10:08.835')
     self.assertEqual(event_objects[1].timestamp, expected_timestamp)
-    self.assertEqual(event_objects[2].timestamp, expected_timestamp)
-    self.assertEqual(event_objects[3].timestamp, expected_timestamp)
 
     expected_timestamp = timelib.Timestamp.CopyFromString(
-        u'2013-08-01 21:22:58.344')
-    self.assertEqual(event_objects[4].timestamp, expected_timestamp)
-    self.assertEqual(event_objects[5].timestamp, expected_timestamp)
-
-    expected_msg = (
-        u'[global.cpp:626!logVersionInfo] (DETAIL) 17.0.2011.0627 (Ship)')
-    expected_msg_short = (
-        u'17.0.2011.0627 (Ship)')
-    self._TestGetMessageStrings(
-        event_objects[0], expected_msg, expected_msg_short)
-
-    expected_msg = (
-        u'SyncToken = LM%3d12345678905670%3bID%3d1234567890E059C0!'
-        u'103%3bLR%3d12345678905623%3aEP%3d2')
-    expected_msg_short = (
-        u'SyncToken = LM%3d12345678905670%3bID%3d1234567890E059C0!'
-        u'103%3bLR%3d1234567890...')
-    self._TestGetMessageStrings(
-        event_objects[3], expected_msg, expected_msg_short)
+        u'2013-08-12 02:52:32.976')
+    self.assertEqual(event_objects[11].timestamp, expected_timestamp)
 
     expected_string = (
-        u'SyncToken = Not a sync token (\xe0\xe8\xec\xf2\xf9)!')
+        u'[WNS absconn.cpp(177) VRB] Received data from server,dwID=0x0;'
+        'dwSize=0x15a;pbData=GET 5 WNS 331 Context: 2891  <channel-response>'
+        '<id>1;13714367258539257282</id><exp>2013-09-11T02:52:37Z</exp><url>'
+        'https://bn1.notify.windows.com/?token=AgYAAAAdkHjSxiNH1mbF0Rp5TIv0K'
+        'z317BKYIAfBNO6szULCOEE2393owBINnPC5xoika5SJlNtXZ%2bwzaRVsPRcP1p64XF'
+        'n90vGwr07DGZxfna%2bxBpBBplzZhLV9y%2fNV%2bBPxNmTI5sRgaZ%2foGvYCIj6Md'
+        'eU1</url></channel-response>')
+    expected_string_short = (
+        u'Received data from server,dwID=0x0;dwSize=0x15a;pbData=GET 5 WNS '
+        '331 Context:...')
     self._TestGetMessageStrings(
-        event_objects[17], expected_string, expected_string)
+        event_objects[11], expected_string, expected_string_short)
+
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2013-08-12 03:18:57.232')
+    self.assertEqual(event_objects[13].timestamp, expected_timestamp)
+
+    expected_string = (
+        u'Logging started. Version= 17.0.2011.0627 StartLocalTime: '
+        '2013-08-11-231857.232 PID=0x1ef0 TID=0x1ef4 ContinuedFrom=')
+    expected_string_short = (
+        u'Logging started. Version= 17.0.2011.0627 StartLocalTime: '
+        '2013-08-11-231857.23...')
+    self._TestGetMessageStrings(
+        event_objects[13], expected_string, expected_string_short)
+
+    expected_timestamp = timelib.Timestamp.CopyFromString(
+        u'2013-08-31 03:45:37.940')
+    self.assertEqual(event_objects[15].timestamp, expected_timestamp)
+
+    expected_string = (
+        u'[PAL cwinhttp.cpp(1581) VRB] ,output=GET <- /MyData/LiveFolders?'
+        'Filter=changes&InlineBlobs=false&MaxItemCount=50&SyncToken=LM%3d6351'
+        '1875645970%3bID%3d7F095149027848ED!103%3bLR%3d63513517536493%3bEP%3d'
+        '2%3bTD%3dTrue&View=SkyDriveSync;m_httpStatus=0x130;hr=8004db30;'
+        'm_pSink=null;cb=0x0;msec=0x4e')
+    expected_string_short = (
+        u',output=GET <- /MyData/LiveFolders?Filter=changes&InlineBlobs='
+        'false&MaxItemCo...')
+    self._TestGetMessageStrings(
+        event_objects[15], expected_string, expected_string_short)
 
 
 if __name__ == '__main__':
