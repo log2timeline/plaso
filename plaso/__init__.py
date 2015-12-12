788346eaf2a89cf0f20df8036f04fa3c5d0787f6
Joachim Metz <joachim.metz@gmail.com>
2015-12-12 13:06:46 +0100

    Code review: 276430043: Worked on tests and fix for PE parser #265

--- plaso/__init__.py
+++ plaso/__init__.py
@@ -3,7 +3,7 @@
 __version__ = '1.3.1'
 
 VERSION_DEV = True
-VERSION_DATE = '20151211'
+VERSION_DATE = '20151212'
 
 
 def GetVersion():
--- tests/frontend/log2timeline.py
+++ tests/frontend/log2timeline.py
@@ -12,20 +12,60 @@ from tests.frontend import test_lib
 class Log2TimelineFrontendTest(test_lib.FrontendTestCase):
   """Tests for the log2timeline front-end."""
 
+  def testGetFiltersInformation(self):
+    """Tests the _GetFiltersInformation function."""
+    test_front_end = log2timeline.Log2TimelineFrontend()
+    filters_info = test_front_end._GetFiltersInformation()
+
+    self.assertIsNotNone(filters_info)
+
+    available_filter_names = [name for name, _ in filters_info]
+    self.assertIn(u'DynamicFilter', available_filter_names)
+    self.assertIn(u'EventObjectFilter', available_filter_names)
+
+  def testGetOutputModulesInformation(self):
+    """Tests the _GetOutputModulesInformation function."""
+    test_front_end = log2timeline.Log2TimelineFrontend()
+    modules_info = test_front_end._GetOutputModulesInformation()
+
+    self.assertIsNotNone(modules_info)
+
+    available_module_names = [name for name, _ in modules_info]
+    self.assertIn(u'dynamic', available_module_names)
+    self.assertIn(u'json', available_module_names)
+
+  def testGetDisabledOutputClasses(self):
+    """Tests the GetDisabledOutputClasses function."""
+    test_front_end = log2timeline.Log2TimelineFrontend()
+    output_classes = list(test_front_end.GetDisabledOutputClasses())
+
+    self.assertIsNotNone(output_classes)
+
+  def testGetOutputClasses(self):
+    """Tests the GetOutputClasses function."""
+    test_front_end = log2timeline.Log2TimelineFrontend()
+    output_classes = list(test_front_end.GetOutputClasses())
+
+    self.assertIsNotNone(output_classes)
+
+    available_output_names = [name for name, _ in output_classes]
+    self.assertIn(u'dynamic', available_output_names)
+    self.assertIn(u'json', available_output_names)
+
   def testGetPluginData(self):
     """Tests the GetPluginData function."""
     test_front_end = log2timeline.Log2TimelineFrontend()
     plugin_info = test_front_end.GetPluginData()
 
     self.assertIn(u'Hashers', plugin_info)
-    available_hasher_names = []
-    for hasher_info in plugin_info[u'Hashers']:
-      available_hasher_names.append(hasher_info[0])
+
+    available_hasher_names = [name for name, _ in plugin_info[u'Hashers']]
     self.assertIn(u'sha256', available_hasher_names)
     self.assertIn(u'sha1', available_hasher_names)
 
     self.assertIn(u'Parsers', plugin_info)
     self.assertIsNotNone(plugin_info[u'Parsers'])
+
     self.assertIn(u'Parser Plugins', plugin_info)
     self.assertIsNotNone(plugin_info[u'Parser Plugins'])
 
