ea216b04071de834cd5352176bfa3ff90675bcfe
Joachim Metz <joachim.metz@gmail.com>
2015-12-19 20:52:07 +0100

    Updated Mac OS X packaging scripts.

--- config/macosx/make_dist.sh
+++ config/macosx/make_dist.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 #
-# Copyright 2014 The Plaso Project Authors.
-# Please see the AUTHORS file for details on individual authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 # Script to make a plaso Mac OS X distribution package.
 
 EXIT_SUCCESS=0;
 EXIT_FAILURE=1;
 
-if test ! -d dependencies;
+MACOSX_VERSION=`sw_vers -productVersion | awk -F '.' '{print $1 "." $2}'`;
+PLASO_VERSION=`grep -e '^__version' plaso/__init__.py | sed -e "s/^[^=]*= '\([^']*\)'/\1/g"`;
+PKG_IDENTIFIER="com.github.log2timeline.plaso";
+PKG_FILENAME="../python-plaso-${PLASO_VERSION}.pkg";
+DEPSDIR="../l2tdevtools/build";
+DISTDIR="plaso-${PLASO_VERSION}";
+DISTFILE="../plaso-${PLASO_VERSION}_macosx-${MACOSX_VERSION}.dmg";
+
+if test ! -d ${DEPSDIR};
 then
-  echo "Missing dependencies directory.";
+  echo "Missing dependencies directory: ${DEPSDIR}.";
 
   exit ${EXIT_FAILURE};
 fi
@@ -34,50 +27,66 @@ then
   exit ${EXIT_FAILURE};
 fi
 
-MACOSX_VERSION=`sw_vers -productVersion | awk -F '.' '{print $1 "." $2}'`;
-PLASO_VERSION=`grep -e '^__version' plaso/__init__.py | sed -e "s/^[^=]*= '\([^']*\)'/\1/g"`;
-PKG_IDENTIFIER="com.github.log2timeline.plaso";
-PKG_FILENAME="../python-plaso-${PLASO_VERSION}.pkg";
-DISTDIR="plaso-${PLASO_VERSION}";
-
 if test ! -z $1;
 then
   PLASO_VERSION="${PLASO_VERSION}-$1";
 fi
 
-rm -rf build dist ${DISTDIR} ../${PKG_FILENAME} ../plaso-${PLASO_VERSION}_macosx-${MACOSX_VERSION}.dmg;
+rm -rf build dist tmp ${DISTDIR} ${PKG_FILENAME} ${DISTFILE};
 
-python setup.py bdist
+python setup.py install --root=$PWD/tmp --install-data=/usr/local 
 
-mkdir -p dist/tmp
-cd dist/tmp
-tar xfvz ../*.tar.gz
-cd ../..
+mkdir -p tmp/usr/local/share/doc/plaso
+cp AUTHORS ACKNOWLEDGEMENTS LICENSE tmp/usr/local/share/doc/plaso
 
-mkdir -p dist/tmp/usr/share/doc/plaso
-cp AUTHORS ACKNOWLEDGEMENTS LICENSE dist/tmp/usr/share/doc/plaso
+for PY_FILE in tmp/usr/local/bin/*.py;
+do
+  SH_FILE=${PY_FILE/.py/.sh};
+  cat > ${SH_FILE} << EOT
+#!/bin/sh
+PYTHONPATH=/Library/Python/2.7/site-packages/ \${0/.sh/.py};
+EOT
+  chmod a+x ${SH_FILE};
+done
 
-pkgbuild --root dist/tmp --identifier ${PKG_IDENTIFIER} --version ${PLASO_VERSION} --ownership recommended ${PKG_FILENAME};
+pkgbuild --root tmp --identifier ${PKG_IDENTIFIER} --version ${PLASO_VERSION} --ownership recommended ${PKG_FILENAME};
 
 if test ! -f ${PKG_FILENAME};
 then
-  echo "Missing plaso package: ../python-plaso-${PLASO_VERSION}.pkg file.";
+  echo "Missing plaso package file: ${PKG_FILENAME}";
 
   exit ${EXIT_FAILURE};
 fi
 
-mkdir "${DISTDIR}";
-cp -r config/licenses "${DISTDIR}";
-cp config/macosx/Readme.txt "${DISTDIR}";
+mkdir ${DISTDIR};
 
-sed "s/@VOLUMENAME@/${DISTDIR}/" config/macosx/install.sh.in > "${DISTDIR}/install.sh";
-chmod 755 "${DISTDIR}/install.sh";
+cp config/macosx/Readme.txt ${DISTDIR}/;
 
-mkdir "${DISTDIR}/packages";
-cp dependencies/*.pkg "${DISTDIR}/packages";
-cp ../python-plaso-${PLASO_VERSION}.pkg "${DISTDIR}/packages";
+sed "s/@VOLUMENAME@/${DISTDIR}/" config/macosx/install.sh.in > ${DISTDIR}/install.sh;
+chmod 755 ${DISTDIR}/install.sh;
 
-hdiutil create ../plaso-${PLASO_VERSION}_macosx-${MACOSX_VERSION}.dmg -srcfolder "${DISTDIR}/" -fs HFS+;
+mkdir ${DISTDIR}/packages;
+
+IFS="
+";
+
+for DEPENDENCY_DMG in `ls -1 ${DEPSDIR}/*.dmg`;
+do
+  # TODO: skip hachoir packages.
+  DEPENDENCY_PKG=`basename ${DEPENDENCY_DMG/.dmg/.pkg}`;
+  hdiutil attach ${DEPENDENCY_DMG};
+  cp -rf /Volumes/${DEPENDENCY_PKG}/${DEPENDENCY_PKG} ${DISTDIR}/packages;
+  hdiutil detach /Volumes/${DEPENDENCY_PKG};
+
+  # TODO: copy license files on a per-dependency basis.
+done
+
+cp -r config/licenses ${DISTDIR}/;
+rm -f ${DISTDIR}/licenses/LICENSE.pywin32;
+
+cp -rf ${PKG_FILENAME} ${DISTDIR}/packages;
+
+hdiutil create ${DISTFILE} -srcfolder ${DISTDIR}/ -fs HFS+;
 
 exit ${EXIT_SUCCESS};
 
