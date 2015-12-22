53c57b236e4c32c32f7d3cf0202858f18a9a6576
Joachim Metz <joachim.metz@gmail.com>
2015-12-22 21:22:22 +0100

    Changes to force hidden imports for Windows packaging

--- config/windows/make.bat
+++ config/windows/make.bat
@@ -5,7 +5,7 @@ rmdir /q /s build dist 2> NUL
 set PYTHONPATH=.
 
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --hidden-import artifacts --onedir tools\image_export.py
-C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --hidden-import artifacts --hidden-import requests --hidden-import dpkt --onedir tools\log2timeline.py
+C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --hidden-import artifacts --hidden-import requests --hidden-import dpkt --hidden-import IPython --onedir tools\log2timeline.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --hidden-import artifacts --onedir tools\pinfo.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --hidden-import artifacts --hidden-import IPython --onedir tools\preg.py
 C:\Python27\python.exe ..\pyinstaller\pyinstaller.py --hidden-import artifacts --hidden-import requests --hidden-import dpkt --onedir tools\psort.py
