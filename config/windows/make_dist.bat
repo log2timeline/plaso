94c4f66543e26bedad67a390ca2b10da1be6c426
Joachim Metz <joachim.metz@gmail.com>
2015-12-30 14:29:59 +0100

    Code review: 278680043: Updated Windows packaged version build scripts to included data directory #483

--- config/windows/make_dist.bat
+++ config/windows/make_dist.bat
@@ -4,6 +4,7 @@ del /q /s dist\plaso 2> NUL
 rmdir /q /s dist\plaso 2> NUL
 
 mkdir dist\plaso
+mkdir dist\plaso\data
 mkdir dist\plaso\licenses
 
 xcopy /q /y ACKNOWLEDGEMENTS dist\plaso
@@ -17,3 +18,4 @@ xcopy /q /y /s dist\log2timeline\* dist\plaso
 xcopy /q /y /s dist\pinfo\* dist\plaso
 xcopy /q /y /s dist\preg\* dist\plaso
 xcopy /q /y /s dist\psort\* dist\plaso
+xcopy /q /y data\* dist\plaso\data
