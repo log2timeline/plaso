81be5fc0f00ee8961da165677496c5de5207517a
MacleodKen <kenmacleod2013@gmail.com>
2016-02-15 22:15:47 +0000

    added sqlite parser for kik messenger on ios

--- plaso/parsers/sqlite_plugins/__init__.py
+++ plaso/parsers/sqlite_plugins/__init__.py
@@ -9,6 +9,7 @@ from plaso.parsers.sqlite_plugins import chrome_extension_activity
 from plaso.parsers.sqlite_plugins import firefox
 from plaso.parsers.sqlite_plugins import firefox_cookies
 from plaso.parsers.sqlite_plugins import gdrive
+from plaso.parsers.sqlite_plugins import kik_ios
 from plaso.parsers.sqlite_plugins import ls_quarantine
 from plaso.parsers.sqlite_plugins import mac_document_versions
 from plaso.parsers.sqlite_plugins import mackeeper_cache
