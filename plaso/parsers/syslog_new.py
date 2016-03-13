3e89eac3062000b661cd39db509927220fa954c6
Daniel White <onager@deerpie.com>
2016-03-13 20:04:29 +0100

    Worked on new syslog parser.

--- plaso/parsers/syslog_new.py
+++ plaso/parsers/syslog_new.py
@@ -26,35 +26,63 @@ class NewSyslogParser(text_parser.PyparsingMultiLineTextParser):
         u'fractional_seconds'),
     u'hostname': pyparsing.Word(pyparsing.printables).setResultsName(
         u'hostname'),
-    u'process_name': pyparsing.Word(pyparsing.alphanums).setResultsName(
+    u'process_name': pyparsing.Word(pyparsing.alphanums + u'.').setResultsName(
         u'process_name'),
     u'pid': text_parser.PyparsingConstants.PID.setResultsName(u'pid'),
     u'message': pyparsing.Regex(
-        '.*?(?=($|\n\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}))',re.DOTALL).
-        setResultsName(u'message')
+        '.*?(?=($|\n\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}))', re.DOTALL).
+        setResultsName(u'message'),
+    u'comment_message': (
+        pyparsing.Literal(u'---') +
+        pyparsing.SkipTo(u'---\n', include=True)).setResultsName(u'message')
   }
+  _pyparsing_components[u'date'] = (
+      _pyparsing_components[u'month'] +
+      _pyparsing_components[u'day'] +
+      _pyparsing_components[u'hour'] + pyparsing.Suppress(u':') +
+      _pyparsing_components[u'minute'] + pyparsing.Suppress(u':') +
+      _pyparsing_components[u'second'] + pyparsing.Optional(
+          pyparsing.Suppress(u'.') +
+          _pyparsing_components[u'fractional_seconds']))
 
 
-  LINE_GRAMMAR = (_pyparsing_components[u'month'] +
-                  _pyparsing_components[u'day'] +
-                  _pyparsing_components[u'hour'] + pyparsing.Suppress(u':') +
-                  _pyparsing_components[u'minute'] + pyparsing.Suppress(u':') +
-                  _pyparsing_components[u'second'] + pyparsing.Optional(
-                      pyparsing.Suppress(u'.') +
-                      _pyparsing_components[u'fractional_seconds']) +
-                  _pyparsing_components[u'hostname'] +
-                  _pyparsing_components[u'process_name'] +
-                  pyparsing.Suppress(u'[') + _pyparsing_components[u'pid'] +
-                  pyparsing.Suppress(u']:') + _pyparsing_components[u'message']
-                   + pyparsing.lineEnd())
+  DEFAULT_GRAMMAR = (
+      _pyparsing_components[u'date'] +
+      _pyparsing_components[u'hostname'] +
+      _pyparsing_components[u'process_name'] +
+      pyparsing.Optional(
+          pyparsing.Suppress(u'[') +
+          _pyparsing_components[u'pid'] + pyparsing.Suppress(u']')) +
+      pyparsing.Optional(
+        pyparsing.Suppress(u'<') +
+        pyparsing.Word(pyparsing.alphanums) + pyparsing.Suppress(u'>')) +
+      pyparsing.Optional(pyparsing.Suppress(u':')) +
+      _pyparsing_components[u'message'] + pyparsing.lineEnd())
 
-  LINE_STRUCTURES = [(u'syslog_line', LINE_GRAMMAR)]
+  SYSLOG_COMMENT = (_pyparsing_components[u'date'] + pyparsing.Suppress(u':') +
+                    _pyparsing_components[u'comment_message'])
+
+  LINE_STRUCTURES = [(u'syslog_line', DEFAULT_GRAMMAR),
+                     (u'syslog_comment', SYSLOG_COMMENT)]
 
   def __init__(self):
     """Initialize the parser."""
     super(NewSyslogParser, self).__init__()
     self._year_use = 0
     self._last_month = 0
+
+  def _UpdateYear(self, parser_mediator, month):
+    """Updates the year to use, based on the current month.
+    Args:
+      month: The month observed by the parser, as a number.
+    """
+    if not self._year_use:
+      self.year_use = parser_mediator.GetEstimatedYear()
+
+    if not self._last_month:
+      self._last_month = month
+      return
+
 
   def VerifyStructure(self, parser_mediator, lines):
     """Verify that this is a syslog file.
@@ -78,14 +106,16 @@ class NewSyslogParser(text_parser.PyparsingMultiLineTextParser):
       structure: A pyparsing.ParseResults object from a line in the
                  log file.
     """
-    if not self._year_use:
-      self._year_use = parser_mediator.GetEstimatedYear()
+    # No events for syslog comments.
+    if key == u'syslog_comment':
+      return
 
     month = timelib.MONTH_DICT.get(structure.month.lower(), None)
     if not month:
       parser_mediator.ProduceParserError(u'Invalid month value: {0:s}'.format(
         month))
       return
+    self._UpdateYear(parser_mediator, month)
     timestamp = timelib.Timestamp.FromTimeParts(
         year= self._year_use, month=month, day=structure.day,
         hour=structure.hour, minutes=structure.minute,
--- tests/parsers/syslog_new.py
+++ tests/parsers/syslog_new.py
@@ -15,46 +15,70 @@ class NewSyslogUnitTest(test_lib.ParserTestCase):
   """Tests for the syslog parser."""
 
   def setUp(self):
-    """Sets up the needed objects used throughout the test."""
+    """Makes preparations before running an individual test."""
     self._parser = syslog_new.NewSyslogParser()
 
-  def testParse(self):
-    """Tests the Parse function."""
-    knowledge_base_values = {u'year': 2012}
-    test_file = self._GetTestFilePath([u'syslog'])
+  def testParseRsyslog(self):
+    """Tests the Parse function on an Ubuntu-style syslog file"""
+    knowledge_base_values = {u'year': 2016}
+    test_file = self._GetTestFilePath([u'syslog_rsyslog'])
     event_queue_consumer = self._ParseFile(
-        self._parser, test_file, knowledge_base_values=knowledge_base_values)
+      self._parser, test_file, knowledge_base_values=knowledge_base_values)
     event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
 
-    expected_timestamp = timelib.Timestamp.CopyFromString(
-        u'2012-01-22 07:52:33')
-    self.assertEqual(event_objects[0].timestamp, expected_timestamp)
-    self.assertEqual(event_objects[0].hostname, u'myhostname.myhost.com')
+    self.assertEqual(len(event_objects), 7)
 
-    expected_string = (
-        u'[client, pid: 30840] : INFO No new content.')
-    self._TestGetMessageStrings(
-        event_objects[0], expected_string, expected_string)
+  def testParseOSX(self):
+    """Tests the Parse function on an Ubuntu-style syslog file"""
+    knowledge_base_values = {u'year': 2016}
+    test_file = self._GetTestFilePath([u'syslog_osx'])
+    event_queue_consumer = self._ParseFile(
+      self._parser, test_file, knowledge_base_values=knowledge_base_values)
+    event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
 
-    expected_msg = (
-        u'[aprocess, pid: 101001] : This is a multi-line message that screws up'
-        u'many syslog parsers.')
-    expected_msg_short = (
-        u'[aprocess, pid: 101001] : This is a multi-line message that screws up'
-        u'many sys...')
-    self._TestGetMessageStrings(
-        event_objects[11], expected_msg, expected_msg_short)
+    self.assertEqual(len(event_objects), 2)
 
-    expected_timestamp = timelib.Timestamp.CopyFromString(
-        u'2012-02-29 01:15:43')
-    self.assertEqual(event_objects[6].timestamp, expected_timestamp)
 
-    # Testing year increment.
-    expected_timestamp = timelib.Timestamp.CopyFromString(
-        u'2013-03-23 23:01:18')
-    self.assertEqual(event_objects[8].timestamp, expected_timestamp)
+  def testParse(self):
+   """Tests the Parse function."""
+   knowledge_base_values = {u'year': 2012}
+   test_file = self._GetTestFilePath([u'syslog'])
+   event_queue_consumer = self._ParseFile(
+       self._parser, test_file, knowledge_base_values=knowledge_base_values)
+   event_objects = self._GetEventObjectsFromQueue(event_queue_consumer)
 
-    self.assertEqual(len(event_objects), 13)
+   for event_object in event_objects:
+     print event_object.body
+
+   self.assertEqual(len(event_objects), 11)
+
+   event_timestamp = timelib.Timestamp.CopyToIsoFormat(
+      event_objects[0].timestamp)
+   self.assertEqual(event_timestamp, u'2012-01-22T07:52:33+00:00')
+   self.assertEqual(event_objects[0].hostname, u'myhostname.myhost.com')
+
+   expected_string = (
+       u'[client, pid: 30840] INFO No new content.')
+   self._TestGetMessageStrings(
+       event_objects[0], expected_string, expected_string)
+
+   event_timestamp = timelib.Timestamp.CopyToIsoFormat(
+     event_objects[6].timestamp)
+   self.assertEqual(event_timestamp, u'2012-02-29T01:15:43+00:00')
+
+   # Testing year increment.
+   event_timestamp = timelib.Timestamp.CopyToIsoFormat(
+     event_objects[8].timestamp)
+   self.assertEqual(event_timestamp, u'2013-03-23T23:01:18+00:00')
+
+   expected_msg = (
+       u'[aprocess, pid: 101001] This is a multi-line message that screws up'
+       u'many syslog parsers.')
+   expected_msg_short = (
+       u'[aprocess, pid: 101001] This is a multi-line message that screws up'
+       u'many sys...')
+   self._TestGetMessageStrings(
+       event_objects[11], expected_msg, expected_msg_short)
 
 if __name__ == '__main__':
   unittest.main()
