56d8c6cc9428836702dbc5d2d6f36aefd36c00d0
DC3 <plaso.dev@dc3.mil>
2016-05-11 10:15:48 -0400

    Add winlogon winreg plugin.

---
+++ plaso/parsers/winreg_plugins/winlogon.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""This file contains the Winlogon registry plugin."""
+
+from plaso.containers import windows_events
+from plaso.parsers import winreg
+from plaso.parsers.winreg_plugins import interface
+
+
+class WinlogonPlugin(interface.WindowsRegistryPlugin):
+  """Windows Registry plugin for parsing the Winlogon key."""
+
+  NAME = u'winlogon'
+  DESCRIPTION = u'Parser for winlogon Registry data.'
+
+  FILTERS = frozenset([
+      interface.WindowsRegistryKeyPathFilter(
+          u'HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion'
+          u'\\Winlogon')])
+
+  _TRIGGERS = [
+      u'Lock', u'Logoff', u'Logon', u'Shutdown', u'SmartCardLogonNotify',
+      u'StartScreenSaver', u'StartShell', u'Startup', u'StopScreenSaver',
+      u'Unlock']
+
+  _LOGON_APPLICATIONS = [
+      u'Shell', u'Userinit', u'AppSetup', u'GinaDLL', u'System', u'VmApplet',
+      u'taskman', u'UIHost']
+
+  def GetEntries(self, parser_mediator, registry_key, **kwargs):
+    """Retrieves information from the Winlogon registry key.
+
+    Args:
+        parser_mediator: A parser mediator object (instance of ParserMediator).
+        registry_key: A Windows Registry key (instance of
+                      dfwinreg.WinRegistryKey).
+    """
+    # Get logon applications
+    for application in self._LOGON_APPLICATIONS:
+      command = registry_key.GetValueByName(application)
+      if command:
+        values_dict = {
+            u'Application': application,
+            u'Command': command.GetDataAsObject(),
+            u'Trigger': u'Logon'}
+        event_object = windows_events.WindowsRegistryEvent(
+            registry_key.last_written_time, registry_key.path, values_dict,
+            offset=registry_key.offset, source_append=u': Winlogon')
+        parser_mediator.ProduceEvent(event_object)
+
+    # Get registered DLLs that receive event notifications
+    key = registry_key.GetSubkeyByName(u'Notify')
+    if key:
+      for subkey in key.GetSubkeys():
+        for trigger in self._TRIGGERS:
+          handler = subkey.GetValueByName(trigger)
+          if handler:
+            values_dict = {
+                u'Application': subkey.name,
+                u'Handler': handler.GetDataAsObject(),
+                u'Trigger': trigger}
+
+            command = subkey.GetValueByName(u'DllName')
+            if command:
+              values_dict[u'command'] = command.GetDataAsObject()
+
+            event_object = windows_events.WindowsRegistryEvent(
+                subkey.last_written_time, subkey.path, values_dict,
+                offset=subkey.offset, source_append=u': Winlogon')
+            parser_mediator.ProduceEvent(event_object)
+
+
+winreg.WinRegistryParser.RegisterPlugin(WinlogonPlugin)
