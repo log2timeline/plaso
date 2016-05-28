69c191951d9f8509dc383cfa11ec87b98b6cd593
Jesus Aguilar <jesus@jaguilar.me>
2016-05-28 02:10:24 -0400

    .

---
+++ plaso/formatters/twitter_ios.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""Twitter on iOS 8+ database formatter."""
+
+from plaso.formatters import interface
+from plaso.formatters import manager
+from plaso.lib import errors
+
+class TwitterIOSContactFormatter(interface.ConditionalEventFormatter):
+  """Parent class for Twitter on iOS 8+ contacts formatters."""
+
+  FORMAT_STRING_PIECES = [
+    u'Screen name: {screen_name}',
+    u'Profile picture Url: {profile_url}',
+    u'Name: {name}',
+    u'Location: {location}',
+    u'Description: {description}',
+    u'Url: {url}',
+    u'Following: {following}',
+    u'Number of followers: {followers_cnt}',
+    u'Number of following: {following_cnt}',
+  ]
+
+  FORMAT_STRING_SHORT_PIECES = [
+    u'Screen name: {screen_name}',
+    u'Description: {description}',
+    u'Url: {url}',
+  ]
+
+  SOURCE_LONG = u'Twitter iOS Contacts'
+  SOURCE_SHORT = u'Twitter iOS'
+
+  _FOLLOWING = {
+    0: u'No',
+    1: u'Yes',
+  }
+
+  def GetMessages(self, unused_formatter_mediator, event_object):
+    """Determines the formatted message strings for an event object.
+    Args:
+      formatter_mediator: the formatter mediator object (instance of
+                          FormatterMediator).
+      event_object: the event object (instance of EventObject).
+    Returns:
+      A tuple containing the formatted message string and short message string.
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+
+    if self.DATA_TYPE != event_object.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event_object.data_type))
+
+    event_values = event_object.CopyToDict()
+
+    following = event_values.get(u'following', None)
+    if following is not None:
+      event_values[u'following'] = (
+          self._FOLLOWING.get(following, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
+
+
+class TwitterIOSContactCreationFormatter(TwitterIOSContactFormatter):
+  """Formatter for Twitter on iOS 8+ contacts creation event."""
+  DATA_TYPE = u'twitter:ios:contact_creation'
+
+
+class TwitterIOSContactUpdateFormatter(TwitterIOSContactFormatter):
+  """Formatter for Twitter on iOS 8+ contacts update event."""
+  DATA_TYPE = u'twitter:ios:contact_update'
+
+
+class TwitterIOSStatusFormatter(interface.ConditionalEventFormatter):
+  """Parent class for Twitter on iOS 8+ status formatters."""
+
+  FORMAT_STRING_PIECES = [
+    u'Name: {name}',
+    u'User Id: {user_id}',
+    u'Message: {text}',
+    u'Favorite: {favorited}',
+    u'Retweet Count: {retweet_cnt}',
+    u'Favorite Count: {favorite_cnt}',
+  ]
+
+  FORMAT_STRING_SHORT_PIECES = [
+    u'Name: {name}',
+    u'Message: {text}',
+  ]
+
+  SOURCE_LONG = u'Twitter iOS Status'
+  SOURCE_SHORT = u'Twitter iOS'
+
+  _FAVORITED = {
+    0: u'No',
+    1: u'Yes',
+  }
+
+  def GetMessages(self, unused_formatter_mediator, event_object):
+    """Determines the formatted message strings for an event object.
+    Args:
+      formatter_mediator: the formatter mediator object (instance of
+                          FormatterMediator).
+      event_object: the event object (instance of EventObject).
+    Returns:
+      A tuple containing the formatted message string and short message string.
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+
+    if self.DATA_TYPE != event_object.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event_object.data_type))
+
+    event_values = event_object.CopyToDict()
+
+    favorited = event_values.get(u'favorited', None)
+    if favorited is not None:
+      event_values[u'favorited'] = (
+          self._FAVORITED.get(favorited, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
+
+
+class TwitterIOSStatusCreationFormatter(TwitterIOSStatusFormatter):
+  """Formatter for Twitter on iOS 8+ status creation event."""
+  DATA_TYPE = u'twitter:ios:status_creation'
+
+
+class TwitterIOSStatusUpdateFormatter(TwitterIOSStatusFormatter):
+  """Formatter for Twitter on iOS 8+ status update event."""
+  DATA_TYPE = u'twitter:ios:status_update'
+
+
+manager.FormattersManager.RegisterFormatters([
+    TwitterIOSContactCreationFormatter, TwitterIOSContactUpdateFormatter,
+    TwitterIOSStatusCreationFormatter, TwitterIOSStatusUpdateFormatter])
---
+++ plaso/parsers/sqlite_plugins/twitter_ios.py
@@ -0,0 +1,189 @@
+# -*- coding:utf-8 -*-
+"""Parser for Twitter on iOS 8+ database.
+
+   SQLite database path:
+   /private/var/mobile/Containers/Data/Application/Library/Caches/databases/
+   SQLite database name:
+   twitter.db
+"""
+
+from plaso.containers import time_events
+from plaso.lib import eventdata
+from plaso.parsers import sqlite
+from plaso.parsers.sqlite_plugins import interface
+
+class TwitterIOSContactEvent(time_events.PosixTimeEvent):
+  """Parent class for Twitter on iOS 8+ contacts events.
+
+  Attributes:
+    screen_name: Contact's screen name.
+    name: Contact's profile name.
+    profile_url: Contact's  profile picture url.
+    location: Contact's profile location.
+    url: Contact's profile url.
+    description: Contact's profile description.
+    following: Variable to describe if the contact is following the account.
+    following_cnt: The number of following.
+    followers_cnt: The number of followers.
+  """
+
+  def __init__(
+    self, posix_time, posix_time_description, screen_name, name, profile_url,
+    location, url, description, following, following_cnt, followers_cnt):
+    """Initalizes a TwitterIOSContacts event.
+
+    Args:
+      posix_time: The POSIX time value.
+      posix_time_description: The description of the usage of the timestamp.
+      screen_name: Contact's screen name.
+      name: Contact's profile name.
+      profile_url: Contact's  profile picture url.
+      location: Contact's profile location.
+      url: Contact's profile url.
+      description: Contact's profile description.
+      following: Variable to describe if the contact is following the account.
+      following_cnt: The number of following.
+      followers_cnt: The number of followers.
+    """
+    super(TwitterIOSContactEvent, self).__init__(
+      posix_time, posix_time_description)
+    self.screen_name = screen_name
+    self.name = name
+    self.profile_url = profile_url
+    self.location = location
+    self.url = url
+    self.description = description
+    self.following = following
+    self.following_cnt = following_cnt
+    self.followers_cnt = followers_cnt
+
+
+class TwitterIOSContactCreationEvent(TwitterIOSContactEvent):
+  """Convenience class for Twitter contacts creation events."""
+  DATA_TYPE = u'twitter:ios:contact_creation'
+
+
+class TwitterIOSContactUpdateEvent(TwitterIOSContactEvent):
+  """Convenience class for Twitter contacts update events."""
+  DATA_TYPE = u'twitter:ios:contact_update'
+
+
+class TwitterIOSStatusEvent(time_events.PosixTimeEvent):
+  """Parent class for Twitter on iOS 8+ status events.
+
+  Attributes:
+    posix_time: The POSIX time value.
+    text: The content of the status messsage.
+    user_id: User unique identifier.
+    name: User's profile name.
+    retweet_cnt: Number of times the status has been retweeted. 
+    favorite_cnt: Number of times the status has been favorited.
+    favorited: Variable to mark status as favorite by the account.
+  """
+
+  def __init__(
+    self, posix_time, posix_time_description, text, user_id, name, retweet_cnt,
+    favorite_cnt, favorited):
+    """Initalizes a TwitterIOSStatuses event.
+
+    Args:
+      posix_time: The POSIX time value.
+      posix_time_description: The description of the usage of the timestamp.
+      text: The content of the status messsage.
+      user_id: User unique identifier.
+      name: User's profile name.
+      retweet_cnt: Number of times the status has been retweeted. 
+      favorite_cnt: Number of times the status has been favorited.
+      favorited: Variable to mark status as favorite by the account.
+    """
+    super(TwitterIOSStatusEvent, self).__init__(
+      posix_time, posix_time_description)
+    self.text = text
+    self.user_id = user_id
+    self.name = name
+    self.retweet_cnt = retweet_cnt
+    self.favorite_cnt = favorite_cnt
+    self.favorited = favorited
+
+
+class TwitterIOSStatusCreationEvent(TwitterIOSStatusEvent):
+  """Convenience class for Twitter status creation events."""
+  DATA_TYPE = u'twitter:ios:status_creation'
+
+
+class TwitterIOSStatusUpdateEvent(TwitterIOSStatusEvent):
+  """Convenience class for Twitter status update events."""
+  DATA_TYPE = u'twitter:ios:status_update'
+
+
+class TwitterIOSPlugin(interface.SQLitePlugin):
+  """Parser for Twitter on iOS 8+ database."""
+
+  # twitter_ios or just twitter?
+  NAME = u'twitter_ios'
+  DESCRIPTION = u'Parser for Twitter on iOS 8+ database'
+
+  # frozenset?
+  QUERIES = [
+    ((u'SELECT createdDate, updatedAt, screenName, name, profileImageUrl,'
+      u'location, description, url, following, followersCount, followingCount '
+      u'FROM Users ORDER BY createdDate'), u'ParseContactRow'),
+    ((u'SELECT Statuses.date AS date, Statuses.text AS text, Statuses.userId '
+      u'AS user_id, Users.name AS name, Statuses.retweetCount AS retweetCount, '
+      u'Statuses.favoriteCount AS favoriteCount, Statuses.favorited AS '
+      u'favorited, Statuses.updatedAt AS updatedAt FROM Statuses LEFT join '
+      u'Users ON Statuses.userId = Users.id ORDER BY date'), u'ParseStatusRow')]
+
+  REQUIRED_TABLES = frozenset([u'Lists', u'MyRetweets', u'StatusesShadow',
+    u'UsersShadow', u'ListsShadow', u'Statuses', u'Users'])
+
+  def ParseContactRow(self, parser_mediator, row, query=None, **unused_kwargs):
+    """Parses a contact row from the database.
+    
+    Args:
+      parser_mediator: A parser mediator object (instance of ParserMediator).
+      row: The row resulting from the query.
+      query: Optional query string.
+    """
+
+    if row['createdDate'] is not None:
+      event_object = TwitterIOSContactCreationEvent(
+        row['createdDate'], eventdata.EventTimestamp.CREATION_TIME,
+        row['screenName'], row['name'], row['profileImageUrl'], row['location'],
+        row['url'], row['description'], row['following'], row['followingCount'],
+        row['followersCount'])
+      parser_mediator.ProduceEvent(event_object, query=query) 
+
+    if row['updatedAt'] is not None:
+      event_object = TwitterIOSContactUpdateEvent(
+        row['updatedAt'], eventdata.EventTimestamp.UPDATE_TIME,
+        row['screenName'], row['name'], row['profileImageUrl'], row['location'],
+        row['url'], row['description'], row['following'], row['followingCount'],
+        row['followersCount'])
+      parser_mediator.ProduceEvent(event_object, query=query) 
+
+  def ParseStatusRow(self, parser_mediator, row, query=None, **unused_kwargs):
+    """Parses a contact row from the database.
+    
+    Args:
+      parser_mediator: A parser mediator object (instance of ParserMediator).
+      row: The row resulting from the query.
+      query: Optional query string.
+    """
+
+    if row['date'] is not None:
+      event_object = TwitterIOSStatusCreationEvent(
+        row['date'], eventdata.EventTimestamp.CREATION_TIME, row['text'],
+        row['user_id'], row['name'], row['retweetCount'], row['favoriteCount'],
+        row['favorited'])
+      parser_mediator.ProduceEvent(event_object, query=query)
+
+    if row['updatedAt'] is not None:
+      event_object = TwitterIOSStatusUpdateEvent(
+        row['updatedAt'], eventdata.EventTimestamp.UPDATE_TIME, row['text'],
+        row['user_id'], row['name'], row['retweetCount'], row['favoriteCount'],
+        row['favorited'])
+      parser_mediator.ProduceEvent(event_object, query=query)
+
+
+sqlite.SQLiteParser.RegisterPlugin(TwitterIOSPlugin)
