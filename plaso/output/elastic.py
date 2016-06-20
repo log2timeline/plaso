0c3d5e1c87b6d1dfd5a4ce91bc0303a3664e7a54
Johan Berggren <jberggren@gmail.com>
2016-06-20 12:47:43 +0000

    new elastic output module

--- plaso/output/elastic.py
+++ plaso/output/elastic.py
@@ -1,27 +1,129 @@
 # -*- coding: utf-8 -*-
-"""An output module that saves data into an ElasticSearch database."""
+"""An output module that saves events to Elasticsearch."""
 
+from collections import Counter
 import logging
-import sys
-import uuid
 
 try:
-  import requests
-  import pyelasticsearch
+  from elasticsearch import Elasticsearch
+  from elasticsearch.exceptions import NotFoundError
+  from elasticsearch.exceptions import ConnectionError
 except ImportError:
-  pyelasticsearch = None
+  Elasticsearch = None
 
 from plaso.lib import errors
 from plaso.lib import timelib
 from plaso.output import interface
 from plaso.output import manager
 
+# Configure Elasticsearch logger
+elastic_logger = logging.getLogger(u'elasticsearch.trace')
+elastic_logger.setLevel(logging.WARNING)
+
+
+class ElasticSearchHelper(object):
+  def __init__(
+      self, output_mediator, host, port, flush_interval, index_name, mapping,
+      doc_type):
+    """Create a Elasticsearch client."""
+    super(ElasticSearchHelper, self).__init__()
+    self.client = Elasticsearch([{u'host': host, u'port': port}])
+    self._output_mediator = output_mediator
+    self._index = self._GetOrCreateIndex(index_name, mapping)
+    self._doc_type = doc_type
+    self._flush_interval = flush_interval
+    self._events = []
+    self._counter = Counter()
+
+  def AddEvent(self, event_object, force_flush=False):
+    if event_object:
+      self._events.append(
+          {u'index': {u'_index': self._index, u'_type': self._doc_type}})
+      self._events.append(self._GetSanitizedEventValues(event_object))
+      self._counter[u'events'] += 1
+
+    # Check if we need to flush the queued events
+    if force_flush or self._counter[u'events'] % self._flush_interval == 0:
+      self._FlushEventsToElasticSearch()
+
+  def _GetOrCreateIndex(self, index_name, mapping):
+    """Create Elasticsearch index.
+
+    Args:
+      index_name: Name of the index.
+      mapping: Mapping for the index
+    """
+    try:
+      if not self.client.indices.exists(index_name):
+        self.client.indices.create(
+            index=index_name, body={u'mappings': mapping})
+    except ConnectionError:
+      raise RuntimeError(u'Unable to connect to Elasticsearch backend.')
+    return index_name
+
+  def _GetSanitizedEventValues(self, event_obj):
+    """Builds a dictionary from an event_object.
+
+    The values from the event object need to be sanitized to prevent
+    values causing problems when indexing them with Elasticsearch.
+
+    Args:
+      event_obj: the event object (instance of EventObject).
+
+    Returns:
+      Dictionary with sanitized event object values.
+    """
+    event_values = {}
+    for attribute_name, attribute_value in event_obj.GetAttributes():
+      # Ignore certain attributes that cause issues when indexing.
+      if attribute_name in (u'pathspec', u'regvalue'):
+        continue
+      event_values[attribute_name] = attribute_value
+
+    # Add string representation of the timestamp
+    attribute_value = timelib.Timestamp.RoundToSeconds(event_obj.timestamp)
+    attribute_value = timelib.Timestamp.CopyToIsoFormat(
+        attribute_value, timezone=self._output_mediator.timezone)
+    event_values[u'datetime'] = attribute_value
+
+    message, _ = self._output_mediator.GetFormattedMessages(event_obj)
+    if message is None:
+      raise errors.NoFormatterFound(
+          u'Unable to find event formatter for: {0:s}.'.format(
+              getattr(event_obj, u'data_type', u'UNKNOWN')))
+    event_values[u'message'] = message
+
+    # Tags needs to be a list for Elasticsearch to index correctly.
+    try:
+      labels = list(event_values[u'tag'].labels)
+    except (KeyError, AttributeError):
+      labels = []
+    event_values[u'tag'] = labels
+
+    source_short, source = self._output_mediator.GetFormattedSources(event_obj)
+    if source is None or source_short is None:
+      raise errors.NoFormatterFound(
+          u'Unable to find event formatter for: {0:s}.'.format(
+              getattr(event_obj, u'data_type', u'UNKNOWN')))
+    event_values[u'source_short'] = source_short
+    event_values[u'source_long'] = source
+
+    return event_values
+
+  def _FlushEventsToElasticSearch(self):
+    """Insert events in bulk to Elasticsearch."""
+    self.client.bulk(
+        index=self._index, doc_type=self._doc_type, body=self._events)
+    # Clear the events list
+    self._events = []
+    logging.info(u'{0:d} events added'.format(self._counter[u'events']))
+
 
 class ElasticSearchOutputModule(interface.OutputModule):
-  """Saves the events into an ElasticSearch database."""
+  """Output module for Elasticsearch."""
 
   NAME = u'elastic'
-  DESCRIPTION = u'Saves the events into an ElasticSearch database.'
+  DESCRIPTION = u'Saves the events into an Elasticsearch database.'
 
   def __init__(self, output_mediator):
     """Initializes the output module object.
@@ -30,101 +132,56 @@ class ElasticSearchOutputModule(interface.OutputModule):
       output_mediator: The output mediator object (instance of OutputMediator).
     """
     super(ElasticSearchOutputModule, self).__init__(output_mediator)
-    self._counter = 0
-    self._data = []
-    self._doc_type = None
-    self._elastic_db = None
+
+    self._output_mediator = output_mediator
+    self._host = None
+    self._port = None
+    self._flush_interval = None
     self._index_name = None
-
-  def _EventToDict(self, event_object):
-    """Returns a dict built from an event object.
-
-    Args:
-      event_object: the event object (instance of EventObject).
-    """
-    event_values = {}
-    for attribute_name, attribute_value in event_object.GetAttributes():
-      # Ignore attributes that cause issues (and need correcting).
-      if attribute_name in (u'pathspec', u'regvalue', u'tag'):
-        continue
-
-      event_values[attribute_name] = attribute_value
-
-    # Adding attributes in that are derived.
-
-    # We want to remove millisecond precision (causes some issues in
-    # conversion).
-    attribute_value = timelib.Timestamp.RoundToSeconds(event_object.timestamp)
-    attribute_value = timelib.Timestamp.CopyToIsoFormat(
-        attribute_value, timezone=self._output_mediator.timezone)
-    event_values[u'datetime'] = attribute_value
-
-    message, _ = self._output_mediator.GetFormattedMessages(event_object)
-    if message is None:
-      raise errors.NoFormatterFound(
-          u'Unable to find event formatter for: {0:s}.'.format(
-              getattr(event_object, u'data_type', u'UNKNOWN')))
-
-    event_values[u'message'] = message
-
-    source_short, source = self._output_mediator.GetFormattedSources(
-        event_object)
-    if source is None or source_short is None:
-      raise errors.NoFormatterFound(
-          u'Unable to find event formatter for: {0:s}.'.format(
-              getattr(event_object, u'data_type', u'UNKNOWN')))
-
-    event_values[u'source_short'] = source_short
-    event_values[u'source_long'] = source
-
-    hostname = self._output_mediator.GetHostname(event_object)
-    event_values[u'hostname'] = hostname
-
-    username = self._output_mediator.GetUsername(event_object)
-    event_values[u'username'] = username
-
-    return event_values
+    self._doc_type = None
+    self._mapping = None
+    self._elastic = None
 
   def Close(self):
-    """Disconnects from the elastic search server."""
-    self._elastic_db.bulk_index(self._index_name, self._doc_type, self._data)
-    self._data = []
-    sys.stdout.write(u'. [DONE]\n')
-    sys.stdout.write(u'ElasticSearch index name: {0:s}\n'.format(
-        self._index_name))
-    sys.stdout.flush()
+    """Close connection to the Elasticsearch database.
 
-  def SetCaseName(self, case_name):
-    """Set the case name for the ElasticSearch database connection.
+    Sends the remaining events for indexing and removes the processing status on
+    the Elasticsearch search index object.
+    """
+    self._elastic.AddEvent(event_object=None, force_flush=True)
+
+  def SetServerInformation(self, server, port):
+    self._host = server
+    self._port = port
+    logging.info(u'Server address: {0:s}'.format(self._host))
+    logging.info(u'Server port: {0:d}'.format(self._port))
+
+  def SetFlushInterval(self, flush_interval):
+    """Set the flush interval.
 
     Args:
-      case_name: the case name, used for the name of the index in the database.
+      flush_interval: the flush interval.
     """
-    if case_name:
-      self._index_name = case_name.lower()
-    else:
-      self._index_name = uuid.uuid4().hex
+    self._flush_interval = flush_interval
+    logging.info(u'Flush interval: {0:d}'.format(self._flush_interval))
 
-  def SetDocumentType(self, document_type):
-    """Set the document type for the ElasticSearch database connection.
+  def SetIndexName(self, index_name):
+    """Set the index name.
 
     Args:
-      document_type: the document type for the ElasticSearch database.
+      index_name: the index name.
     """
-    if document_type:
-      self._doc_type = document_type.lower()
-    else:
-      self._doc_type = u'event'
+    self._index_name = index_name
+    logging.info(u'Index name: {0:s}'.format(self._index_name))
 
-  def SetServerInformation(self, elastic_host, elastic_port):
-    """Set the ElasticSearch connection.
+  def SetDocType(self, doc_type):
+    """Set the port.
 
     Args:
-      elastic_host: the hostname or IP address of the ElasticSearch server.
-      elastic_port: the port number that the ElasticSearch is listening on.
+      doc_type: The port to the Elasticsearch server.
     """
-    self._elastic_db = pyelasticsearch.ElasticSearch(
-        u'http://{0:s}:{1:d}'.format(elastic_host, elastic_port))
+    self._doc_type = doc_type
+    logging.info(u'Doc type: {0:s}'.format(self._doc_type))
 
   def WriteEventBody(self, event_object):
     """Writes the body of an event object to the output.
@@ -132,52 +189,18 @@ class ElasticSearchOutputModule(interface.OutputModule):
     Args:
       event_object: the event object (instance of EventObject).
     """
-    self._data.append(self._EventToDict(event_object))
-    self._counter += 1
-
-    # Check if we need to flush.
-    if self._counter % 5000 == 0:
-      self._elastic_db.bulk_index(self._index_name, self._doc_type, self._data)
-      self._data = []
-      sys.stdout.write('.')
-      sys.stdout.flush()
+    self._elastic.AddEvent(event_object)
 
   def WriteHeader(self):
-    """Writes the header to the output."""
-    mapping = {
-        self._doc_type: {
-            u'_timestamp': {
-                u'enabled': True,
-                u'path': u'datetime',
-                u'format': u'date_time_no_millis'},
-        }
-    }
-    # Check if the mappings exist (only create if not there).
-    try:
-      old_mapping_index = self._elastic_db.get_mapping(self._index_name)
-      old_mapping = old_mapping_index.get(self._index_name, {})
-      if self._doc_type not in old_mapping:
-        self._elastic_db.put_mapping(
-            self._index_name, self._doc_type, mapping=mapping)
-    except (pyelasticsearch.ElasticHttpNotFoundError,
-            pyelasticsearch.exceptions.ElasticHttpError):
-      try:
-        self._elastic_db.create_index(self._index_name, settings={
-            u'mappings': mapping})
-      except pyelasticsearch.IndexAlreadyExistsError:
-        raise RuntimeError(u'Unable to created the index')
-    except requests.exceptions.ConnectionError as exception:
-      logging.error(
-          u'Unable to proceed, cannot connect to ElasticSearch backend '
-          u'with error: {0:s}.\nPlease verify connection.'.format(exception))
-      raise RuntimeError(u'Unable to connect to ElasticSearch backend.')
+    """Setup the Elasticsearch index."""
+    if not self._mapping:
+      self._mapping = {}
 
-    # pylint: disable=unexpected-keyword-arg
-    self._elastic_db.health(wait_for_status=u'yellow')
-
-    sys.stdout.write(u'Inserting data')
-    sys.stdout.flush()
+    self._elastic = ElasticSearchHelper(
+        self._output_mediator, self._host, self._port, self._flush_interval,
+        self._index_name, self._mapping, self._doc_type)
+    logging.info(u'Adding events to Elasticsearch..')
 
 
 manager.OutputManager.RegisterOutput(
-    ElasticSearchOutputModule, disabled=pyelasticsearch is None)
+    ElasticSearchOutputModule, disabled=Elasticsearch is None)
