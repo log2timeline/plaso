487d043a8a449a191065876c9b230dcfabbb27e1
Joachim Metz <joachim.metz@gmail.com>
2016-07-07 06:58:10 +0200

    Moved parsers preset and added more tests.

--- tests/winnt/environ_expand.py
+++ tests/winnt/environ_expand.py
@@ -16,6 +16,10 @@ class ExpandWindowsEnvironmentVariablesTest(unittest.TestCase):
     self.assertEqual(expanded_path, u'C:\\Windows\\System32')
 
     expanded_path = environ_expand.ExpandWindowsEnvironmentVariables(
+        u'%SystemRoot%\\System32', None)
+    self.assertEqual(expanded_path, u'%SystemRoot%\\System32')
+
+    expanded_path = environ_expand.ExpandWindowsEnvironmentVariables(
         u'%Bogus%\\System32', {u'SystemRoot': u'C:\\Windows'})
     self.assertEqual(expanded_path, u'%Bogus%\\System32')
 
