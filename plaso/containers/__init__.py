8e48009f308b0332897772c3139856028401ae86
Joachim Metz <joachim.metz@gmail.com>
2016-07-12 07:08:12 +0200

    Changes for linter.

--- plaso/containers/__init__.py
+++ plaso/containers/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """This file contains an import statement for each attribute container."""
 
-from plaso.containers import artifacts
 from plaso.containers import errors
 from plaso.containers import event_sources
 from plaso.containers import events
