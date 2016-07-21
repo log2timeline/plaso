0e4ee4987e71eb58bf4c8dd4fe61754fa9ad7364
Daniel White <onager@deerpie.com>
2016-07-21 17:09:15 +0200

    Ready for final testing.

--- tests/analysis/nsrlsvr.py
+++ tests/analysis/nsrlsvr.py
@@ -14,14 +14,17 @@ from plaso.parsers import pe
 
 from tests.analysis import test_lib
 
-class MockNsrlsvrSocket(object):
+
+class _MockNsrlsvrSocket(object):
   """Mock socket object for testing."""
 
   def __init__(self):
-    super(MockNsrlsvrSocket, self).__init__()
+    """Initializes a mock socket."""
+    super(_MockNsrlsvrSocket, self).__init__()
     self._data = None
 
   def recv(self, unused_buffer_size):
+    """Mocks the socket.recv method."""
     if self._data == u'QUERY {0:s}'.format(NsrlSvrTest.EVENT_1_HASH):
       self._data = None
       return u'OK 1'
@@ -30,39 +33,28 @@ class MockNsrlsvrSocket(object):
       return u'OK 0'
 
   def sendall(self, data):
+    """Mocks the socket.sendall method"""
     self._data = data
 
+
 class NsrlSvrTest(test_lib.AnalysisPluginTestCase):
-  """Tests for the Nsrlsvr analysis plugin."""
+  """Tests for the nsrlsvr analysis plugin."""
   EVENT_1_HASH = (
-      u'2d79fcc6b02a2e183a0cb30e0e25d103f42badda9fbf86bbee06f93aa3855aff')
-  TEST_EVENTS = [{
-      u'timestamp': timelib.Timestamp.CopyFromString(u'2015-01-01 17:00:00'),
-      u'sha256_hash': EVENT_1_HASH,
-      u'uuid': u'8'}]
+    u'2d79fcc6b02a2e183a0cb30e0e25d103f42badda9fbf86bbee06f93aa3855aff')
+  EVENT_2_HASH = (
+    u'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+  TEST_EVENTS = [
+    {u'timestamp': timelib.Timestamp.CopyFromString(u'2015-01-01 17:00:00'),
+      u'sha256_hash': EVENT_1_HASH, u'uuid': u'8'},
+    {u'timestamp': timelib.Timestamp.CopyFromString(u'2016-01-01 17:00:00'),
+      u'sha256_hash': EVENT_2_HASH, u'uuid': u'9'}]
 
-
-  def setUp(self):
-    """Makes preparations before running an individual test."""
-    self.requests_patcher = mock.patch(
-        'socket.create_connection', self._MockCreateConnection)
-    self.requests_patcher.start()
-
-
-  def tearDown(self):
-    """Cleans up after running an individual test."""
-    self.requests_patcher.stop()
-
-  def _MockCreateConnection(self, unused_connection_information):
-    """Mocks the socket create_connection call"""
-    return MockNsrlsvrSocket()
-
-  def _CreateTestEventObject(self, pe_event):
-    """Create a test event object with a particular path.
+  def _CreateTestEventObject(self, event_dictionary):
+    """Create a test event with a set of attributes.
 
     Args:
-      pe_event: A hash containing attributes of an event to add to the
-                     queue.
+      event_dictionary (dict[str, str]: contains attributes of an event to add
+          to the queue.
 
     Returns:
       An event object (instance of EventObject) that contains the necessary
@@ -70,43 +62,60 @@ class NsrlSvrTest(test_lib.AnalysisPluginTestCase):
     """
     test_pathspec = fake_path_spec.FakePathSpec(
         location=u'C:\\WINDOWS\\system32\\evil.exe')
-    event_object = pe.PECompilationEvent(
-        pe_event[u'timestamp'], u'Executable (EXE)', [], '')
+    event_object = pe.PECompilationEvent(event_dictionary[u'timestamp'],
+        u'Executable (EXE)', [], '')
     event_object.pathspec = test_pathspec
-    event_object.sha256_hash = pe_event[u'sha256_hash']
-    event_object.uuid = pe_event[u'uuid']
+    event_object.sha256_hash = event_dictionary[u'sha256_hash']
+    event_object.uuid = event_dictionary[u'uuid']
     return event_object
 
+  def _MockCreateConnection(self, unused_connection_information):
+    """Mocks the socket create_connection call"""
+    return _MockNsrlsvrSocket()
+
+  def setUp(self):
+    """Makes preparations before running an individual test."""
+    self.requests_patcher = mock.patch('socket.create_connection',
+        self._MockCreateConnection)
+    self.requests_patcher.start()
+
+  def tearDown(self):
+    """Cleans up after running an individual test."""
+    self.requests_patcher.stop()
 
   def testNsrlsvrLookup(self):
     """Tests for the nsrlsvr analysis plugin."""
     event_queue = single_process.SingleProcessQueue()
     knowledge_base = self._SetUpKnowledgeBase()
-  
+
     # Fill the incoming queue with events.
     test_queue_producer = plaso_queue.ItemQueueProducer(event_queue)
     events = [self._CreateTestEventObject(test_event) for test_event in
-              self.TEST_EVENTS]
+      self.TEST_EVENTS]
     test_queue_producer.ProduceItems(events)
-  
+
     # Set up the plugin.
     analysis_plugin = nsrlsvr.NsrlsvrAnalysisPlugin(event_queue)
     analysis_plugin.SetHost(u'127.0.0.1')
     analysis_plugin.SetPort(u'9120')
-  
+
     # Run the analysis plugin.
     analysis_report_queue_consumer = self._RunAnalysisPlugin(analysis_plugin,
-      knowledge_base)
+        knowledge_base)
     analysis_reports = self._GetAnalysisReportsFromQueue(
-      analysis_report_queue_consumer)
-  
+        analysis_report_queue_consumer)
+
     self.assertEqual(len(analysis_reports), 1)
     report = analysis_reports[0]
     tags = report.GetTags()
-    self.assertEqual(len(tags), 1)
+    self.assertEqual(len(tags), 2)
     tag = tags[0]
     self.assertEqual(tag.event_uuid, u'8')
     expected_labels = [u'nsrl_present']
+    self.assertEqual(tag.labels, expected_labels)
+    tag = tags[1]
+    self.assertEqual(tag.event_uuid, u'9')
+    expected_labels = [u'nsrl_not_present']
     self.assertEqual(tag.labels, expected_labels)
 
 
