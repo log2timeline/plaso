3ba51735d5b5cfdf626143d7aace0d5c2938ec4a
Joachim Metz <joachim.metz@gmail.com>
2016-07-22 08:36:26 +0200

    Moved AnalyzeFileObject from analyzer manager to worker #843

--- tests/engine/worker.py
+++ tests/engine/worker.py
@@ -7,6 +7,7 @@ import unittest
 from dfvfs.lib import definitions as dfvfs_definitions
 from dfvfs.path import factory as path_spec_factory
 from dfvfs.resolver import context
+from dfvfs.resolver import resolver as path_spec_resolver
 
 from plaso.containers import sessions
 from plaso.engine import knowledge_base
@@ -14,6 +15,7 @@ from plaso.engine import worker
 from plaso.parsers import mediator as parsers_mediator
 from plaso.storage import fake_storage
 
+from tests.analyzers import manager as analyzers_manager_test
 from tests import test_lib as shared_test_lib
 
 
@@ -21,6 +23,21 @@ class EventExtractionWorkerTest(shared_test_lib.BaseTestCase):
   """Tests for the worker object."""
 
   # pylint: disable=protected-access
+
+  def _GetTestFileEntry(self, path_segments):
+    """Creates a file entry that references a file in the test data directory.
+
+    Args:
+      path_segments (list[str]): components of a path to a test file, relative
+          to the test_data directory.
+
+    Returns:
+      dfvfs.FileEntry: file entry.
+    """
+    path = self._GetTestFilePath(path_segments)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_OS, location=path)
+    return path_spec_resolver.Resolver.OpenFileEntry(path_spec)
 
   def _GetTestFilePathSpec(self, path_segments):
     """Retrieves a path specification of a test file in the test data directory.
@@ -50,7 +67,7 @@ class EventExtractionWorkerTest(shared_test_lib.BaseTestCase):
           processed.
     """
     knowledge_base_object = knowledge_base.KnowledgeBase()
-    parser_mediator = parsers_mediator.ParserMediator(
+    mediator = parsers_mediator.ParserMediator(
         storage_writer, knowledge_base_object)
 
     if not extraction_worker:
@@ -62,14 +79,45 @@ class EventExtractionWorkerTest(shared_test_lib.BaseTestCase):
     storage_writer.Open()
     storage_writer.WriteSessionStart()
 
-    extraction_worker.ProcessPathSpec(parser_mediator, path_spec)
+    extraction_worker.ProcessPathSpec(mediator, path_spec)
     event_source = storage_writer.GetFirstWrittenEventSource()
     while event_source:
-      extraction_worker.ProcessPathSpec(parser_mediator, event_source.path_spec)
+      extraction_worker.ProcessPathSpec(mediator, event_source.path_spec)
       event_source = storage_writer.GetNextWrittenEventSource()
 
     storage_writer.WriteSessionCompletion()
     storage_writer.Close()
+
+  def testAnalyzeFileObject(self):
+    """Tests the _AnalyzeFileObject function."""
+    session = sessions.Session()
+    storage_writer = fake_storage.FakeStorageWriter(session)
+    knowledge_base_object = knowledge_base.KnowledgeBase()
+    mediator = parsers_mediator.ParserMediator(
+        storage_writer, knowledge_base_object)
+
+    resolver_context = context.Context()
+    extraction_worker = worker.EventExtractionWorker(resolver_context)
+
+    test_analyzer = analyzers_manager_test.TestAnalyzer()
+    self.assertEqual(len(test_analyzer.GetResults()), 0)
+
+    extraction_worker._analyzers = [test_analyzer]
+
+    file_entry = self._GetTestFileEntry([u'ímynd.dd'])
+    mediator.SetFileEntry(file_entry)
+
+    file_object = file_entry.GetFileObject()
+
+    try:
+      extraction_worker._AnalyzeFileObject(mediator, file_object)
+    finally:
+      file_object.close()
+
+    self.assertEqual(len(mediator._extra_event_attributes), 1)
+
+    event_attribute = mediator._extra_event_attributes.get(u'test_result', None)
+    self.assertEqual(event_attribute, u'is_vegetable')
 
   def testProcessPathSpec(self):
     """Tests the ProcessPathSpec function."""
@@ -179,7 +227,7 @@ class EventExtractionWorkerTest(shared_test_lib.BaseTestCase):
       self.assertEqual(getattr(event, u'md5_hash', None), empty_file_md5)
 
   def testExtractionWorkerYara(self):
-    """Test that the worker applies Yara matching code correctly."""
+    """Tests that the worker applies Yara matching code correctly."""
     resolver_context = context.Context()
     extraction_worker = worker.EventExtractionWorker(resolver_context)
 
@@ -191,14 +239,15 @@ class EventExtractionWorkerTest(shared_test_lib.BaseTestCase):
     self.assertIn(u'yara', extraction_worker.GetAnalyzerNames())
 
     session = sessions.Session()
-    yara_match_name = u'PEfile'
     path_spec = self._GetTestFilePathSpec([u'test_pe.exe'])
     storage_writer = fake_storage.FakeStorageWriter(session)
     self._TestProcessPathSpec(
         storage_writer, path_spec, extraction_worker=extraction_worker)
 
+    expected_yara_match = u'PEfile'
     for event in storage_writer.events:
-      self.assertEqual(getattr(event, u'yara_match', None), yara_match_name)
+      yara_match = getattr(event, u'yara_match', None)
+      self.assertEqual(yara_match, expected_yara_match)
 
 
 if __name__ == '__main__':
