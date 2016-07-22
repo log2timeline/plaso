79cab76777bf198a4b9e7be12c2ce10cb3bf4c5b
Daniel White <onager@deerpie.com>
2016-07-22 08:53:26 +0200

    Added exception handling.

--- plaso/analysis/nsrlsvr.py
+++ plaso/analysis/nsrlsvr.py
@@ -40,12 +40,26 @@ class NsrlsvrAnalyzer(interface.HashAnalyzer):
     # Open a socket
     logging.debug(
         u'Opening connection to {0:s}:{1:d}'.format(self._host, self._port))
-    nsrl_socket = socket.create_connection((self._host, self._port),
-        self._SOCKET_TIMEOUT)
+    try:
+      nsrl_socket = socket.create_connection((self._host, self._port),
+          self._SOCKET_TIMEOUT)
+    except socket.error as exception:
+      logging.error(
+        (u'Error communicating with nsrlsvr {0:s}. nsrlsvr plugin is '
+         u'aborting.').format(exception))
+      self.SignalAbort()
+      return hash_analyses
     for digest in hashes:
       query = u'QUERY {0:s}\n'.format(digest)
-      nsrl_socket.sendall(query)
-      response = nsrl_socket.recv(self._RECEIVE_BUFFER_SIZE)
+      try:
+        nsrl_socket.sendall(query)
+        response = nsrl_socket.recv(self._RECEIVE_BUFFER_SIZE)
+      except socket.error as exception:
+        logging.error(
+          (u'Error communicating with nsrlsvr {0:s}. nsrlsvr plugin is '
+           u'aborting.').format(exception))
+        self.SignalAbort()
+        return hash_analyses
       if response.split(u' ')[1] == u'1':
         hash_analysis = interface.HashAnalysis(digest, True)
         hash_analyses.append(hash_analysis)
