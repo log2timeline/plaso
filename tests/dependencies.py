66e5e5e74ce5d86913a12a42958a6ac629a2bff5
Joachim Metz <joachim.metz@gmail.com>
2016-07-24 18:58:28 +0200

    Code review: 307900043: Added script to generate dependencies #283

---
+++ tests/dependencies.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Tests for the dependencies helper functions."""
+
+import socket
+import unittest
+
+from plaso import dependencies
+
+
+try:
+  hostname = socket.gethostbyname(u'github.com')
+except socket.error:
+  hostname = None
+
+
+class DependenciesTest(unittest.TestCase):
+  """A unit test for the dependencies helper functions."""
+
+  # pylint: disable=protected-access
+
+  # Show full diff results, part of TestCase so does not follow our naming
+  # conventions.
+  maxDiff = None
+
+  def testCheckLibyal(self):
+    """Tests the _CheckLibyal function."""
+    result = dependencies._CheckLibyal(
+        {u'pybde': 20140531}, verbose_output=False)
+    self.assertTrue(result)
+
+    result = dependencies._CheckLibyal({u'bogus': 0}, verbose_output=False)
+    self.assertFalse(result)
+
+  @unittest.skipUnless(hostname, 'no internet connectivity')
+  def testCheckLibyalWithLatestVersionCheck(self):
+    """Tests the _CheckLibyal function with latest version check."""
+    result = dependencies._CheckLibyal(
+        {u'pybde': 20140531}, latest_version_check=True,
+        verbose_output=False)
+    self.assertTrue(result)
+
+  def testCheckPythonModule(self):
+    """Tests the _CheckPythonModule function."""
+    result = dependencies._CheckPythonModule(
+        u'dfdatetime', u'__version__', u'20160319', verbose_output=False)
+    self.assertTrue(result)
+
+    result = dependencies._CheckPythonModule(
+        u'bogus', u'__version__', u'0', verbose_output=False)
+    self.assertFalse(result)
+
+  def testCheckPyTSK(self):
+    """Tests the _CheckPyTSK function."""
+    result = dependencies._CheckPyTSK(verbose_output=False)
+    self.assertTrue(result)
+
+  def testCheckSQLite3(self):
+    """Tests the _CheckSQLite3 function."""
+    result = dependencies._CheckSQLite3(verbose_output=False)
+    self.assertTrue(result)
+
+  @unittest.skipUnless(hostname, 'no internet connectivity')
+  def testDownloadPageContent(self):
+    """Tests the _DownloadPageContent function."""
+    download_url = u'https://github.com/log2timeline/plaso/releases'
+    page_content = dependencies._DownloadPageContent(download_url)
+    self.assertIsNotNone(page_content)
+
+    download_url = u'https://github.com/log2timeline/plaso/bogus'
+    page_content = dependencies._DownloadPageContent(download_url)
+    self.assertIsNone(page_content)
+
+  @unittest.skipUnless(hostname, 'no internet connectivity')
+  def testGetLibyalGithubReleasesLatestVersion(self):
+    """Tests the _GetLibyalGithubReleasesLatestVersion function."""
+    version = dependencies._GetLibyalGithubReleasesLatestVersion(u'libbde')
+    self.assertNotEqual(version, 0)
+
+    version = dependencies._GetLibyalGithubReleasesLatestVersion(u'bogus')
+    self.assertEqual(version, 0)
+
+  def testImportPythonModule(self):
+    """Tests the _ImportPythonModule function."""
+    module = dependencies._ImportPythonModule(u'os')
+    self.assertIsNotNone(module)
+
+    module = dependencies._ImportPythonModule(u'bogus')
+    self.assertIsNone(module)
+
+  def testCheckDependencies(self):
+    """Tests the CheckDependencies function."""
+    result = dependencies.CheckDependencies(verbose_output=False)
+    self.assertTrue(result)
+
+  def testCheckModuleVersion(self):
+    """Tests the CheckModuleVersion function."""
+    dependencies.CheckModuleVersion(u'dfdatetime')
+
+    with self.assertRaises(ImportError):
+      dependencies.CheckModuleVersion(u'bogus')
+
+  # CheckTestDependencies is tested in ./run_tests.py
+
+  def testGetDPKGDepends(self):
+    """Tests the GetDPKGDepends function."""
+    install_requires = dependencies.GetDPKGDepends()
+    self.assertIn(u'libbde-python (>= 20140531)', install_requires)
+
+    install_requires = dependencies.GetDPKGDepends(exclude_version=True)
+    self.assertIn(u'libbde-python', install_requires)
+
+  def testGetInstallRequires(self):
+    """Tests the GetInstallRequires function."""
+    install_requires = dependencies.GetInstallRequires()
+    self.assertIn(u'libbde-python >= 20140531', install_requires)
+
+  def testGetRPMRequires(self):
+    """Tests the GetRPMRequires function."""
+    install_requires = dependencies.GetRPMRequires()
+    self.assertIn(u'libbde-python >= 20140531', install_requires)
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ utils/update_dependencies.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Script to update the dependencies in various configuration files."""
+
+import os
+import sys
+
+# Change PYTHONPATH to include plaso.
+sys.path.insert(0, u'.')
+
+import plaso.dependencies
+
+
+class DPKGControllWriter(object):
+  """Class to help write a dpkg control file."""
+
+  _PATH = os.path.join(u'config', u'dpkg', u'control')
+
+  _MAINTAINER = (
+      u'Log2Timeline maintainers <log2timeline-maintainers@googlegroups.com>')
+
+  _FILE_HEADER = [
+      u'Source: plaso',
+      u'Section: python',
+      u'Priority: extra',
+      u'Maintainer: {0:s}'.format(_MAINTAINER),
+      u'Build-Depends: debhelper (>= 7.0.0), python, python-setuptools',
+      u'Standards-Version: 3.9.5',
+      u'X-Python-Version: >= 2.7',
+      u'Homepage: https://github.com/log2timeline/plaso',
+      u'',
+      u'Package: python-plaso',
+      u'Architecture: all']
+
+  _FILE_FOOTER = [
+      u'Description: Super timeline all the things',
+      u' Log2Timeline is a framework to create super timelines. Its purpose',
+      u' is to extract timestamps from various files found on typical computer',
+      (u' systems and aggregate them. Plaso is the Python rewrite of '
+       u'log2timeline.'),
+      u'']
+
+  def Write(self):
+    """Writes a dpkg control file."""
+    file_content = []
+    file_content.extend(self._FILE_HEADER)
+
+    dependencies = plaso.dependencies.GetDPKGDepends()
+    dependencies = u', '.join(dependencies)
+    file_content.append(
+        u'Depends: {0:s}, ${{python:Depends}}, ${{misc:Depends}}'.format(
+            dependencies))
+
+    file_content.extend(self._FILE_FOOTER)
+
+    file_content = u'\n'.join(file_content)
+    file_content = file_content.encode(u'utf-8')
+
+    with open(self._PATH, 'wb') as file_object:
+      file_object.write(file_content)
+
+
+class GIFTInstallScriptWriter(object):
+  """Class to help write the install_gift_and_dependencies.sh file."""
+
+  _PATH = os.path.join(
+      u'config', u'linux', u'install_gift_and_dependencies.sh')
+
+  _FILE_HEADER = [
+      u'#!/usr/bin/env bash',
+      u'set -e',
+      u'',
+      u'# Dependencies for running Plaso, alphabetized, one per line.',
+      (u'# This should not include packages only required for testing or '
+       u'development.')]
+
+  _FILE_FOOTER = [
+      u'',
+      u'# Additional dependencies for running Plaso tests, alphabetized,',
+      u'# one per line.',
+      u'TEST_DEPENDENCIES="python-coverage',
+      u'                   python-coveralls',
+      u'                   python-docopt',
+      u'                   python-mock";',
+      u'',
+      u'# Additional dependencies for doing Plaso debugging, alphabetized,',
+      u'# one per line.',
+      u'DEBUG_DEPENDENCIES="python-guppy";',
+      u'',
+      u'# Additional dependencies for doing Plaso development, alphabetized,',
+      u'# one per line.',
+      u'DEVELOPMENT_DEPENDENCIES="python-sphinx',
+      u'                          pylint";',
+      u'',
+      u'sudo add-apt-repository ppa:gift/dev -y',
+      u'sudo apt-get update -q',
+      u'sudo apt-get install -y ${PLASO_DEPENDENCIES}',
+      u'',
+      u'if [[ "$*" =~ "include-debug" ]]; then',
+      u'    sudo apt-get install -y ${DEBUG_DEPENDENCIES}',
+      u'fi',
+      u'',
+      u'if [[ "$*" =~ "include-development" ]]; then',
+      u'    sudo apt-get install -y ${DEVELOPMENT_DEPENDENCIES}',
+      u'fi',
+      u'',
+      u'if [[ "$*" =~ "include-test" ]]; then',
+      u'    sudo apt-get install -y ${TEST_DEPENDENCIES}',
+      u'fi',
+      u'']
+
+  def Write(self):
+    """Writes a install_gift_and_dependencies.sh file."""
+    file_content = []
+    file_content.extend(self._FILE_HEADER)
+
+    dependencies = plaso.dependencies.GetDPKGDepends(exclude_version=True)
+    for index, dependency in enumerate(dependencies):
+      if index == 0:
+        file_content.append(u'PLASO_DEPENDENCIES="{0:s}'.format(dependency))
+      elif index + 1 == len(dependencies):
+        file_content.append(u'                    {0:s}";'.format(dependency))
+      else:
+        file_content.append(u'                    {0:s}'.format(dependency))
+
+    file_content.extend(self._FILE_FOOTER)
+
+    file_content = u'\n'.join(file_content)
+    file_content = file_content.encode(u'utf-8')
+
+    with open(self._PATH, 'wb') as file_object:
+      file_object.write(file_content)
+
+
+class SetupCfgWriter(object):
+  """Class to help write a setup.cfg file."""
+
+  _PATH = u'setup.cfg'
+
+  _MAINTAINER = (
+      u'Log2Timeline maintainers <log2timeline-maintainers@googlegroups.com>')
+
+  _FILE_HEADER = [
+      u'[bdist_rpm]',
+      u'release = 1',
+      u'packager = {0:s}'.format(_MAINTAINER),
+      u'doc_files = ACKNOWLEDGEMENTS',
+      u'            AUTHORS',
+      u'            LICENSE',
+      u'            README',
+      u'build_requires = python-setuptools']
+
+  def Write(self):
+    """Writes a setup.cfg file."""
+    file_content = []
+    file_content.extend(self._FILE_HEADER)
+
+    dependencies = plaso.dependencies.GetRPMRequires()
+    for index, dependency in enumerate(dependencies):
+      if index == 0:
+        file_content.append(u'requires = {0:s}'.format(dependency))
+      else:
+        file_content.append(u'           {0:s}'.format(dependency))
+
+    file_content = u'\n'.join(file_content)
+    file_content = file_content.encode(u'utf-8')
+
+    with open(self._PATH, 'wb') as file_object:
+      file_object.write(file_content)
+
+class ToxIniWriter(object):
+  """Class to help write a tox.ini file."""
+
+  _PATH = u'tox.ini'
+
+  _FILE_HEADER = [
+      u'[tox]',
+      u'envlist = py27, py34',
+      u'',
+      u'[testenv]',
+      u'pip_pre = True',
+      u'sitepackages = True',
+      u'setenv =',
+      u'    PYTHONPATH = {toxinidir}',
+      u'deps =',
+      u'    pip >= 7.0.0',
+      u'    pytest',
+      u'    mock']
+
+  _FILE_FOOTER = [
+      u'commands = python run_tests.py',
+      u'']
+
+  def Write(self):
+    """Writes a tox.ini file."""
+    file_content = []
+    file_content.extend(self._FILE_HEADER)
+
+    for dependency in plaso.dependencies.GetInstallRequires():
+      file_content.append(u'    {0:s}'.format(dependency))
+
+    file_content.extend(self._FILE_FOOTER)
+
+    file_content = u'\n'.join(file_content)
+    file_content = file_content.encode(u'utf-8')
+
+    with open(self._PATH, 'wb') as file_object:
+      file_object.write(file_content)
+
+
+if __name__ == u'__main__':
+  writer = DPKGControllWriter()
+  writer.Write()
+
+  writer = GIFTInstallScriptWriter()
+  writer.Write()
+
+  writer = SetupCfgWriter()
+  writer.Write()
+
+  writer = ToxIniWriter()
+  writer.Write()
