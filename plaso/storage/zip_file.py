7785d9a4c391a0e3424196652e0234e9152c1037
Joachim Metz <joachim.metz@gmail.com>
2016-07-31 11:46:11 +0200

    Changes for Windows.

--- plaso/storage/zip_file.py
+++ plaso/storage/zip_file.py
@@ -131,6 +131,7 @@ import logging
 import os
 import shutil
 import tempfile
+import time
 import warnings
 import zipfile
 
@@ -2379,7 +2380,17 @@ class ZIPStorageFile(interface.BaseFileStorage):
     self._is_open = False
 
     if self._path != self._zipfile_path and os.path.exists(self._zipfile_path):
-      os.rename(self._zipfile_path, self._path)
+      # On Windows the file can sometimes be still in use and we have to wait.
+      attempt = 1
+      while True:
+        try:
+          os.rename(self._zipfile_path, self._path)
+        except OSError:
+          if attempt >= 3:
+            raise
+          time.sleep(0.5)
+          attempt += 1
+
       directory_name = os.path.dirname(self._zipfile_path)
       os.rmdir(directory_name)
 
