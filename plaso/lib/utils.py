944c28e144869c597e4ae9211791fb6b2c8bb508
Joachim Metz <joachim.metz@gmail.com>
2016-08-01 18:32:53 +0200

    Code review: 303220043: Removed utils.GetUnicodeString #160

--- plaso/lib/utils.py
+++ plaso/lib/utils.py
@@ -6,15 +6,6 @@ import logging
 from plaso.lib import py2to3
 
 
-# TODO: remove this function
-# Do not use this function
-def GetUnicodeString(string):
-  """Converts the string to Unicode if necessary."""
-  if not isinstance(string, py2to3.UNICODE_TYPE):
-    return str(string).decode(u'utf8', errors=u'ignore')
-  return string
-
-
 def IsText(bytes_in, encoding=None):
   """Examine the bytes in and determine if they are indicative of a text.
 
@@ -28,12 +19,12 @@ def IsText(bytes_in, encoding=None):
   sequence is not text, but a byte sequence.
 
   Args:
-    bytes_in: The byte sequence passed to the method that needs examination.
-    encoding: Optional encoding to test, if not defined only ASCII, UTF-8 and
-    UTF-16 are tried.
+    bytes_in (bytes): byte stream to examine.
+    encoding (Optional[str]): encoding to test, if not defined ASCII and UTF-8
+        are tried.
 
   Returns:
-    Boolean value indicating whether or not the byte sequence is a text or not.
+    bool: True if the bytes stream contains text.
   """
   # TODO: Improve speed and accuracy of this method.
   # Start with the assumption we are dealing with a text.
@@ -54,29 +45,20 @@ def IsText(bytes_in, encoding=None):
 
   # Check if this is UTF-8
   try:
-    _ = bytes_in.decode('utf-8')
+    bytes_in.decode(u'utf-8')
     return True
+
   except UnicodeDecodeError:
     pass
 
-  # TODO: UTF 16 decode is successful in too
-  # many edge cases where we are not really dealing with
-  # a text at all. Leaving this out for now, consider
-  # re-enabling or making a better determination.
-  #try:
-  #  _ = bytes_in.decode('utf-16-le')
-  #  return True
-  #except UnicodeDecodeError:
-  #  pass
-
   if encoding:
     try:
-      _ = bytes_in.decode(encoding)
+      bytes_in.decode(encoding)
       return True
+
+    except LookupError:
+      logging.error(u'Unuppported encoding: {0:s}'.format(encoding))
     except UnicodeDecodeError:
       pass
-    except LookupError:
-      logging.error(
-          u'String encoding not recognized: {0:s}'.format(encoding))
 
   return False
