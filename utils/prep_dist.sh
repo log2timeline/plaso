7abb71580c67c397a59d5ffc91c26c2a176d48f0
Joachim Metz <joachim.metz@gmail.com>
2016-08-06 10:40:16 +0200

    Code review: 306050043: Removed licenses and changed prep dist script to copy them from l2tdevtools.

--- utils/prep_dist.sh
+++ utils/prep_dist.sh
@@ -26,5 +26,32 @@ sed -i"~" -e "${SED_SCRIPT}" plaso/parsers/presets.py
 
 sed -i"~" '/hachoir_/d' plaso/dependencies.py
 
+if ! test -d ../l2tdevtools;
+then
+	echo "Unable to locate l2tdevtools.";
+
+	exit ${EXIT_FAILURE};
+fi
+
+rm -rf config/licenses
+
+mkdir config/licenses
+
+DEPENDENCIES=`cat ../l2tdevtools/data/presets.ini | grep -A1 '\[plaso\]' | tail -n1 | sed 's/projects: //' | tr ',' ' '`;
+
+for DEPENDENCY in ${DEPENDENCIES};
+do
+	cp "../l2tdevtools/data/licenses/LICENSE.${DEPENDENCY}" config/licenses/
+done
+
+rm -f config/licenses/LICENSE.hachoir-*
+rm -f config/licenses/LICENSE.guppy
+rm -f config/licenses/LICENSE.libexe
+rm -f config/licenses/LICENSE.libwrc
+rm -f config/licenses/LICENSE.mock
+rm -f config/licenses/LICENSE.pbr
+
+python ./utils/update_dependencies.py
+
 exit ${EXIT_SUCCESS};
 
