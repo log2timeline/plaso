842676d0fb31188c4a0bbfba0ddf4279de0a8482
Joachim Metz <joachim.metz@gmail.com>
2016-08-08 20:10:40 +0200

    Code review: 308040043: Small fixes.

--- plaso/multi_processing/analysis_process.py
+++ plaso/multi_processing/analysis_process.py
@@ -37,6 +37,7 @@ class AnalysisProcess(base_process.MultiProcessBaseProcess):
     self._analysis_mediator = None
     self._analysis_plugin = analysis_plugin
     self._data_location = data_location
+    self._debug_output = False
     self._event_filter_expression = event_filter_expression
     self._event_queue = event_queue
     self._knowledge_base = knowledge_base
@@ -166,7 +167,7 @@ class AnalysisProcess(base_process.MultiProcessBaseProcess):
         self._name, self._pid))
 
     try:
-      self._task_queue.Close(abort=self._abort)
+      self._event_queue.Close(abort=self._abort)
     except errors.QueueAlreadyClosed:
       logging.error(u'Queue for {0:s} was already closed.'.format(self.name))
 
