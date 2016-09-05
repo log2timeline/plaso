388f23aae909864349f208a3b23748cef19e9b78
Daniel White <dmwhite@google.com>
2016-09-05 16:20:44 +0200

    Update for efilter 1.5, add tagging test for contains operator.

--- tests/analysis/tagging.py
+++ tests/analysis/tagging.py
@@ -109,7 +109,7 @@ class TaggingAnalysisPluginTest(test_lib.AnalysisPluginTestCase):
     labels = []
     for tag in tags:
       labels.extend(tag.labels)
-    self.assertEqual(len(labels), 4)
+    self.assertEqual(len(labels), 5)
 
     labels = []
     for tag in tags:
@@ -120,16 +120,18 @@ class TaggingAnalysisPluginTest(test_lib.AnalysisPluginTestCase):
     self.assertIn(u'application_execution', labels)
     # This is from a tag rule declared in dotty syntax.
     self.assertIn(u'login_attempt', labels)
+    # This is from a rule using the "contains" operator
+    self.assertIn(u'text_contains', labels)
 
   def testParseTaggingFile(self):
     """Tests the _ParseTaggingFile function."""
-    plugin = tagging.TaggingPlugin()
+    plugin = tagging.TaggingAnalysisPlugin()
     test_path = self._GetTestFilePath([self._TEST_TAG_FILE_NAME])
 
     tag_expression = plugin._ParseTaggingFile(test_path)
     self.assertEqual(len(tag_expression.children), 5)
 
-    plugin = tagging.TaggingPlugin()
+    plugin = tagging.TaggingAnalysisPlugin()
     test_path = self._GetTestFilePath([self._INVALID_TEST_TAG_FILE_NAME])
 
     tag_expression = plugin._ParseTaggingFile(test_path)
