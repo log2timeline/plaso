7534493c8b4cd61d1e75910fcf3be522a592b8a8
Daniel White <onager@deerpie.com>
2016-10-22 16:41:42 +0200

    Changes after review.

--- plaso/multi_processing/task_engine.py
+++ plaso/multi_processing/task_engine.py
@@ -226,7 +226,8 @@ class TaskMultiProcessEngine(engine.MultiProcessEngine):
               task)
         except IOError as exception:
           logging.error(
-              u'Unable to merge results of task: {0:s} with error: {1:s}'.format(
+              u'Unable to merge results of task: {0:s} '
+              u'with error: {1:s}'.format(
                   task.identifier, exception))
           self._storage_merge_reader = None
 
