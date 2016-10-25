e7dfab0099f586a61fc4b383b2108d2e2ad0bcf1
Romain Gayon <romaing@google.com>
2016-10-25 16:36:29 +0200

    fix test

--- tests/parsers/systemd_journal.py
+++ tests/parsers/systemd_journal.py
@@ -15,7 +15,8 @@ class SystemdJournalParserTest(test_lib.ParserTestCase):
   def testParse(self):
     """Tests the Parse function."""
     parser_object = systemd_journal.SystemdJournalParser()
-    journal = self._ParseFile([u'systemd', u'journal', u'system.journal'], parser_object)
+    journal = self._ParseFile([u'systemd', u'journal', u'system.journal'],
+                              parser_object)
 
     self.assertEqual(
         len(journal.events), parser_object.journal_header.n_entries)
@@ -25,14 +26,19 @@ class SystemdJournalParserTest(test_lib.ParserTestCase):
     expected_timestamp = timelib.Timestamp.CopyFromString(
         u'2011-07-27 06:41:47')
 
-    expected_msg = u'[test-VirtualBox/49cd46492584496585e147de34ed5816 systemd-journald[577]] Runtime journal (/run/log/journal/) is 1.2M, max 9.9M, 8.6M free.'
-    expected_msg_short=u'[test-VirtualBox/49cd46492584496585e147de34ed5816 systemd-journald[577]] Runt...'
+    expected_msg = (u'test-VirtualBox systemd-journald[577] Runtime journal '
+                    u'(/run/log/journal/) is 1.2M, max 9.9M, 8.6M free.')
+    expected_msg_short = (u'test-VirtualBox systemd-journald[577] Runtime '
+                          u'journal (/run/log/journal/) is ...')
     self._TestGetMessageStrings(event_object, expected_msg, expected_msg_short)
 
   def testParseDirty(self):
     """Tests the Parse function on a 'dirty' journal file."""
     parser_object = systemd_journal.SystemdJournalParser()
-    journal = self._ParseFile([u'systemd', u'journal', u'system@00053f9c9a4c1e0e-2e18a70e8b327fed.journal~'], parser_object)
+    journal = self._ParseFile([
+        u'systemd', u'journal',
+        u'system@00053f9c9a4c1e0e-2e18a70e8b327fed.journalTILDE'
+    ], parser_object)
 
     self.assertEqual(
         len(journal.events), 2211)
@@ -42,8 +48,10 @@ class SystemdJournalParserTest(test_lib.ParserTestCase):
     expected_timestamp = timelib.Timestamp.CopyFromString(
         u'2011-07-27 06:41:47')
 
-    expected_msg = u'[test-VirtualBox/49cd46492584496585e147de34ed5816 systemd-journald[569]] Runtime journal (/run/log/journal/) is 1.2M, max 9.9M, 8.6M free.'
-    expected_msg_short=u'[test-VirtualBox/49cd46492584496585e147de34ed5816 systemd-journald[569]] Runt...'
+    expected_msg = (u'test-VirtualBox systemd-journald[569] Runtime journal '
+                    u'(/run/log/journal/) is 1.2M, max 9.9M, 8.6M free.')
+    expected_msg_short = (u'test-VirtualBox systemd-journald[569] Runtime '
+                          u'journal (/run/log/journal/) is ...')
     self._TestGetMessageStrings(event_object, expected_msg, expected_msg_short)
 
 
