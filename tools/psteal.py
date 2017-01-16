7dc21e2bfe745af2946eebd274b8a31dfeb3e187
Joachim Metz <joachim.metz@gmail.com>
2017-01-16 22:37:06 +0100

    Changes to parser manager #1073

--- tools/psteal.py
+++ tools/psteal.py
@@ -163,7 +163,6 @@ class PstealTool(extract_analyze_tool.ExtractionAndAnalysisTool):
     session = self._extraction_front_end.CreateSession(
         command_line_arguments=self._command_line_arguments,
         filter_file=self._filter_file,
-        parser_filter_expression=self._parser_filter_expression,
         preferred_encoding=self.preferred_encoding,
         preferred_year=self._preferred_year)
 
