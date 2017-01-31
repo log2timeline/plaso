0162c45cd55495b1ce9260a94917ee582d01f874
Daniel White <dmwhite@google.com>
2017-01-31 11:18:22 +0100

    Disable zip file stream size check. Workaround for #1149

--- plaso/storage/zip_file.py
+++ plaso/storage/zip_file.py
@@ -549,7 +549,7 @@ class _SerializedDataStream(object):
       data (bytes): data.
 
     Returns:
-      int: offset of the entry within the temporary file.
+      int: offset of the entry within the data stream.
 
     Raises:
       IOError: if the serialized data stream was not opened for writing or
@@ -559,10 +559,11 @@ class _SerializedDataStream(object):
       raise IOError(u'Unable to write to closed serialized data stream.')
 
     data_size = len(data)
-    data_end_offset = (
-        self._file_object.tell() + self._DATA_ENTRY_SIZE + data_size)
-    if data_end_offset > self._maximum_data_size:
-      raise IOError(u'Unable to write data entry size value out of bounds.')
+    # TODO: Fix maximum size limit handling to create new stream.
+    # data_end_offset = (
+    #    self._file_object.tell() + self._DATA_ENTRY_SIZE + data_size)
+    # if data_end_offset > self._maximum_data_size:
+    #  raise IOError(u'Unable to write data entry size value out of bounds.')
 
     data_size = construct.ULInt32(u'size').build(data_size)
     self._file_object.write(data_size)
