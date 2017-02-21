4fd4ad0bb7767a31ed12fbf80dda8334fefd449f
Joachim Metz <joachim.metz@gmail.com>
2017-02-21 05:54:13 +0100

    Code review: 315300043: Added event data and date time values event #910

--- plaso/containers/time_events.py
+++ plaso/containers/time_events.py
@@ -34,6 +34,29 @@ class TimestampEvent(events.EventObject):
       self.data_type = data_type
 
 
+class DateTimeValuesEvent(TimestampEvent):
+  """Convenience class for a dfdatetime-based event."""
+
+  def __init__(
+      self, date_time, date_time_description, data_type=None, time_zone=None):
+    """Initializes an event.
+
+    Args:
+      date_time (dfdatetime.DateTimeValues): date and time values.
+      date_time_description (str): description of the meaning of the date and
+          time values.
+      data_type (Optional[str]): event data type. If the data type is not set
+          it is derived from the DATA_TYPE class attribute.
+      time_zone (Optional[datetime.tzinfo]): time zone.
+    """
+    timestamp = date_time.GetPlasoTimestamp()
+    if date_time.is_local_time and time_zone:
+      timestamp = timelib.Timestamp.LocaltimeToUTC(timestamp, time_zone)
+
+    super(DateTimeValuesEvent, self).__init__(
+        timestamp, date_time_description, data_type=data_type)
+
+
 class CocoaTimeEvent(TimestampEvent):
   """Convenience class for a Cocoa time-based event."""
 
