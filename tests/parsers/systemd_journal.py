eacdea934ef537c1ce4488becaf07d3746e2b9a0
Romain Gayon <romaing@google.com>
2017-03-24 16:15:23 +0100

    skip message

--- tests/parsers/systemd_journal.py
+++ tests/parsers/systemd_journal.py
@@ -19,7 +19,7 @@ class SystemdJournalParserTest(test_lib.ParserTestCase):
       from plaso.parsers import systemd_journal
     except ImportError as e:
       if e.message.find(u'lzma') > 0:
-        raise unittest.SkipTest('criteria')
+        raise unittest.SkipTest('python lzma is not installed')
 
     parser_object = systemd_journal.SystemdJournalParser()
     journal = self._ParseFile([
@@ -60,7 +60,7 @@ class SystemdJournalParserTest(test_lib.ParserTestCase):
       from plaso.parsers import systemd_journal
     except ImportError as e:
       if e.message.find(u'lzma') > 0:
-        raise unittest.SkipTest('criteria')
+        raise unittest.SkipTest('python lzma is not installed')
 
     storage_writer = self._CreateStorageWriter()
     parser_mediator = self._CreateParserMediator(storage_writer)
