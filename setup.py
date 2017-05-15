cf6b52da0d62017f044e49f15f9195d0c7fa610b
Joachim Metz <joachim.metz@gmail.com>
2017-05-15 22:12:06 +0200

    Updated information in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""This is the setup file for the project. The standard setup rules apply:
-
-   python setup.py build
-   sudo python setup.py install
-"""
+"""Installation and deployment script."""
 
 from __future__ import print_function
 import glob
