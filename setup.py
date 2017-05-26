4def43fdcaec0b5df28a91cfebbe5256c2e5c357
Joachim Metz <joachim.metz@gmail.com>
2017-05-26 09:05:15 +0200

    Changed setup.py and dpgk files

--- setup.py
+++ setup.py
@@ -9,14 +9,19 @@ import os
 import sys
 
 try:
-  from setuptools import find_packages, setup, Command
+  from setuptools import find_packages, setup
 except ImportError:
-  from distutils.core import find_packages, setup, Command
+  from distutils.core import find_packages, setup
 
 try:
-  from setuptools.commands.bdist_rpm import bdist_rpm
+  from distutils.command.bdist_msi import bdist_msi
 except ImportError:
+  bdist_msi = None
+
+try:
   from distutils.command.bdist_rpm import bdist_rpm
+except ImportError:
+  bdist_rpm = None
 
 try:
   from setuptools.commands.sdist import sdist
@@ -26,8 +31,7 @@ except ImportError:
 # Change PYTHONPATH to include plaso.
 sys.path.insert(0, '.')
 
-import plaso
-import plaso.dependencies
+import plaso  # pylint: disable=wrong-import-position
 
 
 version_tuple = (sys.version_info[0], sys.version_info[1])
@@ -48,6 +52,90 @@ elif version_tuple[0] == 3 and version_tuple < (3, 4):
   sys.exit(1)
 
 
+if not bdist_msi:
+  BdistMSICommand = None
+else:
+  class BdistMSICommand(bdist_msi):
+    """Custom handler for the bdist_msi command."""
+
+    def run(self):
+      """Builds an MSI."""
+      # Command bdist_msi does not support the library version, neither a date
+      # as a version but if we suffix it with .1 everything is fine.
+      self.distribution.metadata.version += '.1'
+
+      bdist_msi.run(self)
+
+
+if not bdist_rpm:
+  BdistRPMCommand = None
+else:
+  class BdistRPMCommand(bdist_rpm):
+    """Custom handler for the bdist_rpm command."""
+
+    def _make_spec_file(self):
+      """Generates the text of an RPM spec file.
+
+      Returns:
+        list[str]: lines of the RPM spec file.
+      """
+      # Note that bdist_rpm can be an old style class.
+      if issubclass(BdistRPMCommand, object):
+        spec_file = super(BdistRPMCommand, self)._make_spec_file()
+      else:
+        spec_file = bdist_rpm._make_spec_file(self)
+
+      if sys.version_info[0] < 3:
+        python_package = 'python'
+      else:
+        python_package = 'python3'
+
+      description = []
+      summary = ''
+      in_description = False
+
+      python_spec_file = []
+      for line in iter(spec_file):
+        if line.startswith('Summary: '):
+          summary = line
+
+        elif line.startswith('BuildRequires: '):
+          line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+
+        elif line.startswith('Requires: '):
+          if python_package == 'python3':
+            line = line.replace('python', 'python3')
+
+        elif line.startswith('%description'):
+          in_description = True
+
+        elif line.startswith('%files'):
+          line = '%files -f INSTALLED_FILES -n {0:s}-%{{name}}'.format(
+              python_package)
+
+        elif line.startswith('%prep'):
+          in_description = False
+
+          python_spec_file.append(
+              '%package -n {0:s}-%{{name}}'.format(python_package))
+          python_spec_file.append('{0:s}'.format(summary))
+          python_spec_file.append('')
+          python_spec_file.append(
+              '%description -n {0:s}-%{{name}}'.format(python_package))
+          python_spec_file.extend(description)
+
+        elif in_description:
+          # Ignore leading white lines in the description.
+          if not description and not line:
+            continue
+
+          description.append(line)
+
+        python_spec_file.append(line)
+
+      return python_spec_file
+
+
 def GetScripts():
   """List up all scripts that should be runable from the command line."""
   scripts = []
@@ -65,73 +153,6 @@ def GetScripts():
   return scripts
 
 
-class BdistRPMCommand(bdist_rpm):
-  """Custom handler for the bdist_rpm command."""
-
-  def _make_spec_file(self):
-    """Generates the text of an RPM spec file.
-
-    Returns:
-      A list of strings containing the lines of text.
-    """
-    # Note that bdist_rpm can be an old style class.
-    if issubclass(BdistRPMCommand, object):
-      spec_file = super(BdistRPMCommand, self)._make_spec_file()
-    else:
-      spec_file = bdist_rpm._make_spec_file(self)
-
-    if sys.version_info[0] < 3:
-      python_package = 'python'
-    else:
-      python_package = 'python3'
-
-    description = []
-    summary = ''
-    in_description = False
-
-    python_spec_file = []
-    for index, line in enumerate(spec_file):
-      if line.startswith('Summary: '):
-        summary = line
-
-      elif line.startswith('BuildRequires: '):
-        line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
-
-      elif line.startswith('Requires: '):
-        if python_package == 'python3':
-          line = line.replace('python-backports-lzma', '')
-          line = line.replace('python', 'python3')
-
-      elif line.startswith('%description'):
-        in_description = True
-
-      elif line.startswith('%files'):
-        line = '%files -f INSTALLED_FILES -n {0:s}-%{{name}}'.format(
-           python_package)
-
-      elif line.startswith('%prep'):
-        in_description = False
-
-        python_spec_file.append(
-            '%package -n {0:s}-%{{name}}'.format(python_package))
-        python_spec_file.append('{0:s}'.format(summary))
-        python_spec_file.append('')
-        python_spec_file.append(
-            '%description -n {0:s}-%{{name}}'.format(python_package))
-        python_spec_file.extend(description)
-
-      elif in_description:
-        # Ignore leading white lines in the description.
-        if not description and not line:
-          continue
-
-        description.append(line)
-
-      python_spec_file.append(line)
-
-    return python_spec_file
-
-
 if version_tuple[0] == 2:
   encoding = sys.stdin.encoding
 
@@ -146,13 +167,6 @@ if version_tuple[0] == 2:
   sys.setdefaultencoding(encoding)
 
 
-plaso_version = plaso.__version__
-
-# Command bdist_msi does not support the library version, neither a date
-# as a version but if we suffix it with .1 everything is fine.
-if 'bdist_msi' in sys.argv:
-  plaso_version += '.1'
-
 # Unicode in the description will break python-setuptools, hence
 # "Plaso Langar Að Safna Öllu" was removed.
 plaso_description = 'Super timeline all the things'
@@ -164,7 +178,7 @@ plaso_long_description = (
 
 setup(
     name='plaso',
-    version=plaso_version,
+    version=plaso.__version__,
     description=plaso_description,
     long_description=plaso_long_description,
     license='Apache License, Version 2.0',
@@ -173,6 +187,7 @@ setup(
     maintainer_email='log2timeline-maintainers@googlegroups.com',
     scripts=GetScripts(),
     cmdclass={
+        'bdist_msi': BdistMSICommand,
         'bdist_rpm': BdistRPMCommand,
         'sdist_test_data': sdist},
     classifiers=[
@@ -191,7 +206,4 @@ setup(
         ('share/doc/plaso', [
             'AUTHORS', 'ACKNOWLEDGEMENTS', 'LICENSE', 'README']),
     ],
-    # TODO: this is disabled for now since setup.py will actually try
-    # to install the depencies directly from pypi.
-    # install_requires=plaso.dependencies.GetInstallRequires(),
 )
