656f5446ec96040902eafb7d5688f443c34f44f7
Romain Gayon <romaing@google.com>
2017-06-19 16:48:59 +0200

    split stuff from the definition

--- tests/end-to-end.py
+++ tests/end-to-end.py
@@ -659,8 +659,14 @@ class ExtractAndOutputTestCase(TestCase):
     if test_definition.extract_options is None:
       test_definition.extract_options = []
     elif isinstance(test_definition.extract_options, STRING_TYPES):
-      test_definition.extract_options = test_definition.extract_options.split(
-          u',')
+      tmp_extract_options = []
+      for option_and_value in test_definition.extract_options.split(
+          u' '):
+        if option_and_value.find(u'=')>0:
+          tmp_extract_options.extend(option_and_value.split(u'='))
+        else:
+          tmp_extract_options.append(option_and_value)
+      test_definition.extract_options = tmp_extract_options
 
     test_definition.output_file = test_definition_reader.GetConfigValue(
         test_definition.name, u'output_file')
