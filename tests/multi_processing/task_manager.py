9860609166de3e7f7c61047b8550ff49e82da1e6
Daniel White <onager@deerpie.com>
2017-07-06 14:06:24 +0200

    Add task retrying

--- tests/multi_processing/task_manager.py
+++ tests/multi_processing/task_manager.py
@@ -105,7 +105,7 @@ class TaskManagerTestCase(shared_test_lib.BaseTestCase):
 
     retry_task = manager.GetRetryTask()
     self.assertIsNotNone(retry_task)
-    self.assertEqual(task.identifier, retry_task.original_task_identifier)
+    self.assertEqual(task.identifier, retry_task.retry_identifier)
     self.assertTrue(task.retried)
     manager.CompleteTask(retry_task)
 
