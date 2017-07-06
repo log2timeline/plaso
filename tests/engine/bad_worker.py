3311a40d830af7dbbbef785cb864c05c64917f1c
Daniel White <onager@deerpie.com>
2017-07-06 16:57:15 +0200

    Add a worker that causes errors

--- tests/engine/bad_worker.py
+++ tests/engine/bad_worker.py
@@ -3,15 +3,17 @@
 
 import time
 import random
+import logging
 from plaso.engine import worker
+from plaso.multi_processing import engine
 
 class BadWorker(worker.EventExtractionWorker):
   
   def _ProcessFileEntryDataStream(
       self, mediator, file_entry, data_stream_name):
-    option = random.randint(1, 4)
-    if option == 1:
-      self._ExhaustMemory()
+    option = random.randint(1, 10)
+    #  if option == 1:
+      # self._ExhaustMemory()
     if option == 2:
       self._NeverReturn()
     if option == 3:
@@ -21,16 +23,19 @@ class BadWorker(worker.EventExtractionWorker):
 
   def _ExhaustMemory(self):
     """Perpetually expand memory."""
+    logging.debug('Starting to bloat memory')
     bloat = 'a'
-    while True:
+    while bloat < engine.MultiProcessEngine._DEFAULT_WORKER_MEMORY_LIMIT:
       bloat += bloat
       time.sleep(2)
 
   def _NeverReturn(self):
     """Block forever."""
+    logging.debug('Never returning')
     while True:
       time.sleep(30)
 
   def _TakeALongTime(self):
     """Wait for 10 minutes before returning, to simulate slow processing."""
+    logging.debug('Waiting 10 minutes')
     time.sleep(10*60)
