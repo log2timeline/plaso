e601cd7dedeef24ff96d0db0248d599b028a862a
Joachim Metz <joachim.metz@gmail.com>
2017-07-12 06:26:29 +0200

    Code review: 323230043: Moved update dependencies script to l2tdevtools

--- config/linux/gift_ppa_install.sh
+++ config/linux/gift_ppa_install.sh
@@ -3,55 +3,55 @@ set -e
 
 # Dependencies for running Plaso, alphabetized, one per line.
 # This should not include packages only required for testing or development.
-PLASO_DEPENDENCIES="libbde-python
-                    libesedb-python
-                    libevt-python
-                    libevtx-python
-                    libewf-python
-                    libfsntfs-python
-                    libfvde-python
-                    libfwnt-python
-                    libfwsi-python
-                    liblnk-python
-                    libmsiecf-python
-                    libolecf-python
-                    libqcow-python
-                    libregf-python
-                    libscca-python
-                    libsigscan-python
-                    libsmdev-python
-                    libsmraw-python
-                    libvhdi-python
-                    libvmdk-python
-                    libvshadow-python
-                    libvslvm-python
-                    python-artifacts
-                    python-bencode
-                    python-binplist
-                    python-construct
-                    python-crypto
-                    python-dateutil
-                    python-dfdatetime
-                    python-dfvfs
-                    python-dfwinreg
-                    python-dpkt
-                    python-efilter
-                    python-hachoir-core
-                    python-hachoir-metadata
-                    python-hachoir-parser
-                    python-lzma
-                    python-pefile
-                    python-psutil
-                    python-pyparsing
-                    python-pysqlite
-                    python-pytsk3
-                    python-requests
-                    python-six
-                    python-tz
-                    python-xlsxwriter
-                    python-yaml
-                    python-yara
-                    python-zmq";
+PYTHON2_DEPENDENCIES="libbde-python
+                      libesedb-python
+                      libevt-python
+                      libevtx-python
+                      libewf-python
+                      libfsntfs-python
+                      libfvde-python
+                      libfwnt-python
+                      libfwsi-python
+                      liblnk-python
+                      libmsiecf-python
+                      libolecf-python
+                      libqcow-python
+                      libregf-python
+                      libscca-python
+                      libsigscan-python
+                      libsmdev-python
+                      libsmraw-python
+                      libvhdi-python
+                      libvmdk-python
+                      libvshadow-python
+                      libvslvm-python
+                      python-artifacts
+                      python-bencode
+                      python-binplist
+                      python-construct
+                      python-crypto
+                      python-dateutil
+                      python-dfdatetime
+                      python-dfvfs
+                      python-dfwinreg
+                      python-dpkt
+                      python-efilter
+                      python-hachoir-core
+                      python-hachoir-metadata
+                      python-hachoir-parser
+                      python-lzma
+                      python-pefile
+                      python-psutil
+                      python-pyparsing
+                      python-pysqlite
+                      python-pytsk3
+                      python-requests
+                      python-six
+                      python-tz
+                      python-xlsxwriter
+                      python-yaml
+                      python-yara
+                      python-zmq";
 
 # Additional dependencies for running Plaso tests, alphabetized,
 # one per line.
@@ -112,7 +112,7 @@ DEBUG_DEPENDENCIES="libbde-dbg
 
 sudo add-apt-repository ppa:gift/dev -y
 sudo apt-get update -q
-sudo apt-get install -y ${PLASO_DEPENDENCIES}
+sudo apt-get install -y ${PYTHON2_DEPENDENCIES}
 
 if [[ "$*" =~ "include-debug" ]]; then
     sudo apt-get install -y ${DEBUG_DEPENDENCIES}
