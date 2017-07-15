1f4030c63a36c3ed415ca07d093c59dfd394c96f
Joachim Metz <joachim.metz@gmail.com>
2017-07-15 11:50:35 +0200

    Changes after review and merge

--- plaso/cli/psteal_tool.py
+++ plaso/cli/psteal_tool.py
@@ -23,6 +23,7 @@ from plaso import output   # pylint: disable=unused-import
 
 from plaso.cli import status_view
 from plaso.cli import storage_media_tool
+from plaso.cli import tool_options
 from plaso.cli import tools
 from plaso.cli import views
 from plaso.cli.helpers import manager as helpers_manager
