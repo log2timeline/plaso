876d97f6a880e951df1a6137073e3efe14012036
Joachim Metz <joachim.metz@gmail.com>
2017-08-05 09:58:09 +0200

    Code review: 322400043: Changes for pylint 1.7 upgrade

--- plaso/filters/event_filter.py
+++ plaso/filters/event_filter.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-"""The event object filter."""
+"""The event filter."""
+
+from __future__ import unicode_literals
 
 from plaso.filters import interface
 from plaso.filters import manager
@@ -7,7 +9,12 @@ from plaso.lib import errors
 
 
 class EventObjectFilter(interface.FilterObject):
-  """Class that implements an event object filter."""
+  """Event filter."""
+
+  def __init__(self):
+    """Initializes an event filter."""
+    super(EventObjectFilter, self).__init__()
+    self._decision = None
 
   def CompileFilter(self, filter_expression):
     """Compiles the filter expression.
@@ -15,31 +22,31 @@ class EventObjectFilter(interface.FilterObject):
     The filter expression contains an object filter expression.
 
     Args:
-      filter_expression: string that contains the filter expression.
+      filter_expression (str): filter expression.
 
     Raises:
       WrongPlugin: if the filter could not be compiled.
     """
     matcher = self._GetMatcher(filter_expression)
     if not matcher:
-      raise errors.WrongPlugin(u'Malformed filter expression.')
+      raise errors.WrongPlugin('Malformed filter expression.')
 
     self._filter_expression = filter_expression
     self._matcher = matcher
 
-  def Match(self, event_object):
-    """Determines if an event object matches the filter.
+  def Match(self, event):
+    """Determines if an event matches the filter.
 
     Args:
-      event_object: an event object (instance of EventObject).
+      event (EventObject): an event.
 
     Returns:
-      A boolean value that indicates a match.
+      bool: True if the event matches the filter.
     """
     if not self._matcher:
       return True
 
-    self._decision = self._matcher.Matches(event_object)
+    self._decision = self._matcher.Matches(event)
     return self._decision
 
 
