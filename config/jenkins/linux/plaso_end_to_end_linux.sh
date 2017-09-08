ee99447b7a9032bf686965b1b2727872d8a510d9
Daniel White <onager@deerpie.com>
2017-09-08 13:05:33 +0200

    Add end-to-end script for running on Jenkins

---
+++ config/jenkins/linux/plaso_end_to_end_linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sh ./config/linux/gift_ppa_install.sh include-test
+gsutil cp gs://$CONFIG_BUCKET/$JOB_NAME.ini .
+mkdir -p /tmp/plaso_out
+PYTHONPATH=. python ./tests/end-to-end.py --debug --config $JOB_NAME.ini --sources-directory /media/greendale_images --tools-directory ./tools --results-directory /tmp/plaso_out --references-directory /media/greendale_images
+#Create a directory to store results
+/usr/bin/gsutil cp /tmp/plaso_out/* gs://$RESULTS_BUCKET/build_results/$JOB_NAME/$BUILD_NUMBER/
+/usr/bin/gsutil ls gs://$RESULTS_BUCKET/build_results/$JOB_NAME/gold/
+if [ $? -eq 0 ]; then
+  /usr/bin/gsutil rm gs://$RESULTS_BUCKET/build_results/$JOB_NAME/gold/*
+fi
+/usr/bin/gsutil cp gs://$RESULTS_BUCKETt/build_results/$JOB_NAME/$BUILD_NUMBER/* gs://$RESULTS_BUCKET/build_results/$JOB_NAME/gold/
+rm /tmp/plaso_out/*
