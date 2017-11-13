3816b719935180199557c38ace530d56b88472a6
Joachim Metz <joachim.metz@gmail.com>
2017-11-13 18:56:38 +0100

    Code review: 337020043: Clean up of OpenXML parser #1187

--- plaso/parsers/oxml.py
+++ plaso/parsers/oxml.py
@@ -3,7 +3,6 @@
 
 from __future__ import unicode_literals
 
-import logging
 import re
 import struct
 import zipfile
@@ -14,8 +13,9 @@ from dfdatetime import time_elements as dfdatetime_time_elements
 
 from plaso.containers import events
 from plaso.containers import time_events
-from plaso.lib import errors
 from plaso.lib import definitions
+from plaso.lib import errors
+from plaso.lib import py2to3
 from plaso.parsers import interface
 from plaso.parsers import manager
 
@@ -82,7 +82,7 @@ class OpenXMLParser(interface.FileObjectParser):
   NAME = 'openxml'
   DESCRIPTION = 'Parser for OpenXML (OXML) files.'
 
-  _METAKEY_TRANSLATE = {
+  _PROPERTY_NAMES = {
       'creator': 'author',
       'lastModifiedBy': 'last_saved_by',
       'Total_Time': 'total_edit_time',
@@ -100,11 +100,136 @@ class OpenXMLParser(interface.FileObjectParser):
   _FILES_REQUIRED = frozenset([
       '[Content_Types].xml', '_rels/.rels', 'docProps/core.xml'])
 
-  def _FixString(self, key):
-    """Convert CamelCase to lower_with_underscore."""
+  def _GetPropertyValue(self, parser_mediator, properties, property_name):
+    """Retrieves a property value.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfvfs.
+      properties (dict[str, object]): properties.
+      property_name (str): name of the property.
+
+    Returns:
+      str: property value.
+    """
+    property_value = properties.get(property_name, None)
+    if isinstance(property_value, py2to3.BYTES_TYPE):
+      try:
+        # TODO: get encoding form XML metadata.
+        property_value = property_value.decode('utf-8')
+      except UnicodeDecodeError:
+        parser_mediator.ProduceExtractionError(
+            'unable to decode property: {0:s}'.format(property_name))
+
+    return property_value
+
+  def _FormatPropertyName(self, property_name):
+    """Formats a camel case property name as snake case.
+
+    Args:
+      property_name (str): property name in camel case.
+
+    Returns:
+      str: property name in snake case.
+    """
     # TODO: Add Unicode support.
-    fix_key = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', key)
+    fix_key = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', property_name)
     return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', fix_key).lower()
+
+  def _ParsePropertiesXMLFile(self, xml_data):
+    """Parses a properties XML file.
+
+    Args:
+      xml_data (bytes): data of a _rels/.rels XML file.
+
+    Returns:
+      dict[str, object]: properties.
+
+    Raises:
+      zipfile.BadZipfile: if the properties XML file cannot be read.
+    """
+    xml_root = ElementTree.fromstring(xml_data)
+
+    properties = {}
+    for xml_element in xml_root.iter():
+      if not xml_element.text:
+        continue
+
+      # The property name is formatted as: {URL}name
+      # For example: {http://purl.org/dc/terms/}modified
+      _, _, name = xml_element.tag.partition('}')
+
+      # Do not including the 'lpstr' attribute because it is very verbose.
+      if name == 'lpstr':
+        continue
+
+      property_name = self._PROPERTY_NAMES.get(name, None)
+      if not property_name:
+        property_name = self._FormatPropertyName(name)
+
+      properties[property_name] = xml_element.text
+
+    return properties
+
+  def _ParseRelationshipsXMLFile(self, xml_data):
+    """Parses the relationships XML file (_rels/.rels).
+
+    Args:
+      xml_data (bytes): data of a _rels/.rels XML file.
+
+    Returns:
+      list[str]: property file paths. The path is relative to the root of
+          the ZIP file.
+
+    Raises:
+      zipfile.BadZipfile: if the relationship XML file cannot be read.
+    """
+    xml_root = ElementTree.fromstring(xml_data)
+
+    property_files = []
+    for xml_element in xml_root.iter():
+      type_attribute = xml_element.get('Type')
+      if 'properties' in repr(type_attribute):
+        target_attribute = xml_element.get('Target')
+        property_files.append(target_attribute)
+
+    return property_files
+
+  def _ProduceEvent(
+      self, parser_mediator, event_data, properties, property_name,
+      timestamp_description, error_description):
+    """Produces an event.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfvfs.
+      event_data (OpenXMLEventData): event data.
+      properties (dict[str, object]): properties.
+      property_name (str): name of the date and time property.
+      timestamp_description (str): description of the meaning of the timestamp
+          value.
+      error_description (str): description of the meaning of the timestamp
+          value for error reporting purposes.
+    """
+    time_string = properties.get(property_name, None)
+    if not time_string:
+      return
+
+    # Date and time strings are in ISO 8601 format either with 1 second
+    # or 100th nano second precision. For example:
+    # 2012-11-07T23:29:00Z
+    # 2012-03-05T20:40:00.0000000Z
+    date_time = dfdatetime_time_elements.TimeElements()
+
+    try:
+      date_time.CopyFromStringISO8601(time_string)
+
+      event = time_events.DateTimeValuesEvent(date_time, timestamp_description)
+      parser_mediator.ProduceEventWithEventData(event, event_data)
+    except ValueError as exception:
+      parser_mediator.ProduceExtractionError(
+          'unsupported {0:s}: {1:s} with error: {2!s}'.format(
+              error_description, time_string, exception))
 
   # pylint: disable=arguments-differ
   def ParseFileObject(self, parser_mediator, file_object):
@@ -118,134 +243,107 @@ class OpenXMLParser(interface.FileObjectParser):
     Raises:
       UnableToParseFile: when the file cannot be parsed.
     """
-    file_name = parser_mediator.GetDisplayName()
+    display_name = parser_mediator.GetDisplayName()
 
     if not zipfile.is_zipfile(file_object):
       raise errors.UnableToParseFile(
           '[{0:s}] unable to parse file: {1:s} with error: {2:s}'.format(
-              self.NAME, file_name, 'Not a Zip file.'))
+              self.NAME, display_name, 'Not a Zip file.'))
 
     # Some non-ZIP files pass the first test but will fail with a negative
     # seek (IOError) or another error.
     try:
-      zip_container = zipfile.ZipFile(file_object, 'r')
-    except (IOError, struct.error, zipfile.BadZipfile, zipfile.LargeZipFile):
+      zip_file = zipfile.ZipFile(file_object, 'r')
+    except (zipfile.BadZipfile, struct.error, zipfile.LargeZipFile):
       raise errors.UnableToParseFile(
           '[{0:s}] unable to parse file: {1:s} with error: {2:s}'.format(
-              self.NAME, file_name, 'Bad Zip file.'))
+              self.NAME, display_name, 'Bad Zip file.'))
 
-    zip_name_list = set(zip_container.namelist())
+    zip_name_list = set(zip_file.namelist())
 
     if not self._FILES_REQUIRED.issubset(zip_name_list):
       raise errors.UnableToParseFile(
           '[{0:s}] unable to parse file: {1:s} with error: {2:s}'.format(
-              self.NAME, file_name, 'OXML element(s) missing.'))
-
-    metadata = {}
-    timestamps = {}
+              self.NAME, display_name, 'OXML element(s) missing.'))
 
     try:
-      rels_xml = zip_container.read('_rels/.rels')
-    except zipfile.BadZipfile as exception:
-      parser_mediator.ProduceExtractionError(
-          'Unable to parse file with error: {0!s}'.format(exception))
+      xml_data = zip_file.read('_rels/.rels')
+      property_files = self._ParseRelationshipsXMLFile(xml_data)
+    except (
+        IndexError, IOError, KeyError, OverflowError, ValueError,
+        zipfile.BadZipfile) as exception:
+      parser_mediator.ProduceExtractionError((
+          'Unable to parse relationships XML file: _rels/.rels with error: '
+          '{0!s}').format(exception))
       return
 
-    rels_root = ElementTree.fromstring(rels_xml)
+    metadata = {}
 
-    for properties in rels_root.iter():
-      if 'properties' in repr(properties.get('Type')):
-        try:
-          xml = zip_container.read(properties.get('Target'))
-          root = ElementTree.fromstring(xml)
-        except (
-            OverflowError, IndexError, KeyError, ValueError,
-            zipfile.BadZipfile) as exception:
-          logging.warning(
-              '[{0:s}] unable to read property with error: {1!s}.'.format(
-                  self.NAME, exception))
-          continue
+    for path in property_files:
+      try:
+        xml_data = zip_file.read(path)
+        properties = self._ParsePropertiesXMLFile(xml_data)
+      except (
+          IndexError, IOError, KeyError, OverflowError, ValueError,
+          zipfile.BadZipfile) as exception:
+        parser_mediator.ProduceExtractionError((
+            'Unable to parse properties XML file: {0:s} with error: '
+            '{1!s}').format(path, exception))
+        continue
 
-        for element in root.iter():
-          if element.text:
-            _, _, tag = element.tag.partition('}')
-            # Not including the 'lpstr' attribute because it is
-            # very verbose.
-            if tag == 'lpstr':
-              continue
-
-            if tag in ('created', 'modified', 'lastPrinted'):
-              timestamps[tag] = element.text
-            else:
-              tag_name = self._METAKEY_TRANSLATE.get(tag, self._FixString(tag))
-              metadata[tag_name] = element.text
+      metadata.update(properties)
 
     event_data = OpenXMLEventData()
-    event_data.app_version = metadata.get('app_version', None)
-    event_data.author = metadata.get('author', None)
-    event_data.creating_app = metadata.get('creating_app', None)
-    event_data.doc_security = metadata.get('doc_security', None)
-    event_data.hyperlinks_changed = metadata.get('hyperlinks_changed', None)
-    event_data.i4 = metadata.get('i4', None)
-    event_data.last_saved_by = metadata.get('last_saved_by', None)
-    event_data.links_up_to_date = metadata.get('links_up_to_date', None)
-    event_data.number_of_characters = metadata.get(
-        'number_of_characters', None)
-    event_data.number_of_characters_with_spaces = metadata.get(
-        'number_of_characters_with_spaces', None)
-    event_data.number_of_lines = metadata.get('number_of_lines', None)
-    event_data.number_of_pages = metadata.get('number_of_pages', None)
-    event_data.number_of_paragraphs = metadata.get(
-        'number_of_paragraphs', None)
-    event_data.number_of_words = metadata.get('number_of_words', None)
-    event_data.revision_number = metadata.get('revision_number', None)
-    event_data.scale_crop = metadata.get('scale_crop', None)
-    event_data.shared_doc = metadata.get('shared_doc', None)
-    event_data.template = metadata.get('template', None)
-    event_data.total_time = metadata.get('total_time', None)
+    event_data.app_version = self._GetPropertyValue(
+        parser_mediator, metadata, 'app_version')
+    event_data.app_version = self._GetPropertyValue(
+        parser_mediator, metadata, 'app_version')
+    event_data.author = self._GetPropertyValue(
+        parser_mediator, metadata, 'author')
+    event_data.creating_app = self._GetPropertyValue(
+        parser_mediator, metadata, 'creating_app')
+    event_data.doc_security = self._GetPropertyValue(
+        parser_mediator, metadata, 'doc_security')
+    event_data.hyperlinks_changed = self._GetPropertyValue(
+        parser_mediator, metadata, 'hyperlinks_changed')
+    event_data.i4 = self._GetPropertyValue(
+        parser_mediator, metadata, 'i4')
+    event_data.last_saved_by = self._GetPropertyValue(
+        parser_mediator, metadata, 'last_saved_by')
+    event_data.links_up_to_date = self._GetPropertyValue(
+        parser_mediator, metadata, 'links_up_to_date')
+    event_data.number_of_characters = self._GetPropertyValue(
+        parser_mediator, metadata, 'number_of_characters')
+    event_data.number_of_characters_with_spaces = self._GetPropertyValue(
+        parser_mediator, metadata, 'number_of_characters_with_spaces')
+    event_data.number_of_lines = self._GetPropertyValue(
+        parser_mediator, metadata, 'number_of_lines')
+    event_data.number_of_pages = self._GetPropertyValue(
+        parser_mediator, metadata, 'number_of_pages')
+    event_data.number_of_paragraphs = self._GetPropertyValue(
+        parser_mediator, metadata, 'number_of_paragraphs')
+    event_data.number_of_words = self._GetPropertyValue(
+        parser_mediator, metadata, 'number_of_words')
+    event_data.revision_number = self._GetPropertyValue(
+        parser_mediator, metadata, 'revision_number')
+    event_data.scale_crop = self._GetPropertyValue(
+        parser_mediator, metadata, 'scale_crop')
+    event_data.shared_doc = self._GetPropertyValue(
+        parser_mediator, metadata, 'shared_doc')
+    event_data.template = self._GetPropertyValue(
+        parser_mediator, metadata, 'template')
+    event_data.total_time = self._GetPropertyValue(
+        parser_mediator, metadata, 'total_time')
 
-    # Date and time strings are in ISO 8601 format with 1 second precision.
-    # For example: 2012-11-07T23:29:00Z
-    date_time = dfdatetime_time_elements.TimeElements()
-
-    time_string = timestamps.get('created', None)
-    if time_string:
-      try:
-        date_time.CopyFromStringISO8601(time_string)
-
-        event = time_events.DateTimeValuesEvent(
-            date_time, definitions.TIME_DESCRIPTION_CREATION)
-        parser_mediator.ProduceEventWithEventData(event, event_data)
-      except ValueError as exception:
-        parser_mediator.ProduceExtractionError(
-            'unsupported created time: {0:s} with error: {1!s}'.format(
-                time_string, exception))
-
-    time_string = timestamps.get('modified', None)
-    if time_string:
-      try:
-        date_time.CopyFromStringISO8601(time_string)
-
-        event = time_events.DateTimeValuesEvent(
-            date_time, definitions.TIME_DESCRIPTION_MODIFICATION)
-        parser_mediator.ProduceEventWithEventData(event, event_data)
-      except ValueError as exception:
-        parser_mediator.ProduceExtractionError(
-            'unsupported modified time: {0:s} with error: {1!s}'.format(
-                time_string, exception))
-
-    time_string = timestamps.get('lastPrinted', None)
-    if time_string:
-      try:
-        date_time.CopyFromStringISO8601(time_string)
-
-        event = time_events.DateTimeValuesEvent(
-            date_time, definitions.TIME_DESCRIPTION_LAST_PRINTED)
-        parser_mediator.ProduceEventWithEventData(event, event_data)
-      except ValueError as exception:
-        parser_mediator.ProduceExtractionError(
-            'unsupported last printed time: {0:s} with error: {1!s}'.format(
-                time_string, exception))
+    self._ProduceEvent(
+        parser_mediator, event_data, metadata, 'created',
+        definitions.TIME_DESCRIPTION_CREATION, 'creation time')
+    self._ProduceEvent(
+        parser_mediator, event_data, metadata, 'modified',
+        definitions.TIME_DESCRIPTION_MODIFICATION, 'modification time')
+    self._ProduceEvent(
+        parser_mediator, event_data, metadata, 'last_printed',
+        definitions.TIME_DESCRIPTION_LAST_PRINTED, 'last printed time')
 
 
 manager.ParsersManager.RegisterParser(OpenXMLParser)
--- tests/parsers/oxml.py
+++ tests/parsers/oxml.py
@@ -18,9 +18,95 @@ from tests.parsers import test_lib
 class OXMLTest(test_lib.ParserTestCase):
   """Tests for the OXML parser."""
 
+  # pylint: disable=protected-access
+
+  _PROPERTIES_XML_DATA = ''.join([
+      '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',
+      ('<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/'
+       'package/2006/metadata/core-properties" xmlns:dc="http://purl.org/'
+       'dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" '
+       'xmlns:dcmitype="http://purl.org/dc/dcmitype/" '
+       'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'),
+      '<dc:creator>Nides</dc:creator>',
+      '<cp:lastModifiedBy>Nides</cp:lastModifiedBy>',
+      '<cp:revision>3</cp:revision>',
+      ('<dcterms:created xsi:type="dcterms:W3CDTF">2012-11-07T23:29:00.1234567Z'
+       '</dcterms:created>'),
+      ('<dcterms:modified xsi:type="dcterms:W3CDTF">2013-08-25T22:18:00Z'
+       '</dcterms:modified>'),
+      '</cp:coreProperties>'])
+
+  _RELATIONSHIPS_XML_DATA = ''.join([
+      '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',
+      ('<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/'
+       'relationships">'),
+      ('<Relationship Id="rId3" Type="http://schemas.openxmlformats.org/'
+       'package/2006/relationships/metadata/core-properties" Target="docProps/'
+       'core.xml"/>'),
+      ('<Relationship Id="rId2" Type="http://schemas.openxmlformats.org/'
+       'package/2006/relationships/metadata/thumbnail" Target="docProps/'
+       'thumbnail.emf"/>'),
+      ('<Relationship Id="rId1" Type="http://schemas.openxmlformats.org/'
+       'officeDocument/2006/relationships/officeDocument" Target="word/'
+       'document.xml"/>'),
+      ('<Relationship Id="rId4" Type="http://schemas.openxmlformats.org/'
+       'officeDocument/2006/relationships/extended-properties" '
+       'Target="docProps/app.xml"/>'),
+      '</Relationships>'])
+
+  # TODO: add tests for _FormatPropertyName.
+
+  def testParsePropertiesXMLFile(self):
+    """Tests the _ParsePropertiesXMLFile function."""
+    parser = oxml.OpenXMLParser()
+
+    expected_properties = {
+        'author': 'Nides',
+        'created': '2012-11-07T23:29:00.1234567Z',
+        'last_saved_by': 'Nides',
+        'modified': '2013-08-25T22:18:00Z',
+        'revision_number': '3'}
+
+    properties = parser._ParsePropertiesXMLFile(self._PROPERTIES_XML_DATA)
+    self.assertEqual(properties, expected_properties)
+
+  def testParseRelationshipsXMLFile(self):
+    """Tests the _ParseRelationshipsXMLFile function."""
+    parser = oxml.OpenXMLParser()
+
+    expected_property_files = ['docProps/core.xml', 'docProps/app.xml']
+
+    property_files = parser._ParseRelationshipsXMLFile(
+        self._RELATIONSHIPS_XML_DATA)
+    self.assertEqual(property_files, expected_property_files)
+
+  def testProduceEvent(self):
+    """Tests the _ProduceEvent function."""
+    storage_writer = self._CreateStorageWriter()
+    parser_mediator = self._CreateParserMediator(storage_writer)
+    event_data = oxml.OpenXMLEventData()
+
+    parser = oxml.OpenXMLParser()
+
+    properties = parser._ParsePropertiesXMLFile(self._PROPERTIES_XML_DATA)
+
+    # Test parsing a date and time string in intervals of 1 s.
+    parser._ProduceEvent(
+        parser_mediator, event_data, properties, 'modified',
+        definitions.TIME_DESCRIPTION_MODIFICATION, 'modification time')
+
+    self.assertEqual(storage_writer.number_of_events, 1)
+
+    # Test parsing a date and time string in intervals of 100 ns.
+    parser._ProduceEvent(
+        parser_mediator, event_data, properties, 'created',
+        definitions.TIME_DESCRIPTION_CREATION, 'creation time')
+
+    self.assertEqual(storage_writer.number_of_events, 2)
+
   @shared_test_lib.skipUnlessHasTestFile(['Document.docx'])
-  def testParse(self):
-    """Tests the Parse function."""
+  def testParseFileObject(self):
+    """Tests the ParseFileObject function."""
     parser = oxml.OpenXMLParser()
     storage_writer = self._ParseFile(['Document.docx'], parser)
 
@@ -30,8 +116,7 @@ class OXMLTest(test_lib.ParserTestCase):
 
     event = events[0]
 
-    expected_timestamp = timelib.Timestamp.CopyFromString(
-        '2012-11-07 23:29:00')
+    expected_timestamp = timelib.Timestamp.CopyFromString('2012-11-07 23:29:00')
     self.assertEqual(event.timestamp, expected_timestamp)
     self.assertEqual(
         event.timestamp_desc, definitions.TIME_DESCRIPTION_CREATION)
@@ -54,8 +139,7 @@ class OXMLTest(test_lib.ParserTestCase):
     self.assertEqual(event.revision_number, '3')
     self.assertEqual(event.last_saved_by, 'Nides')
     self.assertEqual(event.author, 'Nides')
-    self.assertEqual(
-        event.creating_app, 'Microsoft Office Word')
+    self.assertEqual(event.creating_app, 'Microsoft Office Word')
     self.assertEqual(event.template, 'Normal.dotm')
 
     expected_message = (
