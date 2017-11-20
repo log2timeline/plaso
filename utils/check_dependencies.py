f797f33db41446eb6ecafb0c15a140d749d983e8
Joachim Metz <joachim.metz@gmail.com>
2017-11-20 07:37:13 +0100

    Code review: 333140043: Made Unicode strings the default in utilility scripts #1268

--- utils/check_dependencies.py
+++ utils/check_dependencies.py
@@ -3,20 +3,22 @@
 """Script to check for the availability and version of dependencies."""
 
 from __future__ import print_function
+from __future__ import unicode_literals
+
 import sys
 
 # Change PYTHONPATH to include dependencies.
-sys.path.insert(0, u'.')
+sys.path.insert(0, '.')
 
 import utils.dependencies  # pylint: disable=wrong-import-position
 
 
-if __name__ == u'__main__':
+if __name__ == '__main__':
   dependency_helper = utils.dependencies.DependencyHelper()
 
   if not dependency_helper.CheckDependencies():
     build_instructions_url = (
-        u'https://github.com/log2timeline/plaso/wiki/Users-Guide')
+        'https://github.com/log2timeline/plaso/wiki/Users-Guide')
 
-    print(u'See: {0:s} on how to set up plaso.'.format(build_instructions_url))
-    print(u'')
+    print('See: {0:s} on how to set up plaso.'.format(build_instructions_url))
+    print('')
