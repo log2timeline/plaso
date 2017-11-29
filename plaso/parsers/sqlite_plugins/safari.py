d462306ffea3d1cbb6ea02b4263057682b28e5e5
chima Uzoekwe <mails.chima@gmail.com>
2017-11-28 16:25:26 -0800

    .

--- plaso/parsers/sqlite_plugins/safari.py
+++ plaso/parsers/sqlite_plugins/safari.py
@@ -69,7 +69,7 @@ class SafariHistoryPluginSqlite(interface.SQLitePlugin):
   	       u'should_recompute_derived_visit_counts INTEGER NOT NULL, '
           u'visit_count_score INTEGER NOT NULL)'),
 		    u'history_tombstones': (
-			                   u'CREATE TABLE history_tombstones (id INTEGER PRIMARY KEY AUTOINCREMENT, '
+			     u'CREATE TABLE history_tombstones (id INTEGER PRIMARY KEY AUTOINCREMENT, '
 			     u'start_time REAL NOT NULL, end_time REAL NOT NULL, '
 			     u'url TEXT,generation INTEGER NOT NULL DEFAULT 0)'),
 		  u'metadata': (
