d4667d07f8d29149c09dda9f0da2bc0be8d54527
chima Uzoekwe <mails.chima@gmail.com>
2017-12-07 07:05:38 -0800

    .

--- tests/formatters/safari.py
+++ tests/formatters/safari.py
@@ -23,16 +23,13 @@ class SafariHistoryFormatterTest(test_lib.EventFormatterTestCase):
     """Tests the GetFormatStringAttributeNames function."""
     event_formatter = safari.SafariHistoryFormatter()
 
-    expected_attribute_names = [
-        'url',
-        'title',
-        'display_title',
-        'visit_count']
+    expected_attribute_names = ['url', 'title', 'display_title', 'visit_count']
 
-    self._TestGetFormatStringAttributeNames(
-        event_formatter, expected_attribute_names)
+    self._TestGetFormatStringAttributeNames(event_formatter,
+                                            expected_attribute_names)
 
   # TODO: add test for GetMessages.
+
 
 class SafariHistoryFormatterSqlite(test_lib.EventFormatterTestCase):
   """Tests for the Safari history event formatter."""
@@ -47,15 +44,11 @@ class SafariHistoryFormatterSqlite(test_lib.EventFormatterTestCase):
     event_formatter = safari.SafariHistoryFormatterSqlite()
 
     expected_attribute_names = [
-        'url',
-        'title',
-        'visit_count',
-        'host',
-        'was_http_non_get']
+        'url', 'title', 'visit_count', 'host', 'was_http_non_get'
+    ]
 
-    self._TestGetFormatStringAttributeNames(
-        event_formatter, expected_attribute_names)
-
+    self._TestGetFormatStringAttributeNames(event_formatter,
+                                            expected_attribute_names)
 
 
 if __name__ == '__main__':
