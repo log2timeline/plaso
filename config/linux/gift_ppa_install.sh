db11c5e8dc34f7521f25ac3bbd3c947ed363df93
Joachim Metz <joachim.metz@gmail.com>
2017-12-17 08:55:50 +0100

    Updated dependencies

--- config/linux/gift_ppa_install.sh
+++ config/linux/gift_ppa_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Dependencies for running Plaso, alphabetized, one per line.
+# Dependencies for running plaso, alphabetized, one per line.
 # This should not include packages only required for testing or development.
 PYTHON2_DEPENDENCIES="libbde-python
                       libesedb-python
@@ -58,16 +58,16 @@ PYTHON2_DEPENDENCIES="libbde-python
                       python-yara
                       python-zmq";
 
-# Additional dependencies for running Plaso tests, alphabetized,
+# Additional dependencies for running plaso tests, alphabetized,
 # one per line.
 TEST_DEPENDENCIES="python-mock";
 
-# Additional dependencies for doing Plaso development, alphabetized,
+# Additional dependencies for doing plaso development, alphabetized,
 # one per line.
 DEVELOPMENT_DEPENDENCIES="python-sphinx
                           pylint";
 
-# Additional dependencies for doing Plaso debugging, alphabetized,
+# Additional dependencies for doing plaso debugging, alphabetized,
 # one per line.
 DEBUG_DEPENDENCIES="libbde-dbg
                     libbde-python-dbg
