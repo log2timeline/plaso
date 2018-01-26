52eebc48de3a00a021652538967e7380b1d91e30
Daniel White <onager@deerpie.com>
2018-01-26 17:59:44 +0800

    Add parser attribute to event #1687

--- plaso/parsers/mediator.py
+++ plaso/parsers/mediator.py
@@ -520,6 +520,9 @@ class ParserMediator(object):
     if self._last_event_data_identifier:
       event.SetEventDataIdentifier(self._last_event_data_identifier)
 
+    # TODO: rename this to event.parser_chain or equivalent.
+    event.parser_chain = self.GetParserChain()
+
     self._storage_writer.AddEvent(event)
     self._number_of_events += 1
 
