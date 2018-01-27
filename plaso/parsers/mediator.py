cc792c60c40affdca1a85386750b14f8e72757c9
Daniel White <onager@deerpie.com>
2018-01-27 19:44:11 +0100

    Code review: 338420043: Added parser attribute to event #1687

--- plaso/parsers/mediator.py
+++ plaso/parsers/mediator.py
@@ -520,6 +520,10 @@ class ParserMediator(object):
     if self._last_event_data_identifier:
       event.SetEventDataIdentifier(self._last_event_data_identifier)
 
+    # TODO: remove this after structural fix is in place
+    # https://github.com/log2timeline/plaso/issues/1691
+    event.parser_chain = self.GetParserChain()
+
     self._storage_writer.AddEvent(event)
     self._number_of_events += 1
 
