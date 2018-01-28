3be7f6180d28b2762dd28a9829e8e8a1820f7daf
Daniel White <onager@deerpie.com>
2018-01-28 15:07:17 +0800

    Code review: 338430043: Fixed parser attribute name #1687

--- plaso/parsers/mediator.py
+++ plaso/parsers/mediator.py
@@ -522,7 +522,7 @@ class ParserMediator(object):
 
     # TODO: remove this after structural fix is in place
     # https://github.com/log2timeline/plaso/issues/1691
-    event.parser_chain = self.GetParserChain()
+    event.parser = self.GetParserChain()
 
     self._storage_writer.AddEvent(event)
     self._number_of_events += 1
