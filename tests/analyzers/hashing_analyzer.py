8919c7d663c3cfa9a69f75a69bd6469bf4b94b50
Joachim Metz <joachim.metz@gmail.com>
2018-03-29 13:03:27 +0200

    Code review: 342860043: Addtional updates to shebang of tests

--- tests/analyzers/hashing_analyzer.py
+++ tests/analyzers/hashing_analyzer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tests for the Hashing analyzer."""
 
--- tests/cli/helpers/profiling.py
+++ tests/cli/helpers/profiling.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tests for the profiling CLI arguments helper."""
 
