8e192d7ea357be31ee98726cb3f1904940cd696e
Joachim Metz <joachim.metz@gmail.com>
2018-03-29 07:23:40 +0200

    Code review: 340760043: Added deprecated decorator

---
+++ plaso/lib/decorators.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Function decorators."""
+
+from __future__ import unicode_literals
+
+import warnings
+
+
+# pylint: disable=invalid-name
+
+def deprecated(function):
+  """Decorator to mark functions or methods as deprecated."""
+
+  def IssueDeprecationWarning(*args, **kwargs):
+    """Issue a deprecation warning."""
+    warnings.simplefilter('default', DeprecationWarning)
+    warnings.warn('Call to deprecated function: {0:s}.'.format(
+        function.__name__), category=DeprecationWarning, stacklevel=2)
+
+    return function(*args, **kwargs)
+
+  IssueDeprecationWarning.__name__ = function.__name__
+  IssueDeprecationWarning.__doc__ = function.__doc__
+  IssueDeprecationWarning.__dict__.update(function.__dict__)
+  return IssueDeprecationWarning
