732fb3464ee22e69c8110829a0c9a2dfc3c2a535
Joachim Metz <joachim.metz@gmail.com>
2018-03-30 07:19:21 +0200

    Changed CPU time profiler to store samples instead of summary #1779

--- plaso/multi_processing/psort.py
+++ plaso/multi_processing/psort.py
@@ -726,7 +726,7 @@ class PsortMultiProcessEngine(multi_process_engine.MultiProcessEngine):
     """Stops profiling."""
     if self._guppy_memory_profiler:
       self._guppy_memory_profiler.Sample()
-      self._guppy_memory_profiler.Stop(
+      self._guppy_memory_profiler.Stop()
       self._guppy_memory_profiler = None
 
     if self._processing_profiler:
@@ -736,7 +736,6 @@ class PsortMultiProcessEngine(multi_process_engine.MultiProcessEngine):
     if self._serializers_profiler:
       self._serializers_profiler.Stop()
       self._serializers_profiler = None
-
 
   def _UpdateProcessingStatus(self, pid, process_status, used_memory):
     """Updates the processing status.
