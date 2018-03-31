4d08c5cbe16d682dbdaaae6f6cb18486631bc960
Daniel White <onager@deerpie.com>
2018-03-31 09:23:36 +0200

    Fix linter error, change error message

--- plaso/engine/worker.py
+++ plaso/engine/worker.py
@@ -635,7 +635,7 @@ class EventExtractionWorker(object):
           if self._CanSkipDataStream(file_entry, data_stream):
             logging.debug(
                 ('[ProcessFileEntry] Skipping datastream {0:s} '
-                 'for {0:1s}: {2:s}').format(
+                 'for {1:s}: {2:s}').format(
                      data_stream.name, file_entry.type, display_name))
             continue
 
