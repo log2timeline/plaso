a48203b343730d3b3756c4588e6082ad6f467e15
Joachim Metz <joachim.metz@gmail.com>
2018-03-31 13:49:31 +0200

    Code review: 341010043: Improved task manager tests and code clean up

--- tests/multi_processing/task_manager.py
+++ tests/multi_processing/task_manager.py
@@ -15,39 +15,460 @@ from plaso.multi_processing import task_manager
 from tests import test_lib as shared_test_lib
 
 
-class TaskManagerTestCase(shared_test_lib.BaseTestCase):
-  """Tests for the TaskManager."""
+class PendingMergeTaskHeapTest(shared_test_lib.BaseTestCase):
+  """Tests for the heap to manage pending merge tasks."""
 
   # pylint: disable=protected-access
 
-  _TEST_SESSION_IDENTIFIER = '4'
+  def testContains(self):
+    """Tests the __contains__ function."""
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    heap = task_manager._PendingMergeTaskHeap()
+    heap.PushTask(task)
+
+    self.assertIn(task.identifier, heap)
+
+  def testLengthProperty(self):
+    """Tests the __len__ function."""
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    heap = task_manager._PendingMergeTaskHeap()
+    self.assertEqual(len(heap), 0)
+
+    heap.PushTask(task)
+    self.assertEqual(len(heap), 1)
+
+  def testPeekTask(self):
+    """Tests the PeekTask function."""
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    heap = task_manager._PendingMergeTaskHeap()
+
+    result_task = heap.PeekTask()
+    self.assertIsNone(result_task)
+
+    heap.PushTask(task)
+    self.assertEqual(len(heap), 1)
+
+    result_task = heap.PeekTask()
+    self.assertEqual(len(heap), 1)
+    self.assertEqual(result_task, task)
+
+  def testPopTask(self):
+    """Tests the PopTask function."""
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    heap = task_manager._PendingMergeTaskHeap()
+
+    result_task = heap.PopTask()
+    self.assertIsNone(result_task)
+
+    heap.PushTask(task)
+    self.assertEqual(len(heap), 1)
+
+    result_task = heap.PopTask()
+    self.assertEqual(len(heap), 0)
+    self.assertEqual(result_task, task)
+
+  def testPushTask(self):
+    """Tests the PushTask function."""
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    heap = task_manager._PendingMergeTaskHeap()
+    self.assertEqual(len(heap), 0)
+
+    heap.PushTask(task)
+    self.assertEqual(len(heap), 1)
+
+    task = tasks.Task()
+    task.storage_file_size = 100
+
+    heap.PushTask(task)
+    self.assertEqual(len(heap), 2)
+
+    task = tasks.Task()
+    with self.assertRaises(ValueError):
+      heap.PushTask(task)
+
+
+class TaskManagerTest(shared_test_lib.BaseTestCase):
+  """Tests for the task manager."""
+
+  # pylint: disable=protected-access
+
+  _TEST_SESSION_IDENTIFIER = 'b362ab408b054cb08cf69b4c7989df89'
+
+  def testHasTasksPendingMerge(self):
+    """Tests the _HasTasksPendingMerge function."""
+    manager = task_manager.TaskManager()
+
+    result = manager._HasTasksPendingMerge()
+    self.assertFalse(result)
+
+    # TODO: test True condition.
+
+  def testHasTasksPendingRetry(self):
+    """Tests the _HasTasksPendingRetry function."""
+    manager = task_manager.TaskManager()
+
+    result = manager._HasTasksPendingRetry()
+    self.assertFalse(result)
+
+    # TODO: test True condition.
+
+  def testTaskIsRetriable(self):
+    """Tests the _TaskIsRetriable function."""
+    task = tasks.Task()
+
+    manager = task_manager.TaskManager()
+
+    result = manager._TaskIsRetriable(task)
+    self.assertTrue(result)
+
+    task.retried = True
+    result = manager._TaskIsRetriable(task)
+    self.assertFalse(result)
+
+  def testTimeoutTasks(self):
+    """Tests the _TimeoutTasks function."""
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    manager = task_manager.TaskManager()
+
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+
+    tasks_for_timeout = {}
+    manager._TimeoutTasks(tasks_for_timeout)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+
+    tasks_for_timeout = {task.identifier: task}
+
+    manager._TimeoutTasks(tasks_for_timeout)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertNotEqual(tasks_for_timeout, {})
+
+    task.start_time -= 2 * manager._TASK_INACTIVE_TIME
+
+    manager._TimeoutTasks(tasks_for_timeout)
+    self.assertEqual(len(manager._tasks_abandoned), 1)
+    self.assertEqual(tasks_for_timeout, {})
 
   def testCreateTask(self):
-    """Tests the CreateTask methods."""
+    """Tests the CreateTask function."""
     manager = task_manager.TaskManager()
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+
     task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    self.assertEqual(len(manager._tasks_queued), 1)
+
     self.assertIsInstance(task, tasks.Task)
     self.assertEqual(task.session_identifier, self._TEST_SESSION_IDENTIFIER)
     self.assertIsNotNone(task.identifier)
+    self.assertIsNone(task.last_processing_time)
 
-  def testTaskProcessing(self):
-    """Tests the UpdateTaskAsProcessing and GetTasksCheckMerge methods."""
+  def testCompleteTask(self):
+    """Tests the CompleteTask function."""
     manager = task_manager.TaskManager()
-    tasks_processing = manager.GetTasksCheckMerge()
-    self.assertEqual(0, len(tasks_processing))
+
+    task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    self.assertEqual(len(manager._tasks_queued), 1)
+
+    manager.CompleteTask(task)
+    self.assertEqual(len(manager._tasks_queued), 0)
+
+    # TODO: improve test coverage.
+
+  def testGetAbandonedTasks(self):
+    """Tests the GetAbandonedTasks function."""
+    manager = task_manager.TaskManager()
+
+    result_tasks = manager.GetAbandonedTasks()
+    self.assertEqual(result_tasks, [])
+
+    # TODO: improve test coverage.
+
+  def testGetRetryTask(self):
+    """Tests the GetRetryTask function."""
+    manager = task_manager.TaskManager()
+
+    result_task = manager.GetRetryTask()
+    self.assertIsNone(result_task)
+
+    # TODO: improve test coverage.
+
+  def testGetStatusInformation(self):
+    """Tests the GetStatusInformation function."""
+    manager = task_manager.TaskManager()
+    manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+
+    result_status = manager.GetStatusInformation()
+    self.assertIsNotNone(result_status)
+
+    self.assertEqual(result_status.number_of_abandoned_tasks, 0)
+    self.assertEqual(result_status.number_of_queued_tasks, 1)
+    self.assertEqual(result_status.number_of_tasks_pending_merge, 0)
+    self.assertEqual(result_status.number_of_tasks_processing, 0)
+    self.assertEqual(result_status.total_number_of_tasks, 1)
+
+  def testGetTasksCheckMerge(self):
+    """Tests the GetTasksCheckMerge function."""
+    manager = task_manager.TaskManager()
+    task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+
+    result_tasks = manager.GetTasksCheckMerge()
+    self.assertEqual(result_tasks, [task])
+
+  def testGetTaskPendingMerge(self):
+    """Tests the GetTaskPendingMerge function."""
+    current_task = tasks.Task()
+    current_task.storage_file_size = 10
+
+    manager = task_manager.TaskManager()
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+    self.assertEqual(len(manager._tasks_merging), 0)
+
+    task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    task.storage_file_size = 10
+
+    self.assertEqual(len(manager._tasks_queued), 1)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+    self.assertEqual(len(manager._tasks_merging), 0)
+
+    # Determine if there are tasks pending merge when _tasks_pending_merge is
+    # empty.
+    result_task = manager.GetTaskPendingMerge(current_task)
+    self.assertIsNone(result_task)
+
+    self.assertEqual(len(manager._tasks_queued), 1)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+    self.assertEqual(len(manager._tasks_merging), 0)
+
+    manager.UpdateTaskAsPendingMerge(task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+    self.assertEqual(len(manager._tasks_merging), 0)
+
+    # Determine if there are tasks pending merge when there is a current task
+    # with a higher merge priority.
+    current_task.merge_priority = 1
+    task.merge_priority = 1000
+
+    result_task = manager.GetTaskPendingMerge(current_task)
+    self.assertIsNone(result_task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+    self.assertEqual(len(manager._tasks_merging), 0)
+
+    # Determine if there are tasks pending merge.
+    result_task = manager.GetTaskPendingMerge(None)
+    self.assertIsNotNone(result_task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+    self.assertEqual(len(manager._tasks_merging), 1)
+
+  def testHasPendingTasks(self):
+    """Tests the HasPendingTasks function."""
+    manager = task_manager.TaskManager()
+
+    result = manager.HasPendingTasks()
+    self.assertFalse(result)
+
+    manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+
+    result = manager.HasPendingTasks()
+    self.assertTrue(result)
+
+    # TODO: improve test coverage.
+
+  def testUpdateTaskAsPendingMerge(self):
+    """Tests the UpdateTaskAsPendingMerge function."""
+    manager = task_manager.TaskManager()
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    task.storage_file_size = 10
+
+    self.assertEqual(len(manager._tasks_queued), 1)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    # Indicate to the task manager that a queued task is pending merge.
+    manager.UpdateTaskAsPendingMerge(task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+
+    # Indicate to the task manager that a processing task is pending merge.
+    task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    task.storage_file_size = 10
+
+    self.assertEqual(len(manager._tasks_queued), 1)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+
+    manager.UpdateTaskAsProcessingByIdentifier(task.identifier)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 1)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+
+    manager.UpdateTaskAsPendingMerge(task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 2)
+
+    # Indicate to the task manager that an abandoned task is pending merge.
+    task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    task.storage_file_size = 10
+
+    self.assertEqual(len(manager._tasks_queued), 1)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 2)
+
+    task.start_time -= 2 * manager._TASK_INACTIVE_TIME
+
+    manager._TimeoutTasks(manager._tasks_queued)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 1)
+    self.assertEqual(len(manager._tasks_pending_merge), 2)
+
+    manager.UpdateTaskAsPendingMerge(task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 3)
+
+    # Indicate to the task manager that an unknown task is pending merge.
+    task = tasks.Task()
+    task.storage_file_size = 10
+
+    with self.assertRaises(KeyError):
+      manager.UpdateTaskAsProcessingByIdentifier(task.identifier)
+
+  def testUpdateTasksAsPendingMerge(self):
+    """Tests the UpdateTasksAsPendingMerge function."""
+    manager = task_manager.TaskManager()
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    task1 = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    task1.storage_file_size = 10
+
+    task2 = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
+    task2.storage_file_size = 10
+
+    self.assertEqual(len(manager._tasks_queued), 2)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    manager.UpdateTasksAsPendingMerge([task1, task2])
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 2)
+
+  def testUpdateTaskAsProcessingByIdentifier(self):
+    """Tests the UpdateTaskAsProcessingByIdentifier function."""
+    manager = task_manager.TaskManager()
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
 
     task = manager.CreateTask(self._TEST_SESSION_IDENTIFIER)
     self.assertIsNone(task.last_processing_time)
 
+    self.assertEqual(len(manager._tasks_queued), 1)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    # Indicate to the task manager that the task is processing.
     manager.UpdateTaskAsProcessingByIdentifier(task.identifier)
-    tasks_processing = manager.GetTasksCheckMerge()
-    self.assertEqual(1, len(tasks_processing))
-    task = tasks_processing[0]
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 1)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    # Check if the last processing time was set.
     self.assertIsNotNone(task.last_processing_time)
 
-    # Simulate the foreman marking a task as complete while the manager
-    # believed it to still be processing.
-    manager.CompleteTask(task)
+    last_processing_time = task.last_processing_time
+
+    time.sleep(0.01)
+
+    # Indicate to the task manager that the task is still processing.
+    manager.UpdateTaskAsProcessingByIdentifier(task.identifier)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 1)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 0)
+
+    # Check if the last processing time was updated.
+    self.assertGreater(task.last_processing_time, last_processing_time)
+
+    # TODO: test abandoned task
+
+    # Indicate to the task manager that the task is pending merge.
+    task.storage_file_size = 10
+
+    manager.UpdateTaskAsPendingMerge(task)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+
+    manager.UpdateTaskAsProcessingByIdentifier(task.identifier)
+
+    self.assertEqual(len(manager._tasks_queued), 0)
+    self.assertEqual(len(manager._tasks_processing), 0)
+    self.assertEqual(len(manager._tasks_abandoned), 0)
+    self.assertEqual(len(manager._tasks_pending_merge), 1)
+
+    # Indicate to the task manager that an unknown task is pending merge.
+    task = tasks.Task()
+
+    with self.assertRaises(KeyError):
+      manager.UpdateTaskAsProcessingByIdentifier(task.identifier)
+
+  # TODO: determine what to do with the following tests.
 
   def testPendingMerge(self):
     """Tests the UpdateTaskPendingMerge and GetTaskPending merge methods."""
