34eb7298ee27e4b9a9259b0cc43cb403e483617b
Joachim Metz <joachim.metz@gmail.com>
2018-05-01 20:22:20 +0200

    Added elasticsearch-py as dependency

--- tests/output/timesketch_out.py
+++ tests/output/timesketch_out.py
@@ -24,7 +24,7 @@ from tests.output import test_lib
 # Mock the imports if timesketch is not available. If timesketch is
 # not available the timesketch attribute is set to None in the
 # output module.
-if timesketch_out.timesketch:
+if timesketch_out:
   timesketch_mock = Mock()
   timesketch_mock.create_app = MagicMock()
 
@@ -47,6 +47,7 @@ class TimesketchTestConfig(object):
   flush_interval = 1000
 
 
+@unittest.skipIf(timesketch_out == None, 'missing timesketch')
 class TimesketchOutputModuleTest(test_lib.OutputModuleTestCase):
   """Tests for the Timesketch output module."""
 
