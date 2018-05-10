d293b66eae3da8476b27d95d7287fba6a4fe85f2
Joachim Metz <joachim.metz@gmail.com>
2018-05-10 10:14:12 +0200

    Moved creation of date time string into separate method in 4n6time output module

--- plaso/output/shared_4n6time.py
+++ plaso/output/shared_4n6time.py
@@ -3,8 +3,6 @@
 
 from __future__ import unicode_literals
 
-from dfdatetime import posix_time as dfdatetime_posix_time
-
 from plaso.lib import definitions
 from plaso.lib import errors
 from plaso.lib import timelib
