8147f7c0ac16f9bda59bff51c5ea72b0fc86584c
Joachim Metz <joachim.metz@gmail.com>
2018-05-27 11:30:02 +0200

    Migrated winjob construct-based parser to use dtfabric #1893

--- plaso/parsers/winjob.py
+++ plaso/parsers/winjob.py
@@ -3,18 +3,19 @@
 
 from __future__ import unicode_literals
 
-import construct
+import os
 
 from dfdatetime import definitions as dfdatetime_definitions
 from dfdatetime import systemtime as dfdatetime_systemtime
 from dfdatetime import time_elements as dfdatetime_time_elements
 
+from dtfabric.runtime import fabric as dtfabric_fabric
+
 from plaso.containers import events
 from plaso.containers import time_events
-from plaso.lib import binary
 from plaso.lib import errors
 from plaso.lib import definitions
-from plaso.parsers import interface
+from plaso.parsers import data_formats
 from plaso.parsers import manager
 
 
@@ -43,11 +44,28 @@ class WinJobEventData(events.EventData):
     self.working_directory = None
 
 
-class WinJobParser(interface.FileObjectParser):
+class WinJobParser(data_formats.DataFormatParser):
   """Parse Windows Scheduled Task files for job events."""
 
   NAME = 'winjob'
   DESCRIPTION = 'Parser for Windows Scheduled Task job (or At-job) files.'
+
+  _DATA_TYPE_FABRIC_DEFINITION_FILE = os.path.join(
+      os.path.dirname(__file__), 'winjob.yaml')
+
+  with open(_DATA_TYPE_FABRIC_DEFINITION_FILE, 'rb') as file_object:
+    _DATA_TYPE_FABRIC_DEFINITION = file_object.read()
+
+  _DATA_TYPE_FABRIC = dtfabric_fabric.DataTypeFabric(
+      yaml_definition=_DATA_TYPE_FABRIC_DEFINITION)
+
+  _FIXED_LENGTH_DATA_SECTION = _DATA_TYPE_FABRIC.CreateDataTypeMap(
+      'job_fixed_length_data_section')
+
+  _FIXED_LENGTH_DATA_SECTION_SIZE = _FIXED_LENGTH_DATA_SECTION.GetByteSize()
+
+  _VARIABLE_LENGTH_DATA_SECTION = _DATA_TYPE_FABRIC.CreateDataTypeMap(
+      'job_variable_length_data_section')
 
   _EMPTY_SYSTEM_TIME_TUPLE = (0, 0, 0, 0, 0, 0, 0, 0)
 
@@ -62,140 +80,45 @@ class WinJobParser(interface.FileObjectParser):
       0x0a00: 'Windows 10',
   }
 
-  _JOB_FIXED_LENGTH_SECTION_STRUCT = construct.Struct(
-      'job_fixed_length_section',
-      construct.ULInt16('product_version'),
-      construct.ULInt16('format_version'),
-      construct.Bytes('job_uuid', 16),
-      construct.ULInt16('application_length_offset'),
-      construct.ULInt16('trigger_offset'),
-      construct.ULInt16('error_retry_count'),
-      construct.ULInt16('error_retry_interval'),
-      construct.ULInt16('idle_deadline'),
-      construct.ULInt16('idle_wait'),
-      construct.ULInt32('priority'),
-      construct.ULInt32('max_run_time'),
-      construct.ULInt32('exit_code'),
-      construct.ULInt32('status'),
-      construct.ULInt32('flags'),
-      construct.Struct(
-          'last_run_time',
-          construct.ULInt16('year'),
-          construct.ULInt16('month'),
-          construct.ULInt16('weekday'),
-          construct.ULInt16('day'),
-          construct.ULInt16('hours'),
-          construct.ULInt16('minutes'),
-          construct.ULInt16('seconds'),
-          construct.ULInt16('milliseconds')))
+  def _ParseEventData(self, variable_length_section):
+    """Parses the event data form a variable-length data section.
 
-  # Using Construct's utf-16 encoding here will create strings with their
-  # null terminators exposed. Instead, we'll read these variables raw and
-  # convert them using Plaso's ReadUTF16() for proper formatting.
-  _JOB_VARIABLE_STRUCT = construct.Struct(
-      'job_variable_length_section',
-      construct.ULInt16('running_instance_count'),
-      construct.ULInt16('application_length'),
-      construct.String(
-          'application',
-          lambda ctx: ctx.application_length * 2),
-      construct.ULInt16('parameter_length'),
-      construct.String(
-          'parameter',
-          lambda ctx: ctx.parameter_length * 2),
-      construct.ULInt16('working_directory_length'),
-      construct.String(
-          'working_directory',
-          lambda ctx: ctx.working_directory_length * 2),
-      construct.ULInt16('username_length'),
-      construct.String(
-          'username',
-          lambda ctx: ctx.username_length * 2),
-      construct.ULInt16('comment_length'),
-      construct.String(
-          'comment',
-          lambda ctx: ctx.comment_length * 2),
-      construct.ULInt16('userdata_length'),
-      construct.String(
-          'userdata',
-          lambda ctx: ctx.userdata_length),
-      construct.ULInt16('reserved_length'),
-      construct.String(
-          'reserved',
-          lambda ctx: ctx.reserved_length),
-      construct.ULInt16('number_of_triggers'))
+    Args:
+      variable_length_section (job_fixed_length_data_section): a variable-length
+          data section.
 
-  _TRIGGER_STRUCT = construct.Struct(
-      'trigger',
-      construct.ULInt16('size'),
-      construct.ULInt16('reserved1'),
-      construct.ULInt16('start_year'),
-      construct.ULInt16('start_month'),
-      construct.ULInt16('start_day'),
-      construct.ULInt16('end_year'),
-      construct.ULInt16('end_month'),
-      construct.ULInt16('end_day'),
-      construct.ULInt16('start_hour'),
-      construct.ULInt16('start_minute'),
-      construct.ULInt32('duration'),
-      construct.ULInt32('interval'),
-      construct.ULInt32('trigger_flags'),
-      construct.ULInt32('trigger_type'),
-      construct.ULInt16('trigger_arg0'),
-      construct.ULInt16('trigger_arg1'),
-      construct.ULInt16('trigger_arg2'),
-      construct.ULInt16('trigger_padding'),
-      construct.ULInt16('trigger_reserved2'),
-      construct.ULInt16('trigger_reserved3'))
+    Returns:
+      WinJobEventData: event data of the job file.
+    """
+    event_data = WinJobEventData()
+    event_data.application = (
+        variable_length_section.application_name.string.rstrip('\x00'))
+    event_data.comment = variable_length_section.comment.string.rstrip('\x00')
+    event_data.parameters = (
+        variable_length_section.parameters.string.rstrip('\x00'))
+    event_data.username = variable_length_section.author.string.rstrip('\x00')
+    event_data.working_directory = (
+        variable_length_section.working_directory.string.rstrip('\x00'))
 
-  def ParseFileObject(self, parser_mediator, file_object, **kwargs):
-    """Parses a Windows job file-like object.
+    return event_data
+
+  def _ParseLastRunTime(self, parser_mediator, fixed_length_section):
+    """Parses the last run time from a fixed-length data section.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
           and other components, such as storage and dfvfs.
-      file_object (dfvfs.FileIO): a file-like object.
+      fixed_length_section (job_fixed_length_data_section): a fixed-length
+          data section.
 
-    Raises:
-      UnableToParseFile: when the file cannot be parsed.
+    Returns:
+      dfdatetime.DateTimeValues: last run date and time or None if not
+          available.
     """
-    try:
-      header_struct = self._JOB_FIXED_LENGTH_SECTION_STRUCT.parse_stream(
-          file_object)
-    except (IOError, construct.FieldError) as exception:
-      raise errors.UnableToParseFile(
-          'Unable to parse fixed-length section with error: {0!s}'.format(
-              exception))
-
-    if not header_struct.product_version in self._PRODUCT_VERSIONS:
-      raise errors.UnableToParseFile(
-          'Unsupported product version in: 0x{0:04x}'.format(
-              header_struct.product_version))
-
-    if not header_struct.format_version == 1:
-      raise errors.UnableToParseFile(
-          'Unsupported format version in: {0:d}'.format(
-              header_struct.format_version))
-
-    try:
-      job_variable_struct = self._JOB_VARIABLE_STRUCT.parse_stream(file_object)
-    except (IOError, construct.FieldError) as exception:
-      raise errors.UnableToParseFile(
-          'Unable to parse variable-length section with error: {0!s}'.format(
-              exception))
-
-    event_data = WinJobEventData()
-    event_data.application = binary.ReadUTF16(job_variable_struct.application)
-    event_data.comment = binary.ReadUTF16(job_variable_struct.comment)
-    event_data.parameters = binary.ReadUTF16(job_variable_struct.parameter)
-    event_data.username = binary.ReadUTF16(job_variable_struct.username)
-    event_data.working_directory = binary.ReadUTF16(
-        job_variable_struct.working_directory)
-
-    systemtime_struct = header_struct.last_run_time
+    systemtime_struct = fixed_length_section.last_run_time
     system_time_tuple = (
         systemtime_struct.year, systemtime_struct.month,
-        systemtime_struct.weekday, systemtime_struct.day,
+        systemtime_struct.weekday, systemtime_struct.day_of_month,
         systemtime_struct.hours, systemtime_struct.minutes,
         systemtime_struct.seconds, systemtime_struct.milliseconds)
 
@@ -208,66 +131,135 @@ class WinJobParser(interface.FileObjectParser):
         parser_mediator.ProduceExtractionError(
             'invalid last run time: {0!s}'.format(system_time_tuple))
 
+    return date_time
+
+  def _ParseTriggerEndTime(self, parser_mediator, trigger):
+    """Parses the end time from a trigger.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfvfs.
+      trigger (job_trigger): a trigger.
+
+    Returns:
+      dfdatetime.DateTimeValues: last run date and time or None if not
+          available.
+    """
+    time_elements_tuple = (
+        trigger.end_date.year, trigger.end_date.month,
+        trigger.end_date.day_of_month, 0, 0, 0)
+
+    date_time = None
+    if time_elements_tuple != (0, 0, 0, 0, 0, 0):
+      try:
+        date_time = dfdatetime_time_elements.TimeElements(
+            time_elements_tuple=time_elements_tuple)
+        date_time.is_local_time = True
+        # TODO: add functionality to dfdatetime to control precision.
+        date_time._precision = dfdatetime_definitions.PRECISION_1_DAY  # pylint: disable=protected-access
+      except ValueError:
+        parser_mediator.ProduceExtractionError(
+            'invalid trigger end time: {0!s}'.format(time_elements_tuple))
+
+    return date_time
+
+  def _ParseTriggerStartTime(self, parser_mediator, trigger):
+    """Parses the start time from a trigger.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfvfs.
+      trigger (job_trigger): a trigger.
+
+    Returns:
+      dfdatetime.DateTimeValues: last run date and time or None if not
+          available.
+    """
+    time_elements_tuple = (
+        trigger.start_date.year, trigger.start_date.month,
+        trigger.start_date.day_of_month, trigger.start_time.hours,
+        trigger.start_time.minutes, 0)
+
+    date_time = None
+    if time_elements_tuple != (0, 0, 0, 0, 0, 0):
+      try:
+        date_time = dfdatetime_time_elements.TimeElements(
+            time_elements_tuple=time_elements_tuple)
+        date_time.is_local_time = True
+        # TODO: add functionality to dfdatetime to control precision.
+        date_time._precision = dfdatetime_definitions.PRECISION_1_MINUTE  # pylint: disable=protected-access
+      except ValueError:
+        parser_mediator.ProduceExtractionError(
+            'invalid trigger start time: {0!s}'.format(time_elements_tuple))
+
+    return date_time
+
+  def ParseFileObject(self, parser_mediator, file_object, **kwargs):
+    """Parses a Windows job file-like object.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfvfs.
+      file_object (dfvfs.FileIO): a file-like object.
+
+    Raises:
+      UnableToParseFile: when the file cannot be parsed.
+    """
+    file_offset = 0
+
+    try:
+      fixed_length_section = self._ReadStructure(
+          file_object, file_offset, self._FIXED_LENGTH_DATA_SECTION_SIZE,
+          self._FIXED_LENGTH_DATA_SECTION, 'fixed-length data section')
+    except (ValueError, errors.ParseError) as exception:
+      raise errors.UnableToParseFile(
+          'Unable to parse fixed-length data section with error: {0!s}'.format(
+              exception))
+
+    if not fixed_length_section.product_version in self._PRODUCT_VERSIONS:
+      raise errors.UnableToParseFile(
+          'Unsupported product version in: 0x{0:04x}'.format(
+              fixed_length_section.product_version))
+
+    if not fixed_length_section.format_version == 1:
+      raise errors.UnableToParseFile(
+          'Unsupported format version in: {0:d}'.format(
+              fixed_length_section.format_version))
+
+    file_offset += self._FIXED_LENGTH_DATA_SECTION_SIZE
+    data_size = file_object.get_size() - file_offset
+
+    try:
+      variable_length_section = self._ReadStructure(
+          file_object, file_offset, data_size,
+          self._VARIABLE_LENGTH_DATA_SECTION, 'variable-length data section')
+    except (ValueError, errors.ParseError) as exception:
+      raise errors.UnableToParseFile((
+          'Unable to parse variable-length data section with error: '
+          '{0!s}').format(exception))
+
+    event_data = self._ParseEventData(variable_length_section)
+
+    date_time = self._ParseLastRunTime(parser_mediator, fixed_length_section)
     if date_time:
       event = time_events.DateTimeValuesEvent(
           date_time, definitions.TIME_DESCRIPTION_LAST_RUN)
       parser_mediator.ProduceEventWithEventData(event, event_data)
 
-    for index in range(job_variable_struct.number_of_triggers):
-      try:
-        trigger_struct = self._TRIGGER_STRUCT.parse_stream(file_object)
-      except (IOError, construct.FieldError) as exception:
-        parser_mediator.ProduceExtractionError(
-            'unable to parse trigger: {0:d} with error: {1!s}'.format(
-                index, exception))
-        return
+    for trigger in variable_length_section.triggers.triggers_array:
+      event_data.trigger_type = trigger.trigger_type
 
-      event_data.trigger_type = trigger_struct.trigger_type
+      date_time = self._ParseTriggerStartTime(parser_mediator, trigger)
+      if date_time:
+        event = time_events.DateTimeValuesEvent(
+            date_time, 'Scheduled to start', time_zone=parser_mediator.timezone)
+        parser_mediator.ProduceEventWithEventData(event, event_data)
 
-      time_elements_tuple = (
-          trigger_struct.start_year, trigger_struct.start_month,
-          trigger_struct.start_day, trigger_struct.start_hour,
-          trigger_struct.start_minute, 0)
-
-      if time_elements_tuple != (0, 0, 0, 0, 0, 0):
-        try:
-          date_time = dfdatetime_time_elements.TimeElements(
-              time_elements_tuple=time_elements_tuple)
-          date_time.is_local_time = True
-          # TODO: add functionality to dfdatetime to control precision.
-          date_time._precision = dfdatetime_definitions.PRECISION_1_MINUTE  # pylint: disable=protected-access
-        except ValueError:
-          date_time = None
-          parser_mediator.ProduceExtractionError(
-              'invalid trigger start time: {0!s}'.format(time_elements_tuple))
-
-        if date_time:
-          event = time_events.DateTimeValuesEvent(
-              date_time, 'Scheduled to start',
-              time_zone=parser_mediator.timezone)
-          parser_mediator.ProduceEventWithEventData(event, event_data)
-
-      time_elements_tuple = (
-          trigger_struct.end_year, trigger_struct.end_month,
-          trigger_struct.end_day, 0, 0, 0)
-
-      if time_elements_tuple != (0, 0, 0, 0, 0, 0):
-        try:
-          date_time = dfdatetime_time_elements.TimeElements(
-              time_elements_tuple=time_elements_tuple)
-          date_time.is_local_time = True
-          # TODO: add functionality to dfdatetime to control precision.
-          date_time._precision = dfdatetime_definitions.PRECISION_1_DAY  # pylint: disable=protected-access
-        except ValueError:
-          date_time = None
-          parser_mediator.ProduceExtractionError(
-              'invalid trigger end time: {0!s}'.format(time_elements_tuple))
-
-        if date_time:
-          event = time_events.DateTimeValuesEvent(
-              date_time, 'Scheduled to end',
-              time_zone=parser_mediator.timezone)
-          parser_mediator.ProduceEventWithEventData(event, event_data)
+      date_time = self._ParseTriggerEndTime(parser_mediator, trigger)
+      if date_time:
+        event = time_events.DateTimeValuesEvent(
+            date_time, 'Scheduled to end', time_zone=parser_mediator.timezone)
+        parser_mediator.ProduceEventWithEventData(event, event_data)
 
     # TODO: create a timeless event object if last_run_time and
     # trigger_start_time are None? What should be the description of
