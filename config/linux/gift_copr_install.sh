1dc1f459bcf61102a7225b51d7e93deb993e2f98
Joachim Metz <joachim.metz@gmail.com>
2018-07-02 20:10:40 +0200

    Code review: 342180043: Changes for rpm packaging

--- config/linux/gift_copr_install.sh
+++ config/linux/gift_copr_install.sh
@@ -52,7 +52,6 @@ PYTHON2_DEPENDENCIES="PyYAML
                       python-hachoir-parser
                       python-pefile
                       python-psutil
-                      python-pysqlite
                       python-pytsk3
                       python-requests
                       python-six
@@ -60,6 +59,7 @@ PYTHON2_DEPENDENCIES="PyYAML
                       python2-future
                       python2-idna
                       python2-pyparsing
+                      python2-sqlite3dbm
                       python2-urllib3
                       python2-yara
                       python2-zmq
