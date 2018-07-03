ba92d51fd1b541d57b41c11f43c73dee9dad3313
Joachim Metz <joachim.metz@gmail.com>
2018-07-03 10:30:42 +0200

    365740043: Migrated winreg SAM construct-based plugin to use dtfabric #1893 (#1992)

--- plaso/parsers/winreg_plugins/sam_users.py
+++ plaso/parsers/winreg_plugins/sam_users.py
@@ -3,15 +3,15 @@
 
 from __future__ import unicode_literals
 
-import construct
-
 from dfdatetime import filetime as dfdatetime_filetime
 
 from plaso.containers import events
 from plaso.containers import time_events
 from plaso.containers import windows_events
 from plaso.lib import definitions
+from plaso.lib import errors
 from plaso.parsers import winreg
+from plaso.parsers.winreg_plugins import dtfabric_plugin
 from plaso.parsers.winreg_plugins import interface
 
 
@@ -40,7 +40,8 @@ class SAMUsersWindowsRegistryEventData(events.EventData):
     self.username = None
 
 
-class SAMUsersWindowsRegistryPlugin(interface.WindowsRegistryPlugin):
+class SAMUsersWindowsRegistryPlugin(
+    dtfabric_plugin.DtFabricBaseWindowsRegistryPlugin):
   """Windows Registry plugin for SAM Users Account information."""
 
   NAME = 'windows_sam_users'
@@ -50,24 +51,68 @@ class SAMUsersWindowsRegistryPlugin(interface.WindowsRegistryPlugin):
       interface.WindowsRegistryKeyPathFilter(
           'HKEY_LOCAL_MACHINE\\SAM\\SAM\\Domains\\Account\\Users')])
 
-  _F_VALUE_STRUCT = construct.Struct(
-      'f_struct',
-      construct.Padding(8),
-      construct.ULInt64('last_login'),
-      construct.Padding(8),
-      construct.ULInt64('password_reset'),
-      construct.Padding(16),
-      construct.ULInt16('rid'),
-      construct.Padding(16),
-      construct.ULInt8('login_count'))
+  _DEFINITION_FILE = 'sam_users.yaml'
 
-  _V_VALUE_HEADER = construct.Struct(
-      'v_header',
-      construct.Array(11, construct.ULInt32('values')))
-
-  _V_VALUE_STRINGS_OFFSET = 204
+  _V_VALUE_STRINGS_OFFSET = 0xcc
 
   _SOURCE_APPEND = ': User Account Information'
+
+  def _ParseFValue(self, registry_key):
+    """Parses an F value.
+
+    Args:
+      registry_key (dfwinreg.WinRegistryKey): Windows Registry key.
+
+    Returns:
+      f_value: F value stored in the Windows Registry key.
+
+    Raises:
+      ParseError: if the Windows Registry key does not contain an F value or
+          F value cannot be parsed.
+    """
+    registry_value = registry_key.GetValueByName('F')
+    if not registry_value:
+      raise errors.ParseError(
+          'missing value: "F" in Windows Registry key: {0:s}.'.format(
+              registry_key.name))
+
+    f_value_map = self._GetDataTypeMap('f_value')
+
+    try:
+      return self._ReadStructureFromByteStream(
+          registry_value.data, 0, f_value_map)
+    except (ValueError, errors.ParseError) as exception:
+      raise errors.ParseError(exception)
+
+  def _ParseVValueString(
+      self, parser_mediator, data, user_information_descriptor):
+    """Parses a V value string.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfvfs.
+      data (bytes): Windows Registry V value data.
+      user_information_descriptor (user_information_descriptor): V value
+          user information descriptor.
+
+    Returns:
+      str: string value stored in the Windows Registry V value data.
+    """
+    data_start_offset = (
+        user_information_descriptor.offset + self._V_VALUE_STRINGS_OFFSET)
+    data_end_offset = data_start_offset + user_information_descriptor.size
+    descriptor_data = data[data_start_offset:data_end_offset]
+
+    try:
+      username = descriptor_data.decode('utf-16-le')
+    except (UnicodeDecodeError, UnicodeEncodeError) as exception:
+      username = descriptor_data.decode('utf-16-le', errors='replace')
+      parser_mediator.ProduceExtractionError((
+          'unable to decode V value string with error: {0!s}. Characters '
+          'that cannot be decoded will be replaced with "?" or '
+          '"\\ufffd".').format(exception))
+
+    return username
 
   def ExtractEvents(self, parser_mediator, registry_key, **kwargs):
     """Extracts events from a Windows Registry key.
@@ -90,85 +135,47 @@ class SAMUsersWindowsRegistryPlugin(interface.WindowsRegistryPlugin):
       if subkey.name == 'Names':
         continue
 
-      f_value = subkey.GetValueByName('F')
-      if not f_value:
+      try:
+        f_value = self._ParseFValue(subkey)
+      except errors.ParseError as exception:
         parser_mediator.ProduceExtractionError(
-            'missing Registry value: "F" in subkey: {0:s}.'.format(
-                subkey.name))
+            'unable to parse F value with error: {0!s}'.format(exception))
         continue
 
-      v_value = subkey.GetValueByName('V')
-      if not v_value:
+      registry_value = subkey.GetValueByName('V')
+      if not registry_value:
         parser_mediator.ProduceExtractionError(
             'missing Registry value: "V" in subkey: {0:s}.'.format(
                 subkey.name))
         continue
 
+      v_value_map = self._GetDataTypeMap('v_value')
+
       try:
-        f_data_struct = self._F_VALUE_STRUCT.parse(f_value.data)
-      except construct.FieldError as exception:
-        parser_mediator.ProduceExtractionError((
-            'unable to parse Registry value: "F" in subkey: {0:s} '
-            'with error: {1!s}.').format(subkey.name, exception))
+        v_value = self._ReadStructureFromByteStream(
+            registry_value.data, 0, v_value_map)
+      except (ValueError, errors.ParseError) as exception:
+        parser_mediator.ProduceExtractionError(
+            'unable to parse V value with error: {0!s}'.format(exception))
         continue
 
-      try:
-        v_data_struct = self._V_VALUE_HEADER.parse(v_value.data)
-      except construct.FieldError as exception:
-        parser_mediator.ProduceExtractionError((
-            'unable to parse Registry value: "V" in subkey: {0:s} '
-            'with error: {1!s}.').format(subkey.name, exception))
-        continue
+      username = self._ParseVValueString(
+          parser_mediator, registry_value.data, v_value[1])
 
-      v_header_values = v_data_struct.values()[0]
+      fullname = self._ParseVValueString(
+          parser_mediator, registry_value.data, v_value[2])
 
-      data_start_offset = v_header_values[3] + self._V_VALUE_STRINGS_OFFSET
-      data_end_offset = v_header_values[4] + data_start_offset
-      utf16_stream = v_value.data[data_start_offset:data_end_offset]
-
-      try:
-        username = utf16_stream.decode('utf-16-le')
-      except (UnicodeDecodeError, UnicodeEncodeError) as exception:
-        username = utf16_stream.decode('utf-16-le', errors='replace')
-        parser_mediator.ProduceExtractionError((
-            'unable to decode username string with error: {0!s}. Characters '
-            'that cannot be decoded will be replaced with "?" or '
-            '"\\ufffd".').format(exception))
-
-      data_start_offset = v_header_values[6] + self._V_VALUE_STRINGS_OFFSET
-      data_end_offset = v_header_values[7] + data_start_offset
-      utf16_stream = v_value.data[data_start_offset:data_end_offset]
-
-      try:
-        fullname = utf16_stream.decode('utf-16-le')
-      except (UnicodeDecodeError, UnicodeEncodeError) as exception:
-        fullname = utf16_stream.decode('utf-16-le', errors='replace')
-        parser_mediator.ProduceExtractionError((
-            'unable to decode fullname string with error: {0!s}. Characters '
-            'that cannot be decoded will be replaced with "?" or '
-            '"\\ufffd".').format(exception))
-
-      data_start_offset = v_header_values[9] + self._V_VALUE_STRINGS_OFFSET
-      data_end_offset = v_header_values[10] + data_start_offset
-      utf16_stream = v_value.data[data_start_offset:data_end_offset]
-
-      try:
-        comments = utf16_stream.decode('utf-16-le')
-      except (UnicodeDecodeError, UnicodeEncodeError) as exception:
-        comments = utf16_stream.decode('utf-16-le', errors='replace')
-        parser_mediator.ProduceExtractionError((
-            'unable to decode comments string with error: {0!s}. Characters '
-            'that cannot be decoded will be replaced with "?" or '
-            '"\\ufffd".').format(exception))
+      comments = self._ParseVValueString(
+          parser_mediator, registry_value.data, v_value[3])
 
       last_written_time = last_written_time_per_username.get(username, None)
 
-      # TODO: check if subkey.name == f_data_struct.rid
+      # TODO: check if subkey.name == f_value.rid
 
       if last_written_time:
         values_dict = {
-            'account_rid': f_data_struct.rid,
-            'login_count': f_data_struct.login_count}
+            'account_rid': f_value.rid,
+            'login_count': f_value.number_of_logons}
 
         if username:
           values_dict['username'] = username
@@ -179,7 +186,6 @@ class SAMUsersWindowsRegistryPlugin(interface.WindowsRegistryPlugin):
 
         event_data = windows_events.WindowsRegistryEventData()
         event_data.key_path = registry_key.path
-        event_data.offset = registry_key.offset
         event_data.regvalue = values_dict
         event_data.source_append = self._SOURCE_APPEND
 
@@ -188,24 +194,23 @@ class SAMUsersWindowsRegistryPlugin(interface.WindowsRegistryPlugin):
         parser_mediator.ProduceEventWithEventData(event, event_data)
 
       event_data = SAMUsersWindowsRegistryEventData()
-      event_data.account_rid = f_data_struct.rid
+      event_data.account_rid = f_value.rid
       event_data.comments = comments
       event_data.fullname = fullname
       event_data.key_path = registry_key.path
-      event_data.login_count = f_data_struct.login_count
-      event_data.offset = f_value.offset
+      event_data.login_count = f_value.number_of_logons
       event_data.username = username
 
-      if f_data_struct.last_login != 0:
+      if f_value.last_login_time != 0:
         date_time = dfdatetime_filetime.Filetime(
-            timestamp=f_data_struct.last_login)
+            timestamp=f_value.last_login_time)
         event = time_events.DateTimeValuesEvent(
             date_time, definitions.TIME_DESCRIPTION_LAST_LOGIN)
         parser_mediator.ProduceEventWithEventData(event, event_data)
 
-      if f_data_struct.password_reset != 0:
+      if f_value.last_password_set_time != 0:
         date_time = dfdatetime_filetime.Filetime(
-            timestamp=f_data_struct.password_reset)
+            timestamp=f_value.last_password_set_time)
         event = time_events.DateTimeValuesEvent(
             date_time, definitions.TIME_DESCRIPTION_LAST_PASSWORD_RESET)
         parser_mediator.ProduceEventWithEventData(event, event_data)
