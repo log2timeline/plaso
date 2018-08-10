1bd12f3fff4db68e32aeaf8d7addd56b1be26002
Everest Stefan Munro-Zeisberger <evmunro1@gmail.com>
2018-08-10 14:47:49 +0200

    Add parser and tests for Windows Timeline SQLite DB (#2076)

--- plaso/formatters/__init__.py
+++ plaso/formatters/__init__.py
@@ -83,6 +83,7 @@ from plaso.formatters import userassist
 from plaso.formatters import utmp
 from plaso.formatters import utmpx
 from plaso.formatters import windows
+from plaso.formatters import windows_timeline
 from plaso.formatters import winevt
 from plaso.formatters import winevtx
 from plaso.formatters import winfirewall
