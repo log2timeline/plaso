c78286e071184bc5b83cce8a2418ace2d13a2af2
Daniel White <onager@deerpie.com>
2018-09-07 12:50:57 +0200

    Added ppa install script for python3

--- config/jenkins/linux/run_end_to_end_tests_py3.sh
+++ config/jenkins/linux/run_end_to_end_tests_py3.sh
@@ -35,7 +35,7 @@ PYTHONPATH=. ./utils/check_dependencies.py
 
 # Start the end-to-end tests in the background so we can capture the PID of
 # the process while the script is running.
-PYTHONPATH=. ./tests/end-to-end.py --config ${CONFIGURATION_FILE} --sources-directory ${SOURCES_DIRECTORY} --tools-directory ./tools --results-directory ${RESULTS_DIRECTORY} --references-directory ${REFERENCES_DIRECTORY} &
+PYTHONPATH=. python3 ./tests/end-to-end.py --config ${CONFIGURATION_FILE} --sources-directory ${SOURCES_DIRECTORY} --tools-directory ./tools --results-directory ${RESULTS_DIRECTORY} --references-directory ${REFERENCES_DIRECTORY} &
 
 PID_COMMAND=$!;
 
