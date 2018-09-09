433ecbae5867c988d25d4eccbacca734a421e594
Joachim Metz <joachim.metz@gmail.com>
2018-09-09 06:33:52 +0200

    Update santa.py

--- tests/parsers/santa.py
+++ tests/parsers/santa.py
@@ -22,11 +22,11 @@ class SantaUnitTest(test_lib.ParserTestCase):
     parser = santa.SantaParser()
     storage_writer = self._ParseFile(['santa.log'], parser)
 
-    self.assertEqual(storage_writer.number_of_errors, 0)
-
     # Test file contains 194 lines
     # - 3 lines should be skipped in the results.
     # - 17 new events should be added from existing lines.
+
+    self.assertEqual(storage_writer.number_of_errors, 0)
     self.assertEqual(storage_writer.number_of_events, 208)
 
     # The order in which DSVParser generates events is nondeterministic
