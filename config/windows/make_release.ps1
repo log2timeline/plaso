38afc8c0eb8798e132516c564003a606ddee4e9f
Daniel White <onager@deerpie.com>
2018-11-05 17:51:00 -0800

    Add dtfabric definitions for plist and esedb plugins

--- config/windows/make_release.ps1
+++ config/windows/make_release.ps1
@@ -215,10 +215,14 @@ xcopy /q /y dist\dfwinreg\dfwinreg\*.yaml dist\plaso\dfwinreg
 
 # Copy the plaso yaml (dtFabric definition) files
 mkdir dist\plaso\plaso\parsers
+mkdir dist\plaso\plaso\parsers\esedb_plugins
 mkdir dist\plaso\plaso\parsers\olecf_plugins
+mkdir dist\plaso\plaso\parsers\plist_plugins
 mkdir dist\plaso\plaso\parsers\winreg_plugins
 xcopy /q /y plaso\parsers\*.yaml dist\plaso\plaso\parsers
+xcopy /q /y plaso\parsers\esedb_plugins\*.yaml dist\plaso\plaso\parsers\esedb_plugins
 xcopy /q /y plaso\parsers\olecf_plugins\*.yaml dist\plaso\plaso\parsers\olecf_plugins
+xcopy /q /y plaso\parsers\plist_plugins\*.yaml dist\plaso\plaso\parsers\plist_plugins
 xcopy /q /y plaso\parsers\winreg_plugins\*.yaml dist\plaso\plaso\parsers\winreg_plugins
 
 # Makes plaso-<version>-<architecture>.zip
