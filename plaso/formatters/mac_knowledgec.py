4c396bc159f7ce39369989a243172fcfbc786caf
Joaquin Moreno Garijo <bastionado@gmail.com>
2019-02-09 08:26:59 -0800

    Update lint

--- plaso/formatters/mac_knowledgec.py
+++ plaso/formatters/mac_knowledgec.py
@@ -16,11 +16,11 @@ class MacKnowledgeCApplicationFormatter(interface.ConditionalEventFormatter):
       'Application {bundle_id} executed',
       'during {usage_in_seconds} seconds']
 
-  FORMAT_STRING_SHORT_PIECES = [
-      'Application {bundle_id}']
+  FORMAT_STRING_SHORT_PIECES = ['Application {bundle_id}']
 
   SOURCE_LONG = 'KnowledgeC Application'
   SOURCE_SHORT = 'HISTORY'
+
 
 class MacKnowledgeCSafariFormatter(interface.ConditionalEventFormatter):
   """Formatter for a MacOS KnowledgeC Safari event."""
@@ -32,13 +32,12 @@ class MacKnowledgeCSafariFormatter(interface.ConditionalEventFormatter):
       'with title {uri_title}'
       'and was visited during {usage_in_seconds} seconds']
 
-  FORMAT_STRING_SHORT_PIECES = [
-      'Safari open uri {uri}']
+  FORMAT_STRING_SHORT_PIECES = ['Safari open uri {uri}']
 
   SOURCE_LONG = 'KnowledgeC Safari'
   SOURCE_SHORT = 'WEBHIST'
 
 
 manager.FormattersManager.RegisterFormatters([
-  MacKnowledgeCApplicationFormatter,
-  MacKnowledgeCSafariFormatter])
+    MacKnowledgeCApplicationFormatter,
+    MacKnowledgeCSafariFormatter])
