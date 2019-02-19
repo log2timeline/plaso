5ed5e96514e89446a1edb6cbc3a6aa9bd33f78ef
Ali Memarzia <alimemzi@gmail.com>
2019-02-19 11:13:31 -0800

    Mac OS Note parser completed.

---
+++ plaso/formatters/mac_notes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""mac notes formatter."""
+from __future__ import unicode_literals
+
+from plaso.formatters import interface
+from plaso.formatters import manager
+
+
+class MacNotesNotesFormatter(interface.ConditionalEventFormatter):
+  """mac notes zhtmlstring event formatter."""
+
+  DATA_TYPE = 'mac:notes:zhtmlstring'
+  """Correct Format String Pieces where needed"""
+
+  FORMAT_STRING_PIECES = [
+      'note_body:{zhtmlstring}',
+      'last_modified:{last_modified_time}']
+
+  FORMAT_STRING_SHORT_PIECES = ['note_body:{zhtmlstring}']
+
+  SOURCE_LONG = 'Mac Notes Zhtmlstring'
+  SOURCE_SHORT = 'Mac Notes'
+
+
+manager.FormattersManager.RegisterFormatter(MacNotesNotesFormatter)
