bd44c38a1a90dfd38d94d7fd2f1e1c3a6b79ad19
Daniel White <onager@deerpie.com>
2019-02-20 14:12:16 +0100

    Removed Tox from Python3 dependencies, removed pylint2 repository (#2361)

--- config/linux/gift_ppa_install_py3.sh
+++ config/linux/gift_ppa_install_py3.sh
@@ -64,8 +64,7 @@ PYTHON3_DEPENDENCIES="libbde-python3
 # Additional dependencies for running tests, alphabetized, one per line.
 TEST_DEPENDENCIES="python3-mock
                    python3-pbr
-                   python3-setuptools
-                   python3-tox";
+                   python3-setuptools"
 
 # Additional dependencies for development, alphabetized, one per line.
 DEVELOPMENT_DEPENDENCIES="pylint
