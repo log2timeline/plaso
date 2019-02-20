f34e08fe1573d362480ada2c6459361f3ba89a85
Ali Memarzia <alimemzi@gmail.com>
2019-02-19 17:17:32 -0800

    Added beautifulsoup to dependencies

--- config/linux/gift_ppa_install_py3.sh
+++ config/linux/gift_ppa_install_py3.sh
@@ -34,6 +34,7 @@ PYTHON3_DEPENDENCIES="libbde-python3
                       python3-artifacts
                       python3-bencode
                       python3-biplist
+                      python3-bs4
                       python3-certifi
                       python3-chardet
                       python3-crypto
