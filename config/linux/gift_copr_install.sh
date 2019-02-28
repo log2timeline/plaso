bacb4d7f17c9ca403e1cb2b6ebb48eb6dbc3505c
Ali Memarzia <alimemzi@gmail.com>
2019-02-27 21:57:39 -0800

    Will this work?

--- config/linux/gift_copr_install.sh
+++ config/linux/gift_copr_install.sh
@@ -34,6 +34,7 @@ PYTHON2_DEPENDENCIES="libbde-python2
                       python2-XlsxWriter
                       python2-artifacts
                       python2-backports-lzma
+                      python2-beautifulsoup4
                       python2-bencode
                       python2-biplist
                       python2-certifi
