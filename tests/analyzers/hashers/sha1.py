bc6314896bb2f5cfa4c319972abfadfef3f9c441
Daniel White <onager@deerpie.com>
2019-04-03 14:24:10 +0200

    Removed GetBinaryDigest method in hasher interface (#2443)

--- tests/analyzers/hashers/sha1.py
+++ tests/analyzers/hashers/sha1.py
@@ -4,7 +4,6 @@
 
 from __future__ import unicode_literals
 
-import codecs
 import unittest
 
 from plaso.analyzers.hashers import sha1
@@ -25,11 +24,6 @@ class SHA1Test(test_lib.HasherTestCase):
     self._AssertTestPathStringDigestMatch(
         hasher, ['empty_file'], expected_sha1)
 
-    hasher = sha1.SHA1Hasher()
-    expected_binary_sha1 = codecs.decode(expected_sha1, 'hex')
-    self._AssertTestPathBinaryDigestMatch(
-        hasher, ['empty_file'], expected_binary_sha1)
-
   @shared_test_lib.skipUnlessHasTestFile(['ímynd.dd'])
   def testFileHashMatchesKnownFile(self):
     """Tests that hasher matches the hash of an empty file."""
@@ -37,12 +31,6 @@ class SHA1Test(test_lib.HasherTestCase):
 
     hasher = sha1.SHA1Hasher()
     self._AssertTestPathStringDigestMatch(hasher, ['ímynd.dd'], expected_sha1)
-
-    hasher = sha1.SHA1Hasher()
-
-    expected_binary_sha1 = codecs.decode(expected_sha1, 'hex')
-    self._AssertTestPathBinaryDigestMatch(
-        hasher, ['ímynd.dd'], expected_binary_sha1)
 
 
 if __name__ == '__main__':
