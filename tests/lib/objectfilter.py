198b578e9cde2f0b40c372ea979d95c91187a1b0
Joachim Metz <joachim.metz@gmail.com>
2019-05-08 08:58:39 +0200

    Merged SearchParser into objectfilter Parser

--- tests/lib/objectfilter.py
+++ tests/lib/objectfilter.py
@@ -9,8 +9,180 @@ import unittest
 from plaso.lib import errors
 from plaso.lib import objectfilter
 
+from tests import test_lib as shared_test_lib
+
+
+class TestFilterImplementation(objectfilter.BaseFilterImplementation):
+  """Filter implementation for testing."""
+
+  AndFilter = objectfilter.AndFilter
+  IdentityFilter = objectfilter.IdentityFilter
+
+
+class TokenTest(shared_test_lib.BaseTestCase):
+  """Tests the event filter parser token."""
+
+  def testInitialize(self):
+    """Tests the __init__ function."""
+    token = objectfilter.Token('STRING', r'\\(.)', 'StringEscape', None)
+    self.assertIsNotNone(token)
+
+
+class ExpressionTest(shared_test_lib.BaseTestCase):
+  """Tests the event filter parser expression."""
+
+  def testAddArg(self):
+    """Tests the AddArg function."""
+    expression = objectfilter.Expression()
+
+    expression.AddArg('argument1')
+
+    with self.assertRaises(errors.ParseError):
+      expression.AddArg('argument2')
+
+  def testSetAttribute(self):
+    """Tests the SetAttribute function."""
+    expression = objectfilter.Expression()
+
+    expression.SetAttribute('attribute')
+
+  def testSetOperator(self):
+    """Tests the SetOperator function."""
+    expression = objectfilter.Expression()
+
+    expression.SetOperator('=')
+
+
+class BinaryExpressionTest(shared_test_lib.BaseTestCase):
+  """Tests the event filter parser binary expression."""
+
+  def testAddOperands(self):
+    """Tests the AddOperands function."""
+    expression = objectfilter.BinaryExpression(operator='and')
+    left_hand_expression = objectfilter.Expression()
+    right_hand_expression = objectfilter.Expression()
+
+    expression.AddOperands(left_hand_expression, right_hand_expression)
+
+    with self.assertRaises(errors.ParseError):
+      expression.AddOperands(None, right_hand_expression)
+
+    with self.assertRaises(errors.ParseError):
+      expression.AddOperands(left_hand_expression, None)
+
+  def testCompile(self):
+    """Tests the Compile function."""
+    expression = objectfilter.BinaryExpression(operator='and')
+
+    expression.Compile(TestFilterImplementation)
+
+    expression = objectfilter.BinaryExpression()
+
+    with self.assertRaises(errors.ParseError):
+      expression.Compile(TestFilterImplementation)
+
+
+class IdentityExpressionTest(shared_test_lib.BaseTestCase):
+  """Tests the event filter parser identify expression."""
+
+  def testCompile(self):
+    """Tests the Compile function."""
+    expression = objectfilter.IdentityExpression()
+    expression.Compile(TestFilterImplementation)
+
+
+class ParserTest(shared_test_lib.BaseTestCase):
+  """Tests the objectfilter parser."""
+
+  # TODO: add tests for _CombineBinaryExpressions
+  # TODO: add tests for _CombineParenthesis
+  # TODO: add tests for BinaryOperator
+
+  def testBracketClose(self):
+    """Tests the BracketClose function."""
+    parser = objectfilter.Parser()
+
+    parser.BracketClose()
+
+  def testBracketOpen(self):
+    """Tests the BracketOpen function."""
+    parser = objectfilter.Parser()
+
+    parser.BracketOpen()
+
+  def testClose(self):
+    """Tests the Close function."""
+    parser = objectfilter.Parser()
+
+    with self.assertRaises(errors.ParseError):
+      parser.Close()
+
+  def testDefault(self):
+    """Tests the Default function."""
+    parser = objectfilter.Parser()
+
+    parser.Default()
+
+  def testCompile(self):
+    """Tests the Compile function."""
+    parser = objectfilter.Parser()
+
+    result = parser.Empty()
+    self.assertTrue(result)
+
+  # TODO: add tests for Error
+
+  def testFeed(self):
+    """Tests the Feed function."""
+    parser = objectfilter.Parser()
+
+    parser.Feed('data')
+
+  # TODO: add tests for InsertArg
+  # TODO: add tests for NextToken
+  # TODO: add tests for Parse
+  # TODO: add tests for PopState
+  # TODO: add tests for PushBack
+  # TODO: add tests for PushState
+  # TODO: add tests for Reduce
+  # TODO: add tests for StringEscape
+
+  def testStringFinish(self):
+    """Tests the StringFinish function."""
+    parser = objectfilter.Parser()
+
+    state = parser.StringFinish()
+    self.assertIsNone(state)
+
+  def testStringInsert(self):
+    """Tests the StringInsert function."""
+    parser = objectfilter.Parser()
+
+    parser.StringStart()
+    parser.StringInsert(string='string')
+
+  def testStringStart(self):
+    """Tests the StringStart function."""
+    parser = objectfilter.Parser()
+
+    parser.StringStart()
+
+  def testStoreAttribute(self):
+    """Tests the StoreAttribute function."""
+    parser = objectfilter.Parser()
+
+    token = parser.StoreAttribute(string='attribute')
+    self.assertEqual(token, 'OPERATOR')
+
+  def testStoreOperator(self):
+    """Tests the StoreOperator function."""
+    parser = objectfilter.Parser()
+
+    parser.StoreOperator(string='and')
+
 
 # pylint: disable=missing-docstring
+
 
 class DummyObject(object):
   def __init__(self, key, value):
@@ -282,11 +454,11 @@ class ObjectFilterTest(unittest.TestCase):
 
   def testContext(self):
     self.assertRaises(
-        objectfilter.InvalidNumberOfOperands, objectfilter.Context,
+        errors.InvalidNumberOfOperands, objectfilter.Context,
         arguments=['context'], value_expander=self.value_expander)
 
     self.assertRaises(
-        objectfilter.InvalidNumberOfOperands, objectfilter.Context,
+        errors.InvalidNumberOfOperands, objectfilter.Context,
         arguments=[
             'context', objectfilter.Equals(
                 arguments=['path', 'value'],
