8ecb8f0b9745720dcfa544655510fb87ed24ab68
Daniel White <onager@deerpie.com>
2019-06-23 05:57:34 +0200

    Added support for Apache vhost_combined log format #2460 (#2645)

--- tests/parsers/apache_access.py
+++ tests/parsers/apache_access.py
@@ -21,7 +21,7 @@ class ApacheAccessUnitTest(test_lib.ParserTestCase):
     storage_writer = self._ParseFile(['access.log'], parser)
 
     self.assertEqual(storage_writer.number_of_warnings, 1)
-    self.assertEqual(storage_writer.number_of_events, 7)
+    self.assertEqual(storage_writer.number_of_events, 11)
 
     # The order in which DSVParser generates events is nondeterministic
     # hence we sort the events.
@@ -51,13 +51,11 @@ class ApacheAccessUnitTest(test_lib.ParserTestCase):
         'http_request: GET /wp-content/themes/darkmode/evil.php?cmd=uname+-a '
         'HTTP/1.1 from: 192.168.0.2 code: 200 referer: http://localhost/ '
         'user_agent: Mozilla/5.0 (X11; Linux i686; rv:2.0b12pre) '
-        'Gecko/20100101 Firefox/4'
-    )
+        'Gecko/20100101 Firefox/4')
 
     expected_short_message = (
         'GET /wp-content/themes/darkmode/evil.php?cmd=uname+-a HTTP/1.1 from: '
-        '192.168.0.2'
-    )
+        '192.168.0.2')
 
     self._TestGetMessageStrings(event, expected_message, expected_short_message)
 
@@ -78,13 +76,11 @@ class ApacheAccessUnitTest(test_lib.ParserTestCase):
 
     expected_message = (
         'http_request: GET /wp-content/themes/darkmode/header.php?install2 '
-        'HTTP/1.1 from: 10.0.0.1 code: 200'
-    )
+        'HTTP/1.1 from: 10.0.0.1 code: 200')
 
     expected_short_message = (
         'GET /wp-content/themes/darkmode/header.php?install2 HTTP/1.1 from: '
-        '10.0.0.1'
-    )
+        '10.0.0.1')
 
     self._TestGetMessageStrings(event, expected_message, expected_short_message)
 
@@ -96,6 +92,37 @@ class ApacheAccessUnitTest(test_lib.ParserTestCase):
         'unable to parse log line: "46.118.127.106 - - [20/May/2015:12:05:17 '
         '+0000] "GET /scripts/grok-py-test/co..." at offset: 1589'))
     self.assertEqual(warning.parser_chain, 'apache_access')
+
+
+    # Test vhost_combined log format event.
+    event = events[9]
+    self.CheckTimestamp(event.timestamp, '2018-01-13 19:31:17.000000')
+
+    self.assertEqual(event.ip_address, '192.168.0.2')
+    self.assertEqual(event.remote_name, '-')
+    self.assertEqual(event.user_name, '-')
+
+    self.assertEqual(
+        event.http_request,
+        'GET /wp-content/themes/darkmode/evil.php HTTP/1.1')
+
+    self.assertEqual(event.http_response_code, 200)
+    self.assertEqual(event.http_response_bytes, 1063)
+
+    expected_message = (
+        'http_request: GET /wp-content/themes/darkmode/evil.php HTTP/1.1 '
+        'from: 192.168.0.2 '
+        'code: 200 '
+        'referer: - '
+        'user_agent: Mozilla/5.0 (Windows NT 7.1) AppleWebKit/534.30 '
+        '(KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30 '
+        'server_name: plaso.log2timeline.net '
+        'port: 443')
+
+    expected_short_message = (
+        'GET /wp-content/themes/darkmode/evil.php HTTP/1.1 from: 192.168.0.2')
+
+    self._TestGetMessageStrings(event, expected_message, expected_short_message)
 
 
 if __name__ == '__main__':
