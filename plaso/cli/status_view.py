74e17b26b754762d842a6f666f827c4c19b2ac83
Joachim Metz <joachim.metz@gmail.com>
2019-11-07 19:37:35 +0100

    Update status_view.py

--- plaso/cli/status_view.py
+++ plaso/cli/status_view.py
@@ -339,23 +339,24 @@ class StatusView(object):
     Args:
       processing_status (ProcessingStatus): processing status.
     """
-    if not processing_status:
-      processing_time = 0
-    else:
+    processing_time = 0
+    if processing_status:
       processing_time = time.time() - processing_status.start_time
-    days, remainder = divmod(int(processing_time), 24 * 60 * 60)
-    hours, remainder = divmod(remainder, 60 * 60)
-    minutes, seconds = divmod(remainder, 60)
+
+    processing_time, seconds = divmod(int(processing_time), 60)
+    processing_time, minutes = divmod(processing_time, 60)
+    days, hours = divmod(processing_time, 24)
+
     if days == 0:
-      days = ''
+      days_string = ''
     elif days == 1:
-      days = '1 day, '
+      days_string = '1 day, '
     else:
-      days = '{0:d} days, '.format(days)
+      days_string = '{0:d} days, '.format(days)
 
     self._output_writer.Write(
         'Processing time\t\t: {0:s}{1:02d}:{2:02d}:{3:02d}\n'.format(
-            days, hours, minutes, seconds))
+            days_string, hours, minutes, seconds))
 
   def _PrintTasksStatus(self, processing_status):
     """Prints the status of the tasks.
