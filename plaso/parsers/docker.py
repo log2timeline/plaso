e8304898307f7bb9405085cac18c5ada7d1a5a19
Romain Gayon <romaing@google.com>
2019-12-10 17:04:42 +0100

    Fix checking for config filenam

--- plaso/parsers/docker.py
+++ plaso/parsers/docker.py
@@ -293,9 +293,9 @@ class DockerJSONParser(interface.FileObjectParser):
     split_path = file_system.SplitPath(json_file_path)
     try:
       if 'containers' in split_path:
-        #if 'config.json' in split_path:
-        #Change because the name can be config.v2.json ...
-        if split_path and split_path[-1].startswith('config') and split_path[-1].endswith('.json'):
+        # For our intent, both version of the config file can be parsed
+        # the same way
+        if split_path[-1] in ['config.json', 'config.v2.json']:
           self._ParseContainerConfigJSON(parser_mediator, file_object)
         if json_file_path.endswith('-json.log'):
           self._ParseContainerLogJSON(parser_mediator, file_object)
