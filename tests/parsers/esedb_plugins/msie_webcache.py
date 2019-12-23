386647c0f5443f07aeb48e4c55f76e9203fbd405
Joachim Metz <joachim.metz@gmail.com>
2019-12-23 16:05:18 +0100

    Changes to support new WebCacheV01 format

--- tests/parsers/esedb_plugins/msie_webcache.py
+++ tests/parsers/esedb_plugins/msie_webcache.py
@@ -16,8 +16,29 @@ from tests.parsers.esedb_plugins import test_lib
 class MsieWebCacheESEDBPluginTest(test_lib.ESEDBPluginTestCase):
   """Tests for the MSIE WebCache ESE database plugin."""
 
-  def testProcess(self):
-    """Tests the Process function."""
+  # pylint: disable=protected-access
+
+  def testConvertHeadersValues(self):
+    """Tests the _ConvertHeadersValues function."""
+    plugin = msie_webcache.MsieWebCacheESEDBPlugin()
+
+    binary_value = (
+        b'HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n'
+        b'X-Content-Type-Options: nosniff\r\nContent-Length: 2759\r\n'
+        b'X-XSS-Protection: 1; mode=block\r\n'
+        b'Alternate-Protocol: 80:quic\r\n\r\n')
+
+    expected_headers_value = (
+        '[HTTP/1.1 200 OK; Content-Type: image/png; '
+        'X-Content-Type-Options: nosniff; Content-Length: 2759; '
+        'X-XSS-Protection: 1; mode=block; '
+        'Alternate-Protocol: 80:quic]')
+
+    headers_value = plugin._ConvertHeadersValues(binary_value)
+    self.assertEqual(headers_value, expected_headers_value)
+
+  def testProcessOnDatabaseWithPartitionsTable(self):
+    """Tests the Process function on database with a Partitions table."""
     plugin = msie_webcache.MsieWebCacheESEDBPlugin()
     storage_writer = self._ParseESEDBFileWithPlugin(
         ['WebCacheV01.dat'], plugin)
@@ -48,6 +69,48 @@ class MsieWebCacheESEDBPluginTest(test_lib.ESEDBPluginTestCase):
     expected_short_message = (
         'Directory: C:\\Users\\test\\AppData\\Local\\Microsoft\\Windows\\'
         'INetCache\\IE\\')
+
+    self._TestGetMessageStrings(
+        event_data, expected_message, expected_short_message)
+
+  def testProcessOnDatabaseWithPartitionsExTable(self):
+    """Tests the Process function on database with a PartitionsEx table."""
+    plugin = msie_webcache.MsieWebCacheESEDBPlugin()
+    storage_writer = self._ParseESEDBFileWithPlugin(
+        ['PartitionsEx-WebCacheV01.dat'], plugin)
+
+    self.assertEqual(storage_writer.number_of_warnings, 3)
+    self.assertEqual(storage_writer.number_of_events, 4014)
+
+    # The order in which ESEDBPlugin._GetRecordValues() generates events is
+    # nondeterministic hence we sort the events.
+    events = list(storage_writer.GetSortedEvents())
+
+    event = events[100]
+
+    self.CheckTimestamp(event.timestamp, '2019-03-20 17:22:14.000000')
+    self.assertEqual(
+        event.timestamp_desc, definitions.TIME_DESCRIPTION_MODIFICATION)
+
+    event_data = self._GetEventDataOfEvent(storage_writer, event)
+    self.assertEqual(event_data.container_identifier, 14)
+
+    expected_message = (
+        'URL: https://www.bing.com/rs/3R/kD/ic/878ca0cd/b83d57c0.svg '
+        'Access count: 5 '
+        'Sync count: 0 '
+        'Filename: b83d57c0[1].svg '
+        'Cached file size: 726 '
+        'Response headers: [HTTP/1.1 200; '
+        'content-length: 726; '
+        'content-type: image/svg+xml; '
+        'x-cache: TCP_HIT; '
+        'x-msedge-ref: Ref A: 3CD5FCBC8EAD4E0A80FA41A62FBC8CCC '
+        'Ref B: PRAEDGE0910 Ref C: 2019-12-16T20:55:28Z; '
+        'date: Mon, 16 Dec 2019 20:55:28 GMT] '
+        'Entry identifier: 63 Container identifier: 14 Cache identifier: 0')
+    expected_short_message = (
+        'URL: https://www.bing.com/rs/3R/kD/ic/878ca0cd/b83d57c0.svg')
 
     self._TestGetMessageStrings(
         event_data, expected_message, expected_short_message)
