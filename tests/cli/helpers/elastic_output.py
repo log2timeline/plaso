635802503ed27400c5c6216bafbc935865c029b7
Billaud <wbillaud.exterieur@naval-group.com>
2020-05-26 09:52:12 +0200

    Adapt test to modification

--- tests/cli/helpers/elastic_output.py
+++ tests/cli/helpers/elastic_output.py
@@ -15,16 +15,16 @@ from tests.cli import test_lib as cli_test_lib
 from tests.cli.helpers import test_lib
 
 
-class ElasticSearchOutputArgumentsHelperTest(
-    test_lib.OutputModuleArgumentsHelperTest):
-  """Tests the Elastic Search output module CLI arguments helper."""
+class ElasticSearchOutputArgumentsHelperTest(test_lib.OutputModuleArgumentsHelperTest):
+    """Tests the Elastic Search output module CLI arguments helper."""
 
-  # pylint: disable=no-member,protected-access
+    # pylint: disable=no-member,protected-access
 
-  _EXPECTED_OUTPUT = """\
+    _EXPECTED_OUTPUT = """\
 usage: cli_helper.py [--index_name INDEX_NAME] [--doc_type DOCUMENT_TYPE]
                      [--flush_interval FLUSH_INTERVAL] [--raw_fields]
-                     [--elastic_user ELASTIC_USER] [--use_ssl]
+                     [--elastic_user ELASTIC_USER]
+                     [--elastic_password ELASTIC_PASSWORD] [--use_ssl]
                      [--ca_certificates_file_path CA_CERTIFICATES_FILE_PATH]
                      [--elastic_url_prefix ELASTIC_URL_PREFIX]
                      [--server HOSTNAME] [--port PORT]
@@ -38,6 +38,10 @@ optional arguments:
   --doc_type DOCUMENT_TYPE
                         Name of the document type that will be used in
                         ElasticSearch.
+  --elastic_password ELASTIC_PASSWORD
+                        Password to use for Elasticsearch authentication. Can
+                        also be set with the environment variable
+                        PLASO_ELASTIC_PASSWORD                        
   --elastic_url_prefix ELASTIC_URL_PREFIX
                         URL prefix for elastic search.
   --elastic_user ELASTIC_USER
@@ -54,32 +58,35 @@ optional arguments:
   --use_ssl             Enforces use of ssl.
 """
 
-  def testAddArguments(self):
-    """Tests the AddArguments function."""
-    argument_parser = argparse.ArgumentParser(
-        prog='cli_helper.py',
-        description='Test argument parser.', add_help=False,
-        formatter_class=cli_test_lib.SortedArgumentsHelpFormatter)
+    def testAddArguments(self):
+        """Tests the AddArguments function."""
+        argument_parser = argparse.ArgumentParser(
+            prog="cli_helper.py",
+            description="Test argument parser.",
+            add_help=False,
+            formatter_class=cli_test_lib.SortedArgumentsHelpFormatter,
+        )
 
-    elastic_output.ElasticSearchOutputArgumentsHelper.AddArguments(
-        argument_parser)
+        elastic_output.ElasticSearchOutputArgumentsHelper.AddArguments(argument_parser)
 
-    output = self._RunArgparseFormatHelp(argument_parser)
-    self.assertEqual(output, self._EXPECTED_OUTPUT)
+        output = self._RunArgparseFormatHelp(argument_parser)
+        self.assertEqual(output, self._EXPECTED_OUTPUT)
 
-  def testParseOptions(self):
-    """Tests the ParseOptions function."""
-    options = cli_test_lib.TestOptions()
+    def testParseOptions(self):
+        """Tests the ParseOptions function."""
+        options = cli_test_lib.TestOptions()
 
-    output_mediator = self._CreateOutputMediator()
-    output_module = elastic.ElasticsearchOutputModule(output_mediator)
-    elastic_output.ElasticSearchOutputArgumentsHelper.ParseOptions(
-        options, output_module)
+        output_mediator = self._CreateOutputMediator()
+        output_module = elastic.ElasticsearchOutputModule(output_mediator)
+        elastic_output.ElasticSearchOutputArgumentsHelper.ParseOptions(
+            options, output_module
+        )
 
-    with self.assertRaises(errors.BadConfigObject):
-      elastic_output.ElasticSearchOutputArgumentsHelper.ParseOptions(
-          options, None)
+        with self.assertRaises(errors.BadConfigObject):
+            elastic_output.ElasticSearchOutputArgumentsHelper.ParseOptions(
+                options, None
+            )
 
 
-if __name__ == '__main__':
-  unittest.main()
+if __name__ == "__main__":
+    unittest.main()
