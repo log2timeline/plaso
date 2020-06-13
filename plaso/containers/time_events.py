3505d60cafadf62c5214fdafa7bb0ca61387ce51
Joachim Metz <joachim.metz@gmail.com>
2020-06-13 20:18:43 +0200

    Removed TimestampEvent #910

--- plaso/containers/time_events.py
+++ plaso/containers/time_events.py
@@ -7,53 +7,29 @@ from plaso.containers import events
 from plaso.lib import timelib
 
 
-class TimestampEvent(events.EventObject):
-  """Plaso timestamp-based event attribute container.
+class DateTimeValuesEvent(events.EventObject):
+  """dfDateTime date time values-based event attribute container.
 
   Attributes:
-    data_type (str): event data type.
     timestamp (int): timestamp, which contains the number of microseconds
         since January 1, 1970, 00:00:00 UTC.
     timestamp_desc (str): description of the meaning of the timestamp.
   """
 
-  def __init__(self, timestamp, timestamp_description, data_type=None):
-    """Initializes an event.
-
-    Args:
-      timestamp (int): timestamp, which contains the number of microseconds
-          since January 1, 1970, 00:00:00 UTC.
-      timestamp_description (str): description of the meaning of the timestamp
-          value.
-      data_type (Optional[str]): event data type. If the data type is not set
-          it is derived from the DATA_TYPE class attribute.
-    """
-    super(TimestampEvent, self).__init__()
-    self.timestamp = timestamp
-    self.timestamp_desc = timestamp_description
-
-    if data_type:
-      self.data_type = data_type
-
-
-class DateTimeValuesEvent(TimestampEvent):
-  """dfDateTime date time values-based event attribute container."""
-
   def __init__(
-      self, date_time, date_time_description, data_type=None, time_zone=None):
+      self, date_time, date_time_description, time_zone=None):
     """Initializes an event.
 
     Args:
       date_time (dfdatetime.DateTimeValues): date and time values.
       date_time_description (str): description of the meaning of the date and
           time values.
-      data_type (Optional[str]): event data type. If the data type is not set
-          it is derived from the DATA_TYPE class attribute.
       time_zone (Optional[datetime.tzinfo]): time zone.
     """
     timestamp = date_time.GetPlasoTimestamp()
     if date_time.is_local_time and time_zone:
       timestamp = timelib.Timestamp.LocaltimeToUTC(timestamp, time_zone)
 
-    super(DateTimeValuesEvent, self).__init__(
-        timestamp, date_time_description, data_type=data_type)
+    super(DateTimeValuesEvent, self).__init__()
+    self.timestamp = timestamp
+    self.timestamp_desc = date_time_description
