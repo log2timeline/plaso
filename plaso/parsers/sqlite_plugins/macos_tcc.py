b52445042b82600be6e01f90d35195953e914c49
Thomas Chopitea <tomchop@gmail.com>
2020-06-22 10:54:50 +0200

    Fix docstring

--- plaso/parsers/sqlite_plugins/macos_tcc.py
+++ plaso/parsers/sqlite_plugins/macos_tcc.py
@@ -22,8 +22,8 @@ class MacOSTCCEntry(events.EventData):
   Attributes:
     allowed (bool): whether access to the service was allowed.
     client (str): name of the client requesting access to the service.
-    prompt_count (int): number of times an appplication prompted for access
-        to a service.
+    prompt_count (int): number of times an appplication prompted the user for
+        access to a service.
     service (str): name of the service.
   """
 
