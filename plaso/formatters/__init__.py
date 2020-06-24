66766cfeaa540f02f514a49eddac9f83664600b3
Joachim Metz <joachim.metz@gmail.com>
2020-06-24 13:56:24 +0200

    Changed formatters to use configuration #444

--- plaso/formatters/__init__.py
+++ plaso/formatters/__init__.py
@@ -10,11 +10,6 @@ from plaso.formatters import file_system
 from plaso.formatters import firefox
 from plaso.formatters import fseventsd
 from plaso.formatters import gdrive
-from plaso.formatters import hangouts_messages
-from plaso.formatters import imessage
-from plaso.formatters import kik_ios
-from plaso.formatters import mac_notificationcenter
-from plaso.formatters import macos_tcc
 from plaso.formatters import msiecf
 from plaso.formatters import olecf
 from plaso.formatters import recycler
@@ -24,13 +19,8 @@ from plaso.formatters import shell_items
 from plaso.formatters import symantec
 from plaso.formatters import tango_android
 from plaso.formatters import trendmicroav
-from plaso.formatters import twitter_android
-from plaso.formatters import twitter_ios
-from plaso.formatters import utmp
-from plaso.formatters import utmpx
 from plaso.formatters import winevt
 from plaso.formatters import winevtx
-from plaso.formatters import winjob
 from plaso.formatters import winlnk
 from plaso.formatters import winprefetch
 from plaso.formatters import winreg
