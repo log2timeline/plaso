d915d01cddd46da9f1b4fbb168e4bde331846603
Joachim Metz <joachim.metz@gmail.com>
2020-12-05 21:32:25 +0100

    Updated pylintrc configuration to version 2.6.x (#3326)

--- plaso/cli/psteal_tool.py
+++ plaso/cli/psteal_tool.py
@@ -385,6 +385,9 @@ class PstealTool(
     else:
       logger.debug('Starting extraction in multi process mode.')
 
+      # The following overrides are neede because pylint 2.6.0 gets confused
+      # about which ProcessSources to check against.
+      # pylint: disable=no-value-for-parameter,unexpected-keyword-arg
       processing_status = extraction_engine.ProcessSources(
           session, self._source_path_specs, storage_writer, configuration,
           enable_sigsegv_handler=self._enable_sigsegv_handler,
