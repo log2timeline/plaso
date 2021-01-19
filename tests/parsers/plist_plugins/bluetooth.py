bf468b48fe0121d3bdea591fd9dd266f0f7d9d10
Joachim Metz <joachim.metz@gmail.com>
2021-01-19 08:59:39 +0100

    Removed _TestGetMessageStrings from parser tests #3380

--- tests/parsers/plist_plugins/bluetooth.py
+++ tests/parsers/plist_plugins/bluetooth.py
@@ -28,46 +28,14 @@ class TestBluetoothPlugin(test_lib.PlistPluginTestCase):
     # hence we sort the events.
     events = list(storage_writer.GetSortedEvents())
 
-    paired_events = []
-    timestamps = []
-    for event in events:
-      timestamps.append(event.timestamp)
+    expected_event_values = {
+        'data_type': 'plist:key',
+        'desc': 'Paired:True Name:Apple Magic Trackpad 2',
+        'key': '44-00-00-00-00-04',
+        'root': '/DeviceCache',
+        'timestamp': '2012-11-02 01:13:17.324095'}
 
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      if event_data.desc.startswith('Paired'):
-        paired_events.append(event)
-
-    # Ensure all 14 events and times from the plist are parsed correctly.
-    self.assertEqual(len(timestamps), 14)
-
-    expected_timestamps = frozenset([
-        1341957896010535, 1341957896010535, 1350666385239662, 1350666391557044,
-        1341957900020117, 1302199013524275, 1301012201414766, 1351818797324095,
-        1351818797324095, 1351819298997673, 1351818803000000, 1351827808261762,
-        1345251268370453, 1345251192528750])
-
-    self.assertEqual(frozenset(timestamps), expected_timestamps)
-
-    # Ensure two paired devices are matched.
-    self.assertEqual(len(paired_events), 2)
-
-    # One of the paired event descriptions should contain the string:
-    # Paired:True Name:Apple Magic Trackpad 2.
-    paired_descriptions = []
-    for event in paired_events:
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      paired_descriptions.append(event_data.desc)
-
-    self.assertTrue(
-        'Paired:True Name:Apple Magic Trackpad 2' in paired_descriptions)
-
-    expected_string = (
-        '/DeviceCache/44-00-00-00-00-04 '
-        'Paired:True '
-        'Name:Apple Magic Trackpad 2')
-
-    event_data = self._GetEventDataOfEvent(storage_writer, events[10])
-    self._TestGetMessageStrings(event_data, expected_string, expected_string)
+    self.CheckEventValues(storage_writer, events[10], expected_event_values)
 
 
 if __name__ == '__main__':
