0ff56a907fdce3de73e2c06a15398c3f2a4073b4
Joachim Metz <joachim.metz@gmail.com>
2021-05-23 11:16:46 +0200

    Changes to support pefile 2021.5.13 #3591 (#3628)

--- tests/parsers/pe.py
+++ tests/parsers/pe.py
@@ -17,17 +17,18 @@ class PECOFFTest(test_lib.ParserTestCase):
     parser = pe.PEParser()
     storage_writer = self._ParseFile(['test_pe.exe'], parser)
 
-    self.assertEqual(storage_writer.number_of_warnings, 0)
     self.assertEqual(storage_writer.number_of_events, 3)
+    self.assertEqual(storage_writer.number_of_extraction_warnings, 0)
+    self.assertEqual(storage_writer.number_of_recovery_warnings, 0)
 
-    events = list(storage_writer.GetEvents())
+    events = list(storage_writer.GetSortedEvents())
 
     expected_event_values = {
         'data_type': 'pe:compilation:compilation_time',
         'pe_type': 'Executable (EXE)',
         'timestamp': '2015-04-21 14:53:56.000000'}
 
-    self.CheckEventValues(storage_writer, events[0], expected_event_values)
+    self.CheckEventValues(storage_writer, events[2], expected_event_values)
 
     expected_event_values = {
         'data_type': 'pe:import:import_time',
@@ -42,21 +43,23 @@ class PECOFFTest(test_lib.ParserTestCase):
         'pe_type': 'Executable (EXE)',
         'timestamp': '2015-04-21 14:53:54.000000'}
 
-    self.CheckEventValues(storage_writer, events[2], expected_event_values)
+    self.CheckEventValues(storage_writer, events[0], expected_event_values)
 
   def testParseFileObjectOnDriver(self):
     """Tests the ParseFileObject on a PE driver (SYS) file."""
     parser = pe.PEParser()
     storage_writer = self._ParseFile(['test_driver.sys'], parser)
 
-    self.assertEqual(storage_writer.number_of_warnings, 0)
     self.assertEqual(storage_writer.number_of_events, 1)
+    self.assertEqual(storage_writer.number_of_extraction_warnings, 0)
+    self.assertEqual(storage_writer.number_of_recovery_warnings, 0)
 
-    events = list(storage_writer.GetEvents())
+    events = list(storage_writer.GetSortedEvents())
 
+    # Note that pefile pre 2021.5.13 indicated pe_type is 'Driver (SYS)'
     expected_event_values = {
         'data_type': 'pe:compilation:compilation_time',
-        'pe_type': 'Driver (SYS)',
+        'pe_type': 'Executable (EXE)',
         'timestamp': '2015-04-21 14:53:54.000000'}
 
     self.CheckEventValues(storage_writer, events[0], expected_event_values)
