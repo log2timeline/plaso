1470ad7e0e2b37c33b92c417a0e1ea5e8c1cc988
SydP <syd.pleno@gmail.com>
2021-07-13 22:54:18 +1000

    Update locate parser to use dtFabric instead of struct

--- plaso/parsers/locate.py
+++ plaso/parsers/locate.py
@@ -3,14 +3,12 @@
 
 import os
 
-from struct import unpack as struct_unpack
-from struct import error as struct_error
-
 from dfdatetime import posix_time
 
 from plaso.containers import events
 from plaso.containers import time_events
 from plaso.lib import definitions
+from plaso.lib import dtfabric_helper
 from plaso.lib import errors
 from plaso.lib import specification
 from plaso.parsers import interface
@@ -32,43 +30,25 @@ class LocateDatabaseEvent(events.EventData):
     self.folder_path = None
 
 
-class LocateDatabaseFile(object):
+class LocateDatabaseFile(dtfabric_helper.DtFabricHelper):
   """Locate database file."""
 
+  _DEFINITION_FILE = os.path.join(
+    os.path.dirname(__file__), 'locate.yaml')
+
   DB_MAGIC = b"\x00mlocate"
-
-  # header struct format
-  _DB_HEADER = ">8sIbb2s"
-
-  # directory struct format
-  _DB_DIRECTORY = ">QI4s"
-
-  # Entry Types
-  _DBE_NORMAL = 0
-  _DBE_DIRECTORY = 1
-  _DBE_END = 2
 
   def __init__(self):
     """Initialises a locate database file."""
     super(LocateDatabaseFile, self).__init__()
     self.root_path = None
     self._file_object = None
+    self._file_offset = 0
 
   def Close(self):
     """Closes the file."""
-    self._file_object.Close()
-
-  def _ParseCString(self):
-    """Parse a C-style string from the file object."""
-    root_path = [self._file_object.read(1)]
-    # if len(root_path) != 1:
-    #  raise IOError
-    while root_path[-1] != b'\x00':
-      c = self._file_object.read(1)
-      # if len(c) != 1:
-      #  raise IOError
-      root_path.append(c)
-    return b"".join(root_path[:-1])
+    self._file_object = None
+    self._file_offset = 0
 
   def ParsePaths(self):
     """Retrieves the paths and the last created|modified time
@@ -80,30 +60,48 @@ class LocateDatabaseFile(object):
     Raises:
       UnableToParseFile: if the file cannot be parsed
     """
+    directory_header_map = self._GetDataTypeMap('directory_header')
+    directory_entry_map = self._GetDataTypeMap('directory_entry')
+    cstring_map = self._GetDataTypeMap('cstring')
+
     while True:
+      if self._file_object.tell() + 16 > self._file_object.get_size():
+        break
       try:
-        dir_buffer = self._file_object.read(16)
-        if len(dir_buffer) != 16:
-          break
-        (time_sec, time_nanosec, _pad) = \
-          struct_unpack(self._DB_DIRECTORY, dir_buffer)
-        dir_name = self._ParseCString()
+        directory_header, directory_header_size = (
+          self._ReadStructureFromFileObject(self._file_object,
+            self._file_offset, directory_header_map))
+        self._file_offset += directory_header_size
+      except (ValueError, errors.ParseError) as exception:
+        raise errors.UnableToParseFile(
+          'Unable to parse locate directory header with error {0!s}'.format(
+            exception))
 
-        dir_timestamp = time_sec*definitions.NANOSECONDS_PER_SECOND
-        dir_timestamp += time_nanosec
-        posix_timestamp = posix_time.PosixTimeInNanoseconds(
-          timestamp=dir_timestamp)
-        yield dir_name.decode(), posix_timestamp
+      directory_timestamp = (directory_header.time_sec *
+        definitions.NANOSECONDS_PER_SECOND)
+      directory_timestamp += directory_header.time_nsec
+      posix_timestamp = posix_time.PosixTimeInNanoseconds(
+        timestamp=directory_timestamp)
+      yield directory_header.name, posix_timestamp
 
-        # skip over file / subdirectory names as they don't have any timestamps
-        while True:
-          db_type = int.from_bytes(self._file_object.read(1), byteorder="big")
-
-          if db_type == self._DBE_END:
+      # skip over file / subdirectory names as they don't have any timestamps
+      while True:
+        try:
+          directory_entry, directory_entry_size = (
+            self._ReadStructureFromFileObject(self._file_object,
+              self._file_offset, directory_entry_map))
+          self._file_offset += directory_entry_size
+          if directory_entry.type == 0x02:
             break
-          self._ParseCString()
-      except struct_error:
-        raise errors.UnableToParseFile('Error in header')
+
+          _, data_size = self._ReadStructureFromFileObject(
+            self._file_object, self._file_offset, cstring_map)
+          self._file_offset += data_size
+        except (ValueError, errors.ParseError) as exception:
+          raise errors.UnableToParseFile(
+            'Unable to parse locate directory entry with error {0!s}'.format(
+              exception))
+
 
   def Open(self, file_object):
     """Opens a Locate database file.
@@ -121,24 +119,32 @@ class LocateDatabaseFile(object):
       raise ValueError('Missing file object.')
 
     self._file_object = file_object
-    self._file_object.seek(0, os.SEEK_SET)
 
-    header_bytes = self._file_object.read(16)
-    if len(header_bytes) != 16:
-      raise errors.UnableToParseFile('Unable to read the file header')
+    locate_database_header_map = self._GetDataTypeMap('locate_database_header')
 
     try:
-      (magic, conf_size, _version, _check_visibility, _pad) = \
-        struct_unpack(self._DB_HEADER, header_bytes)
-      if magic != self.DB_MAGIC:
-        raise errors.UnableToParseFile('Invalid file magic')
+      locate_database_header, data_size = self._ReadStructureFromFileObject(
+        self._file_object, self._file_offset, locate_database_header_map)
+      self._file_offset += data_size
+    except (ValueError, errors.ParseError) as exception:
+      raise errors.UnableToParseFile(
+        'Unable to parse locate database header with error: {0!s}'.format(
+          exception))
+    if locate_database_header.signature != self.DB_MAGIC:
+      raise errors.UnableToParseFile('Invalid file magic')
 
-      self.root_path = self._ParseCString()
+    cstring_map = self._GetDataTypeMap('cstring')
+    try:
+      self.root_path, data_size = self._ReadStructureFromFileObject(
+        self._file_object, self._file_offset, cstring_map)
+      self._file_offset += data_size
+    except (ValueError, errors.ParseError) as exception:
+      raise errors.UnableToParseFile(
+        'Unable to parse root path with error: {0!s}'.format(
+          exception))
 
-      # skip configuration section for now
-      self._file_object.seek(conf_size, os.SEEK_CUR)
-    except struct_error:
-      raise errors.UnableToParseFile('Unable to unpack the file header')
+    # skip configuration section for now
+    self._file_offset += locate_database_header.conf_size
 
 
 class LocateDatabaseParser(interface.FileObjectParser):
@@ -152,7 +158,7 @@ class LocateDatabaseParser(interface.FileObjectParser):
     """Retrieves the format specification
 
     Returns:
-      FormatSpecification: format spefication.
+      FormatSpecification: format specifation.
     """
     format_specification = specification.FormatSpecification(cls.NAME)
     format_specification.AddNewSignature(LocateDatabaseFile.DB_MAGIC, offset=0)
@@ -179,13 +185,14 @@ class LocateDatabaseParser(interface.FileObjectParser):
         event_data = LocateDatabaseEvent()
         event_data.folder_path = folder_path
 
-        event = time_events.DateTimeValuesEvent(folder_timestamp,
-          definitions.TIME_DESCRIPTION_MODIFICATION)
+        event = time_events.DateTimeValuesEvent(
+          folder_timestamp, definitions.TIME_DESCRIPTION_MODIFICATION)
         parser_mediator.ProduceEventWithEventData(event, event_data)
     except Exception as exception:  # pylint: disable=broad-except
       raise errors.UnableToParseFile(
-        'unable to parse locate database with error: {exception}'.format(
-          exception=exception))
+        'unable to parse locate database with error: {0!s}'.format(exception))
+    finally:
+      locate_file.Close()
 
 
 manager.ParsersManager.RegisterParser(LocateDatabaseParser)
