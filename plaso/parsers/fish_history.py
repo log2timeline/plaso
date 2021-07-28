9850a796efbb46cc0448a3d9a4b92f7002453531
Syd Pleno <syd.pleno@gmail.com>
2021-07-28 04:04:56 -0700

    Updates to avoid large files / consuming large amounts of memory

--- plaso/parsers/fish_history.py
+++ plaso/parsers/fish_history.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """Parser for fish history files."""
-import io
+import os
 import re
 
 import yaml
 
 from dfdatetime import posix_time as dfdatetime_posix_time
 
+from dfvfs.helpers import text_file
+
 from plaso.containers import events
 from plaso.containers import time_events
 from plaso.lib import errors
 from plaso.lib import definitions
-from plaso.parsers import logger
 from plaso.parsers import manager
 from plaso.parsers import interface
 
@@ -39,6 +40,9 @@ class FishHistoryParser(interface.FileObjectParser):
 
   _ENCODING = 'utf-8'
 
+  # 50MB is the maximum fish history file that will be parsed
+  _MAXIMUM_FISH_HISTORY_FILE_SIZE = 1024 * 1024 * 50
+
   _FILENAME = 'fish_history'
   _YAML_FORMAT_RE_1 = re.compile(r'^- cmd: \S+')
   _YAML_FORMAT_RE_2 = re.compile(r'  when: [0-9]{9}')
@@ -58,19 +62,25 @@ class FishHistoryParser(interface.FileObjectParser):
     filename = parser_mediator.GetFilename()
 
     if filename != self._FILENAME:
-      raise errors.UnableToParseFile('Not a fish history file.')
+      raise errors.UnableToParseFile('Not a Fish history file.')
 
-    file_contents = file_object.read()
+    text_file_object = text_file.TextFile(file_object, encoding='utf-8')
 
-    file_contents_io = io.StringIO(file_contents.decode())
-    header_line_1 = file_contents_io.readline()
-    header_line_2 = file_contents_io.readline()
+    header_line_1 = text_file_object.readline()
+    header_line_2 = text_file_object.readline()
     if (not self._YAML_FORMAT_RE_1.match(header_line_1) or
         not self._YAML_FORMAT_RE_2.match(header_line_2)):
-      raise errors.UnableToParseFile('Not a valid fish history file.')
+      raise errors.UnableToParseFile('Not a valid Fish history file.')
+
+    file_size = file_object.get_size()
+    if file_size > self._MAXIMUM_FISH_HISTORY_FILE_SIZE:
+      parser_mediator.ProduceExtractionWarning(
+          'File size of {0}B exceeds limit for a Fish history file'.format(
+              file_size))
 
     try:
-      fish_history = yaml.safe_load(file_contents)
+      file_object.seek(0, os.SEEK_SET)
+      fish_history = yaml.safe_load(file_object)
     except yaml.YAMLError as exception:
       raise errors.ParseError(
           'Error while loading/parsing YAML with error {0:s}'.format(
@@ -88,7 +98,7 @@ class FishHistoryParser(interface.FileObjectParser):
         if not isinstance(last_executed, int):
           last_executed = int(last_executed, 10)
       except (TypeError, ValueError) as exception:
-        logger.debug(
+        parser_mediator.ProduceExtractionWarning(
             'Invalid timestamp {0!s}, skipping record'.format(exception))
         continue
       date_time = dfdatetime_posix_time.PosixTime(timestamp=last_executed)
