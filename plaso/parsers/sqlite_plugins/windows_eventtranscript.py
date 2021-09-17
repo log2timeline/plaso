a109052637c71e658ba82950c5a0ef45bc867c1a
Roshan Maskey <rmaskey@google.com>
2021-09-17 14:59:48 +1000

    Fixed fields and formatting issues

--- plaso/parsers/sqlite_plugins/windows_eventtranscript.py
+++ plaso/parsers/sqlite_plugins/windows_eventtranscript.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""SQLite parser plugin for Windows diagnosis EventTranscript database files."""
+"""SQLite parser plugin for Windows Diagnosis EventTranscript database file."""
 import json
 
 from dfdatetime import filetime as dfdatetime_filetime
@@ -15,42 +15,38 @@ class EventTranscriptEventData(events.EventData):
   """Windows diagnosis EventTranscript event data.
 
   Attributes:
-    sid (str): SID of the user account.
-    timestamp (int): Timestamp in UTC
-    raw_payload (str): JSON payload
+    sid (str): Windows Security ID of a user account.
+    timestamp (int): Event timestamp in UTC
+    raw_payload (str): JSON payload specific to event name
     full_event_name (str): Diagnosis full event name.
     full_event_name_hash (int): Hash of full event name.
     event_keywords (int): Event keywords
     is_core (int): Boolean value represted as integer.
     provider_group_id (int): Provider group ID.
-    logging_binary_name (str): Binary executed.
+    logging_binary_name (str): Binary that generated the event.
     friendly_logging_binary_name (str): Friendly name for logging binary.
     compressed_payload_size (int): Size of compressed payload.
     producer_id (int): Producer ID.
-    tag_id (int): Tag ID
-    tag_name (str): Tag name
-    locale_name (str): Locale name
     description (str): Tag description
 
     The section below contains partially parsed payload data.
     ver (str): Payload version
     name (str): Name of the event. This contains the same information as
-      full_event_name.
+        full_event_name.
     time (str): Timestamp of the event as string in UTC. This is the same time
-      recorded in timestamp field.
+        recorded in timestamp field.
     ikey (str): iKey
     ext (str): This field contains the additional information about the event.
-      The information is JSON document stored as string.
+        The information is JSON document stored as string.
     data (str): This field contains the important information about the event.
-      The field contains JSON data stored as string. Due to the dynamic nature
-      of the data, extracting field is not practicle.
+        The field contains JSON data stored as string. Due to the dynamic nature
+        of the data, extracting field is not practicle.
 
-    # This section contains selective fields extracted from payload
+    # This section contains selective fields extracted from payload data field.
     app_name (str): Application name.
     app_version (str): Applciation version.
     app_root_dir_path (str): Application root directory.
     app_install_date (str): Application installed date.
-    app_start_time (str): Application start date/time.
   """
 
   DATA_TYPE = 'windows:diagnosis:eventtranscript'
@@ -70,24 +66,23 @@ class EventTranscriptEventData(events.EventData):
     self.friendly_logging_binary_name = None
     self.compressed_payload_size = None
     self.producer_id = None
-    self.producer_id_text = None
-    self.tag_id = None
-    self.locale_name = None
-    self.tag_name = None
-    self.description = None
-    # Selective payload fields
+
+    # Fields extracted from payload structure
     self.ver = None
     self.name = None
     self.time = None
     self.ikey = None
     self.ext = None
     self.data = None
-    # Parsed payload fields
+
+    # The fields below are extracted from the diagnosis events
+    # - Microsoft.Windows.Inventory.Core.InventoryApplicationAdd
+    # - Win32kTraceLogging.AppInteractivitySummary
     self.app_name = None
     self.app_version = None
     self.app_root_dir_path = None
     self.app_install_date = None
-    self.app_start_time = None
+
 
 class EventTranscriptPlugin(interface.SQLitePlugin):
   """SQLite parser plugin for Windows diagnosis EventTranscript database files.
@@ -96,25 +91,26 @@ class EventTranscriptPlugin(interface.SQLitePlugin):
   EventTranscript.db
   """
 
-  NAME = 'windows_diagnosis_eventtranscript'
+  NAME = 'windows:diagnosis:eventtranscript'
   DATA_FORMAT = (
       'Windows diagnosis EventTranscript SQLite database (EventTranscript.db)'
       ' file')
 
   REQUIRED_STRUCTURE = {
-    'events_persisted': frozenset([
-      'sid', 'timestamp', 'payload', 'full_event_name', 'full_event_name_hash',
-      'event_keywords', 'is_core', 'provider_group_id', 'logging_binary_name',
-      'friendly_logging_binary_name', 'compressed_payload_size', 'producer_id'
-    ]),
-    'event_tags': frozenset([
-      'full_event_name_hash', 'tag_id']),
-    'tag_descriptions': frozenset([
-      'tag_id', 'locale_name', 'tag_name', 'description']),
-    'producers': frozenset([
-      'producer_id', 'producer_id_text'
-    ]),
-  }
+      'events_persisted': frozenset([
+        'sid',
+        'timestamp',
+        'payload',
+        'full_event_name',
+        'full_event_name_hash',
+        'event_keywords',
+        'is_core',
+        'provider_group_id',
+        'logging_binary_name',
+        'friendly_logging_binary_name',
+        'compressed_payload_size',
+        'producer_id'
+      ])}
 
   QUERIES = [
       (('SELECT events_persisted.sid,'
@@ -128,40 +124,33 @@ class EventTranscriptPlugin(interface.SQLitePlugin):
       'events_persisted.logging_binary_name,'
       'events_persisted.friendly_logging_binary_name,'
       'events_persisted.compressed_payload_size,'
-      'events_persisted.producer_id,'
-      'producers.producer_id_text,'
-      'event_tags.tag_id,'
-      'tag_descriptions.locale_name,'
-      'tag_descriptions.tag_name,'
-      'tag_descriptions.description '
-      'from events_persisted, event_tags, producers, tag_descriptions '
-      'where '
-      'events_persisted.producer_id=producers.producer_id and '
-      'events_persisted.full_event_name_hash=event_tags.full_event_name_hash '
-      'and event_tags.tag_id=tag_descriptions.tag_id'),
+      'events_persisted.producer_id '
+      'from events_persisted'),
       'ParseEventTranscriptRow')]
 
   SCHEMAS = [{
       'events_persisted': (
-        'CREATE TABLE events_persisted ( sid TEXT, timestamp INTEGER, payload '
-        'TEXT, full_event_name TEXT, full_event_name_hash INTEGER, '
-        'event_keywords INTEGER, is_core INTEGER, provider_group_id INTEGER, '
-        'logging_binary_name TEXT, friendly_logging_binary_name TEXT, '
-        'compressed_payload_size INTEGER, producer_id INTEGER, extra1 TEXT, '
-        'extra2 TEXT, extra3 TEXT, FOREIGN KEY(provider_group_id)  '
-        'REFERENCES provider_groups(group_id), CONSTRAINT fk_producer_id '
-        'FOREIGN KEY(producer_id) REFERENCES producers(producer_id) ON DELETE '
-        'CASCADE)'),
-      'event_tags': (
-        'CREATE TABLE event_tags (full_event_name_hash INTGER, tag_id INTEGER)'
-      ),
-      'producers': (
-        'CREATE TABLE producers (producer_id INTEGER, producer_id_text TEXT)'
-      ),
-      'tag_descriptions': (
-        'CREATE TABLE tag_descriptions (tag_id INTEGER, locale_name TEXT, '
-        'tag_name TEXT, description TEXT)')
-  }]
+        'CREATE TABLE events_persisted ('
+          'sid TEXT,'
+          'timestamp INTEGER,'
+          'payload TEXT,'
+          'full_event_name TEXT,'
+          'full_event_name_hash INTEGER,'
+          'event_keywords INTEGER,'
+          'is_core INTEGER, '
+          'provider_group_id INTEGER,'
+          'logging_binary_name TEXT,'
+          'friendly_logging_binary_name TEXT, '
+          'compressed_payload_size INTEGER,'
+          'producer_id INTEGER,'
+          'extra1 TEXT, '
+          'extra2 TEXT,'
+          'extra3 TEXT,'
+          'FOREIGN KEY(provider_group_id) '
+          'REFERENCES provider_groups(group_id), '
+          'CONSTRAINT fk_producer_id '
+          'FOREIGN KEY(producer_id) '
+          'REFERENCES producers(producer_id) ON DELETE CASCADE)')}]
 
   def ParseEventTranscriptRow(
       self, parser_mediator, query, row, **unused_kwargs):
@@ -198,16 +187,6 @@ class EventTranscriptPlugin(interface.SQLitePlugin):
         query_hash, row, 'compressed_payload_size')
     event_data.producer_id = self._GetRowValue(
         query_hash, row, 'producer_id')
-    event_data.producer_id_text = self._GetRowValue(
-        query_hash, row, 'producer_id_text')
-    event_data.tag_id = self._GetRowValue(
-        query_hash, row, 'tag_id')
-    event_data.locale_name = self._GetRowValue(
-        query_hash, row, 'locale_name')
-    event_data.tag_name = self._GetRowValue(
-        query_hash, row, 'tag_name')
-    event_data.description = self._GetRowValue(
-        query_hash, row, 'description')
 
     # Parse raw_payload
     payload = json.loads(self._GetRowValue(
@@ -220,16 +199,17 @@ class EventTranscriptPlugin(interface.SQLitePlugin):
     event_data.data = json.dumps(payload['data'], separators=(',',':'))
 
     # Parse payload for key fields
-    #pylint: disable=line-too-long, C0301
-    if payload['name'] == 'Microsoft.Windows.Inventory.Core.InventoryApplicationAdd':
+    # Microsoft.Windows.Inventory.Core.InventoryApplicationAdd
+    if payload['name'] == '''Microsoft.Windows.
+Inventory.Core.InventoryApplicationAdd''':
       event_data.app_name = payload['data']['Name']
       event_data.app_version = payload['data']['Version']
       event_data.app_root_dir_path = payload['data']['RootDirPath']
       event_data.app_install_date = payload['data']['InstallDate']
 
+    # Win32kTraceLogging.AppInteractivitySummary
     if payload['name'] == 'Win32kTraceLogging.AppInteractivitySummary':
       event_data.app_version = payload['data']['AppVersion']
-      event_data.app_start_time = payload['data']['AggregationStartTime']
 
     timestamp = self._GetRowValue(
         query_hash, row, 'timestamp')
@@ -237,7 +217,8 @@ class EventTranscriptPlugin(interface.SQLitePlugin):
       date_time = dfdatetime_filetime.Filetime(
           timestamp=timestamp)
       event = time_events.DateTimeValuesEvent(
-          date_time, definitions.TIME_DESCRIPTION_START)
+          date_time, definitions.TIME_DESCRIPTION_RECORDED)
       parser_mediator.ProduceEventWithEventData(event, event_data)
+
 
 sqlite.SQLiteParser.RegisterPlugin(EventTranscriptPlugin)
--- tests/parsers/sqlite_plugins/windows_eventtranscript.py
+++ tests/parsers/sqlite_plugins/windows_eventtranscript.py
@@ -1,38 +1,47 @@
 # -*- coding: utf-8 -*-
-"""Tests for the Windows diagnosis EventTranscript database plugin."""
+"""Tests for the Windows Diagnosis EventTranscript database plugin."""
 
 import unittest
 
-#pylint: disable=line-too-long, C0301
-from plaso.parsers.sqlite_plugins import windows_eventtranscript as eventtranscript
+from plaso.parsers.sqlite_plugins import windows_eventtranscript
 
 from tests.parsers.sqlite_plugins import test_lib
 
 
 class EventTranscriptPluginTest(test_lib.SQLitePluginTestCase):
-  """Tests for the Windows diagnosis EventTranscript database plugin."""
+  """Tests for the Windows Diagnosis EventTranscript database plugin."""
 
   def testProcess(self):
-    """Tests the Process function on a Windows diagnosis EventTranscript
+    """Tests the Process function on a Windows Diagnosis EventTranscript
     database file.
     """
-    plugin = eventtranscript.EventTranscriptPlugin()
+    plugin = windows_eventtranscript.EventTranscriptPlugin()
     storage_writer = self._ParseDatabaseFileWithPlugin(
         ['EventTranscript.db'], plugin)
 
-    self.assertEqual(storage_writer.number_of_events, 4595)
+    self.assertEqual(storage_writer.number_of_events, 2526)
     self.assertEqual(storage_writer.number_of_extraction_warnings, 0)
     self.assertEqual(storage_writer.number_of_recovery_warnings, 0)
 
     events = list(storage_writer.GetEvents())
 
-    # Check the first page visited event.
+    # Check the first record in the events_persisted table.
     expected_event_values = {
         'data_type': 'windows:diagnosis:eventtranscript',
         'date_time': '2021-08-17 11:27:06.2573347',
         'sid': '',
-        # pylint: disable=line-too-long
-        'raw_payload': '{"ver":"4.0","name":"TelClientSyntheticScenario.AcceptingScenario_0","time":"2021-08-17T11:27:06.2573347Z","iKey":"o:0a89d516ae714e01ae89c96d185e9ae3","ext":{"utc":{"eventFlags":257,"pgName":"WIN","flags":469762632,"epoch":"1000837","seq":217},"metadata":{"privTags":16777216,"f":{"ScenarioId":8}},"os":{"bootId":10,"name":"Windows","ver":"10.0.19041.1165.amd64fre.vb_release.191206-1406"},"app":{"asId":63},"device":{"localId":"s:5A2CD9B2-5E46-4396-99A8-5C79DF95E368","deviceClass":"Windows.Desktop"},"protocol":{"devMake":"innotek GmbH","devModel":"VirtualBox"},"loc":{"tz":"+10:00"}},"data":{"ScenarioId":"7E9FEC95-51F6-482B-9D9E-7507E2B17F07","ScenarioName":"RDP Diagnostics Start Desktop OD V8"}}',
+        'raw_payload': (
+            '{"ver":"4.0","name":"TelClientSyntheticScenario.AcceptingScenar'
+            'io_0","time":"2021-08-17T11:27:06.2573347Z","iKey":"o:0a89d516ae'
+            '714e01ae89c96d185e9ae3","ext":{"utc":{"eventFlags":257,"pgName":'
+            '"WIN","flags":469762632,"epoch":"1000837","seq":217},"metadata":'
+            '{"privTags":16777216,"f":{"ScenarioId":8}},"os":{"bootId":10,"na'
+            'me":"Windows","ver":"10.0.19041.1165.amd64fre.vb_release.191206-'
+            '1406"},"app":{"asId":63},"device":{"localId":"s:5A2CD9B2-5E46-43'
+            '96-99A8-5C79DF95E368","deviceClass":"Windows.Desktop"},"protocol'
+            '":{"devMake":"innotek GmbH","devModel":"VirtualBox"},"loc":{"tz"'
+            ':"+10:00"}},"data":{"ScenarioId":"7E9FEC95-51F6-482B-9D9E-7507E2'
+            'B17F07","ScenarioName":"RDP Diagnostics Start Desktop OD V8"}}'),
         'full_event_name': 'TelClientSyntheticScenario.AcceptingScenario_0',
         'full_event_name_hash': 2681079708,
         'event_keywords': 140737488355328,
@@ -42,17 +51,25 @@ class EventTranscriptPluginTest(test_lib.SQLitePluginTestCase):
         'friendly_logging_binary_name': '?unknown?',
         'compressed_payload_size': 50,
         'producer_id': 1,
-        'producer_id_text': 'Windows',
-        'tag_id': 24,
-        'locale_name': 'en-US',
-        'tag_name': 'Product and Service Performance',
-        'description': 'Data collected about the measurement, performance and operation of the capabilities of the product or service.  This data represents information about the capability and its use, with a focus on providing the capabilities of the product or service.',
         'ver': '4.0',
         'name': 'TelClientSyntheticScenario.AcceptingScenario_0',
         'time': '2021-08-17T11:27:06.2573347Z',
         'ikey': 'o:0a89d516ae714e01ae89c96d185e9ae3',
-        'ext': '{"utc":{"eventFlags":257,"pgName":"WIN","flags":469762632,"epoch":"1000837","seq":217},"metadata":{"privTags":16777216,"f":{"ScenarioId":8}},"os":{"bootId":10,"name":"Windows","ver":"10.0.19041.1165.amd64fre.vb_release.191206-1406"},"app":{"asId":63},"device":{"localId":"s:5A2CD9B2-5E46-4396-99A8-5C79DF95E368","deviceClass":"Windows.Desktop"},"protocol":{"devMake":"innotek GmbH","devModel":"VirtualBox"},"loc":{"tz":"+10:00"}}',
-        'data': '{"ScenarioId":"7E9FEC95-51F6-482B-9D9E-7507E2B17F07","ScenarioName":"RDP Diagnostics Start Desktop OD V8"}',
+        'ext': (
+            '{"utc":{"eventFlags":257,"pgName":"WIN","flags":469'
+            '762632,"epoch":"1000837","seq":217},"metadata":{"privTags":16777'
+            '216,"f":{"ScenarioId":8}},"os":{"bootId":10,"name":"Windows","ve'
+            'r":"10.0.19041.1165.amd64fre.vb_release.191206-1406"},"app":{"as'
+            'Id":63},"device":{"localId":"s:5A2CD9B2-5E46-4396-99A8-5C79DF95E'
+            '368","deviceClass":"Windows.Desktop"},"protocol":{"devMake":"inn'
+            'otek GmbH","devModel":"VirtualBox"},"loc":{"tz":"+10:00"}}'),
+        'data': (
+            '{"ScenarioId":"7E9FEC95-51F6-482B-9D9E-7507E2B17F07","ScenarioNa'
+            'me":"RDP Diagnostics Start Desktop OD V8"}'),
+        'app_name': None,
+        'app_version': None,
+        'app_root_dir_path': None,
+        'app_install_date': None,
         }
 
     self.CheckEventValues(storage_writer, events[0], expected_event_values)
