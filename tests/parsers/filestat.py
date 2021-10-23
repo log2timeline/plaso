69bd9ae777d3facf085ebe8eea095de268608869
Joachim Metz <joachim.metz@gmail.com>
2021-10-23 14:14:10 +0200

    Added extended attribute names to filestat #3808 (#3893)

--- tests/parsers/filestat.py
+++ tests/parsers/filestat.py
@@ -19,10 +19,45 @@ from tests.parsers import test_lib
 class FileStatTest(test_lib.ParserTestCase):
   """Tests for filestat parser."""
 
-  def testTSKFile(self):
-    """Read a file within an image file and make few tests."""
-    parser = filestat.FileStatParser()
+  def testExt4File(self):
+    """Test a file in an ext4 file system."""
+    test_file_path = self._GetTestFilePath(['ext4.raw'])
+    self._SkipIfPathNotExists(test_file_path)
 
+    os_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_OS, location=test_file_path)
+    ext_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_EXT, inode=14,
+        location='/passwords.txt', parent=os_path_spec)
+
+    parser = filestat.FileStatParser()
+    storage_writer = self._ParseFileByPathSpec(ext_path_spec, parser)
+
+    self.assertEqual(storage_writer.number_of_events, 4)
+    self.assertEqual(storage_writer.number_of_extraction_warnings, 0)
+    self.assertEqual(storage_writer.number_of_recovery_warnings, 0)
+
+    events = list(storage_writer.GetEvents())
+
+    expected_event_values = {
+        'attribute_names': ['security.selinux'],
+        'data_type': 'fs:stat',
+        'date_time': '2021-07-22 14:07:32.842610820',
+        'display_name': 'EXT:/passwords.txt',
+        'file_entry_type': 'file',
+        'file_size': 116,
+        'file_system_type': 'EXT',
+        'group_identifier': 1000,
+        'inode': 14,
+        'mode': 0o664,
+        'number_of_links': 1,
+        'owner_identifier': 1000,
+        'timestamp_desc': definitions.TIME_DESCRIPTION_LAST_ACCESS}
+
+    self.CheckEventValues(storage_writer, events[0], expected_event_values)
+
+  def testExt2FileWithTSK(self):
+    """Test a file in an ext2 file system with TSK."""
     test_file_path = self._GetTestFilePath(['ímynd.dd'])
     self._SkipIfPathNotExists(test_file_path)
 
@@ -32,6 +67,7 @@ class FileStatTest(test_lib.ParserTestCase):
         dfvfs_definitions.TYPE_INDICATOR_TSK, inode=15,
         location='/passwords.txt', parent=os_path_spec)
 
+    parser = filestat.FileStatParser()
     storage_writer = self._ParseFileByPathSpec(tsk_path_spec, parser)
 
     self.assertEqual(storage_writer.number_of_events, 3)
@@ -40,7 +76,10 @@ class FileStatTest(test_lib.ParserTestCase):
 
     events = list(storage_writer.GetEvents())
 
+    # Note that attribute support in pytsk/libtsk is currently broken
+    # also see: https://github.com/py4n6/pytsk/issues/79
     expected_event_values = {
+        'attribute_names': None,
         'data_type': 'fs:stat',
         'date_time': '2012-05-25 16:00:53',
         'display_name': 'TSK:/passwords.txt',
@@ -56,10 +95,43 @@ class FileStatTest(test_lib.ParserTestCase):
 
     self.CheckEventValues(storage_writer, events[0], expected_event_values)
 
+  def testNtfsFile(self):
+    """Test a file in a NTFS file system."""
+    test_file_path = self._GetTestFilePath(['vsstest.qcow2'])
+    self._SkipIfPathNotExists(test_file_path)
+
+    os_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_OS, location=test_file_path)
+    qcow_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_QCOW, parent=os_path_spec)
+    ext_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_NTFS,
+        location='\\$Extend\\$RmMetadata\\$TxfLog\\$TxfLog.blf',
+        parent=qcow_path_spec)
+
+    parser = filestat.FileStatParser()
+    storage_writer = self._ParseFileByPathSpec(ext_path_spec, parser)
+
+    self.assertEqual(storage_writer.number_of_events, 4)
+    self.assertEqual(storage_writer.number_of_extraction_warnings, 0)
+    self.assertEqual(storage_writer.number_of_recovery_warnings, 0)
+
+    events = list(storage_writer.GetEvents())
+
+    expected_event_values = {
+        'attribute_names': None,
+        'data_type': 'fs:stat',
+        'date_time': '2013-12-03 06:30:42.9779097',
+        'display_name': 'NTFS:\\$Extend\\$RmMetadata\\$TxfLog\\$TxfLog.blf',
+        'file_entry_type': 'file',
+        'file_size': 65536,
+        'file_system_type': 'NTFS',
+        'timestamp_desc': definitions.TIME_DESCRIPTION_LAST_ACCESS}
+
+    self.CheckEventValues(storage_writer, events[0], expected_event_values)
+
   def testZipFile(self):
     """Test a ZIP file."""
-    parser = filestat.FileStatParser()
-
     test_file_path = self._GetTestFilePath(['syslog.zip'])
     self._SkipIfPathNotExists(test_file_path)
 
@@ -69,6 +141,7 @@ class FileStatTest(test_lib.ParserTestCase):
         dfvfs_definitions.TYPE_INDICATOR_ZIP, location='/syslog',
         parent=os_path_spec)
 
+    parser = filestat.FileStatParser()
     storage_writer = self._ParseFileByPathSpec(zip_path_spec, parser)
 
     self.assertEqual(storage_writer.number_of_events, 1)
@@ -91,8 +164,6 @@ class FileStatTest(test_lib.ParserTestCase):
 
   def testGzipFile(self):
     """Test a GZIP file."""
-    parser = filestat.FileStatParser()
-
     test_file_path = self._GetTestFilePath(['syslog.gz'])
     self._SkipIfPathNotExists(test_file_path)
 
@@ -101,6 +172,7 @@ class FileStatTest(test_lib.ParserTestCase):
     gzip_path_spec = path_spec_factory.Factory.NewPathSpec(
         dfvfs_definitions.TYPE_INDICATOR_GZIP, parent=os_path_spec)
 
+    parser = filestat.FileStatParser()
     storage_writer = self._ParseFileByPathSpec(gzip_path_spec, parser)
 
     self.assertEqual(storage_writer.number_of_events, 1)
@@ -125,8 +197,6 @@ class FileStatTest(test_lib.ParserTestCase):
 
   def testTarFile(self):
     """Test a TAR file."""
-    parser = filestat.FileStatParser()
-
     test_file_path = self._GetTestFilePath(['syslog.tar'])
     self._SkipIfPathNotExists(test_file_path)
 
@@ -136,6 +206,7 @@ class FileStatTest(test_lib.ParserTestCase):
         dfvfs_definitions.TYPE_INDICATOR_TAR, location='/syslog',
         parent=os_path_spec)
 
+    parser = filestat.FileStatParser()
     storage_writer = self._ParseFileByPathSpec(tar_path_spec, parser)
 
     self.assertEqual(storage_writer.number_of_events, 1)
@@ -158,8 +229,6 @@ class FileStatTest(test_lib.ParserTestCase):
 
   def testNestedFile(self):
     """Test a nested file."""
-    parser = filestat.FileStatParser()
-
     test_file_path = self._GetTestFilePath(['syslog.tgz'])
     self._SkipIfPathNotExists(test_file_path)
 
@@ -171,6 +240,7 @@ class FileStatTest(test_lib.ParserTestCase):
         dfvfs_definitions.TYPE_INDICATOR_TAR, location='/syslog',
         parent=gzip_path_spec)
 
+    parser = filestat.FileStatParser()
     storage_writer = self._ParseFileByPathSpec(tar_path_spec, parser)
 
     self.assertEqual(storage_writer.number_of_events, 1)
@@ -223,8 +293,6 @@ class FileStatTest(test_lib.ParserTestCase):
 
   def testNestedTSK(self):
     """Test a nested TSK file."""
-    parser = filestat.FileStatParser()
-
     test_file_path = self._GetTestFilePath(['syslog_image.dd'])
     self._SkipIfPathNotExists(test_file_path)
 
@@ -237,6 +305,7 @@ class FileStatTest(test_lib.ParserTestCase):
         dfvfs_definitions.TYPE_INDICATOR_ZIP, location='/syslog',
         parent=tsk_path_spec)
 
+    parser = filestat.FileStatParser()
     storage_writer = self._ParseFileByPathSpec(zip_path_spec, parser)
 
     self.assertEqual(storage_writer.number_of_events, 1)
