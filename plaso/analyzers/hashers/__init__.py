19575185d4fefd734e9f4a40d324caea8dcb4e81
Joachim Metz <joachim.metz@gmail.com>
2021-12-21 15:55:17 +0100

    Added missing entropy hasher import and updated test (#3996)

--- plaso/analyzers/hashers/__init__.py
+++ plaso/analyzers/hashers/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """This file imports Python modules that register hashers."""
 
+from plaso.analyzers.hashers import entropy
 from plaso.analyzers.hashers import md5
 from plaso.analyzers.hashers import sha1
 from plaso.analyzers.hashers import sha256
