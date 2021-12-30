9164985214fd0db41115494d3cb9d8d7e151dfa3
jleaniz <leaniz@google.com>
2021-12-30 17:58:14 +0000

    change attribute type to list, style fixes

--- plaso/parsers/aws_elb_access.py
+++ plaso/parsers/aws_elb_access.py
@@ -3,6 +3,14 @@
 
 This parser is based on the log format documented at
 https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html
+
+Note:
+The AWS documentation is not clear about the meaning
+of the "target_port_list" field. The assumption is
+that it refers to a list of possible backend instances'
+IP addreses that could receive the client's request.
+This parser stores the "target_port_list"data in the
+"destination_list" attribute of an EventData object.
 """
 import pyparsing
 
@@ -22,41 +30,45 @@ class AWSELBEventData(events.EventData):
   Attributes:
     request_type (str): The type of request or connection.
     resource_identifier (str): The resource ID of the load balancer.
-    client_ip_address (str): The IP address of the requesting client.
-    client_port (int): The port of the requesting client.
-    target_ip_address (str): The IP address of the target that processed
+    source_ip_address (str): The IP address of the requesting source.
+    source_port (int): The port of the requesting source.
+    destination_ip_address (str): The IP address of the destination
+        that processed this request.
+    destination_port (int): The port of the destination that processed
         this request.
-    target_port (int): The port of the target that processed this request.
     request_processing_time (str): The total duration from
         the time the load balancer received the request until the
-        time it sent the request to a target.
-    target_processing_time (str): The total duration from
-        the time the load balancer sent the request to a target until
-        the target started to send the response headers.
+        time it sent the request to a destination.
+    destination_processing_time (str): The total duration from
+        the time the load balancer sent the request to a destination until
+        the destination started to send the response headers.
     response_processing_time (str): The total processing duration.
-    target_status_code (int): The status code of the response from the target.
+    destination_status_code (int): The status code of the response
+        from the destination.
     received_bytes (int): The size of the request, in bytes, received from
-        the client.
-    sent_bytes (int): The size of the response, in bytes, sent to the client.
+        the source.
+    sent_bytes (int): The size of the response, in bytes, sent to the source.
     user_agent (str): A User-Agent string.
     ssl_cipher (str): The SSL cipher of the HTTPS listener.
     ssl_protocol (str): The SSL protocol of the HTTPS listener.
-    target_group_arn (str): The Amazon Resource Name (ARN) of the target group.
+    destination_group_arn (str): The Amazon Resource Name (ARN) of the
+        destination group.
     trace_identifier (str): The contents of the X-Amzn-Trace-Id header.
     domain_name (str): The SNI domain provided by the
-        client during the TLS handshake.
+        source during the TLS handshake.
     chosen_cert_arn (str): The ARN of the certificate
-        presented to the client.
+        presented to the source.
     matched_rule_priority (int): The priority value of the rule that
         matched the request.
     request_creation_time (str): The time when the load balancer received the
-        request from the client, in ISO 8601 format.
+        request from the source, in ISO 8601 format.
     actions_executed (str): The actions taken when processing the request.
-    redirect_url (str): The URL of the redirect target.
-    error_reason (str): The error reason code, enclosed in double quotes.
-    target_port_list (str): A space-delimited list of IP addresses and ports
-        for the targets that processed this request.
-    target_status_code_list (str): A space-delimited list of status codes.
+    redirect_url (str): The URL of the redirect destination.
+    error_reason (str): The error reason code, enclosed in
+        double quotes.
+    destination_list (str): A space-delimited list of IP addresses
+        and ports for the destinations that processed this request.
+    destination_status_code_list (str): A space-delimited list of status codes.
     classification (str): The classification for desync mitigation.
     classification_reason (str): The classification reason code.
   """
@@ -68,22 +80,22 @@ class AWSELBEventData(events.EventData):
     super(AWSELBEventData, self).__init__(data_type=self.DATA_TYPE)
     self.request_type = None
     self.resource_identifier = None
-    self.client_ip_address = None
-    self.client_port = None
-    self.target_ip_address = None
-    self.target_port = None
+    self.source_ip_address = None
+    self.source_port = None
+    self.destination_ip_address = None
+    self.destination_port = None
     self.request_processing_time = None
-    self.target_processing_time = None
+    self.destination_processing_time = None
     self.response_processing_time = None
     self.elb_status_code = None
-    self.target_status_code = None
+    self.destination_status_code = None
     self.received_bytes = None
     self.sent_bytes = None
     self.request = None
     self.user_agent = None
     self.ssl_cipher = None
     self.ssl_protocol = None
-    self.target_group_arn = None
+    self.destination_group_arn = None
     self.trace_identifier = None
     self.domain_name = None
     self.chosen_cert_arn = None
@@ -92,8 +104,8 @@ class AWSELBEventData(events.EventData):
     self.actions_executed = None
     self.redirect_url = None
     self.error_reason = None
-    self.target_port_list = None
-    self.target_status_code_list = None
+    self.destination_list = None
+    self.destination_status_code_list = None
     self.classification = None
     self.classification_reason = None
 
@@ -121,12 +133,12 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
       text_parser.ConvertTokenToInteger) | BLANK
 
   _CLIENT_IP_ADDRESS_PORT = pyparsing.Group(
-      text_parser.PyparsingConstants.IP_ADDRESS('client_ip_address') +
-          pyparsing.Suppress(":") + _PORT('client_port') | BLANK)
+      text_parser.PyparsingConstants.IP_ADDRESS('source_ip_address') +
+          pyparsing.Suppress(':') + _PORT('source_port') | BLANK)
 
-  _TARGET_IP_ADDRESS_PORT = pyparsing.Group(
-      text_parser.PyparsingConstants.IP_ADDRESS('target_ip_address') +
-          pyparsing.Suppress(":") + _PORT('target_port') | BLANK)
+  _DESTINATION_IP_ADDRESS_PORT = pyparsing.Group(
+      text_parser.PyparsingConstants.IP_ADDRESS('destination_ip_address') +
+          pyparsing.Suppress(':') + _PORT('destination_port') | BLANK)
 
   _DATE_TIME_ISOFORMAT_STRING = pyparsing.Combine(
       pyparsing.Word(pyparsing.nums, exact=4) + pyparsing.Literal('-') +
@@ -142,13 +154,13 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
       _WORD.setResultsName('request_type') +
       _DATE_TIME_ISOFORMAT_STRING.setResultsName('time') +
       _WORD.setResultsName('resource_identifier') +
-      _CLIENT_IP_ADDRESS_PORT.setResultsName('client_ip_port') +
-      _TARGET_IP_ADDRESS_PORT.setResultsName('target_ip_port') +
+      _CLIENT_IP_ADDRESS_PORT.setResultsName('source_ip_port') +
+      _DESTINATION_IP_ADDRESS_PORT.setResultsName('destination_ip_port') +
       _FLOAT.setResultsName('request_processing_time') +
-      _FLOAT.setResultsName('target_processing_time') +
+      _FLOAT.setResultsName('destination_processing_time') +
       _FLOAT.setResultsName('response_processing_time') +
       _INTEGER.setResultsName('elb_status_code') +
-      _INTEGER.setResultsName('target_status_code') +
+      _INTEGER.setResultsName('destination_status_code') +
       _INTEGER.setResultsName('received_bytes') +
       _INTEGER.setResultsName('sent_bytes') +
       pyparsing.quotedString.setResultsName('request')
@@ -157,7 +169,7 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
           .setParseAction(pyparsing.removeQuotes) +
       _WORD.setResultsName('ssl_cipher') +
       _WORD.setResultsName('ssl_protocol') +
-      _WORD.setResultsName('target_group_arn') +
+      _WORD.setResultsName('destination_group_arn') +
       _WORD.setResultsName('trace_identifier') +
       pyparsing.quotedString.setResultsName(
           'domain_name').setParseAction(pyparsing.removeQuotes) +
@@ -172,9 +184,10 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
       pyparsing.quotedString.setResultsName(
           'error_reason').setParseAction(pyparsing.removeQuotes) +
       pyparsing.quotedString.setResultsName(
-          'target_port_list').setParseAction(pyparsing.removeQuotes) +
+          'destination_list').setParseAction(pyparsing.removeQuotes) +
       pyparsing.quotedString.setResultsName(
-          'target_status_code_list').setParseAction(pyparsing.removeQuotes) +
+          'destination_status_code_list').setParseAction(
+              pyparsing.removeQuotes) +
       pyparsing.quotedString.setResultsName(
           'classification').setParseAction(pyparsing.removeQuotes) +
       pyparsing.quotedString.setResultsName(
@@ -184,7 +197,7 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
   LINE_STRUCTURES = [('elb_accesslog', _LOG_LINE)]
 
   def _GetValueFromGroup(self, structure, name, key_name):
-    """Retrieves a value from a Pyparsing.Group structure
+    """Retrieves a value from a Pyparsing.Group structure.
 
     Args:
       structure (pyparsing.ParseResults): tokens from a parsed log line.
@@ -228,62 +241,63 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
         structure, 'request_type')
     event_data.resource_identifier = self._GetValueFromStructure(
         structure, 'resource_identifier')
-    event_data.client_ip_address = self._GetValueFromGroup(structure,
-        'client_ip_port', 'client_ip_address')
-    event_data.client_port = self._GetValueFromGroup(structure,
-        'client_ip_port', 'client_port')
-    event_data.target_ip_address = self._GetValueFromGroup(structure,
-        'target_ip_port', 'target_ip_address')
-    event_data.target_port = self._GetValueFromGroup(structure,
-        'target_ip_port', 'target_port')
-    event_data.request_processing_time = self._GetValueFromStructure(structure,
-        'request_processing_time')
-    event_data.target_processing_time = self._GetValueFromStructure(structure,
-        'target_processing_time')
-    event_data.response_processing_time = self._GetValueFromStructure(structure,
-        'response_processing_time')
-    event_data.elb_status_code = self._GetValueFromStructure(structure,
-        'elb_status_code')
-    event_data.target_status_code = self._GetValueFromStructure(structure,
-        'target_status_code')
-    event_data.received_bytes = self._GetValueFromStructure(structure,
-        'received_bytes')
-    event_data.sent_bytes = self._GetValueFromStructure(structure,
-        'sent_bytes')
-    event_data.request = self._GetValueFromStructure(structure,
-        'request')
-    event_data.user_agent = self._GetValueFromStructure(structure,
-        'user_agent')
-    event_data.ssl_cipher = self._GetValueFromStructure(structure,
-        'ssl_cipher')
-    event_data.ssl_protocol = self._GetValueFromStructure(structure,
-        'ssl_protocol')
-    event_data.target_group_arn = self._GetValueFromStructure(structure,
-        'target_group_arn')
-    event_data.trace_identifier = self._GetValueFromStructure(structure,
-        'trace_identifier')
-    event_data.domain_name = self._GetValueFromStructure(structure,
-        'domain_name')
-    event_data.chosen_cert_arn = self._GetValueFromStructure(structure,
-        'chosen_cert_arn')
-    event_data.matched_rule_priority = self._GetValueFromStructure(structure,
-        'matched_rule_priority')
-    event_data.request_creation_time = self._GetValueFromStructure(structure,
-        'request_creation_time')
-    event_data.actions_executed = self._GetValueFromStructure(structure,
-        'actions_executed')
-    event_data.redirect_url = self._GetValueFromStructure(structure,
-        'redirect_url')
-    event_data.error_reason = self._GetValueFromStructure(structure,
-        'error_reason')
-    event_data.target_port_list = self._GetValueFromStructure(structure,
-        'target_port_list')
-    event_data.target_status_code_list = self._GetValueFromStructure(structure,
-        'target_status_code_list')
-    event_data.classification = self._GetValueFromStructure(structure,
-        'classification')
-    event_data.classification_reason = self._GetValueFromStructure(structure,
-        'classification_reason')
+    event_data.source_ip_address = self._GetValueFromGroup(
+        structure, 'source_ip_port', 'source_ip_address')
+    event_data.source_port = self._GetValueFromGroup(
+        structure, 'source_ip_port', 'source_port')
+    event_data.destination_ip_address = self._GetValueFromGroup(
+        structure, 'destination_ip_port', 'destination_ip_address')
+    event_data.destination_port = self._GetValueFromGroup(
+        structure, 'destination_ip_port', 'destination_port')
+    event_data.request_processing_time = self._GetValueFromStructure(
+        structure, 'request_processing_time')
+    event_data.destination_processing_time = self._GetValueFromStructure(
+        structure, 'destination_processing_time')
+    event_data.response_processing_time = self._GetValueFromStructure(
+        structure, 'response_processing_time')
+    event_data.elb_status_code = self._GetValueFromStructure(
+        structure, 'elb_status_code')
+    event_data.destination_status_code = self._GetValueFromStructure(
+        structure, 'destination_status_code')
+    event_data.received_bytes = self._GetValueFromStructure(
+        structure, 'received_bytes')
+    event_data.sent_bytes = self._GetValueFromStructure(
+        structure, 'sent_bytes')
+    event_data.request = self._GetValueFromStructure(
+        structure, 'request')
+    event_data.user_agent = self._GetValueFromStructure(
+        structure, 'user_agent')
+    event_data.ssl_cipher = self._GetValueFromStructure(
+        structure, 'ssl_cipher')
+    event_data.ssl_protocol = self._GetValueFromStructure(
+        structure, 'ssl_protocol')
+    event_data.destination_group_arn = self._GetValueFromStructure(
+        structure, 'destination_group_arn')
+    event_data.trace_identifier = self._GetValueFromStructure(
+        structure, 'trace_identifier')
+    event_data.domain_name = self._GetValueFromStructure(
+        structure, 'domain_name')
+    event_data.chosen_cert_arn = self._GetValueFromStructure(
+        structure, 'chosen_cert_arn')
+    event_data.matched_rule_priority = self._GetValueFromStructure(
+        structure, 'matched_rule_priority')
+    event_data.request_creation_time = self._GetValueFromStructure(
+        structure, 'request_creation_time')
+    event_data.actions_executed = self._GetValueFromStructure(
+        structure, 'actions_executed')
+    event_data.redirect_url = self._GetValueFromStructure(
+        structure, 'redirect_url')
+    event_data.error_reason = self._GetValueFromStructure(
+        structure, 'error_reason')
+    event_data.destination_status_code_list = self._GetValueFromStructure(
+        structure, 'destination_status_code_list')
+    event_data.classification = self._GetValueFromStructure(
+        structure, 'classification')
+    event_data.classification_reason = self._GetValueFromStructure(
+        structure, 'classification_reason')
+    destination_list = self._GetValueFromStructure(
+        structure, 'destination_list')
+    event_data.destination_list = destination_list.split()
 
     event = time_events.DateTimeValuesEvent(
         date_time, definitions.TIME_DESCRIPTION_RECORDED)
@@ -291,7 +305,7 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
     parser_mediator.ProduceEventWithEventData(event, event_data)
 
   def VerifyStructure(self, parser_mediator, line):
-    """Verify that this file is a valid AWS ELB access log
+    """Verify that this file is a valid AWS ELB access log.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
@@ -307,5 +321,6 @@ class AWSELBParser(text_parser.PyparsingSingleLineTextParser):
       structure = None
 
     return bool(structure)
+
 
 manager.ParsersManager.RegisterParser(AWSELBParser)
