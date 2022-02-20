cfc5d754cef5280d826d21ff682d4cbe393b6b89
Jonathan Greig <jonathangreig@google.com>
2022-02-21 09:12:17 +1100

    Added parser for Azure Activity logs (#4040)

--- plaso/parsers/__init__.py
+++ plaso/parsers/__init__.py
@@ -6,6 +6,7 @@ from plaso.parsers import android_app_usage
 from plaso.parsers import apache_access
 from plaso.parsers import apt_history
 from plaso.parsers import aws_elb_access
+from plaso.parsers import azure_activity_log
 from plaso.parsers import bash_history
 from plaso.parsers import bencode_parser
 from plaso.parsers import bsm
