7ac6d2d69ea5c336342861608724f7952d6c737c
Joachim Metz <joachim.metz@gmail.com>
2022-03-26 16:35:39 +0100

    Changes after review

--- plaso/parsers/__init__.py
+++ plaso/parsers/__init__.py
@@ -27,9 +27,9 @@ from plaso.parsers import gcp_logging
 from plaso.parsers import gdrive_synclog
 from plaso.parsers import google_logging
 from plaso.parsers import iis
+from plaso.parsers import ios_logd
 from plaso.parsers import java_idx
 from plaso.parsers import locate
-from plaso.parsers import ios_logd
 from plaso.parsers import mac_appfirewall
 from plaso.parsers import mac_keychain
 from plaso.parsers import mac_securityd
--- plaso/parsers/ios_logd.py
+++ plaso/parsers/ios_logd.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Parser for the logd.0.log file obtained from iOS Sysdiagnose file dumps."""
+"""Parser for iOS sysdiagnose logd files (logd.0.log)."""
 
 import pyparsing
 
@@ -13,28 +13,28 @@ from plaso.parsers import manager
 from plaso.parsers import text_parser
 
 
-class IOSysdiagnoseLogdData(events.EventData):
-  """logd event data
+class IOSSysdiagnoseLogdData(events.EventData):
+  """iOS sysdiagnose logd event data.
 
   Attributes:
-    logger (str): process that submits the log event
-    body (str): body of the event line
+    body (str): body of the event line.
+    logger (str): name of the process that generated the event.
   """
 
-  DATA_TYPE = "ios_sysdiagnose_logd:line"
+  DATA_TYPE = 'ios:sysdiagnose:logd:line'
 
   def __init__(self):
-    """Initializes event data."""
-    super(IOSysdiagnoseLogdData, self).__init__(data_type=self.DATA_TYPE)
-    self.logger = None
+    """Initializes iOS sysdiagnose logd event data."""
+    super(IOSSysdiagnoseLogdData, self).__init__(data_type=self.DATA_TYPE)
     self.body = None
+    self.logger = None
 
 
-class IOSysdiagnoseLogdParser(text_parser.PyparsingSingleLineTextParser):
-  """Parser for the logd.0.log file obtained from iOS Sysdiagnose file dumps."""
+class IOSSysdiagnoseLogdParser(text_parser.PyparsingSingleLineTextParser):
+  """Parser for iOS sysdiagnose logd files (logd.0.log)."""
 
-  NAME = 'logd.0.log'
-  DATA_FORMAT = 'iOS sysdiagnose log file'
+  NAME = 'ios_sysdiagnose_logd'
+  DATA_FORMAT = 'iOS sysdiagnose logd file'
 
   DATE_ELEMENTS = text_parser.PyparsingConstants.DATE_ELEMENTS
   TIME_ELEMENTS = text_parser.PyparsingConstants.TIME_ELEMENTS
@@ -42,11 +42,12 @@ class IOSysdiagnoseLogdParser(text_parser.PyparsingSingleLineTextParser):
   _TIMESTAMP = DATE_ELEMENTS + TIME_ELEMENTS
 
   _TIME_DELTA = pyparsing.Word(
-      pyparsing.nums + '+' + '-', exact=5)('time_delta')
+      pyparsing.nums + '+' + '-', exact=5).setResultsName('time_delta')
 
-  _LOGGER = pyparsing.SkipTo(':')('logger') + pyparsing.Suppress(': ')
+  _LOGGER = (pyparsing.SkipTo(':').setResultsName('logger') +
+             pyparsing.Suppress(': '))
 
-  _BODY = pyparsing.SkipTo(pyparsing.LineEnd())('body')
+  _BODY = pyparsing.SkipTo(pyparsing.LineEnd()).setResultsName('body')
 
   _LINE_GRAMMAR = _TIMESTAMP + _TIME_DELTA + _LOGGER + _BODY
 
@@ -54,19 +55,19 @@ class IOSysdiagnoseLogdParser(text_parser.PyparsingSingleLineTextParser):
 
   def ParseRecord(self, parser_mediator, key, structure):
     """Parses a log record structure and produces events.
+
     This function takes as an input a parsed pyparsing structure
     and produces an EventObject if possible from that structure.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
-          and other components, such as storage and dfvfs.
+          and other components, such as storage and dfVFS.
       key (str): name of the parsed structure.
       structure (pyparsing.ParseResults): tokens from a parsed log line.
 
     Raises:
       ParseError: when the structure type is unknown.
-   """
-
+    """
     if key != 'log_entry':
       raise errors.ParseError(
           'Unable to parse record, unknown structure: {0:s}'.format(key))
@@ -79,22 +80,29 @@ class IOSysdiagnoseLogdParser(text_parser.PyparsingSingleLineTextParser):
     seconds = self._GetValueFromStructure(structure, 'seconds')
 
     time_delta = self._GetValueFromStructure(structure, 'time_delta')
-    time_deta_hours = time_delta[:3]
-    # dfdatetime takes the time zone offset as an int number of minutes but
-    # inverts the sign for example Easter Standard Time, which is UTC-5:00 needs
-    # to be provided as +300 minutes.
-    time_offset = -1 * int(time_deta_hours) * 60 + int(time_delta[3:])
 
-    event_data = IOSysdiagnoseLogdData()
-    event_data.logger = self._GetValueFromStructure(structure, 'logger')
+    event_data = IOSSysdiagnoseLogdData()
     event_data.body = self._GetValueFromStructure(structure, 'body')
+    event_data.logger = self._GetValueFromStructure(structure, 'logger')
+
+    # dfDateTime takes the time zone offset as number of minutes relative from
+    # UTC. So for Easter Standard Time (EST), which is UTC-5:00 the sign needs
+    # to be converted, to +300 minutes.
+    try:
+      time_delta_hours = int(time_delta[:3], 10)
+      time_delta_minutes = int(time_delta[3:], 10)
+    except (TypeError, ValueError):
+      parser_mediator.ProduceExtractionWarning('unsupported date time value')
+      return
+
+    time_zone_offset = (time_delta_hours * -60) + time_delta_minutes
 
     try:
-      date_time = dfdatetime_time_elements.TimeElements(time_elements_tuple=(
-          year, month, day, hours, minutes, seconds),
-          time_zone_offset=time_offset)
+      date_time = dfdatetime_time_elements.TimeElements(
+          time_elements_tuple=(year, month, day, hours, minutes, seconds),
+          time_zone_offset=time_zone_offset)
     except (TypeError, ValueError):
-      parser_mediator.ProduceExtractionWarning('invalid date time value')
+      parser_mediator.ProduceExtractionWarning('unsupported date time value')
       return
 
     event = time_events.DateTimeValuesEvent(
@@ -103,19 +111,18 @@ class IOSysdiagnoseLogdParser(text_parser.PyparsingSingleLineTextParser):
     parser_mediator.ProduceEventWithEventData(event, event_data)
 
   def VerifyStructure(self, parser_mediator, line):
-    """Verifies that this is a mobile installation log file.
+    """Verifies that this is an iOS sysdiagnose logd file.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between
-          parsers and other components, such as storage and dfvfs.
+          parsers and other components, such as storage and dfVFS.
       line (str): one line from the text file.
 
     Returns:
       bool: True if this is the correct parser, False otherwise.
     """
-
     match_generator = self._LINE_GRAMMAR.scanString(line, maxMatches=1)
     return bool(list(match_generator))
 
 
-manager.ParsersManager.RegisterParser(IOSysdiagnoseLogdParser)
+manager.ParsersManager.RegisterParser(IOSSysdiagnoseLogdParser)
