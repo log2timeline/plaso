23328815dd48ecdd9ba57078cd28976c56c76044
Alexander J <741037+jaegeral@users.noreply.github.com>
2022-03-29 15:43:26 +0200

    Update single_process.py

--- plaso/engine/single_process.py
+++ plaso/engine/single_process.py
@@ -60,7 +60,7 @@ class SingleProcessEngine(engine.BaseEngine):
 
       elif len(self._file_system_cache) == self._FILE_SYSTEM_CACHE_SIZE:
         # Move the file system to the end of the list to preserve the most
-        # recently file system object.
+        # recently file sytem object.
         self._file_system_cache.remove(file_system)
         self._file_system_cache.append(file_system)
 
