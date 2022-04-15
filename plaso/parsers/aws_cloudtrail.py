ae7bf7fd328d266f161d92b50c9b77627488b7cf
Joachim Metz <joachim.metz@gmail.com>
2022-04-15 15:31:29 +0200

    Clean up of AWS CloudTrail log parser (#4074)

--- plaso/parsers/aws_cloudtrail.py
+++ plaso/parsers/aws_cloudtrail.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Parser for AWS Cloudtrail logging saved to a file."""
+"""Parser for AWS CloudTrail log files."""
 
 import json
 import json.decoder as json_decoder
@@ -18,109 +18,126 @@ from plaso.parsers import interface
 
 
 class AWSCloudTrailEventData(events.EventData):
-  """AWS Cloudtrail log event data.
+  """AWS CloudTrail log event data.
 
   Attributes:
-    event_name (str): The logged AWS event name.
-    access_key_id (str): The access key ID associated with the event.
-    event_source (str): The AWS service associated with the event.
-    user_name (str): The account associated with the event.
-    resources (str): The resources associated with the event.
-    cloud_trail_event (str): The logged CloudTrailEvent in full.
-    source_ip (str): The source IP address associated with the event.
-    user_identity_arn (str): The AWS ARN for the user associated with the
-      event.
-    account_id (str): The AWS account ID associated with the event.
+    access_key (str): access key identifier.
+    account_identifier (str): AWS account identifier.
+    cloud_trail_event (str): CloudTrail event.
+    event_name (str): event name.
+    event_source (str): AWS service.
+    resources (str): resources.
+    source_ip (str): source IP address.
+    user_identity_arn (str): AWS ARN of the user.
+    user_name (str): name of the AWS user.
   """
 
-  DATA_TYPE = 'aws:cloudtrail:json'
+  DATA_TYPE = 'aws:cloudtrail:entry'
 
   def __init__(self):
     """Initializes event data."""
     super(AWSCloudTrailEventData, self).__init__(data_type=self.DATA_TYPE)
-    self.event_name = None
-    self.access_key_id = None
-    self.event_source = None
-    self.user_name = None
-    self.resources = None
+    self.access_key = None
+    self.account_identifier = None
     self.cloud_trail_event = None
+    self.event_name = None
+    self.event_source = None
+    self.resources = None
     self.source_ip = None
     self.user_identity_arn = None
-    self.account_id = None
+    self.user_name = None
 
 
 class AWSCloudTrailParser(interface.FileObjectParser):
-  """Parser for AWS Cloudtrail logs saved in JSON-L format."""
+  """Parser for JSON-L AWS CloudTrail log files."""
 
   NAME = 'aws_cloudtrail'
-  DATA_FORMAT = 'AWS Cloudtrail Logging'
+  DATA_FORMAT = 'AWS CloudTrail log'
 
   _ENCODING = 'utf-8'
 
-  def _ParseAWSCloudtrailLog(self, parser_mediator, file_object):
-    """Extract events from AWS Cloudtrail logging in JSON-L format.
+  def _GetJSONValue(self, json_dict, name):
+    """Retrieves a value from a JSON dict.
+
+    Args:
+      json_dict (dict): JSON dictionary.
+      name (str): name of the value to retrieve.
+
+    Returns:
+      object: value of the JSON log entry or None if not set.
+    """
+    json_value = json_dict.get(name)
+    if json_value == '':
+      json_value = None
+    return json_value
+
+  def _ParseAWSCloudTrailLog(self, parser_mediator, json_dict):
+    """Extract events from an AWS CloudTrail log entry.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
-          and other components, such as storage and dfvfs.
-      file_object (dfvfs.FileIO): a file-like object.
+          and other components, such as storage and dfVFS.
+      json_dict (dict): log entry JSON dictionary.
     """
-    text_file_object = text_file.TextFile(file_object)
+    time_string = json_dict.get('EventTime')
+    if not time_string:
+      parser_mediator.ProduceExtractionWarning(
+          'Event time value missing from CloudTrail log entry')
+      return
 
-    for line in text_file_object:
-      json_log_entry = json.loads(line)
+    event_data = AWSCloudTrailEventData()
+    event_data.event_name = self._GetJSONValue(json_dict, 'EventName')
+    event_data.access_key = self._GetJSONValue(json_dict, 'AccessKeyId')
+    event_data.event_source = self._GetJSONValue(json_dict, 'EventSource')
+    event_data.user_name = self._GetJSONValue(json_dict, 'Username')
 
-      time_string = json_log_entry.get('EventTime')
-      if not time_string:
-        continue
+    resource_list = json_dict.get('Resources')
+    # Flatten multiple resources into a string of resource names.
+    if resource_list:
+      event_data.resources = ', '.join(
+          [resource['ResourceName'] for resource in resource_list])
 
-      event_data = AWSCloudTrailEventData()
-      event_data.event_name = json_log_entry.get('EventName')
-      event_data.access_key_id = json_log_entry.get('AccessKeyId')
-      event_data.event_source = json_log_entry.get('EventSource')
-      event_data.user_name = json_log_entry.get('Username')
-
-      resource_list = json_log_entry.get('Resources')
-      # Flatten multiple resources into a string of resource names.
-      if resource_list:
-        event_data.resources = ', '.join(
-            [resource['ResourceName'] for resource in resource_list])
-
-      event_data.cloud_trail_event = json_log_entry.get('CloudTrailEvent')
-      if event_data.cloud_trail_event:
-        cloud_trail_event_json = None
-        try:
-          cloud_trail_event_json = json.loads(event_data.cloud_trail_event)
-        except json_decoder.JSONDecodeError as exception:
-          parser_mediator.ProduceExtractionWarning(
-              f'Unable to decode CloudTrailEvent JSON {str(exception)}')
-
-        if cloud_trail_event_json:
-          event_data.source_ip = cloud_trail_event_json.get('sourceIPAddress')
-          user_id_json = cloud_trail_event_json.get('userIdentity')
-          if user_id_json:
-            event_data.user_identity_arn = user_id_json.get('arn')
-            event_data.account_id = cloud_trail_event_json.get('accountId')
-
+    event_data.cloud_trail_event = json_dict.get('CloudTrailEvent')
+    if event_data.cloud_trail_event:
       try:
-        date_time = dfdatetime_time_elements.TimeElementsInMicroseconds()
-        date_time.CopyFromDateTimeString(time_string)
-      except ValueError as exception:
+        cloud_trail_event_json = json.loads(event_data.cloud_trail_event)
+      except json_decoder.JSONDecodeError as exception:
+        cloud_trail_event_json = None
         parser_mediator.ProduceExtractionWarning(
-            f'Unable to parse time string: {time_string} with error: '
-            f'{str(exception)}')
-        date_time = dfdatetime_semantic_time.InvalidTime()
+            'Unable to decode CloudTrail event with error: {0!s}'.format(
+                exception))
 
-      event = time_events.DateTimeValuesEvent(
-          date_time, definitions.TIME_DESCRIPTION_RECORDED)
-      parser_mediator.ProduceEventWithEventData(event, event_data)
+      if cloud_trail_event_json:
+        event_data.source_ip = self._GetJSONValue(
+            cloud_trail_event_json, 'sourceIPAddress')
+        user_identify_json = self._GetJSONValue(
+            cloud_trail_event_json, 'userIdentity')
+
+        if user_identify_json:
+          event_data.user_identity_arn = self._GetJSONValue(
+              user_identify_json, 'arn')
+          event_data.account_identifier = self._GetJSONValue(
+              cloud_trail_event_json, 'accountId')
+
+    try:
+      date_time = dfdatetime_time_elements.TimeElementsInMicroseconds()
+      date_time.CopyFromDateTimeString(time_string)
+    except ValueError as exception:
+      parser_mediator.ProduceExtractionWarning(
+          'Unable to parse time string: {0:s} with error: {1!s}'.format(
+             time_string, exception))
+      date_time = dfdatetime_semantic_time.InvalidTime()
+
+    event = time_events.DateTimeValuesEvent(
+        date_time, definitions.TIME_DESCRIPTION_RECORDED)
+    parser_mediator.ProduceEventWithEventData(event, event_data)
 
   def ParseFileObject(self, parser_mediator, file_object):
-    """Parses AWS Cloudtrail logging saved in JSON-L format.
+    """Parses an AWS CloudTrail log file-object.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
-          and other components, such as storage and dfvfs.
+          and other components, such as storage and dfVFS.
       file_object (dfvfs.FileIO): a file-like object.
 
     Raises:
@@ -130,15 +147,15 @@ class AWSCloudTrailParser(interface.FileObjectParser):
     if file_object.read(1) != b'{':
       raise errors.WrongParser(
           'is not a valid JSON file, missing opening brace.')
-    file_object.seek(0, os.SEEK_SET)
 
+    file_object.seek(0, os.SEEK_SET)
     text_file_object = text_file.TextFile(file_object)
 
     try:
       first_line = text_file_object.readline()
       first_line_json = json.loads(first_line)
     except json_decoder.JSONDecodeError:
-      raise errors.WrongParser('could not decode json.')
+      raise errors.WrongParser('could not decode JSON.')
 
     if not first_line_json:
       raise errors.WrongParser('no JSON found in file.')
@@ -148,7 +165,11 @@ class AWSCloudTrailParser(interface.FileObjectParser):
           'no "CloudTrailEvent" field, not an AWS log entry.')
 
     file_object.seek(0, os.SEEK_SET)
-    self._ParseAWSCloudtrailLog(parser_mediator, file_object)
+    text_file_object = text_file.TextFile(file_object)
+
+    for line in text_file_object:
+      json_log_entry = json.loads(line)
+      self._ParseAWSCloudTrailLog(parser_mediator, json_log_entry)
 
 
 manager.ParsersManager.RegisterParser(AWSCloudTrailParser)
--- tests/parsers/aws_cloudtrail.py
+++ tests/parsers/aws_cloudtrail.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*
-"""Tests for the AWS Cloudtrail logging parser."""
+"""Tests for the AWS CloudTrail log parser."""
 
 import unittest
 
@@ -8,54 +8,67 @@ from tests.parsers import test_lib
 
 
 class AWSCloudTrailTest(test_lib.ParserTestCase):
-  """Tests for the AWS Cloudtrail logging parser."""
+  """Tests for the AWS CloudTrail log parser."""
 
-  def testParseAWSCloudtrail(self):
-    """Tests that AWS Cloudtrail logs are correctly parsed."""
+  def testParseFile(self):
+    """Test parsing of AWS CloudTrail log file."""
+    parser = aws_cloudtrail.AWSCloudTrailParser()
+    storage_writer = self._ParseFile(['aws_cloudtrail.jsonl'], parser)
 
-    expected_timestamps = [
-      '2022-02-07 22:23:37.000000',
-      '2022-02-07 22:23:18.000000',
-      '2022-02-07 22:23:03.000000',
-      '2022-02-07 22:19:07.000000',
-      '2022-02-07 22:19:05.000000',
-      '2022-02-07 22:19:04.000000',
-    ]
+    number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
+    self.assertEqual(number_of_events, 6)
 
-    expected_events = [
-      {
-        'access_key_id': '0123456789ABCDEFGHIJ',
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'extraction_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'recovery_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    events = list(storage_writer.GetEvents())
+
+    expected_event_values = {
+        'access_key': '0123456789ABCDEFGHIJ',
         'event_name': 'DescribeInstances',
         'event_source': 'ec2.amazonaws.com',
         'source_ip': '1.2.3.4',
+        'timestamp': '2022-02-07 22:23:37.000000',
         'user_identity_arn': 'arn:aws:iam::012345678901:user/fakeusername',
-        'user_name': 'fakeusername'
-      },
-      {
-        'access_key_id': '0123456789ABCDEFGHIJ',
+        'user_name': 'fakeusername'}
+    self.CheckEventValues(storage_writer, events[0], expected_event_values)
+
+    expected_event_values = {
+        'access_key': '0123456789ABCDEFGHIJ',
         'event_name': 'TerminateInstances',
         'event_source': 'ec2.amazonaws.com',
         'resources': 'i-01234567890123456',
         'source_ip': '1.2.3.4',
+        'timestamp': '2022-02-07 22:23:18.000000',
         'user_identity_arn': 'arn:aws:iam::012345678901:user/fakeusername',
-        'user_name': 'fakeusername'
-      },
-      {
-        'access_key_id': '0123456789ABCDEFGHIJ',
+        'user_name': 'fakeusername'}
+    self.CheckEventValues(storage_writer, events[1], expected_event_values)
+
+    expected_event_values = {
+        'access_key': '0123456789ABCDEFGHIJ',
         'event_name': 'StopInstances',
         'event_source': 'ec2.amazonaws.com',
         'resources': 'i-01234567890123456',
         'source_ip': '1.2.3.4',
+        'timestamp': '2022-02-07 22:23:03.000000',
         'user_identity_arn': 'arn:aws:iam::012345678901:user/fakeusername',
-        'user_name': 'fakeusername'
-      },
-      {
+        'user_name': 'fakeusername'}
+    self.CheckEventValues(storage_writer, events[2], expected_event_values)
+
+    expected_event_values = {
         'event_name': 'SharedSnapshotVolumeCreated',
         'event_source': 'ec2.amazonaws.com',
-        'source_ip': 'ec2.amazonaws.com'
-      },
-      {
-        'access_key_id': '0123456789ABCDEFGHIJ',
+        'source_ip': 'ec2.amazonaws.com',
+        'timestamp': '2022-02-07 22:19:07.000000'}
+    self.CheckEventValues(storage_writer, events[3], expected_event_values)
+
+    expected_event_values = {
+        'access_key': '0123456789ABCDEFGHIJ',
         'event_name': 'RunInstances',
         'event_source': 'ec2.amazonaws.com',
         'resources': (
@@ -63,32 +76,21 @@ class AWSCloudTrailTest(test_lib.ParserTestCase):
             'eni-01234567890123456, i-01234567890123456, aws-testing, '
             'no-access, sg-01234567890123456, subnet-01234567890123456'),
         'source_ip': '1.2.3.4',
+        'timestamp': '2022-02-07 22:19:05.000000',
         'user_identity_arn': 'arn:aws:iam::012345678901:user/fakeusername',
-        'user_name': 'fakeusername'
-      },
-      {
-        'access_key_id': '0123456789ABCDEFGHIJ',
+        'user_name': 'fakeusername'}
+    self.CheckEventValues(storage_writer, events[4], expected_event_values)
+
+    expected_event_values = {
+        'access_key': '0123456789ABCDEFGHIJ',
         'event_name': 'CreateSecurityGroup',
         'event_source': 'ec2.amazonaws.com',
         'resources': 'vpc-01234567890123456, no-access, sg-01234567890123456',
         'source_ip': '1.2.3.4',
+        'timestamp': '2022-02-07 22:19:04.000000',
         'user_identity_arn': 'arn:aws:iam::012345678901:user/fakeusername',
-        'user_name': 'fakeusername'
-      },
-    ]
-
-    parser = aws_cloudtrail.AWSCloudTrailParser()
-    storage_writer = self._ParseFile(['aws_cloudtrail.jsonl'], parser)
-
-    self.assertEqual(storage_writer.number_of_events, 6)
-    self.assertEqual(storage_writer.number_of_extraction_warnings, 0)
-    self.assertEqual(storage_writer.number_of_recovery_warnings, 0)
-
-    events = storage_writer.GetEvents()
-    for event, expected_event, expected_timestamp in zip(
-        events, expected_events, expected_timestamps):
-      self.CheckTimestamp(event.timestamp, expected_timestamp)
-      self.CheckEventValues(storage_writer, event, expected_event)
+        'user_name': 'fakeusername'}
+    self.CheckEventValues(storage_writer, events[5], expected_event_values)
 
 
 if __name__ == '__main__':
