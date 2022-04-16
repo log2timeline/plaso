468046eb216ad2ec0ee3fe6a6d58c2fdd8f5d88f
rick-slin <99432032+rick-slin@users.noreply.github.com>
2022-04-16 03:19:08 -0400

    Added iOS mobile installation log files parser (#4060)

---
+++ plaso/parsers/ios_mobile_installation_log.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""Parser for iOS mobile installation log files."""
+
+import pyparsing
+
+from dfdatetime import time_elements as dfdatetime_time_elements
+
+from plaso.containers import events
+from plaso.containers import time_events
+from plaso.lib import errors
+from plaso.lib import definitions
+from plaso.parsers import manager
+from plaso.parsers import text_parser
+
+
+class IOSMobileInstallationEventData(events.EventData):
+  """iOS mobile installation log event data.
+
+  Attributes:
+    body (str): body of the event line.
+    originating_call (str): call that created the entry.
+    process_identifier (str): process_identifier.
+    severity (str): severity of the message.
+  """
+
+  DATA_TYPE = 'ios:mobile_installation:line'
+
+  def __init__(self):
+    """Initializes event data."""
+    super(
+        IOSMobileInstallationEventData, self).__init__(data_type=self.DATA_TYPE)
+    self.body = None
+    self.originating_call = None
+    self.process_identifier = None
+    self.severity = None
+
+
+class IOSMobileInstallationLogParser(text_parser.PyparsingMultiLineTextParser):
+  """Parser for iOS mobile installation log files."""
+
+  NAME = 'ios:mobile_installation:log'
+  DATA_FORMAT = 'iOS mobile installation log'
+
+  MONTHS = {
+      'Jan': 1,
+      'Feb': 2,
+      'Mar': 3,
+      'Apr': 4,
+      'May': 5,
+      'Jun': 6,
+      'Jul': 7,
+      'Aug': 8,
+      'Sep': 9,
+      'Oct': 10,
+      'Nov': 11,
+      'Dec': 12}
+
+  ONE_OR_TWO_DIGITS = text_parser.PyparsingConstants.ONE_OR_TWO_DIGITS
+  FOUR_DIGITS = text_parser.PyparsingConstants.FOUR_DIGITS
+  THREE_LETTERS = text_parser.PyparsingConstants.THREE_LETTERS
+  TIME_ELEMENTS = text_parser.PyparsingConstants.TIME_ELEMENTS
+
+  _TIMESTAMP = (
+      THREE_LETTERS.suppress() +
+      THREE_LETTERS.setResultsName('month') +
+      ONE_OR_TWO_DIGITS.setResultsName('day') + TIME_ELEMENTS +
+      FOUR_DIGITS.setResultsName('year'))
+
+  _ELEMENT_NUMBER = (
+      pyparsing.Suppress('[') +
+      pyparsing.Word(pyparsing.nums).setResultsName('process_identifier') +
+      pyparsing.Suppress(']'))
+
+  _ELEMENT_SEVERITY = (
+      pyparsing.Suppress('<') +
+      pyparsing.Word(pyparsing.alphanums).setResultsName('severity') +
+      pyparsing.Suppress('>'))
+
+  _ELEMENT_ID = (
+      pyparsing.Suppress('(') +
+      pyparsing.Word(pyparsing.alphanums).setResultsName('id') +
+      pyparsing.Suppress(')'))
+
+  _ELEMENT_ORIGINATOR = (
+      (pyparsing.Suppress('+[') | pyparsing.Suppress('-[')) +
+      pyparsing.SkipTo(pyparsing.Literal(']')).setResultsName(
+          'originating_call') +
+      pyparsing.Suppress(']: '))
+
+  _BODY_END = pyparsing.StringEnd() | _TIMESTAMP
+
+  _ELEMENT_BODY = pyparsing.SkipTo(_BODY_END).setResultsName('body')
+
+  _LINE_GRAMMAR = (
+      _TIMESTAMP + _ELEMENT_NUMBER + _ELEMENT_SEVERITY +
+      _ELEMENT_ID + _ELEMENT_ORIGINATOR + _ELEMENT_BODY +
+      pyparsing.ZeroOrMore(pyparsing.lineEnd()))
+
+  LINE_STRUCTURES = [('log_entry', _LINE_GRAMMAR)]
+
+  def ParseRecord(self, parser_mediator, key, structure):
+    """Parses an iOS mobile installation log record.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+        and other components, such as storage and dfvfs.
+      key (str): name of the parsed structure.
+      structure (pyparsing.ParseResults): tokens from a parsed log line.
+
+    Raises:
+      ParseError: when the structure type is unknown.
+    """
+    if key != 'log_entry':
+      raise errors.ParseError(
+          'Unable to parse record, unknown structure: {0:s}'.format(key))
+
+    month_string = self._GetValueFromStructure(structure, 'month')
+
+    year = self._GetValueFromStructure(structure, 'year')
+    month = self.MONTHS.get(month_string)
+    day = self._GetValueFromStructure(structure, 'day')
+    hours = self._GetValueFromStructure(structure, 'hours')
+    minutes = self._GetValueFromStructure(structure, 'minutes')
+    seconds = self._GetValueFromStructure(structure, 'seconds')
+
+    event_data = IOSMobileInstallationEventData()
+    event_data.process_identifier = self._GetValueFromStructure(
+        structure, 'process_identifier')
+    event_data.severity = self._GetValueFromStructure(structure, 'severity')
+    event_data.originating_call = self._GetValueFromStructure(
+        structure, 'originating_call')
+    event_data.body = self._GetValueFromStructure(structure, 'body')
+
+    try:
+      date_time = dfdatetime_time_elements.TimeElements(
+          time_elements_tuple=(year, month, day, hours, minutes, seconds))
+    except (TypeError, ValueError):
+      parser_mediator.ProduceExtractionWarning('invalid date time value')
+      return
+
+    event = time_events.DateTimeValuesEvent(
+        date_time, definitions.TIME_DESCRIPTION_MODIFICATION)
+
+    parser_mediator.ProduceEventWithEventData(event, event_data)
+
+  def VerifyStructure(self, parser_mediator, lines):
+    """Verifies that this is an iOS mobile installation log file.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between
+        parsers and other components, such as storage and dfvfs.
+      lines (str): one or more lines from the text file.
+
+    Returns:
+      bool: True if this is the correct parser, False otherwise.
+    """
+    match_generator = self._LINE_GRAMMAR.scanString(lines, maxMatches=1)
+    return bool(list(match_generator))
+
+
+manager.ParsersManager.RegisterParser(IOSMobileInstallationLogParser)
---
+++ tests/parsers/ios_mobile_installation_log.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Tests for the iOS Mobile Installation log parser."""
+
+import unittest
+
+from plaso.parsers import ios_mobile_installation_log
+
+from tests.parsers import test_lib
+
+
+class IOSMobileInstallationLogParserTest(test_lib.ParserTestCase):
+  """Tests for the iOS Mobile Installation log parser"""
+
+  def testParseLog(self):
+    """Tests the Parse function"""
+    parser = ios_mobile_installation_log.IOSMobileInstallationLogParser()
+    storage_writer = self._ParseFile(['mobile_installation.log.0'], parser)
+
+    number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
+    self.assertEqual(number_of_events, 18)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'extraction_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    events = list(storage_writer.GetEvents())
+
+    expected_event_values = {
+        'body': (
+            'Ignoring plugin at /System/Library/PrivateFrameworks/'
+            'AccessibilityUtilities.framework/PlugIns/'
+            'com.apple.accessibility.Accessibility.HearingAidsTapToRadar.'
+            'appex due to validation issue(s). '
+            'See previous log messages for details.'),
+        'originating_call': (
+            'MILaunchServicesDatabaseGatherer '
+            'enumeratePluginKitPluginsInBundle:updatingPluginParentID:'
+            'ensurePluginsAreExecutable:installProfiles:error:'
+            'enumerator:'),
+        'process_identifier': '176',
+        'severity': 'err',
+        'timestamp': '2021-08-11 05:51:02.000000'}
+
+    self.CheckEventValues(storage_writer, events[7], expected_event_values)
+
+    expected_event_values = {
+        'body': (
+            '47: Failed to load Info.plist from bundle at path /System/'
+            'Library/PrivateFrameworks/'
+            'CoreDuetContext.framework; Extra info about "/System/Library/'
+            'PrivateFrameworks/'
+            'CoreDuetContext.framework/Info.plist": dev=805306369 '
+            'ino=1152921500312080876 mode=0100644 nlink=1 '
+            'uid=0 gid=0 rdev=0 size=800 atime=1577865600.000000 '
+            'mtime=1577865600.000000 '
+            'ctime=1577865600.000000 birthtime=1577865600.000000 '
+            'blksize=4096 blocks=0 flags=0x20 '
+            'firstBytes={length = 4, bytes = 0x62706c69} ACL=<not found> '
+            'extendedAttributes={\n    '
+            '"com.apple.decmpfs" = {length = 817, bytes = 0x66706d63 '
+            '09000000 20030000 00000000 ... 00000000 '
+            '000002a6 };\n} keyCount=22 keySample={ CFBundleName DTXcode '
+            'DTSDKName DTSDKBuild '
+            'CFBundleDevelopmentRegion }'),
+        'originating_call': 'MIBundle _validateWithError:',
+        'process_identifier': '176',
+        'severity': 'err',
+        'timestamp': '2021-08-11 05:51:03.000000'}
+
+    self.CheckEventValues(storage_writer, events[14], expected_event_values)
+
+
+if __name__ == '__main__':
+  unittest.main()
