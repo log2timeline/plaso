e03e65a02a1f3c54739202e9c8b364e62228ba92
Joachim Metz <joachim.metz@gmail.com>
2022-06-06 13:39:59 +0200

    Moved KML event formatting into separate class #3854

--- plaso/output/kml.py
+++ plaso/output/kml.py
@@ -15,6 +15,48 @@ from plaso.output import manager
 from plaso.output import rawpy
 
 
+class KMLEventFormattingHelper(rawpy.NativePythonEventFormattingHelper):
+  """Keyhole Markup Language (KML) XML event formatting helper."""
+
+  def GetFormattedEvent(self, event, event_data, event_data_stream, event_tag):
+    """Retrieves a string representation of the event.
+
+    Args:
+      event (EventObject): event.
+      event_data (EventData): event data.
+      event_data_stream (EventDataStream): event data stream.
+      event_tag (EventTag): event tag.
+
+    Returns:
+      str: string representation of the event.
+    """
+    event_identifier = event.GetIdentifier()
+
+    description_text = self._GetFormattedEventNativePython(
+        event, event_data, event_data_stream, event_tag)
+
+    placemark_xml_element = ElementTree.Element('Placemark')
+
+    name_xml_element = ElementTree.SubElement(placemark_xml_element, 'name')
+    name_xml_element.text = event_identifier.CopyToString()
+
+    description_xml_element = ElementTree.SubElement(
+        placemark_xml_element, 'description')
+    description_xml_element.text = '{0:s}\n'.format(description_text)
+
+    point_xml_element = ElementTree.SubElement(placemark_xml_element, 'Point')
+
+    coordinates_xml_element = ElementTree.SubElement(
+        point_xml_element, 'coordinates')
+    coordinates_xml_element.text = '{0!s},{1!s}'.format(
+        event_data.longitude, event_data.latitude)
+
+    # Note that ElementTree.tostring() will appropriately escape the input data.
+    xml_string = ElementTree.tostring(placemark_xml_element)
+
+    return codecs.decode(xml_string, self._output_mediator.encoding)
+
+
 class KMLOutputModule(interface.TextFileOutputModule):
   """Output module for a Keyhole Markup Language (KML) XML file."""
 
@@ -27,8 +69,7 @@ class KMLOutputModule(interface.TextFileOutputModule):
     Args:
       output_mediator (OutputMediator): an output mediator.
     """
-    event_formatting_helper = rawpy.NativePythonEventFormattingHelper(
-        output_mediator)
+    event_formatting_helper = KMLEventFormattingHelper(output_mediator)
     super(KMLOutputModule, self).__init__(
         output_mediator, event_formatting_helper)
 
@@ -43,34 +84,9 @@ class KMLOutputModule(interface.TextFileOutputModule):
     """
     latitude = getattr(event_data, 'latitude', None)
     longitude = getattr(event_data, 'longitude', None)
-    if latitude is not None and longitude is not None:
-      placemark_xml_element = ElementTree.Element('Placemark')
-
-      name_xml_element = ElementTree.SubElement(placemark_xml_element, 'name')
-
-      event_identifier = event.GetIdentifier()
-      name_xml_element.text = '{0!s}'.format(event_identifier.CopyToString())
-
-      description_xml_element = ElementTree.SubElement(
-          placemark_xml_element, 'description')
-
-      description_text = self._event_formatting_helper.GetFormattedEvent(
+    if None not in (latitude, longitude):
+      output_text = self._event_formatting_helper.GetFormattedEvent(
           event, event_data, event_data_stream, event_tag)
-
-      description_xml_element.text = '{0:s}\n'.format(description_text)
-
-      point_xml_element = ElementTree.SubElement(
-          placemark_xml_element, 'Point')
-
-      coordinates_xml_element = ElementTree.SubElement(
-          point_xml_element, 'coordinates')
-      coordinates_xml_element.text = '{0!s},{1!s}'.format(longitude, latitude)
-
-      # Note that ElementTree.tostring() will appropriately escape
-      # the input data.
-      xml_string = ElementTree.tostring(placemark_xml_element)
-
-      output_text = codecs.decode(xml_string, self._output_mediator.encoding)
       self.WriteText(output_text)
 
   def WriteHeader(self):
