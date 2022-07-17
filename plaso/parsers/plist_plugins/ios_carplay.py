0c6535b9e7e73661237ba666a1d54c5aea8df426
Hudan Studiawan <studiawan@gmail.com>
2022-07-17 12:03:33 +0700

    Add plist parser for iOS CarPlay app

---
+++ plaso/parsers/plist_plugins/ios_carplay.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""Plist parser plugin for Car Play App plist files. 
+   It contains history of opened apps on Car Play App.
+"""
+
+from dfdatetime import posix_time
+
+from plaso.containers import plist_event
+from plaso.containers import time_events
+from plaso.lib import definitions
+from plaso.parsers import plist
+from plaso.parsers.plist_plugins import interface
+
+
+class IOSCarPlayPlugin(interface.PlistPlugin):
+    """Plist parser plugin for IOS Car Play App plist files.
+
+    """
+
+    NAME = 'ios_carplay'
+    DATA_TYPE = 'ios:carplay:history'
+    DATA_FORMAT = 'iOS Car Play App plist file'
+
+    PLIST_PATH_FILTERS = frozenset([
+      interface.PlistPathFilter('com.apple.CarPlayApp.plist')])
+
+    PLIST_KEYS = frozenset(['CARRecentAppHistory'])
+
+    def _ParsePlist(
+      self, parser_mediator, match=None, top_level=None, **unused_kwargs):
+        """Extract relevant Car Play App entries.
+        """
+        recent_app_history = match.get('CARRecentAppHistory', {})
+        for parameter, value in recent_app_history.items():
+            event_data = plist_event.PlistTimeEventData()
+            event_data.root = '/CARRecentAppHistory'
+
+            datetime_value = value
+            if datetime_value:
+                event_data.desc = parameter
+                event_data.key = parameter
+
+                # Convert the floating point value to an integer.
+                # TODO: add support for the fractional part of the floating point value.
+                timestamp = int(datetime_value)
+                date_time = posix_time.PosixTime(timestamp=timestamp)
+
+                event = time_events.DateTimeValuesEvent(
+                    date_time, definitions.TIME_DESCRIPTION_WRITTEN)
+                parser_mediator.ProduceEventWithEventData(event, event_data)
+
+
+plist.PlistParser.RegisterPlugin(IOSCarPlayPlugin)
