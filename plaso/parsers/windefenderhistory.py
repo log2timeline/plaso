9dca627f18a1dc55bad0fad3ee09f913a10f07c3
Fry <fryx0r@gmail.com>
2022-07-21 15:42:48 +1000

    Move to the dtformat parser

--- plaso/parsers/windefenderhistory.py
+++ plaso/parsers/windefenderhistory.py
@@ -4,6 +4,7 @@
 from datetime import datetime, timedelta
 import os
 
+from dtfabric.runtime import data_maps as dtfabric_data_maps
 from dfdatetime import time_elements as dfdatetime_time_elements
 
 from plaso.containers import events
@@ -42,10 +43,81 @@ class WinDefenderHistoryParser(interface.FileObjectParser,
 
     NAME = 'windefenderhistory'
 
+    _FILE_HEADER = b'\x08\x00\x00\x00\x08'
     _FILE_SIGNATURE = "Magic.Version"
 
+    _VALUE_DESCRIPTIONS = [{
+        0: 'Threat identifier',
+        1: 'Identifier'
+    }, {
+        0: 'UnknownMagic1',
+        1: 'Threat name',
+        4: 'Category'
+    }, {
+        0: 'UnknownMagic2',
+        1: 'Resource type',
+        2: 'Resource location',
+        4: 'Threat tracking data size',
+        5: 'Threat tracking data',
+        6: 'Last threat status change time',
+        12: 'Domain user1',
+        14: 'Process name',
+        18: 'Initial detection time',
+        20: 'Remediation time',
+        24: 'Domain user2'
+    }]
+
+    _CATEGORY_NAME = {
+        0: 'INVALID',
+        1: 'ADWARE',
+        2: 'SPYWARE',
+        3: 'PASSWORDSTEALER',
+        4: 'TROJANDOWNLOADER',
+        5: 'WORM',
+        6: 'BACKDOOR',
+        7: 'REMOTEACCESSTROJAN',
+        8: 'TROJAN',
+        9: 'EMAILFLOODER',
+        10: 'KEYLOGGER',
+        11: 'DIALER',
+        12: 'MONITORINGSOFTWARE',
+        13: 'BROWSERMODIFIER',
+        14: 'COOKIE',
+        15: 'BROWSERPLUGIN',
+        16: 'AOLEXPLOIT',
+        17: 'NUKER',
+        18: 'SECURITYDISABLER',
+        19: 'JOKEPROGRAM',
+        20: 'HOSTILEACTIVEXCONTROL',
+        21: 'SOFTWAREBUNDLER',
+        22: 'STEALTHNOTIFIER',
+        23: 'SETTINGSMODIFIER',
+        24: 'TOOLBAR',
+        25: 'REMOTECONTROLSOFTWARE',
+        26: 'TROJANFTP',
+        27: 'POTENTIALUNWANTEDSOFTWARE',
+        28: 'ICQEXPLOIT',
+        29: 'TROJANTELNET',
+        30: 'FILESHARINGPROGRAM',
+        31: 'MALWARE_CREATION_TOOL',
+        32: 'REMOTE_CONTROL_SOFTWARE',
+        33: 'TOOL',
+        34: 'TROJAN_DENIALOFSERVICE',
+        36: 'TROJAN_DROPPER',
+        37: 'TROJAN_MASSMAILER',
+        38: 'TROJAN_MONITORINGSOFTWARE',
+        39: 'TROJAN_PROXYSERVER',
+        40: 'VIRUS',
+        42: 'KNOWN',
+        43: 'UNKNOWN',
+        44: 'SPP',
+        45: 'BEHAVIOR',
+        46: 'VULNERABILTIY',
+        47: 'POLICY'
+    }
+
     _DEFINITION_FILE = os.path.join(os.path.dirname(__file__),
-                                    'windefenderhistory.yml')
+                                    'detection_history.yaml')
 
     def _ReadFileHeader(self, file_object):
         """Reads the file header.
@@ -53,94 +125,141 @@ class WinDefenderHistoryParser(interface.FileObjectParser,
         Args:
             file_object (file): file-like object.
 
-        Returns:
-            file_header: file header.
-            file_header_size: file header size.
-
         Raises:
             ParseError: if the file header cannot be read.
         """
         data_type_map = self._GetDataTypeMap('defender_file_header')
-
-        file_header, file_header_size = self._ReadStructureFromFileObject(
+        header, header_size = self._ReadStructureFromFileObject(
             file_object, 0, data_type_map)
 
-        if self._FILE_SIGNATURE not in file_header.magic_version:
+        data_type_map = self._GetDataTypeMap('detection_history_value')
+        _, guid_size = self._ReadStructureFromFileObject(
+            file_object, header_size, data_type_map)
+        signature, signature_size = self._ReadStructureFromFileObject(
+            file_object, header_size + guid_size, data_type_map)
+
+        if header.header_bytes != self._FILE_HEADER and self._FILE_SIGNATURE not in signature.value_string:
             raise errors.ParseError('Invalid header')
 
-        return (file_header, file_header_size)
+        return header_size + guid_size + signature_size
 
-    def _CreateDateTime(self, date_time_tuple):
-        nanoseconds = int.from_bytes(date_time_tuple, byteorder='little')
-        epoch = timedelta(microseconds=float(nanoseconds / 10))
+    def _ReadThreatTrackingData(self, threat_tracking_data, file_offset):
+        """Reads the threat tracking data.
+        Args:
+            threat_tracking_data (bytes): threat tracking data.
+            file_offset (int): offset of the threat tracking data relative to
+                the start of the file.
+        Raises:
+            IOError: if the threat tracking data cannot be read.
+        """
+        threat_tracking = {}
+        data_type_map = self._GetDataTypeMap('uint32le')
+
+        values_data_size = self._ReadStructureFromByteStream(
+            threat_tracking_data, 0, data_type_map)
+
+        if values_data_size != 1:
+            values_data_offset = 4
+            values_data_end_offset = values_data_size
+        else:
+            header = self._ReadThreatTrackingHeader(threat_tracking_data)
+
+        values_data_offset = header.header_size + 4
+        values_data_end_offset = header.total_data_size
+
+        while values_data_offset < values_data_end_offset:
+            ttv, data_size = self._ReadThreatTrackingValue(
+                threat_tracking_data[values_data_offset:],
+                file_offset + values_data_offset)
+            if hasattr(ttv, 'value_string'):
+                threat_tracking[ttv.key_string] = ttv.value_string
+            if hasattr(ttv, 'value_integer'):
+                threat_tracking[ttv.key_string] = ttv.value_integer
+            values_data_offset += data_size
+
+        return threat_tracking
+
+    def _ReadThreatTrackingHeader(self, threat_tracking_data):
+        """Reads the threat tracking header.
+
+        Args:
+        threat_tracking_data (bytes): threat tracking data.
+
+        Returns:
+        threat_tracking_header: threat tracking header.
+
+        Raises:
+        IOError: if the threat tracking header cannot be read.
+        """
+        data_type_map = self._GetDataTypeMap('threat_tracking_header')
+
+        threat_tracking_header = self._ReadStructureFromByteStream(
+            threat_tracking_data, 0, data_type_map)
+
+        if threat_tracking_header.header_size > 20:
+            # TODO: debug print additional data.
+            pass
+
+        return threat_tracking_header
+
+    def _ReadThreatTrackingValue(self, threat_tracking_data, file_offset):
+        """Reads the threat tracking value.
+
+        Args:
+        threat_tracking_data (bytes): threat tracking data.
+        file_offset (int): offset of the threat tracking data relative to
+            the start of the file.
+
+        Returns:
+        tuple[threat_tracking_value, int]: threat tracking value and data size.
+
+        Raises:
+        IOError: if the threat tracking value cannot be read.
+        """
+        data_type_map = self._GetDataTypeMap('threat_tracking_value')
+
+        context = dtfabric_data_maps.DataTypeMapContext()
+
+        threat_tracking_value = self._ReadStructureFromByteStream(
+            threat_tracking_data, file_offset, data_type_map, context=context)
+
+        return threat_tracking_value, context.byte_size
+
+    def _CreateDateTime(self, date_time):
+        epoch = timedelta(microseconds=float(date_time / 10))
         dt = datetime(1601, 1, 1) + epoch
         te = dfdatetime_time_elements.TimeElements()
         te.CopyFromDatetime(dt)
         return te
 
-    def _ReadBlock(self, file_object, file_offset, extra_offset, map_name):
-        data_type_map = self._GetDataTypeMap(map_name)
+    def _ReadValue(self, file_object, file_offset):
+        """Reads the value.
+        Args:
+        file_object (file): file-like object.
+        file_offset (int): offset of the value relative to the start of the file.
+        Returns:
+        object: value.
+        Raises:
+        IOError: if the value cannot be read.
+        """
+        data_type_map = self._GetDataTypeMap('detection_history_value')
 
-        return self._ReadStructureFromFileObject(file_object,
-                                                 file_offset + extra_offset,
-                                                 data_type_map)
+        value, _ = self._ReadStructureFromFileObject(file_object, file_offset,
+                                                     data_type_map)
 
-    def _ReadFileSection(self, file_object, size_so_far, offset, event_data,
-                         mediator):
-        file, file_size = self._ReadBlock(file_object, size_so_far, offset,
-                                          'file_section')
-        size_so_far += file_size
+        value_object = None
+        if value.data_type in (0x00000000, 0x00000005, 0x00000006, 0x00000008):
+            value_object = value.value_integer
+        elif value.data_type == 0x0000000a:
+            value_object = value.value_filetime
+        elif value.data_type == 0x00000015:
+            value_object = value.value_string
+        elif value.data_type == 0x0000001e:
+            value_object = value.value_guid
+        elif value.data_type == 0x00000028:
+            value_object = value.data
 
-        file_type = file.file_header.decode('windows-1252').replace('\x00', '')
-        if file_type == "containerfile":
-            event_data.container_filename = file.file_value
-        elif file_type == "webfile":
-            event_data.web_filename = file.file_value
-        elif file_type == "file":
-            event_data.filename = file.file_value
-        else:
-            event_data.extra = file.file_value
-
-        return event_data, size_so_far
-
-    def _ReadKVBlockLoop(self, file_object, size_so_far, event_data,
-                         date_time):
-        while True:
-            try:
-                tk, tk_size = self._ReadBlock(file_object, size_so_far, 0,
-                                              'kv_block_key')
-                if tk.key == '':
-                    break
-                size_so_far += tk_size
-                if tk.value_type == 6:
-                    tv, tv_size = self._ReadBlock(file_object, size_so_far, 0,
-                                                  'kv_block_string_value')
-                    size_so_far += tv_size
-                    if "filename" in tk.key.lower():
-                        event_data.filename = tv.value.rstrip('\x00')
-                    if "Sha256" in tk.key:
-                        event_data.sha256 = tv.value.rstrip('\x00')
-                elif tk.value_type == 3:  # Flag
-                    tv, tv_size = self._ReadBlock(file_object, size_so_far, 0,
-                                                  'kv_block_flags_value')
-                    size_so_far += tv_size
-                elif tk.value_type == 5:  # Bool
-                    size_so_far += 1
-                    continue
-                elif tk.value_type == 4:  # Date Time
-                    tv, tv_size = self._ReadBlock(file_object, size_so_far, 0,
-                                                  'kv_block_bytes_value')
-                    size_so_far += tv_size
-                    if "Time" in tk.key and date_time is None:
-                        date_time = tv.value  # Int tuple
-                else:  # Catch all
-                    tv, tv_size = self._ReadBlock(file_object, size_so_far, 0,
-                                                  'kv_block_bytes_value')
-                    size_so_far += tv_size
-            except errors.ParseError:
-                break
-
-        return event_data, size_so_far, date_time
+        return value_object
 
     def ParseFileObject(self, parser_mediator, file_object):
         """Parses a Windows Defender History file-like object.
@@ -154,83 +273,86 @@ class WinDefenderHistoryParser(interface.FileObjectParser,
       WrongParser: when the file cannot be parsed.
     """
         try:
-            _, file_header_size = self._ReadFileHeader(file_object)
+            file_header_size = self._ReadFileHeader(file_object)
         except (ValueError, errors.ParseError) as e:
             raise errors.WrongParser(
                 ('[{0:s}] {1:s} is not a valid Windows Defender History file'
                  ).format(self.NAME, parser_mediator.GetDisplayName()))
 
-        size_so_far = file_header_size
-
         event_data = WinDefenderHistoryEventData()
-        date_time = None
+        threat_attributes = {}
+        threat_attributes['Resources'] = []
+        value_tuples = []
+        temp_resource_name = ""
 
-        threat, threat_size = self._ReadBlock(file_object, size_so_far, 4,
-                                              'key_value_pair_with_colon')
-        size_so_far += threat_size
+        file_size = file_object.get_size()
+        file_offset = 0
+        while file_offset < file_size:
+            value_object = self._ReadValue(file_object, file_offset)
+            value_tuples.append((file_offset, value_object))
+            file_offset = file_object.tell()
 
-        event_data.threatname = threat.key + ":" + threat.value
+        value_index_set = 0
+        value_index = 0
 
-        _, phb_size = self._ReadBlock(file_object, size_so_far, 0,
-                                      'post_header_block')
-        size_so_far += phb_size
+        for file_offset, value_object in value_tuples:
+            if value_object == 'Magic.Version:1.2':
+                if value_index_set < 2:
+                    value_index_set += 1
+                value_index = 0
 
-        event_data, size_so_far = self._ReadFileSection(
-            file_object, size_so_far, -2, event_data, parser_mediator)
+            if (value_index_set, value_index) == (2, 5):
+                threat_attributes.update(self._ReadThreatTrackingData(value_object, file_offset + 8))
 
-        # Potentially another file block directly after
-        backup_size = size_so_far
-        try:
-            _, phb_size = self._ReadBlock(file_object, size_so_far, 0,
-                                          'post_header_block')
-            size_so_far += phb_size
-            event_data, size_so_far = self._ReadFileSection(
-                file_object, size_so_far, -2, event_data, parser_mediator)
-        except errors.ParseError:
-            size_so_far = backup_size
-            pass
+            else:
+                description = self._VALUE_DESCRIPTIONS[value_index_set].get(
+                    value_index,
+                    'UNKNOWN_{0:d}_{1:d}'.format(value_index_set, value_index))
 
-        _, pfb_size = self._ReadBlock(file_object, size_so_far, 0,
-                                      'post_file_block')
-        size_so_far += pfb_size - 5
+                if (value_index_set, value_index) == (1, 4):
+                    value_string = '{0!s} ({1:s})'.format(
+                        value_object,
+                        self._CATEGORY_NAME.get(value_object, 'UNKNOWN'))
+                else:
+                    value_string = '{0!s}'.format(value_object)
 
-        event_data, size_so_far, date_time = self._ReadKVBlockLoop(
-            file_object, size_so_far, event_data, date_time)
+                if description == "Resource type":
+                    temp_resource_name = value_string
+                elif description == "Resource location" and temp_resource_name is not None:
+                    threat_attributes['Resources'].append({'Type': temp_resource_name, 'Location': value_string})
+                    temp_resource_name = None
+                else:
+                    threat_attributes[description] = value_string
 
-        backup_size = size_so_far
-        while True:
-            try:
-                _, phb_size = self._ReadBlock(file_object, size_so_far, -1,
-                                              'post_header_block')
-                size_so_far += phb_size
+            value_index += 1
 
-                event_data, size_so_far = self._ReadFileSection(
-                    file_object, size_so_far, -3, event_data, parser_mediator)
-
-                _, pfb_size = self._ReadBlock(file_object, size_so_far, 0,
-                                              'post_file_block')
-                size_so_far += pfb_size - 5
-
-                event_data, size_so_far, date_time = self._ReadKVBlockLoop(
-                    file_object, size_so_far, event_data, date_time)
-                backup_size = size_so_far
-            except errors.ParseError:
-                size_so_far = backup_size
-                break
-
-        _, pkvb_size = self._ReadBlock(file_object, size_so_far, -1,
-                                       'post_kv_block')
-        size_so_far += pkvb_size + 1
-
-        u, _ = self._ReadBlock(file_object, size_so_far, -4,
-                               'user_and_process')
-
-        event_data.host_and_user = u.user
-        event_data.process = u.process
+        event_data.threatname = threat_attributes.get('Threat name', 'UNKNOWN')
+        filenames = [x['Location'] for x in threat_attributes['Resources'] if x['Type'] == "file"]
+        if len(filenames) > 0:
+            event_data.filename = filenames[0]
+        else:
+            event_data.filename = 'UNKNOWN'
+            if 'CONTEXT_DATA_FILENAME' in threat_attributes:
+                event_data.filename = threat_attributes['CONTEXT_DATA_FILENAME']
+            if 'CONTEXT_DATA_PROCESS_PPID' in threat_attributes:
+                event_data.filename += "," + threat_attributes['CONTEXT_DATA_PROCESS_PPID']
+        webfiles = [x['Location'] for x in threat_attributes['Resources'] if x['Type'] == "webfile"]
+        if len(webfiles) > 0:
+            event_data.web_filename = ", ".join(webfiles)
+        containerfiles = [x['Location'] for x in threat_attributes['Resources'] if x['Type'] == "containerfile"]
+        if len(containerfiles) > 0:
+            event_data.container_filename = ", ".join(containerfiles)
+        extrafiles = [x['Location'] for x in threat_attributes['Resources'] if "file" not in x['Type']]
+        if len(extrafiles):
+            event_data.extra = ", ".join(extrafiles)
+        event_data.sha256 = threat_attributes.get('ThreatTrackingSha256', 'UNKNOWN')
+        event_data.host_and_user = threat_attributes.get('Domain user1', 'UNKNOWN')
+        event_data.process = threat_attributes.get('Process name', 'Unknown')
 
         event = time_events.DateTimeValuesEvent(
-            self._CreateDateTime(date_time),
+            self._CreateDateTime(threat_attributes.get('ThreatTrackingStartTime', 0)),
             definitions.TIME_DESCRIPTION_RECORDED)
+        
         parser_mediator.ProduceEventWithEventData(event, event_data)
 
 
