ba954d30883c2e6c8eabc3d0546118c337c1ddca
Fry <fryx0r@gmail.com>
2022-07-22 15:52:11 +1000

    Free to use generated test cases

--- tests/parsers/windefenderhistory.py
+++ tests/parsers/windefenderhistory.py
@@ -16,8 +16,7 @@ class WinDefenderHistoryUnitTest(test_lib.ParserTestCase):
     """Tests parsing a webfile Detection History file."""
     parser = windefenderhistory.WinDefenderHistoryParser()
     storage_writer = self._ParseFile([
-      'defender/'
-      'f34a0f70391bc24ecd465f713c6b068cc24dd05d71994924abbb438ba9eecf62'],
+        'FC380697-A68D-4C94-B67F-9B6449039463'],
     parser)
 
     number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
@@ -31,29 +30,26 @@ class WinDefenderHistoryUnitTest(test_lib.ParserTestCase):
     for event in storage_writer.GetEvents():
       event_data = self._GetEventDataOfEvent(storage_writer, event)
       self.assertEqual(
-          'C:\\Users\\Luqman\\Downloads\\Ransomware.Jigsaw.zip',
+          'C:\\Users\\testuser\\Downloads\\PotentiallyUnwanted.exe',
           event_data.filename)
       self.assertEqual(
-          'C:\\Users\\Luqman\\Downloads\\Ransomware.Jigsaw.zip|http://10.1.20'
-          '.13:8000/Ransomware.Jigsaw.zip|pid:1020,'
-          'ProcessStart:132525829301711007',
-          event_data.web_filename)
-      self.assertEqual(
-          '86a391fe7a237f4f17846c53d71e45820411d1a9a6e0c16f22a11ebc491ff9ff',
+          'd6f6c6b9fde37694e12b12009ad11ab9ec8dd0f193e7319c523933bdad8a50ad',
           event_data.sha256)
-      self.assertEqual('Trojan:Win32/Vigorf.A', event_data.threat_name)
-      self.assertEqual('DESKTOP-38RB3EF\\Luqman',
+      self.assertEqual('PUA:Win32/EICAR_Test_File', event_data.threat_name)
+      self.assertEqual(
+          'C:\\Users\\testuser\\Downloads\\PotentiallyUnwanted.exe|'
+          'http://amtso.eicar.org/PotentiallyUnwanted.exe|'
+          'pid:19588,ProcessStart:133029415464710822',
+          event_data.web_filename)
+      self.assertEqual('FRYPTOP\\testuser',
           event_data.host_and_user)
-      self.assertEqual('Unknown', event_data.process)
-      self.assertEqual('2020-12-16 09:02:12',
-          event.date_time.CopyToDateTimeString())
+      self.assertEqual('C:\\Windows\\explorer.exe', event_data.process)
 
   def testContainerDetection(self):
     """Tests parsing a containerfile Detection History file."""
     parser = windefenderhistory.WinDefenderHistoryParser()
     storage_writer = self._ParseFile([
-        'defender/'
-        '5f747e1ce825ffb36d0cbd7e9348886e79a71f7f317b1c0afe11f6baafa35015'],
+      '6AFE33A0-19BA-4FFF-892F-B700539D7D63'],
     parser)
 
     number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
@@ -67,132 +63,21 @@ class WinDefenderHistoryUnitTest(test_lib.ParserTestCase):
     for event in storage_writer.GetEvents():
       event_data = self._GetEventDataOfEvent(storage_writer, event)
       self.assertEqual(
-          'C:\\Users\\bkara\\AppData\\Local\\Mozilla\\Firefox\\Profiles\\2k4x3'
-          'nr9.default-release\\cache2\\entries\\32F0AE83BF7D7CFE8D5B679DE93E8'
-          'E189345BB6C->(GZip)->(SCRIPT0001)',
+          'C:\\Users\\testuser\\Downloads\\eicar_com.zip->eicar.com',
           event_data.filename)
       self.assertEqual(
-          '82e242b2951403ef2d288fb97f428b830900530f85f1b4689a6130b328aa4c45',
-          event_data.sha256)
-      self.assertEqual('Trojan:HTML/Phish.VS!MSR', event_data.threat_name)
+        'C:\\Users\\testuser\\Downloads\\eicar_com.zip',
+        event_data.container_filename)
       self.assertEqual(
-          'C:\\Users\\bkara\\AppData\\Local\\Mozilla\\Firefox\\Profiles\\2k4x3'
-          'nr9.default-release\\cache2\\entries\\32F0AE83BF7D7CFE8D5B679DE93E8'
-          'E189345BB6C',
-          event_data.container_filename)
-      self.assertEqual('DESKTOP-E9T3V5E\\bkara',
+          '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f',
+          event_data.sha256)
+      self.assertEqual('Virus:DOS/EICAR_Test_File', event_data.threat_name)
+      self.assertEqual('FRYPTOP\\testuser',
           event_data.host_and_user)
       self.assertEqual('Unknown', event_data.process)
+      self.assertEqual('2022-07-22 05:36:18',
+          event.date_time.CopyToDateTimeString())
 
-  def testRegistryBasedDetection(self):
-    """Tests parsing a regkey Detection History file."""
-    parser = windefenderhistory.WinDefenderHistoryParser()
-    storage_writer = self._ParseFile([
-        'defender/'
-        '233c51b726822b2223142d8d05976509936ce28323006f7f8a89ca1e8b7f14b6'],
-    parser)
-
-    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-        'extraction_warning')
-    self.assertEqual(number_of_warnings, 0)
-
-    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-        'recovery_warning')
-    self.assertEqual(number_of_warnings, 0)
-
-    for event in storage_writer.GetEvents():
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      self.assertEqual('VirTool:Win32/DefenderTamperingRestore',
-                        event_data.threat_name)
-      self.assertEqual('UNKNOWN', event_data.filename)
-      self.assertEqual('UNKNOWN', event_data.sha256)
-      self.assertEqual(
-          'hklm\\software\\microsoft\\windows defender\\\\DisableAntiSpyware',
-          event_data.extra)
-      self.assertEqual('NT AUTHORITY\\SYSTEM', event_data.host_and_user)
-      self.assertEqual('Unknown', event_data.process)
-
-  def testProcessBasedDetection(self):
-    """Tests parsing a process:// Detection History file."""
-    parser = windefenderhistory.WinDefenderHistoryParser()
-    storage_writer = self._ParseFile([
-        'defender/'
-        '535d641be9356b67a4fa15fafd516187fa6c2e585e16be6813b56f47ead81c83'],
-    parser)
-
-    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-        'extraction_warning')
-    self.assertEqual(number_of_warnings, 0)
-
-    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-        'recovery_warning')
-    self.assertEqual(number_of_warnings, 0)
-
-    for event in storage_writer.GetEvents():
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      self.assertEqual('Trojan:Win32/Delf.BB', event_data.threat_name)
-      self.assertEqual(
-          'process://C:\\Program Files (x86)\\DesktopCentral_Agent'
-          '\\dcconfig.exe,pid:53668,ProcessStart:133004481826348874',
-          event_data.filename)
-      self.assertEqual(
-          '4219fd3fd695a020bf5d300d7b9cf80806af2aa626c59713587d7f6c650e58bb',
-          event_data.sha256)
-      self.assertEqual('pid:53668,ProcessStart:133004481826348874',
-          event_data.extra)
-      self.assertEqual('NT AUTHORITY\\SYSTEM', event_data.host_and_user)
-      self.assertEqual(
-          'C:\\Program Files (x86)\\DesktopCentral_Agent\\dcconfig.exe',
-          event_data.process)
-
-  def testNoFilenameFrenchSystem(self):
-    """Tests parsing a french Detection History file with a ThreatData key."""
-    parser = windefenderhistory.WinDefenderHistoryParser()
-    storage_writer = self._ParseFile([
-        'defender/'
-        'b7c5b9dc8388a709849bbf6c7a03b3a55ae8a1edaa87c16779bf01668ebce299'],
-    parser)
-
-    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-        'extraction_warning')
-    self.assertEqual(number_of_warnings, 0)
-
-    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-        'recovery_warning')
-    self.assertEqual(number_of_warnings, 0)
-
-    for event in storage_writer.GetEvents():
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      self.assertEqual('VirTool:Win32/ExcludeProc.D',
-          event_data.threat_name)
-      self.assertEqual('UNKNOWN', event_data.filename)
-      self.assertEqual('UNKNOWN', event_data.sha256)
-      self.assertEqual(
-          'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe '
-          '-EncodedCommand QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0A'
-          'RQB4AGMAbAB1AHMAaQBvAG4ARQB4AHQAZQBuAHMAaQBvAG4AIABAACgAJwBlAHgA'
-          'ZQAnACwAJwBkAGwAbAAnACkAIAAtAEYAbwByAGMAZQA=, C:\\Windows\\System'
-          '32\\WindowsPowerShell\\v1.0\\powershell.exe -EncodedCommand QQBk'
-          'AGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHMAaQBv'
-          'AG4AUABhAHQAaAAgAEAAKAAkAGUAbgB2ADoAVQBzAGUAcgBQAHIAbwBmAGkAbABl'
-          'ACwAJABlAG4AdgA6AFMAeQBzAHQAZQBtAEQAcgBpAHYAZQApACAALQBGAG8AcgBj'
-          'AGUA, i AQAAAIkSBYAAAAAAAAAAAGchilvXwQAAoQYaJ1g0xpRzihIDOX+yDaJM'
-          'TaBWaXJUb29sOldpbjMyL0V4Y2x1ZGVQcm9jLkQAAAAAAAAAAAAAAAAAAAAAAAAA'
-          'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA== 57 10 C:\\Windows\\System32\\Wi'
-          'ndowsPowerShell\\v1.0\\powershell.exe powershell -EncodedCommand'
-          ' QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHM'
-          'AaQBvAG4ARQB4AHQAZQBuAHMAaQBvAG4AIABAACgAJwBlAHgAZQAnACwAJwBkAGw'
-          'AbAAnACkAIAAtAEYAbwByAGMAZQA=, i AQAAAIkSBYAAAAAAAAAAAGchilvXwQA'
-          'AoQYaJ1g0xpRzihIDOX+yDaJMTaBWaXJUb29sOldpbjMyL0V4Y2x1ZGVQcm9jLkQ'
-          'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA== 57 10 '
-          'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe p'
-          'owershell -EncodedCommand QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAG'
-          'MAZQAgAC0ARQB4AGMAbAB1AHMAaQBvAG4AUABhAHQAaAAgAEAAKAAkAGUAbgB2AD'
-          'oAVQBzAGUAcgBQAHIAbwBmAGkAbABlACwAJABlAG4AdgA6AFMAeQBzAHQAZQBtAE'
-          'QAcgBpAHYAZQApACAALQBGAG8AcgBjAGUA',
-          event_data.extra)
-      self.assertEqual('AUTORITE NT\\Système', event_data.host_and_user)
-      self.assertEqual('Unknown', event_data.process)
 
 if __name__ == '__main__':
   unittest.main()
