ffbe59e9c809098549028537be3328efa3b42e84
sydp <syd.pleno@gmail.com>
2022-08-27 01:27:53 +1000

    Update

--- plaso/parsers/android_logcat.py
+++ plaso/parsers/android_logcat.py
@@ -16,13 +16,13 @@ from plaso.parsers import text_parser
 
 class AndroidLogcatEventData(events.EventData):
   """Android logcat event data.
-  
+
   Attributes:
     message (str): the log message.
     pid (int): process identifier (PID) that created the logcat line.
-    priority (str): a character in the set {V, D, I, W, E, F, S}, which is 
+    priority (str): a character in the set {V, D, I, W, E, F, S}, which is
         ordered from lowest to highest priority.
-    tag (str): the tag that indicates the system component from which the 
+    tag (str): the tag that indicates the system component from which the
         logcat line originates.
     tid (int): thread identifier (TID) that created the logcat line.
   """
@@ -50,7 +50,7 @@ class AndroidLogcatParser(text_parser.PyparsingSingleLineTextParser):
   _ANDROID_DATE_GROUP = pyparsing.Combine(
       pyparsing.Word(pyparsing.nums, exact=2) + pyparsing.Literal('-') +
       pyparsing.Word(pyparsing.nums, exact=2))
-  
+
   _ANDROID_TIME_GROUP = pyparsing.Combine(
       pyparsing.Word(pyparsing.nums, exact=2) + pyparsing.Literal(':') +
       pyparsing.Word(pyparsing.nums, exact=2) + pyparsing.Literal(':') +
@@ -60,7 +60,7 @@ class AndroidLogcatParser(text_parser.PyparsingSingleLineTextParser):
   _ANDROID_THREADTIME_LINE = (
     _ANDROID_DATE_GROUP.setResultsName('date') +
     _ANDROID_TIME_GROUP.setResultsName('time') +
-    pyparsing.Word(pyparsing.nums).setResultsName('pid') + 
+    pyparsing.Word(pyparsing.nums).setResultsName('pid') +
     pyparsing.Word(pyparsing.nums).setResultsName('tid') +
     pyparsing.Word('VDIWEFS', exact=1).setResultsName('priority') +
     pyparsing.Word(
@@ -75,7 +75,7 @@ class AndroidLogcatParser(text_parser.PyparsingSingleLineTextParser):
 
   def ParseRecord(self, parser_mediator, key, structure):
     """Parses a matching entry.
-    
+
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
         and other components, such as storage and dfvfs.
@@ -88,16 +88,18 @@ class AndroidLogcatParser(text_parser.PyparsingSingleLineTextParser):
     if key not in self._SUPPORTED_KEYS:
       raise errors.ParseError(
           'Unable to parse record, unknown structure: {0:s}'.format(key))
-      
+
     new_date_time = dfdatetime_time_elements.TimeElementsInMilliseconds()
     date = self._GetValueFromStructure(structure, 'date')
     time = self._GetValueFromStructure(structure, 'time')
     try:
+      # TODO: remove hard coded year and use preferred year argument
       new_date_time.CopyFromStringISO8601(f'2022-{date}T{time}Z')
     except ValueError as error:
       parser_mediator.ProduceExtractionWarning(
         'invalid date time value: {0:s}'.format(error))
-    
+      return
+
     if key == 'threadtime_line':
       event_data = AndroidLogcatEventData()
       event_data.message = self._GetValueFromStructure(structure, 'message')
@@ -107,8 +109,8 @@ class AndroidLogcatParser(text_parser.PyparsingSingleLineTextParser):
       event_data.tid = self._GetValueFromStructure(structure, 'tid')
 
       event = time_events.DateTimeValuesEvent(
-        new_date_time, definitions.TIME_DESCRIPTION_RECORDED)
-      
+          new_date_time, definitions.TIME_DESCRIPTION_RECORDED)
+
       parser_mediator.ProduceEventWithEventData(event, event_data)
 
   # pylint: disable=unused-argument
