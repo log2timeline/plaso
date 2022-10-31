06267dec4814f9aa936f33c1e9a56ff0cf365de5
Joachim Metz <joachim.metz@gmail.com>
2022-10-31 05:58:58 +0100

    Changes to split event_data and event generation (#4340)

--- plaso/parsers/text_plugins/apache_access.py
+++ plaso/parsers/text_plugins/apache_access.py
@@ -10,15 +10,13 @@ import pyparsing
 from dfdatetime import time_elements as dfdatetime_time_elements
 
 from plaso.containers import events
-from plaso.containers import time_events
 from plaso.lib import errors
-from plaso.lib import definitions
 from plaso.parsers import text_parser
 from plaso.parsers.text_plugins import interface
 
 
-class ApacheAccessEventData(events.EventData):
-  """Apache access event data.
+class ApacheAccessLogEventData(events.EventData):
+  """Apache access log event data.
 
   Attributes:
     http_request_referer (str): http request referer header information.
@@ -28,16 +26,18 @@ class ApacheAccessEventData(events.EventData):
     http_response_code (int): http response code from server.
     ip_address (str): IPv4 or IPv6 addresses.
     port_number (int): canonical port of the server serving the request.
+    recorded_time (dfdatetime.DateTimeValues): date and time the log entry
+        was recorded.
     remote_name (str): remote logname (from identd, if supplied).
     server_name (str): canonical hostname of the server serving the request.
     user_name (str): logged user name.
   """
 
-  DATA_TYPE = 'apache:access'
+  DATA_TYPE = 'apache:access_log:entry'
 
   def __init__(self):
     """Initializes event data."""
-    super(ApacheAccessEventData, self).__init__(data_type=self.DATA_TYPE)
+    super(ApacheAccessLogEventData, self).__init__(data_type=self.DATA_TYPE)
     self.http_request = None
     self.http_request_referer = None
     self.http_request_user_agent = None
@@ -45,6 +45,7 @@ class ApacheAccessEventData(events.EventData):
     self.http_response_code = None
     self.ip_address = None
     self.port_number = None
+    self.recorded_time = None
     self.remote_name = None
     self.server_name = None
     self.user_name = None
@@ -83,18 +84,17 @@ class ApacheAccessLogTextPlugin(interface.TextPlugin):
 
   _THREE_LETTERS = pyparsing.Word(pyparsing.alphas, exact=3)
 
-  _TIME_ZONE_OFFSET = pyparsing.Combine(
-      pyparsing.oneOf(['-', '+']) +
-      pyparsing.Word(pyparsing.nums, exact=4)).setResultsName('time_offset')
+  _TIME_ZONE_OFFSET = (
+      pyparsing.Word('+-', exact=1) + _TWO_DIGITS + _TWO_DIGITS)
 
-  # Date format [18/Sep/2011:19:18:28 -0400]
+  # Date and time values are formatted as: [18/Sep/2011:19:18:28 -0400]
   _DATE_TIME = pyparsing.Group(
-      pyparsing.Suppress('[') + _TWO_DIGITS.setResultsName('day_of_month') +
-      pyparsing.Suppress('/') + _THREE_LETTERS.setResultsName('month') +
-      pyparsing.Suppress('/') + _FOUR_DIGITS.setResultsName('year') +
-      pyparsing.Suppress(':') + _TWO_DIGITS.setResultsName('hours') +
-      pyparsing.Suppress(':') + _TWO_DIGITS.setResultsName('minutes') +
-      pyparsing.Suppress(':') + _TWO_DIGITS.setResultsName('seconds') +
+      pyparsing.Suppress('[') + _TWO_DIGITS +
+      pyparsing.Suppress('/') + _THREE_LETTERS +
+      pyparsing.Suppress('/') + _FOUR_DIGITS +
+      pyparsing.Suppress(':') + _TWO_DIGITS +
+      pyparsing.Suppress(':') + _TWO_DIGITS +
+      pyparsing.Suppress(':') + _TWO_DIGITS +
       _TIME_ZONE_OFFSET + pyparsing.Suppress(']')).setResultsName('date_time')
 
   _HTTP_REQUEST = (
@@ -182,52 +182,55 @@ class ApacheAccessLogTextPlugin(interface.TextPlugin):
 
   _SUPPORTED_KEYS = frozenset([key for key, _ in _LINE_STRUCTURES])
 
-  def _GetDateTime(self, structure):
-    """Retrieves the date and time from a date and time values structure.
-
-    The date and time values in Apache access log files are formatted as:
-    "[18/Sep/2011:19:18:28 -0400]".
+  def _ParseLogLine(self, parser_mediator, key, structure):
+    """Parses a log line.
 
     Args:
-      structure (pyparsing.ParseResults): structure of tokens derived from a
-          line of a text file.
-
-    Returns:
-      dfdatetime.DateTimeValues: date and time.
-
-    Raises:
-      ValueError: if the structure cannot be converted into a date time string.
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfVFS.
+      key (str): name of the parsed structure.
+      structure (pyparsing.ParseResults): structure of tokens derived from
+          a line of a text file.
     """
-    year = self._GetValueFromStructure(structure, 'year')
-    month = self._GetValueFromStructure(structure, 'month')
-    day_of_month = self._GetValueFromStructure(structure, 'day_of_month')
-    hours = self._GetValueFromStructure(structure, 'hours')
-    minutes = self._GetValueFromStructure(structure, 'minutes')
-    seconds = self._GetValueFromStructure(structure, 'seconds')
-    time_offset = self._GetValueFromStructure(structure, 'time_offset')
+    time_elements_structure = self._GetValueFromStructure(
+        structure, 'date_time')
 
-    try:
-      month = self._MONTH_DICT.get(month.lower(), 0)
-    except AttributeError as exception:
-      raise ValueError('unable to parse month with error: {0!s}.'.format(
-          exception))
+    remote_name = self._GetValueFromStructure(structure, 'remote_name')
+    if remote_name == '-':
+      remote_name = None
 
-    try:
-      time_zone_offset = int(time_offset[1:3], 10) * 60
-      time_zone_offset += int(time_offset[3:5], 10)
-      if time_offset[0] == '-':
-        time_zone_offset *= -1
+    user_name = self._GetValueFromStructure(structure, 'user_name')
+    if user_name == '-':
+      user_name = None
 
-    except (TypeError, ValueError) as exception:
-      raise ValueError(
-          'unable to parse time zone offset with error: {0!s}.'.format(
-              exception))
+    event_data = ApacheAccessLogEventData()
+    event_data.http_request = self._GetValueFromStructure(
+        structure, 'http_request')
+    event_data.http_response_bytes = self._GetValueFromStructure(
+        structure, 'response_bytes')
+    event_data.http_response_code = self._GetValueFromStructure(
+        structure, 'response_code')
+    event_data.ip_address = self._GetValueFromStructure(structure, 'ip_address')
+    event_data.recorded_time = self._ParseTimeElements(time_elements_structure)
+    event_data.remote_name = remote_name
+    event_data.user_name = user_name
 
-    time_elements_tuple = (year, month, day_of_month, hours, minutes, seconds)
+    if key in ('combined_log_format', 'vhost_combined_log_format'):
+      referer = self._GetValueFromStructure(structure, 'referer')
+      if referer == '-':
+        referer = None
 
-    return dfdatetime_time_elements.TimeElements(
-        time_elements_tuple=time_elements_tuple,
-        time_zone_offset=time_zone_offset)
+      event_data.http_request_referer = referer
+      event_data.http_request_user_agent = self._GetValueFromStructure(
+          structure, 'user_agent')
+
+    if key == 'vhost_combined_log_format':
+      event_data.port_number = self._GetValueFromStructure(
+          structure, 'port_number')
+      event_data.server_name = self._GetValueFromStructure(
+          structure, 'server_name')
+
+    parser_mediator.ProduceEventData(event_data)
 
   def _ParseRecord(self, parser_mediator, key, structure):
     """Parses a pyparsing structure.
@@ -245,54 +248,48 @@ class ApacheAccessLogTextPlugin(interface.TextPlugin):
       raise errors.ParseError(
           'Unable to parse record, unknown structure: {0:s}'.format(key))
 
-    date_time_string = self._GetValueFromStructure(structure, 'date_time')
-
     try:
-      date_time = self._GetDateTime(date_time_string)
-    except ValueError as exception:
+      self._ParseLogLine(parser_mediator, key, structure)
+    except errors.ParseError as exception:
       parser_mediator.ProduceExtractionWarning(
-          'unable to parse date time value: {0!s} with error: {1!s}'.format(
-              date_time_string, exception))
-      return
+          'unable to parse log line with error: {0!s}'.format(exception))
 
-    remote_name = self._GetValueFromStructure(structure, 'remote_name')
-    if remote_name == '-':
-      remote_name = None
+  def _ParseTimeElements(self, time_elements_structure):
+    """Parses date and time elements of a log line.
 
-    user_name = self._GetValueFromStructure(structure, 'user_name')
-    if user_name == '-':
-      user_name = None
+    The date and time values in Apache access log files are formatted as:
+    "[18/Sep/2011:19:18:28 -0400]".
 
-    event = time_events.DateTimeValuesEvent(
-        date_time, definitions.TIME_DESCRIPTION_RECORDED)
+    Args:
+      time_elements_structure (pyparsing.ParseResults): date and time elements
+          of a log line.
 
-    event_data = ApacheAccessEventData()
-    event_data.ip_address = self._GetValueFromStructure(structure, 'ip_address')
-    event_data.remote_name = remote_name
-    event_data.user_name = user_name
-    event_data.http_request = self._GetValueFromStructure(
-        structure, 'http_request')
-    event_data.http_response_code = self._GetValueFromStructure(
-        structure, 'response_code')
-    event_data.http_response_bytes = self._GetValueFromStructure(
-        structure, 'response_bytes')
+    Returns:
+      dfdatetime.TimeElements: date and time value.
 
-    if key in ('combined_log_format', 'vhost_combined_log_format'):
-      referer = self._GetValueFromStructure(structure, 'referer')
-      if referer == '-':
-        referer = None
+    Raises:
+      ParseError: if a valid date and time value cannot be derived from
+          the time elements.
+    """
+    try:
+      (day_of_month, month_string, year, hours, minutes, seconds,
+       time_zone_sign, time_zone_hours, time_zone_minutes) = (
+          time_elements_structure)
 
-      event_data.http_request_referer = referer
-      event_data.http_request_user_agent = self._GetValueFromStructure(
-          structure, 'user_agent')
+      month = self._MONTH_DICT.get(month_string.lower(), 0)
 
-    if key == 'vhost_combined_log_format':
-      event_data.server_name = self._GetValueFromStructure(
-          structure, 'server_name')
-      event_data.port_number = self._GetValueFromStructure(
-          structure, 'port_number')
+      time_zone_offset = (time_zone_hours * 60) + time_zone_minutes
+      if time_zone_sign == '-':
+        time_zone_offset *= -1
 
-    parser_mediator.ProduceEventWithEventData(event, event_data)
+      time_elements_tuple = (year, month, day_of_month, hours, minutes, seconds)
+      return dfdatetime_time_elements.TimeElements(
+          time_elements_tuple=time_elements_tuple,
+          time_zone_offset=time_zone_offset)
+
+    except (TypeError, ValueError) as exception:
+      raise errors.ParseError(
+          'Unable to parse time elements with error: {0!s}'.format(exception))
 
   def CheckRequiredFormat(self, parser_mediator, text_file_object):
     """Check if the log record has the minimal structure required by the plugin.
@@ -314,11 +311,12 @@ class ApacheAccessLogTextPlugin(interface.TextPlugin):
     if not parsed_structure:
       return False
 
-    date_time_string = self._GetValueFromStructure(
+    time_elements_structure = self._GetValueFromStructure(
         parsed_structure, 'date_time')
+
     try:
-      self._GetDateTime(date_time_string)
-    except ValueError:
+      self._ParseTimeElements(time_elements_structure)
+    except errors.ParseError:
       return False
 
     return True
--- tests/parsers/text_plugins/apache_access.py
+++ tests/parsers/text_plugins/apache_access.py
@@ -19,6 +19,10 @@ class ApacheAccessLogTextPluginTest(test_lib.TextPluginTestCase):
     storage_writer = self._ParseTextFileWithPlugin(
         ['apache_access.log'], plugin)
 
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 14)
+
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 14)
 
@@ -30,15 +34,10 @@ class ApacheAccessLogTextPluginTest(test_lib.TextPluginTestCase):
         'recovery_warning')
     self.assertEqual(number_of_warnings, 0)
 
-    # The order in which the text parser plugin generates events is
-    # nondeterministic hence we sort the events.
-    events = list(storage_writer.GetSortedEvents())
-
     # Test combined log format event.
     # 13/Jan/2016:19:31:20 +0200
     expected_event_values = {
-        'data_type': 'apache:access',
-        'date_time': '2016-01-13T19:31:20+02:00',
+        'data_type': 'apache:access_log:entry',
         'http_request': (
             'GET /wp-content/themes/darkmode/evil.php?cmd=uname+-a HTTP/1.1'),
         'http_request_referer': 'http://localhost/',
@@ -48,25 +47,61 @@ class ApacheAccessLogTextPluginTest(test_lib.TextPluginTestCase):
         'http_response_code': 200,
         'http_response_bytes': 694,
         'ip_address': '192.168.0.2',
+        'recorded_time': '2016-01-13T19:31:20+02:00',
         'remote_name': None,
-        'timestamp': '2016-01-13 17:31:20.000000',
         'user_name': None}
 
-    self.CheckEventValues(storage_writer, events[2], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 4)
+    self.CheckEventData(event_data, expected_event_values)
 
     # Test common log format parser event.
     expected_event_values = {
-        'data_type': 'apache:access',
-        'date_time': '2016-01-13T19:31:16+00:00',
+        'data_type': 'apache:access_log:entry',
         'http_request': (
             'GET /wp-content/themes/darkmode/header.php?install2 HTTP/1.1'),
         'http_response_code': 200,
         'http_response_bytes': 494,
         'ip_address': '10.0.0.1',
+        'recorded_time': '2016-01-13T19:31:16+00:00',
         'remote_name': None,
         'user_name': None}
 
-    self.CheckEventValues(storage_writer, events[3], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
+    self.CheckEventData(event_data, expected_event_values)
+
+    # Test vhost_combined log format event.
+    expected_event_values = {
+        'data_type': 'apache:access_log:entry',
+        'http_request': 'GET /wp-content/themes/darkmode/evil.php HTTP/1.1',
+        'http_request_referer': None,
+        'http_request_user_agent': (
+            'Mozilla/5.0 (Windows NT 7.1) AppleWebKit/534.30 (KHTML, like '
+            'Gecko) Chrome/12.0.742.112 Safari/534.30'),
+        'http_response_code': 200,
+        'http_response_bytes': 1063,
+        'ip_address': '192.168.0.2',
+        'port_number': 443,
+        'recorded_time': '2018-01-13T19:31:17+00:00',
+        'remote_name': None,
+        'server_name': 'plaso.log2timeline.net',
+        'user_name': None}
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 8)
+    self.CheckEventData(event_data, expected_event_values)
+
+    # Test common log format parser event with Kerberos user name
+    expected_event_values = {
+        'data_type': 'apache:access_log:entry',
+        'http_request': 'GET / HTTP/1.1',
+        'http_response_code': 200,
+        'http_response_bytes': 8264,
+        'ip_address': '192.168.0.64',
+        'recorded_time': '2019-11-16T09:46:42+02:00',
+        'remote_name': None,
+        'user_name': 'pyllyukko@EXAMPLE.COM'}
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 13)
+    self.CheckEventData(event_data, expected_event_values)
 
     # Test an extraction warning.
     generator = storage_writer.GetAttributeContainers(
@@ -80,39 +115,6 @@ class ApacheAccessLogTextPluginTest(test_lib.TextPluginTestCase):
         '+0000] "GET /scripts/grok-py-test/co..." at offset: 1589')
     self.assertEqual(test_warning.message, expected_message)
     self.assertEqual(test_warning.parser_chain, 'text/apache_access')
-
-    # Test vhost_combined log format event.
-    expected_event_values = {
-        'data_type': 'apache:access',
-        'date_time': '2018-01-13T19:31:17+00:00',
-        'http_request': 'GET /wp-content/themes/darkmode/evil.php HTTP/1.1',
-        'http_request_referer': None,
-        'http_request_user_agent': (
-            'Mozilla/5.0 (Windows NT 7.1) AppleWebKit/534.30 (KHTML, like '
-            'Gecko) Chrome/12.0.742.112 Safari/534.30'),
-        'http_response_code': 200,
-        'http_response_bytes': 1063,
-        'ip_address': '192.168.0.2',
-        'port_number': 443,
-        'remote_name': None,
-        'server_name': 'plaso.log2timeline.net',
-        'user_name': None}
-
-    self.CheckEventValues(storage_writer, events[9], expected_event_values)
-
-    # Test common log format parser event with Kerberos user name
-    expected_event_values = {
-        'data_type': 'apache:access',
-        'date_time': '2019-11-16T09:46:42+02:00',
-        'http_request': 'GET / HTTP/1.1',
-        'http_response_code': 200,
-        'http_response_bytes': 8264,
-        'ip_address': '192.168.0.64',
-        'remote_name': None,
-        'timestamp': '2019-11-16 07:46:42.000000',
-        'user_name': 'pyllyukko@EXAMPLE.COM'}
-
-    self.CheckEventValues(storage_writer, events[11], expected_event_values)
 
 
 if __name__ == '__main__':
