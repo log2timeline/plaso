85167bbf08cff97617b33db0c5fd7b850137389e
Joachim Metz <joachim.metz@gmail.com>
2022-11-01 21:24:08 +0100

    Changes to split event_data and event generation (#4348)

--- tests/parsers/text_plugins/selinux.py
+++ tests/parsers/text_plugins/selinux.py
@@ -18,6 +18,10 @@ class SELinuxTextPluginTest(test_lib.TextPluginTestCase):
     storage_writer = self._ParseTextFileWithPlugin(
         ['selinux.log'], plugin, knowledge_base_values={'year': 2013})
 
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 7)
+
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 7)
 
@@ -29,10 +33,6 @@ class SELinuxTextPluginTest(test_lib.TextPluginTestCase):
         'recovery_warning')
     self.assertEqual(number_of_warnings, 0)
 
-    # The order in which the text parser plugin generates events is
-    # nondeterministic hence we sort the events.
-    events = list(storage_writer.GetSortedEvents())
-
     # Test case: normal entry.
     expected_event_values = {
         'audit_type': 'LOGIN',
@@ -40,27 +40,30 @@ class SELinuxTextPluginTest(test_lib.TextPluginTestCase):
             'pid=25443 uid=0 old auid=4294967295 new auid=0 old ses=4294967295 '
             'new ses=1165'),
         'data_type': 'selinux:line',
-        'date_time': '2012-05-24T07:40:01.174000+00:00',
+        'last_written_time': '2012-05-24T07:40:01.174+00:00',
         'pid': '25443'}
 
-    self.CheckEventValues(storage_writer, events[3], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
+    self.CheckEventData(event_data, expected_event_values)
 
     # Test case: short date.
     expected_event_values = {
         'audit_type': 'SHORTDATE',
         'body': 'check rounding',
         'data_type': 'selinux:line',
-        'date_time': '2012-05-24T07:40:01.000000+00:00'}
+        'last_written_time': '2012-05-24T07:40:01.000+00:00'}
 
-    self.CheckEventValues(storage_writer, events[2], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 1)
+    self.CheckEventData(event_data, expected_event_values)
 
     # Test case: no message.
     expected_event_values = {
         'audit_type': 'NOMSG',
         'data_type': 'selinux:line',
-        'date_time': '2012-05-24T07:40:22.174000+00:00'}
+        'last_written_time': '2012-05-24T07:40:22.174+00:00'}
 
-    self.CheckEventValues(storage_writer, events[4], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 2)
+    self.CheckEventData(event_data, expected_event_values)
 
     # Test case: under score.
     expected_event_values = {
@@ -69,10 +72,11 @@ class SELinuxTextPluginTest(test_lib.TextPluginTestCase):
             'pid=25444 uid=0 old auid=4294967295 new auid=54321 old '
             'ses=4294967295 new ses=1166'),
         'data_type': 'selinux:line',
-        'date_time': '2012-05-24T07:47:46.174000+00:00',
+        'last_written_time': '2012-05-24T07:47:46.174+00:00',
         'pid': '25444'}
 
-    self.CheckEventValues(storage_writer, events[5], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 3)
+    self.CheckEventData(event_data, expected_event_values)
 
 
 if __name__ == '__main__':
