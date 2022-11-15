c661eaf55e06f357675a7e68680b817e8d3a637f
Joachim Metz <joachim.metz@gmail.com>
2022-11-15 06:57:25 +0100

    Changes to split event_data and event generation

--- tests/parsers/safari_cookies.py
+++ tests/parsers/safari_cookies.py
@@ -4,7 +4,6 @@
 
 import unittest
 
-from plaso.lib import definitions
 from plaso.parsers import safari_cookies
 
 from tests.parsers import test_lib
@@ -23,6 +22,10 @@ class SafariCookieParserTest(test_lib.ParserTestCase):
     # * 182 events from the safari cookie parser
     # * 25 event from the cookie plugins
 
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 106)
+
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 207)
 
@@ -34,34 +37,6 @@ class SafariCookieParserTest(test_lib.ParserTestCase):
         'recovery_warning')
     self.assertEqual(number_of_warnings, 0)
 
-    events = []
-    for event in storage_writer.GetEvents():
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      if event_data.data_type == 'safari:cookie:entry':
-        events.append(event)
-
-    self.assertEqual(len(events), 182)
-
-    expected_event_values = {
-        'cookie_name': 'GAPS',
-        'data_type': 'safari:cookie:entry',
-        'date_time': '2015-07-08T20:54:03.000000+00:00',
-        'flags': 5,
-        'url': 'accounts.google.com'}
-
-    self.CheckEventValues(storage_writer, events[3], expected_event_values)
-
-    expected_event_values = {
-        'cookie_name': 'nonsession',
-        'data_type': 'safari:cookie:entry',
-        'date_time': '2013-07-08T20:54:50.000000+00:00',
-        'flags': 0,
-        'path': '/',
-        'timestamp_desc': definitions.TIME_DESCRIPTION_CREATION,
-        'url': '.ebay.com'}
-
-    self.CheckEventValues(storage_writer, events[48], expected_event_values)
-
     expected_event_values = {
         'cookie_name': 'fpc',
         'cookie_value': (
@@ -70,11 +45,13 @@ class SafariCookieParserTest(test_lib.ParserTestCase):
             '6JgkFwqO5uHrv4HiL05qb.85Bl.V__HZI5wpAGOGPz1XHhY5mOMH.g.pkVDLli36W'
             '2iuYwA-&v=2'),
         'data_type': 'safari:cookie:entry',
-        'date_time': '2013-07-08T17:24:30.000000+00:00',
+        'expiration_time': '2014-07-08T05:24:29.000000+00:00',
+        'creation_time': '2013-07-08T17:24:30.000000+00:00',
         'path': '/',
         'url': '.www.yahoo.com'}
 
-    self.CheckEventValues(storage_writer, events[52], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 26)
+    self.CheckEventData(event_data, expected_event_values)
 
 
 if __name__ == '__main__':
