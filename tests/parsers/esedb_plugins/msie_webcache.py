ab5224260a5114afc168475f7023cc8f54482d0f
Joachim Metz <joachim.metz@gmail.com>
2022-11-19 08:53:12 +0100

    Changes to split event_data and event generation

--- tests/parsers/esedb_plugins/msie_webcache.py
+++ tests/parsers/esedb_plugins/msie_webcache.py
@@ -4,7 +4,6 @@
 
 import unittest
 
-from plaso.lib import definitions
 from plaso.parsers.esedb_plugins import msie_webcache
 
 from tests.parsers.esedb_plugins import test_lib
@@ -39,6 +38,10 @@ class MsieWebCacheESEDBPluginTest(test_lib.ESEDBPluginTestCase):
     plugin = msie_webcache.MsieWebCacheESEDBPlugin()
     storage_writer = self._ParseESEDBFileWithPlugin(['WebCacheV01.dat'], plugin)
 
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 341)
+
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 1372)
 
@@ -50,28 +53,28 @@ class MsieWebCacheESEDBPluginTest(test_lib.ESEDBPluginTestCase):
         'recovery_warning')
     self.assertEqual(number_of_warnings, 0)
 
-    # The order in which ESEDBPlugin._GetRecordValues() generates events is
-    # nondeterministic hence we sort the events.
-    events = list(storage_writer.GetSortedEvents())
-
     expected_event_values = {
+        'access_time': '2014-05-12T07:30:25.4861987+00:00',
         'container_identifier': 1,
         'data_type': 'msie:webcache:containers',
-        'date_time': '2014-05-12T07:30:25.4861987+00:00',
         'directory': (
             'C:\\Users\\test\\AppData\\Local\\Microsoft\\Windows\\'
             'INetCache\\IE\\'),
         'name': 'Content',
-        'set_identifier': 0,
-        'timestamp_desc': definitions.TIME_DESCRIPTION_LAST_ACCESS}
+        'set_identifier': 0}
 
-    self.CheckEventValues(storage_writer, events[573], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
+    self.CheckEventData(event_data, expected_event_values)
 
   def testProcessOnDatabaseWithPartitionsExTable(self):
     """Tests the Process function on database with a PartitionsEx table."""
     plugin = msie_webcache.MsieWebCacheESEDBPlugin()
     storage_writer = self._ParseESEDBFileWithPlugin(
         ['PartitionsEx-WebCacheV01.dat'], plugin)
+
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 978)
 
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 4200)
@@ -84,10 +87,6 @@ class MsieWebCacheESEDBPluginTest(test_lib.ESEDBPluginTestCase):
         'recovery_warning')
     self.assertEqual(number_of_warnings, 0)
 
-    # The order in which ESEDBPlugin._GetRecordValues() generates events is
-    # nondeterministic hence we sort the events.
-    events = list(storage_writer.GetSortedEvents())
-
     expected_event_values = {
         'access_count': 5,
         'cache_identifier': 0,
@@ -95,18 +94,18 @@ class MsieWebCacheESEDBPluginTest(test_lib.ESEDBPluginTestCase):
         'cached_filename': 'b83d57c0[1].svg',
         'container_identifier': 14,
         'data_type': 'msie:webcache:container',
-        'date_time': '2019-03-20T17:22:14.0000000+00:00',
         'entry_identifier': 63,
-        'sync_count': 0,
+        'modification_time': '2019-03-20T17:22:14.0000000+00:00',
         'response_headers': (
             '[HTTP/1.1 200; content-length: 726; content-type: image/svg+xml; '
             'x-cache: TCP_HIT; x-msedge-ref: Ref A: 3CD5FCBC8EAD4E0A80FA41A62'
             'FBC8CCC Ref B: PRAEDGE0910 Ref C: 2019-12-16T20:55:28Z; date: '
             'Mon, 16 Dec 2019 20:55:28 GMT]'),
-        'timestamp_desc': definitions.TIME_DESCRIPTION_MODIFICATION,
+        'synchronization_count': 0,
         'url': 'https://www.bing.com/rs/3R/kD/ic/878ca0cd/b83d57c0.svg'}
 
-    self.CheckEventValues(storage_writer, events[100], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 199)
+    self.CheckEventData(event_data, expected_event_values)
 
 
 if __name__ == '__main__':
