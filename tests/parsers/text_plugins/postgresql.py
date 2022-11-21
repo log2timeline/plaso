5a066b61934948dcb6800c52d176ccda53639f48
Joachim Metz <joachim.metz@gmail.com>
2022-11-21 06:07:14 +0100

    Migrated PostgreSQL application log text parser to text plugin

--- tests/parsers/text_plugins/postgresql.py
+++ tests/parsers/text_plugins/postgresql.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Tests for PostgreSQL log parser"""
+"""Tests for the PostgreSQL application log file text parser plugin."""
 
 import unittest
 
-from plaso.parsers import postgresql
-from tests.parsers import test_lib
+from plaso.parsers.text_plugins import postgresql
+
+from tests.parsers.text_plugins import test_lib
 
 
-class PostgreSQLParserTest(test_lib.ParserTestCase):
-  """Tests for the PostgreSQL log parser"""
+class PostgreSQLTextPluginTest(test_lib.TextPluginTestCase):
+  """the PostgreSQL application log file text parser plugin."""
 
-  def testParse(self):
-    """Tests the parse function on an example file."""
-    parser = postgresql.PostgreSQLParser()
-    storage_writer = self._ParseFile(['postgresql.log'], parser)
+  def testProcess(self):
+    """Tests the Process function."""
+    plugin = postgresql.PostgreSQLTextPlugin()
+    storage_writer = self._ParseTextFileWithPlugin(['postgresql.log'], plugin)
 
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 20)
@@ -33,11 +34,11 @@ class PostgreSQLParserTest(test_lib.ParserTestCase):
     expected_event_values = {
         'data_type': 'postgresql:application_log:entry',
         'date_time': '2022-04-12T00:16:05.526+00:00',
-        'log_level': 'LOG',
         'log_line': (
             'starting PostgreSQL 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1) '
             'on x86_64-pc-linux-gnu, compiled by gcc '
             '(Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0, 64-bit'),
+        'severity': 'LOG',
         'pid': '7755'}
 
     self.CheckEventValues(storage_writer, events[0], expected_event_values)
@@ -46,9 +47,9 @@ class PostgreSQLParserTest(test_lib.ParserTestCase):
     expected_event_values = {
         'data_type': 'postgresql:application_log:entry',
         'date_time': '2022-04-12T00:24:24.741+00:00',
-        'log_level': 'FATAL',
         'log_line': 'password authentication failed for user "postgres"',
         'pid': '9158',
+        'severity': 'FATAL',
         'user': 'postgres@postgres'}
 
     self.CheckEventValues(storage_writer, events[15], expected_event_values)
@@ -57,13 +58,13 @@ class PostgreSQLParserTest(test_lib.ParserTestCase):
     expected_event_values = {
         'data_type': 'postgresql:application_log:entry',
         'date_time': '2022-04-12T00:24:24.741+00:00',
-        'log_level': 'DETAIL',
         'log_line': (
             'User "postgres" has no password assigned.\n        '
             'Connection matched pg_hba.conf line 96: '
             '"host    all             all             '
             '127.0.0.1/32            md5"'),
-        'pid': '9158'}
+        'pid': '9158',
+        'severity': 'DETAIL'}
 
     self.CheckEventValues(storage_writer, events[16], expected_event_values)
 
@@ -71,10 +72,10 @@ class PostgreSQLParserTest(test_lib.ParserTestCase):
     expected_event_values = {
         'data_type': 'postgresql:application_log:entry',
         'date_time': '2022-07-15T23:04:27',
-        'log_level': 'LOG',
         'log_line': (
             'could not receive data from client: Connection reset by peer'),
         'pid': '203851-1',
+        'severity': 'LOG',
         'timestamp': '2022-07-15 13:04:27.000000'}
 
     self.CheckEventValues(storage_writer, events[19], expected_event_values)
