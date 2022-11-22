7fa391d59546861097c3d7c7e96256a09bd2865f
Joachim Metz <joachim.metz@gmail.com>
2022-11-22 21:08:26 +0100

    Changes to split event_data and event generation

--- plaso/parsers/jsonl_plugins/aws_cloudtrail_log.py
+++ plaso/parsers/jsonl_plugins/aws_cloudtrail_log.py
@@ -5,12 +5,9 @@ import json
 
 from json import decoder as json_decoder
 
-from dfdatetime import semantic_time as dfdatetime_semantic_time
 from dfdatetime import time_elements as dfdatetime_time_elements
 
 from plaso.containers import events
-from plaso.containers import time_events
-from plaso.lib import definitions
 from plaso.parsers import jsonl_parser
 from plaso.parsers.jsonl_plugins import interface
 
@@ -24,6 +21,8 @@ class AWSCloudTrailEventData(events.EventData):
     cloud_trail_event (str): CloudTrail event.
     event_name (str): event name.
     event_source (str): AWS service.
+    recorded_time (dfdatetime.DateTimeValues): date and time the log entry
+        was recorded.
     resources (str): resources.
     source_ip (str): source IP address.
     user_identity_arn (str): AWS ARN of the user.
@@ -40,6 +39,7 @@ class AWSCloudTrailEventData(events.EventData):
     self.cloud_trail_event = None
     self.event_name = None
     self.event_source = None
+    self.recorded_time = None
     self.resources = None
     self.source_ip = None
     self.user_identity_arn = None
@@ -60,57 +60,57 @@ class AWSCloudTrailLogJSONLPlugin(interface.JSONLPlugin):
           and other components, such as storage and dfVFS.
       json_dict (dict): JSON dictionary of the log record.
     """
+    date_time = None
+
     event_time = self._GetJSONValue(json_dict, 'EventTime')
     if not event_time:
       parser_mediator.ProduceExtractionWarning(
           'Event time value missing from CloudTrail log entry')
 
+    else:
+      try:
+        date_time = dfdatetime_time_elements.TimeElementsInMicroseconds()
+        date_time.CopyFromDateTimeString(event_time)
+      except ValueError as exception:
+        parser_mediator.ProduceExtractionWarning(
+            'Unable to parse event time: {0:s} with error: {1!s}'.format(
+               event_time, exception))
+        date_time = None
+
+    resource_list = self._GetJSONValue(
+        json_dict, 'Resources', default_value=[])
+    # Flatten multiple resources into a string of resource names.
+    resources = ', '.join(
+        [resource['ResourceName'] for resource in resource_list])
+
+    cloud_trail_event = self._GetJSONValue(
+        json_dict, 'CloudTrailEvent', default_value='{}')
+    try:
+      cloud_trail_event_json = json.loads(cloud_trail_event)
+    except json_decoder.JSONDecodeError as exception:
+      parser_mediator.ProduceExtractionWarning(
+          'Unable to decode CloudTrail event with error: {0!s}'.format(
+              exception))
+      cloud_trail_event_json = {}
+
+    user_identity_json = self._GetJSONValue(
+        cloud_trail_event_json, 'userIdentity')
+
     event_data = AWSCloudTrailEventData()
-    event_data.event_name = self._GetJSONValue(json_dict, 'EventName')
     event_data.access_key = self._GetJSONValue(json_dict, 'AccessKeyId')
+    event_data.account_identifier = self._GetJSONValue(
+        cloud_trail_event_json, 'accountId')
+    event_data.cloud_trail_event = cloud_trail_event
+    event_data.event_name = self._GetJSONValue(json_dict, 'EventName')
     event_data.event_source = self._GetJSONValue(json_dict, 'EventSource')
+    event_data.recorded_time = date_time
+    event_data.resources = resources or None
+    event_data.source_ip = self._GetJSONValue(
+        cloud_trail_event_json, 'sourceIPAddress')
+    event_data.user_identity_arn = self._GetJSONValue(user_identity_json, 'arn')
     event_data.user_name = self._GetJSONValue(json_dict, 'Username')
 
-    resource_list = json_dict.get('Resources')
-    # Flatten multiple resources into a string of resource names.
-    if resource_list:
-      event_data.resources = ', '.join(
-          [resource['ResourceName'] for resource in resource_list])
-
-    event_data.cloud_trail_event = json_dict.get('CloudTrailEvent')
-    if event_data.cloud_trail_event:
-      try:
-        cloud_trail_event_json = json.loads(event_data.cloud_trail_event)
-      except json_decoder.JSONDecodeError as exception:
-        cloud_trail_event_json = None
-        parser_mediator.ProduceExtractionWarning(
-            'Unable to decode CloudTrail event with error: {0!s}'.format(
-                exception))
-
-      if cloud_trail_event_json:
-        event_data.source_ip = self._GetJSONValue(
-            cloud_trail_event_json, 'sourceIPAddress')
-        user_identify_json = self._GetJSONValue(
-            cloud_trail_event_json, 'userIdentity')
-
-        if user_identify_json:
-          event_data.user_identity_arn = self._GetJSONValue(
-              user_identify_json, 'arn')
-          event_data.account_identifier = self._GetJSONValue(
-              cloud_trail_event_json, 'accountId')
-
-    try:
-      date_time = dfdatetime_time_elements.TimeElementsInMicroseconds()
-      date_time.CopyFromDateTimeString(event_time)
-    except ValueError as exception:
-      parser_mediator.ProduceExtractionWarning(
-          'Unable to parse event time: {0:s} with error: {1!s}'.format(
-             event_time, exception))
-      date_time = dfdatetime_semantic_time.InvalidTime()
-
-    event = time_events.DateTimeValuesEvent(
-        date_time, definitions.TIME_DESCRIPTION_RECORDED)
-    parser_mediator.ProduceEventWithEventData(event, event_data)
+    parser_mediator.ProduceEventData(event_data)
 
   def CheckRequiredFormat(self, json_dict):
     """Check if the log record has the minimal structure required by the plugin.
@@ -121,14 +121,15 @@ class AWSCloudTrailLogJSONLPlugin(interface.JSONLPlugin):
     Returns:
       bool: True if this is the correct parser, False otherwise.
     """
-    cloud_trail_event = json_dict.get('CloudTrailEvent') or None
-    event_time = json_dict.get('EventTime') or None
+    cloud_trail_event = self._GetJSONValue(json_dict, 'CloudTrailEvent')
+    event_time = self._GetJSONValue(json_dict, 'EventTime')
 
     if None in (cloud_trail_event, event_time):
       return False
 
+    date_time = dfdatetime_time_elements.TimeElementsInMicroseconds()
+
     try:
-      date_time = dfdatetime_time_elements.TimeElementsInMicroseconds()
       date_time.CopyFromDateTimeString(event_time)
     except ValueError:
       return False
