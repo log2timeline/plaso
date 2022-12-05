7e658b7eadaa5b60c006972cf4a93872703c0ddd
Joachim Metz <joachim.metz@gmail.com>
2022-12-06 00:12:47 +0100

    Changes to split event_data and event generation (#4460)

--- tests/parsers/sqlite_plugins/firefox_cookies.py
+++ tests/parsers/sqlite_plugins/firefox_cookies.py
@@ -4,7 +4,6 @@
 
 import unittest
 
-from plaso.lib import definitions
 from plaso.parsers.sqlite_plugins import firefox_cookies
 
 from tests.parsers.sqlite_plugins import test_lib
@@ -19,6 +18,10 @@ class FirefoxCookiesPluginTest(test_lib.SQLitePluginTestCase):
     storage_writer = self._ParseDatabaseFileWithPlugin(
         ['firefox_cookies.sqlite'], plugin)
 
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 105)
+
     number_of_events = storage_writer.GetNumberOfAttributeContainers('event')
     self.assertEqual(number_of_events, 295)
 
@@ -30,59 +33,19 @@ class FirefoxCookiesPluginTest(test_lib.SQLitePluginTestCase):
         'recovery_warning')
     self.assertEqual(number_of_warnings, 0)
 
-    # sqlite> SELECT COUNT(id) FROM moz_cookies;
-    # 90
-    # Thus the cookie database contains 93 entries:
-    #   90 Last Access Time
-    #   90 Cookie Expires
-    #   90 Creation Time
-    #
-    # And then in addition the following entries are added due to cookie
-    # plugins (TODO filter these out since adding new cookie plugin will
-    # change this number and thus affect this test):
-    #  15 Last Visited Time
-    #   5 Analytics Previous Time
-    #   5 Analytics Creation Time
-    #
-    # In total: 93 * 3 + 15 + 5 + 5 = 304 events.
-
-    events = []
-    extra_objects = []
-    for event in storage_writer.GetEvents():
-      event_data = self._GetEventDataOfEvent(storage_writer, event)
-      if event_data.data_type == 'firefox:cookie:entry':
-        events.append(event)
-      else:
-        extra_objects.append(event)
-
-    self.assertEqual(len(events), 90 * 3)
-    self.assertGreaterEqual(len(extra_objects), 25)
-
-    # Check one greenqloud.com event
     expected_event_values = {
+        'access_time': '2013-10-30T21:56:03.997686+00:00',
         'cookie_name': '__utma',
+        'creation_time': '2013-10-30T21:56:03.992499+00:00',
         'data_type': 'firefox:cookie:entry',
-        'date_time': '2015-10-30T21:56:03+00:00',
+        'expiration_time': '2015-10-30T21:56:03+00:00',
         'host': 's.greenqloud.com',
         'httponly': False,
         'secure': False,
-        'timestamp_desc': definitions.TIME_DESCRIPTION_EXPIRATION,
         'url': 'http://s.greenqloud.com/'}
 
-    self.CheckEventValues(storage_writer, events[32], expected_event_values)
-
-    # Check one of the visits to pubmatic.com.
-    expected_event_values = {
-        'cookie_name': 'KRTBCOOKIE_391',
-        'data_type': 'firefox:cookie:entry',
-        'date_time': '2013-11-29T21:56:04+00:00',
-        'httponly': False,
-        'path': '/',
-        'secure': False,
-        'timestamp_desc': definitions.TIME_DESCRIPTION_EXPIRATION,
-        'url': 'http://pubmatic.com/'}
-
-    self.CheckEventValues(storage_writer, events[62], expected_event_values)
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 13)
+    self.CheckEventData(event_data, expected_event_values)
 
 
 if __name__ == '__main__':
