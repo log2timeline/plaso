66e2203a177be94d95026e64e138b8d77bf10b36
Joachim Metz <joachim.metz@gmail.com>
2022-12-10 10:14:47 +0100

    Changes to split event_data and event generation

--- config/appveyor/runtests.sh
+++ config/appveyor/runtests.sh
@@ -2,8 +2,8 @@
 # Script to run tests
 
 # Set the following environment variables to build libyal with gettext.
-export CPPFLAGS="-I/usr/local/include -I/usr/local/Cellar/gettext/0.21.1/include ${CPPFLAGS}";
-export LDFLAGS="-L/usr/local/lib -L/usr/local/Cellar/gettext/0.21.1/lib ${LDFLAGS}";
+export CPPFLAGS="-I/usr/local/include -I/usr/local/opt/gettext/include ${CPPFLAGS}";
+export LDFLAGS="-L/usr/local/lib -L/usr/local/opt/gettext/lib ${LDFLAGS}";
 
 # Set the following environment variables to build pycrypto and yara-python.
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include ${CPPFLAGS}";
