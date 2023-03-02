f85bd29fe59a7b4d6a43519cc5378d0dc4c020e9
Romain Gayon <romaing@google.com>
2023-03-02 18:12:28 +0100

    add parser for SIMATIC S7 logs

--- plaso/parsers/wincc.py
+++ plaso/parsers/wincc.py
@@ -11,6 +11,19 @@ from plaso.parsers import interface
 from plaso.parsers import manager
 
 
+class SIMATICS7EventData(events.EventData):
+  """SIMATIC S7 event data."""
+
+  DATA_TYPE = 'wincc:simatic_s7:entry'
+
+  def __init__(self):
+    """Initializes event data."""
+    super(SIMATICS7EventData, self).__init__(data_type=self.DATA_TYPE)
+
+    self.creation_time = None
+    self.message = None
+
+
 class WinCCSysLogEventData(events.EventData):
   """WinCC Sys Log event data."""
 
@@ -28,6 +41,114 @@ class WinCCSysLogEventData(events.EventData):
     self.hostname = None
     self.source = None
     self.message = None
+
+
+class SIMATICLogParser(interface.FileObjectParser):
+  """Text parser plugin for SIMATIC S7 Log files."""
+
+  NAME = 'simatic_s7'
+  DATA_FORMAT = 'SIMATIC S7 Log file'
+
+  DELIMITER = ','
+  ENCODING = 'ascii'
+  END_OF_LINE = '\r\n'
+
+  def _ParseValues(self, parser_mediator, line_number, values):
+    """Parses SIMATIC S7 log file values.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfVFS.
+      line_number (int): number of the line the values were extracted from.
+      values (list[str]): values extracted from the line.
+
+    Raises:
+      WrongParser: when the values cannot be parsed.
+    """
+
+    nb_of_values = len(values)
+    if nb_of_values < 2:
+      error_string = 'Expected at least two values on line {0:d}'.format(
+          line_number)
+      raise errors.WrongParser(error_string)
+
+    # The first lines seem to  always follow the following format:
+    #
+    # 2019-05-27 10:05:43,405 INFO     Log starting ...
+    # 2019-05-27 10:05:43,405 INFO     | LogFileName   : C:\.....
+    # 2019-05-27 10:05:43,419 INFO     | LogFileCount  : 3
+
+    if line_number == 0:
+      expected_str = 'Log starting ...'+self.END_OF_LINE
+      if not values[1].endswith(expected_str):
+        error_string = 'Expected first line to end with "{0:s}"."{1!s}"'.format(
+            expected_str, values[1])
+        raise errors.WrongParser(error_string)
+
+    if line_number == 1:
+      expected_str = '| LogFileName'
+      if values[1].find(expected_str) < 0:
+        error_string = 'Expected second line to contain "{0:s}"'.format(
+            expected_str)
+        raise errors.WrongParser(error_string)
+
+    if line_number == 2:
+      expected_str = '| LogFileCount'
+      if values[1].find(expected_str) < 0:
+        error_string = 'Expected third line to contain {0:s}'.format(
+            expected_str)
+        raise errors.WrongParser(error_string)
+
+    event_data = SIMATICS7EventData()
+    try:
+      date_time = dfdatetime_time_elements.TimeElements()
+      date_time.CopyFromDateTimeString(values[0])
+      event_data.creation_time = date_time
+    except (TypeError, ValueError) as exception:
+      error_string = (
+          'Unable to parse date time value with error: '
+          '{0!s} on line {1:d} {2!s}').format(exception, line_number, values)
+      parser_mediator.ProduceExtractionWarning(error_string)
+
+    event_data.message = ','.join(values[1:]).strip()
+
+    parser_mediator.ProduceEventData(event_data)
+
+  def ParseFileObject(self, parser_mediator, file_object):
+    """Parses a bodyfile file-like object.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfVFS.
+      file_object (dfvfs.FileIO): file-like object.
+
+    Raises:
+      WrongParser: when the file cannot be parsed.
+    """
+    line_reader = text_file.TextFile(
+        file_object, encoding=self.ENCODING, end_of_line=self.END_OF_LINE)
+
+    line_number = 0
+
+    try:
+      line = line_reader.readline()
+    except UnicodeDecodeError as exception:
+      raise errors.WrongParser(
+          'unable to read line: {0:d} with error: {1!s}'.format(
+              line_number, exception))
+
+    while line:
+      values = line.split(self.DELIMITER)
+      self._ParseValues(parser_mediator, line_number, values)
+
+      try:
+        line_number += 1
+        line = line_reader.readline()
+      except UnicodeDecodeError as exception:
+        parser_mediator.ProduceExtractionWarning(
+            'unable to read line: {0:d} with error: {1!s}'.format(
+                line_number, exception))
+        break
 
 
 class WinCCSysLogParser(interface.FileObjectParser):
@@ -54,13 +175,13 @@ class WinCCSysLogParser(interface.FileObjectParser):
       WrongParser: when the values cannot be parsed.
     """
 
-    # id, date, time, integer, type, empty?, hostname, source, (message,
-    # message),  something
-
     nb_of_values = len(values)
     if len(values) < 10:
       error_string = 'invalid number of values : {0:d} in line: {1:d}'.format(
           nb_of_values, line_number)
+
+      # On other lines, we might encounter times where the split() operation
+      # produces more values.
       if first_line:
         raise errors.WrongParser(error_string)
 
@@ -120,7 +241,6 @@ class WinCCSysLogParser(interface.FileObjectParser):
     # is not in future data.
     event_data.unknown_str = values[5]
 
-
     # We are checking this only once, on the first line, as we expect all
     # following lines to contain the same hostname, as the logs are
     # collected from the system generating it.
@@ -155,7 +275,7 @@ class WinCCSysLogParser(interface.FileObjectParser):
     # The last field can contain a string such as MSG_STATE_COME, MSG_STATE_GO,
     # but also sometimes contains a message string.
 
-    text_message = ' '.join(values[8:-1])
+    text_message = ' '.join(values[8:])
     event_data.message = text_message
 
     parser_mediator.ProduceEventData(event_data)
@@ -181,13 +301,16 @@ class WinCCSysLogParser(interface.FileObjectParser):
     # Note that we cannot use the DSVParser here since this WinCC file format is
     # not strict and clean file format.
     line_reader = text_file.TextFile(
-        file_object, encoding=self.ENCODING, end_of_line='\n')
+        file_object, encoding=self.ENCODING, end_of_line='\r\n')
 
     line_number = 0
     first_line = True
 
+    # While the file is in UTF-16, the end_of_line character is '\r\n'.
+    # We manually strip this out.
+
     try:
-      line = line_reader.readline()
+      line = line_reader.readline().strip()
     except UnicodeDecodeError as exception:
       raise errors.WrongParser(
           'unable to read line: {0:d} with error: {1!s}'.format(
@@ -198,7 +321,7 @@ class WinCCSysLogParser(interface.FileObjectParser):
         # It seems that sometimes WinCC logs will end with a line starting like
         # this, with the name of the next log file.
         # But this is not always the case though.
-        line = line_reader.readline()
+        line = line_reader.readline().strip()
         continue
       values = line.split(self.DELIMITER)
       self._ParseValues(parser_mediator, line_number, values, first_line)
@@ -206,7 +329,7 @@ class WinCCSysLogParser(interface.FileObjectParser):
       try:
         line_number += 1
         first_line = False
-        line = line_reader.readline()
+        line = line_reader.readline().strip()
       except UnicodeDecodeError as exception:
         parser_mediator.ProduceExtractionWarning(
             'unable to read line: {0:d} with error: {1!s}'.format(
@@ -215,3 +338,4 @@ class WinCCSysLogParser(interface.FileObjectParser):
 
 
 manager.ParsersManager.RegisterParser(WinCCSysLogParser)
+manager.ParsersManager.RegisterParser(SIMATICLogParser)
--- tests/parsers/wincc.py
+++ tests/parsers/wincc.py
@@ -9,18 +9,48 @@ from plaso.parsers import wincc
 from tests.parsers import test_lib
 
 
+class SIMATICTest(test_lib.ParserTestCase):
+  """Tests for the SIMATIC S7 logs parser."""
+
+  def testParse(self):
+    """Tests the Parse function."""
+    parser = wincc.SIMATICLogParser()
+    storage_writer = self._ParseFile(
+        ['Logs', 'Program Files (x86)', 'Siemens', 'WinCC', 'diagnose',
+         'SIMATIC_S7_Protocol_Suite_01.LOG'], parser)
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 284)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'extraction_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'recovery_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    expected_event_values = {
+        'data_type': 'wincc:simatic_s7:entry',
+        'creation_time': '2019-05-27T10:05:43Z',
+        'message': ('419 INFO     | LogFileCount  : 3')
+    }
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 2)
+    self.CheckEventData(event_data, expected_event_values)
+
+
 class WinCCSyslogTest(test_lib.ParserTestCase):
   """Tests for the Wincc sys logs parser."""
 
   def testParse(self):
-    """Tests the Parse function?"""
+    """Tests the Parse function."""
     parser = wincc.WinCCSysLogParser()
     storage_writer = self._ParseFile(
         ['Logs', 'Program Files (x86)', 'Siemens', 'WinCC', 'diagnose',
          'WinCC_Sys_01.log'], parser)
     number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
         'event_data')
-     self.assertEqual(number_of_event_data, 25)
+    self.assertEqual(number_of_event_data, 25)
 
     number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
         'extraction_warning')
@@ -37,7 +67,7 @@ class WinCCSyslogTest(test_lib.ParserTestCase):
         'event_number': 1012301,
         'unknown_int': 4,
         'unknown_str': '',
-        'hostname': 'B1IZNCOANMSG3',
+        'hostname': 'B1AAAAAAAAAA3',
         'source': 'CCWriteArchiveServer',
         'message': ('[(null) 224]failed to insert into MSARCLONG with '
                     '0x80004005(#0 \'2019-05-27 08:10:03.602\')')
