d40c0c122c8542b0f5afcfe4065191d971a17c51
Joachim Metz <joachim.metz@gmail.com>
2023-03-24 05:54:55 +0100

    Update winpca.py

--- tests/parsers/text_plugins/winpca.py
+++ tests/parsers/text_plugins/winpca.py
@@ -14,8 +14,7 @@ class WinPCALogTextPluginTest(test_lib.TextPluginTestCase):
   def testLaunchDicParser(self):
     """Tests the LaunchDic file parser."""
     plugin = winpca.WinPCADicParser()
-    storage_writer = self._ParseFile(
-        ['PcaAppLaunchDic.txt'], plugin)
+    storage_writer = self._ParseFile(['PcaAppLaunchDic.txt'], plugin)
 
     number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
         'event_data')
@@ -30,7 +29,8 @@ class WinPCALogTextPluginTest(test_lib.TextPluginTestCase):
     self.assertEqual(number_of_warnings, 0)
 
     expected_event_values = {
-        'body': ('C:\\Program Files\\WindowsApps\\MicrosoftTeams_'
+        'body': (
+            'C:\\Program Files\\WindowsApps\\MicrosoftTeams_'
             '22287.702.1670.9453_x64__8wekyb3d8bbwe\\msteams.exe'),
         'last_execution_time': '2022-11-15T00:02:08.476+00:00'}
 
@@ -40,8 +40,7 @@ class WinPCALogTextPluginTest(test_lib.TextPluginTestCase):
   def testDb0Parser(self):
     """Tests the db0 file parser."""
     plugin = winpca.WinPCADB0Parser()
-    storage_writer = self._ParseFile(
-        ['PcaGeneralDb0.txt'], plugin)
+    storage_writer = self._ParseFile(['PcaGeneralDb0.txt'], plugin)
 
     number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
         'event_data')
@@ -56,15 +55,16 @@ class WinPCALogTextPluginTest(test_lib.TextPluginTestCase):
     self.assertEqual(number_of_warnings, 0)
 
     expected_event_values = {
-        'run_status': '2',
-        'body': ('%programfiles%\\windowsapps\\microsoftteams_22287'
+        'body': (
+            '%programfiles%\\windowsapps\\microsoftteams_22287'
             '.702.1670.9453_x64__8wekyb3d8bbwe\\msteamsupdate.exe'),
         'description': 'microsoft teams updater',
-        'vendor': 'microsoft corporation',
-        'version': '22287.702.1670.9453',
-        'program_identifier': '0006132687b1e64961e910ee21d4352afe0800000904',
         'exit_code': 'Abnormal process exit with code 0x4c7',
-        'last_execution_time': '2022-11-14T23:37:11.789+00:00'}
+        'last_execution_time': '2022-11-14T23:37:11.789+00:00',
+        'program_identifier': '0006132687b1e64961e910ee21d4352afe0800000904',
+        'run_status': '2',
+        'vendor': 'microsoft corporation',
+        'version': '22287.702.1670.9453'}
 
     event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
     self.CheckEventData(event_data, expected_event_values)
