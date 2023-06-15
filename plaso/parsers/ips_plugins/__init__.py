484bf62a607496e5b9017780afcb90d1ef04623b
rick-slin <rick0slin@proton.me>
2023-06-15 11:34:20 -0400

    refactored ips_plugins/interface.py, added IPS parser plugin for stacks files.

--- plaso/parsers/ips_plugins/__init__.py
+++ plaso/parsers/ips_plugins/__init__.py
@@ -2,3 +2,4 @@
 """Imports for the ips log parser plugins."""
 
 from plaso.parsers.ips_plugins import recovery_logd
+from plaso.parsers.ips_plugins import stacks_ips
--- plaso/parsers/ips_plugins/interface.py
+++ plaso/parsers/ips_plugins/interface.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """Interface for IPS log file parser plugins."""
+
 import abc
+import pyparsing
+
+from dfdatetime import time_elements as dfdatetime_time_elements
 
 from plaso.parsers import plugins
 
@@ -11,8 +15,74 @@ class IPSPlugin(plugins.BasePlugin):
   NAME = 'ips_plugin'
   DATA_FORMAT = 'ips log file'
 
+  ENCODING = 'utf-8'
+
   REQUIRED_HEADER_KEYS = []
   REQUIRED_CONTENT_KEYS = []
+
+  _TWO_DIGITS = pyparsing.Word(pyparsing.nums, exact=2).setParseAction(
+      lambda tokens: int(tokens[0], 10))
+
+  _FOUR_DIGITS = pyparsing.Word(pyparsing.nums, exact=4).setParseAction(
+      lambda tokens: int(tokens[0], 10))
+
+  _VARYING_DIGITS = pyparsing.Word(pyparsing.nums)
+
+  TIMESTAMP_GRAMMAR = (
+      _FOUR_DIGITS.setResultsName('year') + pyparsing.Suppress('-') +
+      _TWO_DIGITS.setResultsName('month') + pyparsing.Suppress('-') +
+      _TWO_DIGITS.setResultsName('day') +
+      _TWO_DIGITS.setResultsName('hours') + pyparsing.Suppress(':') +
+      _TWO_DIGITS.setResultsName('minutes') + pyparsing.Suppress(':') +
+      _TWO_DIGITS.setResultsName('seconds') + pyparsing.Suppress('.') +
+      _VARYING_DIGITS.setResultsName('fraction') +
+      pyparsing.Word(
+          pyparsing.nums + '+' + '-').setResultsName('timezone_delta'))
+
+  def _ParseTimestampValue(self, parser_mediator, timestamp_text):
+    """Parses a timestamp string.
+
+    Args:
+      parser_mediator (ParserMediator): parser mediator.
+      timestamp_text (str): the timestamp to parse.
+
+    Returns:
+       dfdatetime.TimeElements: date and time
+          or None if not available.
+    """
+    # dfDateTime takes the time zone offset as number of minutes relative from
+    # UTC. So for Easter Standard Time (EST), which is UTC-5:00 the sign needs
+    # to be converted, to +300 minutes.
+
+    parsed_timestamp = self.TIMESTAMP_GRAMMAR.parseString(timestamp_text)
+
+    try:
+      time_delta_hours = int(parsed_timestamp['timezone_delta'][:3], 10)
+      time_delta_minutes = int(parsed_timestamp['timezone_delta'][3:], 10)
+    except (TypeError, ValueError):
+      parser_mediator.ProduceExtractionWarning(
+          'unsupported timezone offset value')
+      return None
+
+    time_zone_offset = (time_delta_hours * 60) + time_delta_minutes
+
+    try:
+      fraction_float = float(f"0.{parsed_timestamp['fraction']}")
+      milliseconds = round(fraction_float * 1000)
+
+      time_element_object = dfdatetime_time_elements.TimeElementsInMilliseconds(
+          time_elements_tuple=(
+              parsed_timestamp['year'], parsed_timestamp['month'],
+              parsed_timestamp['day'], parsed_timestamp['hours'],
+              parsed_timestamp['minutes'], parsed_timestamp['seconds'],
+              milliseconds),
+          time_zone_offset=time_zone_offset)
+
+    except (TypeError, ValueError):
+      parser_mediator.ProduceExtractionWarning('unsupported date time value')
+      return None
+
+    return time_element_object
 
   def CheckRequiredKeys(self, ips_file):
     """Checks if the ips file's header and content have the keys required by
