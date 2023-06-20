62040dbe0125ce876822c75912a74de0bc589cfe
chb2mn <chb2mn@gmail.com>
2023-06-20 16:57:52 +0000

    linting

--- run_tests.py
+++ run_tests.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
   if not dependency_helper.CheckTestDependencies():
     sys.exit(1)
 
-  test_suite = unittest.TestLoader().discover('tests', pattern='*chrome_cache.py')
+  test_suite = unittest.TestLoader().discover('tests', pattern='*.py')
   test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
   if not test_results.wasSuccessful():
     sys.exit(1)
