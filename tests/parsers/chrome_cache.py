e7aaf5d7fc89019c99860a63294c76ffb42bf418
chb2mn <chb2mn@gmail.com>
2023-06-20 16:48:18 +0000

    adding chrome_cache_v3

--- tests/parsers/chrome_cache.py
+++ tests/parsers/chrome_cache.py
@@ -38,6 +38,33 @@ class ChromeCacheParserTest(test_lib.ParserTestCase):
     event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
     self.CheckEventData(event_data, expected_event_values)
 
+  def testParse_v3(self):
+    """Tests the Parse function."""
+    parser = chrome_cache.ChromeCacheParser()
+    storage_writer = self._ParseFile(['chrome_cache_v3', 'index'], parser)
+
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 862)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'extraction_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'recovery_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    expected_event_values = {
+        'creation_time': '2023-06-20T22:38:46.862881+00:00',
+        'data_type': 'chrome:cache:entry',
+        'original_url': ('https://m.media-amazon.com/images/'
+            'G/01/gno/sprites/nav-sprite-global-1x-reorg-privacy'
+            '._CB587940754_.png')
+        }
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
+    self.CheckEventData(event_data, expected_event_values)
 
 if __name__ == '__main__':
   unittest.main()
