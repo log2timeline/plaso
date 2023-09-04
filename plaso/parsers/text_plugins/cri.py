aa75f6e207c6b98b77232924cb45ecb4d33df55c
Syd Pleno <syd.pleno@gmail.com>
2023-09-04 18:09:44 +1000

    Initial commit

--- plaso/parsers/text_plugins/cri.py
+++ plaso/parsers/text_plugins/cri.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
-"""Text file parser plugin for Container Runtime Interface log files."""
+"""Text file parser plugin for Container Runtime Interface (CRI) log format."""
 
 import pyparsing
 
-from dfdatetime import time_elements
+from dfdatetime import golang_time
+
+from plaso.containers import events
+from plaso.lib import errors
+from plaso.parsers.text_plugins import interface
 
 
 class CRIEventData(events.EventData):
-  """CRI event data.
+  """CRI log event data.
 
   Attributes:
     event_datetime (dfdatetime.Golangtime): the datetime of the log message.
@@ -15,33 +19,40 @@ class CRIEventData(events.EventData):
     stream (str): the log stream.  Currently stdout and stderr.
     tag (str): the log tag.  Currently only P (partial) and F (full) are supported.
   """
+  DATA_TYPE = 'crio:container:log:entry'
+
+  def __init__(self):
+    """Initializes event data."""
+    super(CRIEventData, self).__init__(data_type=self.DATA_TYPE)
+    self.event_datetime = None
+    self.log_message = None
+    self.stream = None
+    self.tag = None
+
 
 class CRITextPlugin(interface.TextPlugin):
-  """Text file parser plugin for Container Runtime Interface log files."""
+  """Text file parser plugin for CRI log files."""
 
-  NAME = 'cri'
+  NAME = 'cri_log'
   DATA_FORMAT = 'Container Runtime Interface log file'
 
   ENCODING = 'utf-8'
 
-  _TWO_DIGITS = pyparsing.Word(pyparsing.nums, exact=2).setParseAction(
-      lambda tokens: int(tokens[0], 10))
+  _TWO_DIGITS = pyparsing.Word(pyparsing.nums, exact=2)
 
-  _FOUR_DIGITS = pyparsing.Word(pyparsing.nums, exact=4).setParseAction(
-      lambda tokens: int(tokens[0], 10))
+  _FOUR_DIGITS = pyparsing.Word(pyparsing.nums, exact=4)
 
-  _NINE_DIGITS = pyparsing.Word(pyparsing.nums, exact=9).setParseAction(
-      lambda tokens: int(tokens[0], 10))
+  _NINE_DIGITS = pyparsing.Word(pyparsing.nums, exact=9)
 
   # Date and time values are formatted as: 2016-10-06T00:17:09.669794202Z
-  _DATE_AND_TIME = (
-      _FOUR_DIGITS + pyparsing.Suppress('-') +
-      _TWO_DIGITS + pyparsing.Suppress('-') +
-      _TWO_DIGITS + pyparsing.Suppress('T') +
-      _TWO_DIGITS + pyparsing.Suppress(':') +
-      _TWO_DIGITS + pyparsing.Suppress(':') +
-      _TWO_DIGITS + pyparsing.Suppress('.') +
-      _NINE_DIGITS + pyparsing.Suppress('Z')).setResultsName('date_time')
+  _DATE_AND_TIME = pyparsing.Combine(
+      _FOUR_DIGITS + pyparsing.Literal('-') +
+      _TWO_DIGITS + pyparsing.Literal('-') +
+      _TWO_DIGITS + pyparsing.Literal('T') +
+      _TWO_DIGITS + pyparsing.Literal(':') +
+      _TWO_DIGITS + pyparsing.Literal(':') +
+      _TWO_DIGITS + pyparsing.Literal('.') +
+      _NINE_DIGITS + pyparsing.Literal('Z')).setResultsName('date_time')
 
   _STREAM = (
       pyparsing.Literal('stderr') ^ pyparsing.Literal('stdout')
@@ -52,37 +63,12 @@ class CRITextPlugin(interface.TextPlugin):
 
   _LOG = (
       pyparsing.restOfLine() + pyparsing.Suppress(pyparsing.LineEnd())
-  ).setResultsName('message')
+  ).setResultsName('log_message')
 
   _LOG_LINE = _DATE_AND_TIME + _STREAM + _TAG + _LOG
   _LINE_STRUCTURES = [('log_line', _LOG_LINE)]
 
   VERIFICATION_GRAMMAR = _LOG_LINE
-
-  def _ParseTimeElements(self, time_elements_structure):
-    """Parses date and time elements of a log line.
-
-    Args:
-      time_elements_structure (pyparsing.ParseResults): date and time elements
-          of a log line.
-
-    Returns:
-      dfdatetime.TimeElements: date and time value.
-
-    Raises:
-      ParseError: if a valid date and time value cannot be derived from
-          the time elements.
-    """
-    try:
-      (year, month, day_of_month, hours, minutes, seconds, nanoseconds) = (
-          time_elements_structure)
-
-      return dfdatetime_time_elements.TimeElementsInNanoseconds(
-          time_elements_tuple=time_elements_tuple)
-
-    except (TypeError, ValueError) as exception:
-      raise errors.ParseError(
-          'Unable to parse time elements with error: {0!s}'.format(exception))
 
   def _ParseRecord(self, parser_mediator, key, structure):
     """Parses a pyparsing structure.
@@ -97,11 +83,32 @@ class CRITextPlugin(interface.TextPlugin):
       ParseError: if the structure cannot be parsed.
     """
     if key == 'log_line':
-      time_elements_structure = self._GetValueFromStructure(
-          structure, 'date_time')
+      golang_time_string = self._GetValueFromStructure(structure, 'date_time')
+      golang_time_object = golang_time.GolangTime(
+          golang_timestring=golang_time_string)
 
       event_data = CRIEventData()
-      event_data.event_datetime = self._ParseTimeElements(time_elements_structure)
-      event_data.log_message = self._GetValueFromStructure(structure, 'log_message')
-      event_data.stream = self._GetValueFromStructure(strucutre, 'stream')
+      event_data.event_datetime = golang_time_object
+      event_data.log_message = self._GetValueFromStructure(
+          structure, 'log_message')[0]
+      event_data.stream = self._GetValueFromStructure(structure, 'stream')
       event_data.tag = self._GetValueFromStructure(structure, 'tag')
+      parser_mediator.ProduceEventData(event_data)
+
+  def CheckRequiredFormat(self, parser_mediator, text_reader):
+    """Check if the log record has the minimal structure required by the parser.
+
+    Args:
+      parser_mediator (ParserMediator): mediates interactions between parsers
+          and other components, such as storage and dfVFS.
+      text_reader (EncodedTextReader): text reader.
+
+    Returns:
+      bool: True if this is the correct parser, False otherwise.
+    """
+    try:
+      self._VerifyString(text_reader.lines)
+    except errors.ParseError:
+      return False
+
+    return True
