8676aa7e358481657a8f3ab3c91829a779f13775
Joachim Metz <joachim.metz@gmail.com>
2023-09-24 10:50:24 +0200

    Update mrulistex.py

--- plaso/parsers/winreg_plugins/mrulistex.py
+++ plaso/parsers/winreg_plugins/mrulistex.py
@@ -23,7 +23,7 @@ class MRUListExEventData(events.EventData):
   """MRUListEx event data attribute container.
 
   Attributes:
-    entries (str): most recently used (MRU) entries.
+    entries (list[str]): most recently used (MRU) entries.
     key_path (str): Windows Registry key path.
     last_written_time (dfdatetime.DateTimeValues): entry last written date and
         time.
