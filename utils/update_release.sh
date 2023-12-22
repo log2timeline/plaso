05a16ad6c7a438920b88be324681e20fd38b4b2e
Joachim Metz <joachim.metz@gmail.com>
2023-12-22 07:39:52 +0100

    Updated release script and version

--- utils/update_release.sh
+++ utils/update_release.sh
@@ -12,6 +12,9 @@ DPKG_DATE=`date -R`
 # Update the Python module version.
 sed "s/__version__ = '[0-9]*'/__version__ = '${VERSION}'/" -i plaso/__init__.py
 
+# Update the version in the setuptools configuration.
+sed "s/version = [0-9]*/version = ${VERSION}/" -i setup.cfg
+
 # Update the version in the dpkg configuration files.
 cat > config/dpkg/changelog << EOT
 plaso (${VERSION}-1) unstable; urgency=low
