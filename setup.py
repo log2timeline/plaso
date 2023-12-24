70659c839bb684260865f2f30a51d9a2b9b17a61
Joachim Metz <joachim.metz@gmail.com>
2023-12-24 07:47:53 +0100

    Changes for deployment (#4770)

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 """Installation and deployment script."""
 
+import glob
+import os
+
 from setuptools import setup
 
 
-setup()
+setup(
+    data_files=[
+        ('share/plaso', glob.glob(os.path.join('data', '*.*'))),
+        ('share/plaso/formatters', glob.glob(os.path.join(
+            'data', 'formatters', '*.yaml')))])
