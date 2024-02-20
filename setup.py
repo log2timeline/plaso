e1fbb6de0334440faeacb782ce47267caa5fdee0
Joachim Metz <joachim.metz@gmail.com>
2024-02-20 20:15:56 +0100

    Moved data files into Python module #4769 (#4810)

--- setup.py
+++ setup.py
@@ -2,14 +2,7 @@
 # -*- coding: utf-8 -*-
 """Installation and deployment script."""
 
-import glob
-import os
-
 from setuptools import setup
 
 
-setup(
-    data_files=[
-        ('share/plaso', glob.glob(os.path.join('data', '*.*'))),
-        ('share/plaso/formatters', glob.glob(os.path.join(
-            'data', 'formatters', '*.yaml')))])
+setup()
