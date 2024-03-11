1d08bd8bdeeb0a9659e744ab48da1762f45a77b1
Joachim Metz <joachim.metz@gmail.com>
2024-03-11 19:02:59 +0100

    Improvements to Custom Destinations Jump List parser (#4832)

--- tests/parsers/custom_destinations.py
+++ tests/parsers/custom_destinations.py
@@ -16,7 +16,8 @@ class CustomDestinationsParserTest(test_lib.ParserTestCase):
     """Tests the Parse function."""
     parser = custom_destinations.CustomDestinationsParser()
     storage_writer = self._ParseFile(
-        ['5afe4de1b92fc382.customDestinations-ms'], parser)
+        ['custom_destinations', '5afe4de1b92fc382.customDestinations-ms'],
+        parser)
 
     number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
         'event_data')
@@ -77,6 +78,87 @@ class CustomDestinationsParserTest(test_lib.ParserTestCase):
         'shell_item_path': '<My Computer> C:\\\\Windows\\\\System32'}
 
     event_data = storage_writer.GetAttributeContainerByIndex('event_data', 49)
+    self.CheckEventData(event_data, expected_event_values)
+
+  def testParseWithEmpty(self):
+    """Tests the Parse function with an empty jump list."""
+    parser = custom_destinations.CustomDestinationsParser()
+    storage_writer = self._ParseFile(
+        ['custom_destinations', 'c98dce577f884ef8.customDestinations-ms'],
+        parser)
+
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 0)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'extraction_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'recovery_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+  def testParseWithComplex(self):
+    """Tests the Parse function with a complex jump list."""
+    parser = custom_destinations.CustomDestinationsParser()
+    storage_writer = self._ParseFile(
+        ['custom_destinations', '368d807282ccde9d.customDestinations-ms'],
+        parser)
+
+    number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
+        'event_data')
+    self.assertEqual(number_of_event_data, 12)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'extraction_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
+        'recovery_warning')
+    self.assertEqual(number_of_warnings, 0)
+
+    expected_event_values = {
+        'access_time': '2024-01-16T06:12:42+00:00',
+        'creation_time': '2023-07-12T18:11:20+00:00',
+        'data_type': 'windows:shell_item:file_entry',
+        'file_reference': '407354-78',
+        'long_name': 'test',
+        'modification_time': '2023-07-14T04:04:02+00:00',
+        'name': 'test',
+        'origin': '368d807282ccde9d.customDestinations-ms',
+        'shell_item_path': '<My Computer> C:\\\\test'}
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
+    self.CheckEventData(event_data, expected_event_values)
+
+    expected_event_values = {
+        'access_time': '2024-01-16T06:12:41.2400523+00:00',
+        'command_line_arguments': 'My Arguments',
+        'creation_time': '2023-07-12T18:11:18.2749654+00:00',
+        'data_type': 'windows:lnk:link',
+        'description': None,
+        'drive_serial_number': 0x2ca3d1ae,
+        'drive_type': 3,
+        'env_var_location': None,
+        'file_attribute_flags': 0x00000010,
+        'file_size': 4096,
+        'icon_location': 'My Icon',
+        'link_target': '<My Computer> C:\\\\test',
+        'local_path': 'C:\\\\test',
+        'modification_time': '2023-07-14T04:04:00.3349887+00:00'}
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 1)
+    self.CheckEventData(event_data, expected_event_values)
+
+    expected_event_values = {
+        'creation_time': '2023-07-12T18:06:36.6282931+00:00',
+        'data_type': 'windows:distributed_link_tracking:creation',
+        'mac_address': '52:54:00:ee:b6:05',
+        'origin': '368d807282ccde9d.customDestinations-ms',
+        'uuid': 'd78dbcb3-20de-11ee-a2f8-525400eeb605'}
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 2)
     self.CheckEventData(event_data, expected_event_values)
 
 
