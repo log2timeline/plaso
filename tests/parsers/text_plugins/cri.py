c0dd77ee3d6c067beae7915d62315836b9056cad
Syd Pleno <syd.pleno@gmail.com>
2024-05-05 14:15:10 +1000

    Add new line

--- tests/parsers/text_plugins/cri.py
+++ tests/parsers/text_plugins/cri.py
@@ -62,5 +62,6 @@ class CRILogTextPluginTest(test_lib.TextPluginTestCase):
     event_data = storage_writer.GetAttributeContainerByIndex('event_data', 14)
     self.CheckEventData(event_data, expected_event_values)
 
+
 if __name__ == '__main__':
   unittest.main()
