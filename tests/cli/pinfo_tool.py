eeb9222131de1e9b1428ab700d8615d59f54ad91
Hudan Studiawan <studiawan@gmail.com>
2024-11-27 06:19:16 +0700

    Remove ios_accounts from enabled_parser_names

--- tests/cli/pinfo_tool.py
+++ tests/cli/pinfo_tool.py
@@ -697,7 +697,6 @@ Storage files are different.
         'sqlite/google_drive',
         'sqlite/hangouts_messages',
         'sqlite/imessage',
-        'sqlite/ios_accounts',
         'sqlite/ios_netusage',
         'sqlite/ios_powerlog',
         'sqlite/ios_screentime',
