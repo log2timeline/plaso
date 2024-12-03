b81989b490949c2c42a6783bcf3c62f4c582c17f
Bimo Syahputro <bimo.s@maxxitani.id>
2024-12-03 08:05:52 +0700

    [BIMO] add ios location service, modify init and timeliner.yml file

--- plaso/parsers/plist_plugins/ios_locationservices.py
+++ plaso/parsers/plist_plugins/ios_locationservices.py
@@ -1,68 +1,58 @@
-# -*- coding: utf-8 -*-
-"""Plist parser plugin for Apple iOS routined application plist files.
+import datetime
 
-The plist contains various routine-related information, including task history,
-account details, timestamps for asset updates, and activity attempts/completions.
-"""
-
-from dfdatetime import cocoa_time as dfdatetime_cocoa_time
-
+from dfdatetime import posix_time as dfdatetime_posix_time
 from plaso.containers import events
+from plaso.lib import definitions
 from plaso.parsers import plist
 from plaso.parsers.plist_plugins import interface
 
-
-class RoutinedPlistEventData(events.EventData):
-    """Apple iOS routined application plist event data.
+class IOSRoutinedEventData(events.EventData):
+    """Event data for com.apple.routined plist.
 
     Attributes:
-        key (str): The plist key associated with the event.
-        value (str): The value associated with the key.
+        key (str): Name of the key.
+        value (str): Value associated with the key.
     """
-
     DATA_TYPE = 'ios:routined:entry'
 
     def __init__(self):
         """Initializes event data."""
-        super(RoutinedPlistEventData, self).__init__(data_type=self.DATA_TYPE)
+        super(IOSRoutinedEventData, self).__init__(data_type=self.DATA_TYPE)
         self.key = None
         self.value = None
 
-
-class RoutinedPlistPlugin(interface.PlistPlugin):
-    """Plist parser plugin for Apple iOS routined application plist files."""
+class IOSRoutinedPlistPlugin(interface.PlistPlugin):
+    """Plist parser plugin for com.apple.routined plist."""
 
     NAME = 'ios_routined'
-    DATA_FORMAT = 'Apple iOS routined application plist file'
+    DATA_FORMAT = 'Apple iOS Routined plist file'
 
     PLIST_PATH_FILTERS = frozenset([
         interface.PlistPathFilter('com.apple.routined.plist')
     ])
 
+    PLIST_KEYS = frozenset([
+        'XPCActivityLastAttemptDate.com.apple.routined.assets',
+    ])
+
     def _ParsePlist(self, parser_mediator, match=None, **unused_kwargs):
-        """Extracts routined plist entries.
+        """Extract data from the plist.
 
         Args:
-            parser_mediator (ParserMediator): Mediates interactions between parsers
-                and other components, such as storage and dfVFS.
-            match (Optional[dict[str, object]]): Keys extracted from the plist file.
+            parser_mediator (ParserMediator): mediates interactions between parsers and other components.
+            match (Optional[dict[str, object]]): keys extracted from PLIST_KEYS.
         """
         for key, value in match.items():
-            try:
-                event_data = RoutinedPlistEventData()
-                event_data.key = key
+            event_data = IOSRoutinedEventData()
+            event_data.key = key
 
-                # Attempt to interpret ISO 8601 formatted strings as datetime values.
-                event_data.value = dfdatetime_cocoa_time.CocoaTime(
-                    timestamp=value)
+            if isinstance(value, datetime.datetime):
+                timestamp = int(value.timestamp() * definitions.NANOSECONDS_PER_SECOND)
+                event_data.value = dfdatetime_posix_time.PosixTimeInNanoseconds(
+                    timestamp=timestamp)
+            else:
+                event_data.value = value
 
-                # Produce event data to be recorded by the parser mediator.
-                parser_mediator.ProduceEventData(event_data)
+            parser_mediator.ProduceEventData(event_data)
 
-            except ValueError:
-                # Skip strings that cannot be interpreted as valid datetime values.
-                continue
-
-
-# Register the plugin with the PlistParser to make it available for plist parsing.
-plist.PlistParser.RegisterPlugin(RoutinedPlistPlugin)
+plist.PlistParser.RegisterPlugin(IOSRoutinedPlistPlugin)
