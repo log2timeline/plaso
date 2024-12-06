e9d7e98219248cca47d0343ece1027a462c9ac3c
SanGit56 <radhiyanrmhhisan@gmail.com>
2024-12-06 14:13:12 +0700

    Fix parser plugin, plugin unit test, and parser- plugin initialization

--- plaso/parsers/sqlite_plugins/__init__.py
+++ plaso/parsers/sqlite_plugins/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """Imports for the SQLite database parser plugins."""
 
-from plaso.parsers.sqlite_plugins import android_communication_information
 from plaso.parsers.sqlite_plugins import android_app_usage
 from plaso.parsers.sqlite_plugins import android_calls
 from plaso.parsers.sqlite_plugins import android_hangouts
