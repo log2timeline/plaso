0daf932b4c7a9a438fd20d09e163dc1a33db91c8
Joachim Metz <joachim.metz@gmail.com>
2024-12-26 13:40:48 +0100

    Changes after review

--- plaso/parsers/plist_plugins/ios_mobilebackup.py
+++ plaso/parsers/plist_plugins/ios_mobilebackup.py
@@ -4,82 +4,95 @@
 The plist contains history of opened applications in the Mobile Backup.
 """
 
-import datetime
-from dfdatetime import posix_time as dfdatetime_posix_time
-
 from plaso.containers import events
-from plaso.lib import definitions
 from plaso.parsers import plist
 from plaso.parsers.plist_plugins import interface
 
 
 class IOSMobileBackupEventData(events.EventData):
-  """Apple iOS Mobile Backup history event data.
+  """Apple iOS Mobile Backup event data.
 
   Attributes:
-    activity_description (str): activity description.
-    activity_time (dfdatetime.DateTimeValues): activity last run date and
-        time.
+    account_enabled_time (dfdatetime.DateTimeValues): date and time the mobile
+        backup account was enabled.
+    backup_state_time (dfdatetime.DateTimeValues): date and time if the backup
+        state.
+    backup_state (int): backup state.
   """
 
-  DATA_TYPE = 'ios:mobile:backup:entry'
+  DATA_TYPE = 'ios:mobile_backup:entry'
 
   def __init__(self):
     """Initializes event data."""
     super(IOSMobileBackupEventData, self).__init__(data_type=self.DATA_TYPE)
-    self.activity_description = None
-    self.activity_time = None
+    self.account_enabled_time = None
+    self.backup_state = None
+    self.backup_state_time = None
+
+
+class IOSMobileBackupStateEventData(events.EventData):
+  """Apple iOS Mobile Backup state event data.
+
+  Attributes:
+    backup_time (dfdatetime.DateTimeValues): date and time of the backup.
+    description (str): localized description of the backup state.
+  """
+
+  DATA_TYPE = 'ios:mobile_backup:state'
+
+  def __init__(self):
+    """Initializes event data."""
+    super(IOSMobileBackupStateEventData, self).__init__(
+        data_type=self.DATA_TYPE)
+    self.backup_time = None
+    self.description = None
 
 
 class IOSMobileBackupPlistPlugin(interface.PlistPlugin):
   """Plist parser plugin for Apple iOS Mobile Backup plist files."""
 
-  NAME = 'ios_mobilebackup'
+  NAME = 'ios_mobile_backup'
   DATA_FORMAT = 'Apple iOS Mobile Backup plist file'
 
   PLIST_PATH_FILTERS = frozenset([
       interface.PlistPathFilter('com.apple.MobileBackup.plist')])
 
-  PLIST_KEYS = frozenset(['AccountEnabledDate','BackupStateInfo'])
-
-  def _getEventData(self, description, datetime_value):
-    event_data = IOSMobileBackupEventData()
-    event_data.activity_description = description
-    timestamp = datetime.datetime.timestamp(datetime_value)
-    timestamp = int(timestamp * definitions.NANOSECONDS_PER_SECOND)
-    event_data.activity_time = dfdatetime_posix_time.PosixTimeInNanoseconds(
-        timestamp=timestamp)
-    return event_data
+  PLIST_KEYS = frozenset(['AccountEnabledDate', 'BackupStateInfo'])
 
   # pylint: disable=arguments-differ
-  def _ParsePlist(self, parser_mediator, match=None, **unused_kwargs):
+  def _ParsePlist(self, parser_mediator, top_level=None, **unused_kwargs):
     """Extract Mobile Backup history entries.
 
     Args:
       parser_mediator (ParserMediator): mediates interactions between parsers
           and other components, such as storage and dfVFS.
-      match (Optional[dict[str: object]]): keys extracted from PLIST_KEYS.
+      top_level (Optional[dict[str, object]]): plist top-level item.
     """
+    event_data = IOSMobileBackupEventData()
+    event_data.account_enabled_time = self._GetDateTimeValueFromPlistKey(
+        top_level, 'AccountEnabledDate')
 
-    datetime_value = match.get('AccountEnabledDate', {})
-    parser_mediator.ProduceEventData(
-      self._getEventData("AccountEnabledDate", datetime_value)
-    )
+    # TODO: add support for LastOnConditionEvents
+    # TODO: add support for RemoteConfigurationExpiration
 
-    plist_key = match.get('BackupStateInfo', {})
-    for key, value in plist_key.items():
-      if key == "date":
-        parser_mediator.ProduceEventData(
-          self._getEventData("BackupStateInfo - date", value)
-        )
+    backup_state_info = top_level.get('BackupStateInfo')
+    if backup_state_info:
+      event_data.backup_state = backup_state_info.get('state')
+      # TODO: determine what this time represents.
+      event_data.backup_state_time = self._GetDateTimeValueFromPlistKey(
+          backup_state_info, 'date')
 
-      elif key == "errors":
-        for val in value:
-          parser_mediator.ProduceEventData(
-            self._getEventData(
-              f"BackupStateInfo - {val['localizedDescription']}",
-              val['date']
-            )
-          )
+    parser_mediator.ProduceEventData(event_data)
+
+    if backup_state_info:
+      for error in backup_state_info.get('errors', []):
+        event_data = IOSMobileBackupStateEventData()
+        event_data.description = error.get('localizedDescription')
+        # TODO: determine if this is the start or end time of the backup.
+        event_data.backup_time = self._GetDateTimeValueFromPlistKey(
+            error, 'date')
+
+        parser_mediator.ProduceEventData(event_data)
+
 
 plist.PlistParser.RegisterPlugin(IOSMobileBackupPlistPlugin)
