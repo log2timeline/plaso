8332bd4a3f6a276f75531c788fa50ef4ea2ce34a
Joachim Metz <joachim.metz@gmail.com>
2024-12-26 13:40:48 +0100

    Changes after review

--- tests/parsers/plist_plugins/ios_mobilebackup.py
+++ tests/parsers/plist_plugins/ios_mobilebackup.py
@@ -22,7 +22,7 @@ class IOSMobileBackupPlistPluginTest(test_lib.PlistPluginTestCase):
 
     number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
         'event_data')
-    self.assertEqual(number_of_event_data, 12)
+    self.assertEqual(number_of_event_data, 11)
 
     number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
         'extraction_warning')
@@ -33,11 +33,20 @@ class IOSMobileBackupPlistPluginTest(test_lib.PlistPluginTestCase):
     self.assertEqual(number_of_warnings, 0)
 
     expected_event_values = {
-        'activity_description': 'AccountEnabledDate',
-        'data_type': 'ios:mobile:backup:entry',
-        'activity_time': '2023-04-15T13:58:42.481633792+00:00'}
+        'account_enabled_time': '2023-04-15T13:58:42.481634+00:00',
+        'backup_state': 4,
+        'backup_state_time': '2023-04-15T14:08:21.008032+00:00',
+        'data_type': 'ios:mobile_backup:entry'}
 
     event_data = storage_writer.GetAttributeContainerByIndex('event_data', 0)
+    self.CheckEventData(event_data, expected_event_values)
+
+    expected_event_values = {
+        'backup_time': '2023-05-03T01:11:53.195466+00:00',
+        'data_type': 'ios:mobile_backup:state',
+        'description': 'Backup succeeded'}
+
+    event_data = storage_writer.GetAttributeContainerByIndex('event_data', 1)
     self.CheckEventData(event_data, expected_event_values)
 
 
