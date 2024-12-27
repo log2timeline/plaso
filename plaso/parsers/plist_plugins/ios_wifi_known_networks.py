d3059acfcd78465181886394b2466f0ed283ae24
Joachim Metz <joachim.metz@gmail.com>
2024-12-27 08:16:17 +0100

    Changes after review

--- plaso/parsers/plist_plugins/ios_wifi_known_networks.py
+++ plaso/parsers/plist_plugins/ios_wifi_known_networks.py
@@ -4,87 +4,76 @@
 The plist contains information about WiFi networks the device has connected to.
 """
 
-from dfdatetime import posix_time as dfdatetime_posix_time
 from plaso.containers import events
 from plaso.parsers import plist
 from plaso.parsers.plist_plugins import interface
 
 
 class IOSWiFiKnownNetworksEventData(events.EventData):
-    """Apple iOS WiFi Known Networks event data.
+  """Apple iOS WiFi Known Networks event data.
 
-    Attributes:
-        ssid (str): SSID of the WiFi network.
-        added_at (dfdatetime.DateTimeValues): date the network was added.
-        last_associated (dfdatetime.DateTimeValues): date the network was last associated.
-        bssid (str): BSSID of the WiFi network.
-        channel (int): Channel used by the WiFi network.
-    """
+  Attributes:
+    added_time (dfdatetime.DateTimeValues): date the network was added.
+    bssid (str): BSSID of the WiFi network.
+    channel (int): Channel used by the WiFi network.
+    last_associated_time (dfdatetime.DateTimeValues): date the network was last
+        associated.
+    ssid (str): SSID of the WiFi network.
+  """
 
-    DATA_TYPE = 'ios:wifi:known_networks:knowing'
+  DATA_TYPE = 'ios:wifi:known_networks:entry'
 
-    def __init__(self):
-        """Initializes event data."""
-        super(IOSWiFiKnownNetworksEventData, self).__init__(data_type=self.DATA_TYPE)
-        self.ssid = None
-        self.added_at = None
-        self.last_associated = None
-        self.bssid = None
-        self.channel = None
+  def __init__(self):
+    """Initializes event data."""
+    super(IOSWiFiKnownNetworksEventData, self).__init__(
+        data_type=self.DATA_TYPE)
+    self.added_time = None
+    self.bssid = None
+    self.channel = None
+    self.last_associated_time = None
+    self.ssid = None
 
 
 class IOSWiFiKnownNetworksPlistPlugin(interface.PlistPlugin):
-    """Plist parser plugin for Apple iOS WiFi Known Networks plist files."""
+  """Plist parser plugin for Apple iOS WiFi Known Networks plist files."""
 
-    NAME = 'ios_wifi_known_networks'
-    DATA_FORMAT = 'Apple iOS WiFi Known Networks plist file'
+  NAME = 'ios_wifi_known_networks'
+  DATA_FORMAT = 'Apple iOS WiFi Known Networks plist file'
 
-    PLIST_PATH_FILTERS = frozenset([
-        interface.PlistPathFilter('com.apple.wifi.known-networks.plist')])
+  PLIST_PATH_FILTERS = frozenset([
+      interface.PlistPathFilter('com.apple.wifi.known-networks.plist')])
 
-    PLIST_KEYS = frozenset([])
+  PLIST_KEYS = frozenset([])
 
+  def _ParsePlist(
+      self, parser_mediator, match=None, top_level=None, **unused_kwargs):
+    """Extract WiFi known network entries.
 
-    def _ParsePlist(self, parser_mediator, match=None, top_level=None, **unused_kwargs):
-        print(f"Top-level keys in plist: {list(top_level.keys())}")
-        """Extract WiFi known network entries.
+    Args:
+        parser_mediator (ParserMediator): mediates interactions between parsers
+            and other components, such as storage and dfVFS.
+        match (Optional[dict[str: object]]): keys extracted from PLIST_KEYS.
+        top_level (Optional[dict[str: object]]): entire plist file.
+    """
+    for network_values in top_level.values():
+      event_data = IOSWiFiKnownNetworksEventData()
+      event_data.added_time = self._GetDateTimeValueFromPlistKey(
+          network_values, 'AddedAt')
+      # TODO: add support for JoinedByUserAt
+      # TODO: add support for JoinedBySystemAt
+      # TODO: add support for UpdatedAt
+      event_data.ssid = network_values.get('SSID').decode('utf8')
 
-        Args:
-            parser_mediator (ParserMediator): mediates interactions between parsers
-                and other components, such as storage and dfVFS.
-            match (Optional[dict[str: object]]): keys extracted from PLIST_KEYS.
-            top_level (Optional[dict[str: object]]): entire plist file.
-        """
-        for ssid_key, ssid_data in top_level.items():
-            added_at = ssid_data.get('AddedAt')
-            bssid_list = ssid_data.get('BSSList', [])
+      for bssid_data in network_values.get('BSSList', []):
+        event_data.bssid = bssid_data.get('BSSID')
+        event_data.channel = bssid_data.get('Channel')
+        event_data.last_associated_time = self._GetDateTimeValueFromPlistKey(
+            bssid_data, 'LastAssociatedAt')
 
+        parser_mediator.ProduceEventData(event_data)
 
-            for bssid_data in bssid_list:
-                event_data = IOSWiFiKnownNetworksEventData()
-                event_data.ssid = ssid_key
-
-                if added_at:
-                    added_at_obj = dfdatetime_posix_time.PosixTime(
-                        timestamp=added_at.timestamp())
-                    event_data.added_at = added_at_obj
-                    event_data.added_at_time_str = added_at_obj.CopyToDateTimeString()
-
-                event_data.bssid = bssid_data.get('BSSID')
-                event_data.channel = bssid_data.get('Channel')
-
-
-                last_associated = bssid_data.get('LastAssociatedAt')
-                if last_associated:
-                    last_associated_obj = dfdatetime_posix_time.PosixTime(
-                        timestamp=last_associated.timestamp())
-                    event_data.last_associated = last_associated_obj
-                    event_data.last_associated_time_str = last_associated_obj.CopyToDateTimeString()
-
-                print(f"Debug Event: SSID={event_data.ssid}, Added At={event_data.added_at_time_str}, Last Associated={event_data.last_associated_time_str}")
-
-                parser_mediator.ProduceEventData(event_data)
-
+      # TODO: add support for __OSSpecific__ knownBSSUpdatedDate,
+      # prevJoined and WiFiNetworkPasswordModificationDate
 
 
 plist.PlistParser.RegisterPlugin(IOSWiFiKnownNetworksPlistPlugin)
