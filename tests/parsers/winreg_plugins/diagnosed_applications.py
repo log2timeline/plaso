e591dd379071b3a6c38e518f72aaa4114e8643d7
elad-levi-cyberark <levi.elad@cyberark.com>
2025-03-03 16:14:58 +0200

    Update diagnosed_applications.py

--- tests/parsers/winreg_plugins/diagnosed_applications.py
+++ tests/parsers/winreg_plugins/diagnosed_applications.py
@@ -32,12 +32,13 @@ class WindowsRegistryDiagnosedApplicationsPluginTest(
         'DiagnosedApplications\\chrome.exe'
     )
 
-    value_data = "1db78823a40721d".encode('utf_16_le')
+    value_data = "1db78823a40721d"
     print(value_data)
     registry_value = dfwinreg_fake.FakeWinRegistryValue(
       'LastDetectionTime', data=value_data,
       data_type=dfwinreg_definitions.REG_QWORD)
     registry_key.AddValue(registry_value)
+    print(registry_key.GetValueByName('LastDetectionTime'))
 
     return registry_key
 
