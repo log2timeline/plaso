4a2a791168f333caf448b8b8bfcbf8838ec344f4
Joachim Metz <joachim.metz@gmail.com>
2025-03-03 06:18:09 +0100

    Changes after review

--- plaso/parsers/plist_plugins/__init__.py
+++ plaso/parsers/plist_plugins/__init__.py
@@ -8,6 +8,7 @@ from plaso.parsers.plist_plugins import default
 from plaso.parsers.plist_plugins import install_history
 from plaso.parsers.plist_plugins import ios_carplay
 from plaso.parsers.plist_plugins import ios_identityservices
+from plaso.parsers.plist_plugins import ios_siminfo
 from plaso.parsers.plist_plugins import ipod
 from plaso.parsers.plist_plugins import launchd
 from plaso.parsers.plist_plugins import macos_background_items
@@ -21,6 +22,3 @@ from plaso.parsers.plist_plugins import software_update
 from plaso.parsers.plist_plugins import spotlight_searched_terms
 from plaso.parsers.plist_plugins import spotlight_volume
 from plaso.parsers.plist_plugins import time_machine
-
-# Impor parser yang baru dibuat
-from plaso.parsers.plist_plugins import ios_siminfo
