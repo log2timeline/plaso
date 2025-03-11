2e1ad375064bed844d75f40e9fc1cd647c68d168
elad-levi-cyberark <levi.elad@cyberark.com>
2025-03-11 13:16:43 +0200

    Update diagnosed_applications.py

--- plaso/parsers/winreg_plugins/diagnosed_applications.py
+++ plaso/parsers/winreg_plugins/diagnosed_applications.py
@@ -2,7 +2,14 @@
 """Plug-in to collect evidence of execution from RADAR 
 HeapLeakDetection Diagnosed Applications."""
 
+import os
+
+from dfdatetime import filetime as dfdatetime_filetime
+from dfdatetime import semantic_time as dfdatetime_semantic_time
+
 from plaso.containers import events
+from plaso.lib import dtfabric_helper
+from plaso.lib import errors
 from plaso.parsers import winreg_parser
 from plaso.parsers.winreg_plugins import interface
 
@@ -31,7 +38,8 @@ class WindowsRegistryDiagnosedApplicationsEventData(events.EventData):
     self.last_written_time = None
 
 
-class DiagnosedApplicationsPlugin(interface.WindowsRegistryPlugin):
+class DiagnosedApplicationsPlugin(
+    interface.WindowsRegistryPlugin, dtfabric_helper.DtFabricHelper):
   """Plug-in to collect information about the Motherboard and BIOS."""
 
   NAME = 'diagnosed_applications'
@@ -41,6 +49,38 @@ class DiagnosedApplicationsPlugin(interface.WindowsRegistryPlugin):
     interface.WindowsRegistryKeyPathFilter(
       'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\RADAR\\HeapLeakDetection\\'
       'DiagnosedApplications')])
+  _DEFINITION_FILE = os.path.join(
+    os.path.dirname(__file__), 'filetime.yaml')
+
+  def _ParseFiletime(self, byte_stream):
+    """Parses a FILETIME date and time value from a byte stream.
+
+    Args:
+      byte_stream (bytes): byte stream.
+
+    Returns:
+      dfdatetime.DateTimeValues: a FILETIME date and time values or a semantic
+        date and time values if the FILETIME date and time value is not set.
+
+    Raises:
+      ParseError: if the FILETIME could not be parsed.
+    """
+    filetime_map = self._GetDataTypeMap('filetime')
+
+    try:
+      filetime = self._ReadStructureFromByteStream(
+          byte_stream, 0, filetime_map)
+    except (ValueError, errors.ParseError) as exception:
+      raise errors.ParseError(
+          f'Unable to parse FILETIME value with error: {exception!s}')
+
+    if filetime == 0:
+      return dfdatetime_semantic_time.NotSet()
+
+    try:
+      return dfdatetime_filetime.Filetime(timestamp=filetime)
+    except ValueError:
+      raise errors.ParseError(f'Invalid FILETIME value: 0x{filetime:08x}')
 
   def ExtractEvents(self, parser_mediator, registry_key, **kwargs):
     """Extracts events from a Windows Registry key.
@@ -52,16 +92,13 @@ class DiagnosedApplicationsPlugin(interface.WindowsRegistryPlugin):
     """
     for subkey in registry_key.GetSubkeys():
       event_data = WindowsRegistryDiagnosedApplicationsEventData()
+
       event_data.process_name = subkey.name
-      
-      try:
-        event_data.last_detection_time = self._ParseFiletime(
-          subkey.GetValueByName('LastDetectionTime')
-        )
-      except errors.ParseError as exception:
-        warning = f'unable to parse LastDetectionTime: {exception!s}'
-        parser_mediator.ProduceExtractionWarning(warning)
-      
+      event_data.last_detection_time = self._ParseFiletime(
+        subkey.GetValueByName(
+          "LastDetectionTime"
+        ).data
+      )
       event_data.key_path = subkey.path
       event_data.last_written_time = subkey.last_written_time
   
