b34cc7b134a25b5eea3d3c1427ff5cd2e11acd20
Joachim Metz <joachim.metz@gmail.com>
2025-11-25 08:42:43 +0100

    Changes to Docker configuration and scripts

--- utils/build_docker.sh
+++ utils/build_docker.sh
@@ -2,31 +2,46 @@
 #
 # Script to build a Docker image.
 
-EXIT_FAILURE=1;
-EXIT_SUCCESS=0;
+EXIT_FAILURE=1
+EXIT_SUCCESS=0
 
-LOGFILE="dockerbuild.$$";
+PPA_TRACK="stable"
 
 # Abort on error.
-set -e;
+set -e
 
-cd config/docker;
+pushd config/docker
 
-docker build --no-cache --force-rm -t log2timeline/plaso . 2>&1 | tee ${LOGFILE};
+LOGFILE="dockerbuild.$$"
 
-IDENTIFIER=$( grep -e ' writing image ' ${LOGFILE} | sed 's/^.* writing image //;s/ done$//' );
+docker build \
+	--build-arg PPA_TRACK=${PPA_TRACK} \
+	--no-cache \
+	--force-rm \
+	-t log2timeline/plaso \
+	. 2>&1 | tee ${LOGFILE}
 
-docker run log2timeline/plaso log2timeline --version 2>&1 | tee ${LOGFILE};
+IDENTIFIER=$( grep -e ' writing image ' ${LOGFILE} | sed 's/^.* writing image //;s/ done$//' )
 
-VERSION=$( grep -e '^plaso - log2timeline version ' ${LOGFILE} | sed 's/^plaso - log2timeline version //' );
+docker run \
+	log2timeline/plaso \
+	log2timeline --version 2>&1 | tee ${LOGFILE}
+
+VERSION=$( grep -e '^plaso - log2timeline version ' ${LOGFILE} | sed 's/^plaso - log2timeline version //' )
 
 docker tag ${IDENTIFIER} log2timeline/plaso:${VERSION}
-docker tag ${IDENTIFIER} log2timeline/plaso:staging
-docker tag ${IDENTIFIER} log2timeline/plaso:latest
 
-rm -f ${LOGFILE};
+if [ "${PPA_TRACK}" = "stable" ]; then
+	docker tag ${IDENTIFIER} log2timeline/plaso:latest
+else
+	docker tag ${IDENTIFIER} log2timeline/plaso:${PPA_TRACK}
+fi
+
+rm -f ${LOGFILE}
+
+popd
 
 echo "Docker build successful."
 
-exit ${EXIT_SUCCESS};
+exit ${EXIT_SUCCESS}
 
