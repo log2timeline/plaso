eab3f8a565c6245df837f71f2d436fd97511330a
Joachim Metz <joachim.metz@gmail.com>
2025-12-22 15:14:24 +0100

    Changes for CI tests

--- config/appveyor/install.sh
+++
@@ -1,8 +0,0 @@
-# Script to set up tests on AppVeyor MacOS.
-
-set -e
-
-brew untap homebrew/homebrew-cask-versions
-brew update -q
-brew install -q gettext gnu-sed python@${PYTHON_VERSION} tox || true
-
