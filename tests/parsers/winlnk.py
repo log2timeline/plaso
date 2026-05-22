1951d5b3f7cd1371cf18d2f96b361f04dc5f9c4a
Joachim Metz <joachim.metz@gmail.com>
2026-05-22 12:26:51 +0200

    Code clean up

--- tests/parsers/winlnk.py
+++ tests/parsers/winlnk.py
@@ -12,65 +12,6 @@ class WinLnkParserTest(test_lib.ParserTestCase):
     """Tests for the Windows Shortcut (LNK) parser."""
 
     def testParse(self):
-        """Tests the Parse function."""
-        parser = winlnk.WinLnkParser()
-        storage_writer = self._ParseFile(["example.lnk"], parser)
-
-        # Link information:
-        #   Creation time                  : Jul 13, 2009 23:29:02.849131000 UTC
-        #   Modification time              : Jul 14, 2009 01:39:18.220000000 UTC
-        #   Access time                    : Jul 13, 2009 23:29:02.849131000 UTC
-        #   Description                    : @%windir%\system32\migwiz\wet.dll,-590
-        #   Relative path                  : .\migwiz\migwiz.exe
-        #   Working directory              : %windir%\system32\migwiz
-        #   Icon location                  : %windir%\system32\migwiz\migwiz.exe
-        #   Environment variables location : %windir%\system32\migwiz\migwiz.exe
-
-        number_of_event_data = storage_writer.GetNumberOfAttributeContainers(
-            "event_data"
-        )
-        self.assertEqual(number_of_event_data, 2)
-
-        number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-            "extraction_warning"
-        )
-        self.assertEqual(number_of_warnings, 0)
-
-        number_of_warnings = storage_writer.GetNumberOfAttributeContainers(
-            "recovery_warning"
-        )
-        self.assertEqual(number_of_warnings, 0)
-
-        # Test shortcut event data.
-        expected_event_values = {
-            "access_time": "2009-07-13T23:29:02.8491310+00:00",
-            "data_type": "windows:lnk:link",
-            "description": "@%windir%\\\\system32\\\\migwiz\\\\wet.dll,-590",
-            "creation_time": "2009-07-13T23:29:02.8491310+00:00",
-            "env_var_location": "%windir%\\\\system32\\\\migwiz\\\\migwiz.exe",
-            "file_attribute_flags": 0x00000020,
-            "file_size": 544768,
-            "icon_location": "%windir%\\\\system32\\\\migwiz\\\\migwiz.exe",
-            "modification_time": "2009-07-14T01:39:18.2200000+00:00",
-            "relative_path": ".\\\\migwiz\\\\migwiz.exe",
-            "working_directory": "%windir%\\\\system32\\\\migwiz",
-        }
-
-        event_data = storage_writer.GetAttributeContainerByIndex("event_data", 0)
-        self.CheckEventData(event_data, expected_event_values)
-
-        # Test distributed link tracking event data.
-        expected_event_values = {
-            "creation_time": "2009-07-14T05:45:20.5000123+00:00",
-            "data_type": "windows:distributed_link_tracking:creation",
-            "mac_address": "00:1d:09:fa:5a:1c",
-            "uuid": "846ee3bb-7039-11de-9d20-001d09fa5a1c",
-        }
-
-        event_data = storage_writer.GetAttributeContainerByIndex("event_data", 1)
-        self.CheckEventData(event_data, expected_event_values)
-
-    def testParseLinkTargetIdentifier(self):
         """Tests the Parse function on an LNK with a link target identifier."""
         parser = winlnk.WinLnkParser()
         storage_writer = self._ParseFile(["NeroInfoTool.lnk"], parser)
@@ -122,7 +63,6 @@ class WinLnkParserTest(test_lib.ParserTestCase):
                 "C:\\\\Program Files (x86)\\\\Nero\\\\Nero 9\\\\Nero InfoTool"
             ),
         }
-
         event_data = storage_writer.GetAttributeContainerByIndex("event_data", 5)
         self.CheckEventData(event_data, expected_event_values)
 
@@ -141,8 +81,17 @@ class WinLnkParserTest(test_lib.ParserTestCase):
                 "Nero InfoTool\\\\InfoTool.exe"
             ),
         }
-
         event_data = storage_writer.GetAttributeContainerByIndex("event_data", 4)
+        self.CheckEventData(event_data, expected_event_values)
+
+        # Test distributed link tracking event data.
+        expected_event_values = {
+            "creation_time": "2010-01-29T21:02:17.8401717+00:00",
+            "data_type": "windows:distributed_link_tracking:creation",
+            "mac_address": "70:5a:b6:16:a5:f3",
+            "uuid": "958705b5-0d19-11df-9fe4-705ab616a5f3",
+        }
+        event_data = storage_writer.GetAttributeContainerByIndex("event_data", 6)
         self.CheckEventData(event_data, expected_event_values)
 
     def testParseUnpairedSurrogate(self):
@@ -178,7 +127,6 @@ class WinLnkParserTest(test_lib.ParserTestCase):
             "relative_path": ".\\\\unicode_U+0000d800_\\U0000d800.exe",
             "working_directory": "C:\\\\test",
         }
-
         event_data = storage_writer.GetAttributeContainerByIndex("event_data", 2)
         self.CheckEventData(event_data, expected_event_values)
 
@@ -196,7 +144,6 @@ class WinLnkParserTest(test_lib.ParserTestCase):
                 "<My Computer> C:\\\\test\\\\unicode_U+0000d800_\\U0000d800.exe"
             ),
         }
-
         event_data = storage_writer.GetAttributeContainerByIndex("event_data", 1)
         self.CheckEventData(event_data, expected_event_values)
 
