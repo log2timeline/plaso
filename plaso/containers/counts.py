bb321aeee88d795a52e7e18e780790d9ed565f12
wbi <william.billaud@orange.com>
2026-08-20 22:18:57 +0200

    Bump setuptools version to 77
    Version below generate the following error : `Backend: Wrote response {'code': 1, 'exc_type': 'ValueError', 'exc_msg': "invalid pyproject.toml config: `project.license`.\nconfiguration error: `project.license` must be valid exactly by one definition (2 matches found):\n\n    - keys:\n        'file': {type: string}\n      required: ['file']\n    - keys:\n        'text': {type: string}\n      required: ['text']\n"} `

--- plaso/containers/counts.py
+++ plaso/containers/counts.py
@@ -79,6 +79,7 @@ class ParserCount(interface.AttributeContainer):
         super().__init__()
         self.name = name
         self.number_of_events = number_of_events
+
 @total_ordering
 class WarningCount(interface.AttributeContainer):
     """Warning count attribute container.
